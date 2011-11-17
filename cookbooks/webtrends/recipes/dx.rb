f0418961052ea3c0e6acf2006148c63133b35f5f
Kendrick Martin <kendrick.martin@webtrends.com>
2011-11-17 15:03:41 -0800

    Updating dx deployment.
    Former-commit-id: d1c452b604be53ad7ee8f00f013a228f6af5e877

--- cookbooks/webtrends/recipes/dx.rb
+++ cookbooks/webtrends/recipes/dx.rb
@@ -7,60 +7,88 @@
 #
 # All rights reserved - Do Not Redistribute
 # This recipe installs the needed components to full setup/configure DX.
-
-#include_recipe "iis"
-#include_recipe "windows"
+ 
 require 'cgi'
 
+pod = node['webtrends']['pod']
+pods = data_bag("pods")
+pod_data = data_bag_item('pods', pod)
+master_host = pod_data['master_host']
+user = node['user']
+pass = node['pass']
+
+installdir = node['webtrends']['installdir']
+logdir = node['webtrends']['logdir']
+
+cache_name = pod_data['dx']['cachename']
+cass_snmp = pod_data['cassandra_snmp_comm']
+cass_report_family = pod_data['cassandra_report_column']
+cass_metadata_family = pod_data['cassandra_meta_column']
+app_settings = pod_data['dx']['app_settings_section']
+
+app_settings = "[{'streamingServiceRoot':'https://fdx.webtrends.com/StreamingServices_v3/'}]"
+cass_hosts = "[{Name:'scass01.staging.dmz'},{Name:'scass02.staging.dmz'}, {Name:'scass03.staging.dmz'}, {Name:'scass04.staging.dmz'}]"
+cache_hosts = "[{Name:'xcache01.staging.dmz', Port:'11211'},{Name:'xcache02.staging.dmz', Port:'11211'}]"
+
+common_msi = node['webtrends']['common_msi']
+dx_msi = node['webtrends']['dx']['dx_msi']
 buildURLs = data_bag("buildURLs")
 build_url = data_bag_item('buildURLs', 'latest')
-base_url = build_url['url'] + "/dx/DX_2.2"
-dx_msi_url = base_url + node['webtrends']['dx']['msi_name']
-dx_msi_name = CGI::unescape(::File.basename(dx_msi_url))
-common_msi_name = CGI::unescape(::File.basename(node['webtrends']['common_url']))
-common_msi_short = "WebTrendsCommonLib.msi"
-dir_list = node['dx']['dx_dir']
-cfg_cmds = node['iis']['cfg_cmd']
-legacy_app_pools = node['dx']['legacy_app_pool']
-app_pools = node['dx']['app_pool']
+dx_msi_url = build_url['url']
+dx_msi_url << "dx/"
+dx_msi_url << dx_msi
 
-directory "#{node['dx']['msi_path']}" do
+build_url = data_bag_item('buildURLs', 'latest')
+common_msi_url = build_url['url']
+common_msi_url << common_msi
+build_url = data_bag_item('buildURLs', 'latest')
+base_url = build_url['url']
+app_user = pod_data['ui_acct']
+app_pass = pod_data['ui_pass'] 
+
+dir_list = node['webtrends']['dx']['dx_dir']
+cfg_cmds = node['webtrends']['dx']['cfg_cmd']
+legacy_app_pools = node['webtrends']['dx']['legacy_app_pool']
+app_pools = node['webtrends']['dx']['app_pool']
+msi_path = node['webtrends']['msi_path']
+
+directory msi_path do
 	action [:create]
 	recursive true
 end
 
-directory "D:\\logs" do
+directory logdir do
 	action :create
 end
 
-remote_file "#{node['dx']['msi_path']}\\#{dx_msi_name}" do
+remote_file "#{msi_path}\\#{dx_msi}" do
 	source dx_msi_url
 	action :nothing
 end
 
-remote_file "#{node['dx']['msi_path']}\\#{common_msi_short}" do
-	source node['webtrends']['common_url']
+remote_file "#{msi_path}\\#{common_msi}" do
+	source common_msi_url
 	action :nothing
 end
 
-http_request "HEAD #{node['dx']['base_url']}" do
+http_request "HEAD #{base_url}" do
   message ""
-  url node['dx']['base_url']
+  url base_url
   action :head
-  if File.exists?("#{node['dx']['msi_path']}\\#{dx_msi_name}")
-    headers "If-Modified-Since" => File.mtime("#{node['dx']['msi_path']}\\#{dx_msi_name}").httpdate
+  if File.exists?("#{msi_path}\\#{dx_msi}")
+    headers "If-Modified-Since" => File.mtime("#{msi_path}\\#{dx_msi}").httpdate
   end
-  notifies :create, resources(:remote_file => "#{node['dx']['msi_path']}\\#{dx_msi_name}"), :immediately
+  notifies :create, resources(:remote_file => "#{msi_path}\\#{dx_msi}"), :immediately
 end
 
-http_request "HEAD #{node['webtrends']['base_url']}" do
+http_request "HEAD #{base_url}" do
   message ""
-  url node['webtrends']['base_url']
+  url base_url
   action :head
-  if File.exists?("#{node['dx']['msi_path']}\\#{common_msi_name}")
-    headers "If-Modified-Since" => File.mtime("#{node['dx']['msi_path']}\\#{common_msi_name}").httpdate
+  if File.exists?("#{msi_path}\\#{common_msi}")
+    headers "If-Modified-Since" => File.mtime("#{msi_path}\\#{common_msi}").httpdate
   end
-  notifies :create, resources(:remote_file => "#{node['dx']['msi_path']}\\#{common_msi_short}"), :immediately
+  notifies :create, resources(:remote_file => "#{msi_path}\\#{common_msi}"), :immediately
 end
 
 windows_registry 'HKLM\Software\WebTrends Corporation' do
@@ -94,7 +122,7 @@ end
 iis_site 'DX' do
 	protocol :http
     port 80
-    path "#{node['dx']['site_path']}"
+    path "#{installdir}\\Data Extraction API"
 	action [:add,:start]
 end
 
@@ -103,12 +131,20 @@ legacy_app_pools.each do |pool|
 		thirty_two_bit :true
 		action [:add, :config]
 	end
+	cmd = pool + "-section:system.webServer/handlers /+\"[name='svc-ISAPI-2.0_32bit',path='*.SVC',verb='*',modules='IsapiModule',scriptProcessor='C:\\Windows\\Microsoft.NET\\Framework\\v2.0.50727\\aspnet_isapi.dll']\""
+	iis_config cmd do
+		:config
+	end
 end
 
 app_pools.each do |pool|
 	iis_pool "#{pool}" do	
 		pipeline_mode :Integrated
 		action [:add, :config]
+	end
+	cmd = pool + "-section:system.webServer/handlers /+\"[name='svc-ISAPI-2.0_32bit',path='*.SVC',verb='*',modules='IsapiModule',scriptProcessor='C:\\Windows\\Microsoft.NET\\Framework\\v2.0.50727\\aspnet_isapi.dll']\""
+	iis_config cmd do
+		:config
 	end
 end
 
@@ -125,7 +161,19 @@ windows_firewall 'OEM_DXWS' do
 end
 
 windows_package "CommonLib" do
-	source "#{node['dx']['msi_path']}\\#{common_msi_short}"
-	options "/l*v \"D:\\logs\\Install - Webtrends Commonlib.log\" INSTALLDIR=\"D:\\wrs\" SQL_SERVER_NAME=\"kmasterdb01\" WTMASTER=\"wt_master\" MASTER_USER=\"#{user}\" MASTER_PASS=\"#{pass}\" WTSCHED=\"localhost\" SCHED_USER=\"#{user}\" SCHED_PASS=\"#{pass}\""
+	source "#{msi_path}\\#{common_msi}"
+	options "/l*v \"#{logdir}\\#{common_msi}-Install.log\" INSTALLDIR=\"#{installdir}\" SQL_SERVER_NAME=\"#{master_host}\" WTMASTER=\"wtMaster\"  WTSCHED=\"wt_Sched\""
 	action :install
 end
+
+# windows_package "DXInstall" do
+	# source "#{msi_path}\\#{dx_msi}"
+	# options "/l*v \"#{logdir}\\#{dx_msi}-Install.log\" INSTALLDIR=\"#{installdir}\" WEBSITE_NAME=\"DX\" MASTER_HOST=\"#{master_host}\" WEBSITE_PORT=\"80\" APP_POOL_ACCT=\"#{app_user}\" APP_POOL_PASS=\"#{app_pass}\" CACHEENABLED=\"True\" CACHENAME=\"#{cache_name}\" CACHE_HOSTS=\"#{cache_hosts}\" CASSANDRA_HOSTNAME=\"#{cass_hosts}\" CASSANDRA_SNMP_COMM=\"#{cass_snmp}\" CASSANDRA_REPORT_FAMILY=\"#{cass_report_family}\" CASSANDRA_METADATA_FAMILY=\"#{cass_metadata_family}\" APP_SETTINGS_SECTION=\"#{app_settings}\""
+	# action :install
+# end
+
+powershell "reg-iis" do
+	cwd Chef::Config[:file_cache_path]
+	code <<-EOH
+	"aspnet_regiis -i -enable"
+	
