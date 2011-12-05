07cdecf78d341c47fff2e9e3e40bfe189db2c065
Kendrick Martin <kendrick.martin@webtrends.com>
2011-12-05 14:02:34 -0800

    Updated version number to match sops01

--- cookbooks/dx/metadata.rb
+++ cookbooks/dx/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email "kendrick.martin@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures webtrends"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+version          "0.1.0"
