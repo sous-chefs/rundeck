a23e8814a17d859fd95a6bab4755792e05cca266
Tim Smith <tim.smith@webtrends.com>
2011-12-06 12:30:36 -0800

    Add install of MSMQ, .NET 3.5.1, SNMP, and PowerShell ISE
    Former-commit-id: 026a02b7750219b98164a04eaa3a7383a9e602cf

--- cookbooks/windows/recipes/default.rb
+++ cookbooks/windows/recipes/default.rb
@@ -32,3 +32,28 @@ end
     action :install
   end
 end
+
+# Webtrends System Setup
+
+#Install SNMP feature
+windows_feature "SNMP-Service"
+  action :install
+end
+
+#Install Powershell feature
+windows_feature "PowerShell-ISE"
+  action :install
+end
+
+#Install .NET 3.5.1
+windows_feature "NET-Framework-Core"
+  action :install
+end
+
+
+#Install MSMQ
+windows_feature "MSMQ-Server"
+   action :install
+end
+
+
