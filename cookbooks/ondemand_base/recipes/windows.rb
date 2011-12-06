bde0a69db3dc034ccc5e291561388375f11dc853
Tim Smith <tim.smith@webtrends.com>
2011-12-06 15:54:30 -0800

    Add turning off hibernation, setting high performance power options, and turning off ipv6 to the base windows recipe

--- cookbooks/ondemand_base/recipes/windows.rb
+++ cookbooks/ondemand_base/recipes/windows.rb
@@ -19,3 +19,20 @@ end
 windows_feature "MSMQ-Server"
    action :install
 end
+
+#Turn off hibernation
+execute powercfg do
+	command "powercfg.exe /h off"
+	action :run
+end
+
+#Set high performance power options
+execute powercfg do
+	command "powercfg -s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c"
+	action :run
+end
+
+#Turn off IPv6
+windows_registry 'HKLM\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters' do
+  values 'DisabledComponents' => "0xffffffff"
+end
