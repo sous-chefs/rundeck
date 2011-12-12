be60d77e1c6d953da12fa894bd7883ebc423a557
Kendrick Martin <kendrick.martin@webtrends.com>
2011-12-12 09:03:57 -0800

    Updates to roadrunner cookbook.
    Former-commit-id: c82bd48ea8c1bfc161d7c0f5ca266a2ee17cde39

--- cookbooks/roadrunner/metadata.rb
+++ cookbooks/roadrunner/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "kendrick.martin@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures Roadrunner service"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.10"
+version          "0.0.13"
 supports         "windows"
 depends          "windows", ">= 1.2.6"
