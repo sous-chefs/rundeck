19625f9df40cc5a738ab59213c59dfd9b8c68d3e
unknown <martink@pdxmartink01.webtrends.corp>
2012-01-03 13:42:39 -0800

    Updating wt_dx cookbook to split recipes out for different versions of DX. Added site support for IIS.
    Former-commit-id: e4f1f380f9cdc21e2491b92df509528a99d51286

--- cookbooks/wt_dx/metadata.rb
+++ cookbooks/wt_dx/metadata.rb
@@ -2,8 +2,8 @@ name 'wt_dx'
 maintainer       "Webtrends"
 maintainer_email "kendrick.martin@webtrends.com"
 license          "All rights reserved"
-description      "Installs/Configures webtrends"
+description      "Installs/Configures DX"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.0"
+version          "0.1.28"
 supports         "windows"
-depends          "windows", ">= 1.2.6"
+depends          "iis", ">= 1.0.3"
