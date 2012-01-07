f1eb9a00f5a1717a28640003b2db582d029038c9
Timothy Smith <tim.smith@webtrends.com>
2012-01-07 01:34:10 +0000

    Install VIM and some useful tools.  This was pulled from Optimize.  It's awesome

--- cookbooks/ondemand_base/recipes/ubuntu.rb
+++ cookbooks/ondemand_base/recipes/ubuntu.rb
@@ -1,4 +1,16 @@
 #Make sure that this recipe only runs on ubuntu systems
 if platform?("ubuntu")
 
+include_recipe "ubuntu"
+include_recipe "apt"
+
+# Ubuntu ships with vim-tiny, which lacks many of the basic features found in the vim package
+package "vim"
+
+# Install useful tools
+%w{ manpages man-db lsof mtr strace }.each do |pkg|
+  package pkg
+end
+
+
 end
