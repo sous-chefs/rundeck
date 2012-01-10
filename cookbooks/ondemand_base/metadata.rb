9650b487cee6a3c34dd58c3c68b6549bf8c37558
Tim Smith <tim.smith@webtrends.com>
2012-01-10 21:11:10 +0000

    Add dependencies for Windows / Ubuntu cookbooks to the ondemand_base cookbook
    Former-commit-id: 8f297a3519a85359ec4c9f1a42f01580a1306706

--- cookbooks/ondemand_base/metadata.rb
+++ cookbooks/ondemand_base/metadata.rb
@@ -4,3 +4,5 @@ license          "All rights reserved"
 description      "Installs/Configures ondemand_base"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+depends          "windows"
+depends          "ubuntu"
