eb6185dfea788d0df301674871565e9688e21418
Kendrick Martin <kendrick.martin@webtrends.com>
2012-01-11 21:14:54 +0000

    Added version 22
    Former-commit-id: d424d335d996066d6b19d92ef6ca73d27b3daf58

--- cookbooks/wt_dx/recipes/pre.rb
+++ cookbooks/wt_dx/recipes/pre.rb
@@ -16,47 +16,11 @@
 #######################################################################################
 
 pod = node['webtrends']['pod']
-pods = data_bag("pods")
+
 pod_data = data_bag_item('pods', pod)
-master_host = pod_data['master_host']
-user = node['user']
-pass = node['pass']
 
 installdir = node['webtrends']['installdir']
 logdir = node['webtrends']['logdir']
-
-cache_name = pod_data['dx']['cachename']
-cass_snmp = pod_data['cassandra_snmp_comm']
-cass_report_family = pod_data['cassandra_report_column']
-cass_metadata_family = pod_data['cassandra_meta_column']
-
-app_settings = pod_data['dx']['app_settings_section']
-app_settings = "#{app_settings.to_json}"
-
-endpoint = pod_data['dx']['endpoint_address']
-cass_hosts = pod_data['cassandra_hosts'].map {|x| "Name:" + x}
-cass_hosts = "{#{cass_hosts.to_json}}"
-
-c_hosts = pod_data['cache_hosts']
-cache_hosts = ""
-c_hosts.each {|x| cache_hosts << x.to_json + ", "} #This still leaves one extra , at the eol fixed below
-cache_hosts = "{#{cache_hosts}"
-cache_hosts[-1] = "}" #Final } is replacing the last , in the string
-
-#common_msi = node['webtrends']['common_msi']
-#dx_msi = node['webtrends']['dx']['dx_msi']
-buildURLs = data_bag("buildURLs")
-build_url = data_bag_item('buildURLs', 'latest')
-#dx_msi_url = build_url['url']
-#dx_msi_url << "dx/"
-#dx_msi_url << dx_msi
-
-#common_msi_url = build_url['url']
-#common_msi_url << common_msi
-
-app_user = pod_data['ui_acct']
-app_pass = pod_data['ui_pass'] 
-
 cfg_cmds = node['webtrends']['dx']['cfg_cmd']
 
 directory logdir do
@@ -104,6 +68,14 @@ iis_site 'DX' do
     path "#{installdir}\\Data Extraction API"
 	action [:add,:start]
 end
+
+iis_site 'OEM_DX' do
+	protocol :http
+    port 81
+    path "#{installdir}\\OEM Data Extraction API"
+	action [:add,:start]
+end
+
 
 windows_firewall 'DXWS' do
 	protocol "TCP"
---
+++ cookbooks/wt_dx/recipes/version22.rb
@@ -0,0 +1,30 @@
+pod = node['webtrends']['pod']
+pod_data = data_bag_item('pods', pod)
+c_hosts = pod_data['cache_hosts']
+
+
+#Recipe specific
+cfg_cmds = node['webtrends']['dx']['v2_2']['cfg_cmd']
+app_pool = node['webtrends']['dx']['v2_2']['app_pool']
+installdir = node['webtrends']['installdir']
+installdir_v22 = node['webtrends']['dx']['v2_2']['dir']
+
+template "#{installdir}#{installdir_v22}\\web.config" do
+	source "webConfigv22.erb"
+	variables(
+		:cache_hosts => c_hosts
+	)
+end
+
+iis_pool "#{app_pool}" do
+	pipeline_mode :Integrated
+	runtime_version "4.0"
+	action [:add, :config]
+end
+
+iis_app "OEM_DX" do
+	path "/v2_2"
+	application_pool "#{app_pool}"
+	physical_path "#{installdir}#{installdir_v22}"
+	action :add
+end
