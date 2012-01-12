ae92ed58c349ff08b2b3cae736dfc422558fa0db
Tim Smith <tim.smith@webtrends.com>
2012-01-12 05:27:24 +0000

    Add a file check to the .NET 3.5 install to speed up runtime

--- cookbooks/ms_dotnet35/recipes/default.rb
+++ cookbooks/ms_dotnet35/recipes/default.rb
@@ -1,3 +1,4 @@
+
 #
 # Cookbook Name:: ms_dotnet35
 # Recipe:: default
@@ -7,8 +8,10 @@
 # All rights reserved
 #
 
-#Install .NET 3.5 Feature
+#Install .NET 3.5 Feature if we don't find part of the package arleady installed
 
-windows_feature "NetFx3" do
-  action :install
+if !File.exists?("C:/Windows/Microsoft.NET/Framework64/v2.0.50727/aspnet_regiis.exe")
+    windows_feature "NetFx3" do
+        action :install
+    end
 end
