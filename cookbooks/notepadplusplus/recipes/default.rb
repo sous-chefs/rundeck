d1b70bd14a747a755621e28caf218485b5389285
Tim Smith <tim.smith@webtrends.com>
2012-01-12 01:16:02 +0000

    Create recipe to install Notepad++
    Former-commit-id: f7acaa5508e1446a647c7808540c9ebb9127bf65

---
+++ cookbooks/notepadplusplus/recipes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: notepadplusplus
+# Recipe:: default
+#
+# Copyright 2012, Webtrends Inc.
+#
+# All rights reserved
+#
+# Code based off the PowerShell cookbook by Seth Chisamore
+
+case node['platform']
+when "windows"
+    windows_package "Notepad Plus Plus" do
+        source node['notepadplusplus']['http_url']
+        installer_type :custom
+        options "/S"
+        action :install
+    end
+else
+  Chef::Log.warn('Notepad Plus Plus can only be installed on the Windows platform.')
+end
