6381948f7fb54829bdba02e1f6397c0ffc2fa2c9
Tim Smith <root@chefclient02.devops.dmz>
2012-01-18 23:43:59 +0000

    Get the Linux base role up and running with openssh, ntp, vim, man, and a pile of sys utils
    Former-commit-id: b4d83918af2dc393ce9b70f26761592cbe96e813

--- cookbooks/ondemand_base/recipes/ubuntu.rb
+++ cookbooks/ondemand_base/recipes/ubuntu.rb
@@ -1,9 +1,18 @@
 #Make sure that this recipe only runs on ubuntu systems
 if platform?("ubuntu")
 
+#Base recipes necessary for a functioning system
 include_recipe "ubuntu"
 include_recipe "apt"
+#include_recipe "ad-likewise"
+#include_recipe "sudo"
+include_recipe "openssh"
+include_recipe "ntp"
+
+#Use experience recipes
 include_recipe "vim"
+include_recipe "man"
+
 
 # Install useful tools
 %w{ manpages man-db lsof mtr strace iotop }.each do |pkg|
