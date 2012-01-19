c1182b08da836655d2f3111344db6da86e82662a
Kendrick Martin <kendrick.martin@webtrends.com>
2012-01-19 00:34:30 +0000

    Configured .NET 3.5 cookbook to correctly install

--- cookbooks/ms_dotnet35/recipes/default.rb
+++ cookbooks/ms_dotnet35/recipes/default.rb
@@ -10,8 +10,6 @@
 
 #Install .NET 3.5 Feature if we don't find part of the package arleady installed
 
-if !File.exists?("C:/Windows/Microsoft.NET/Framework64/v2.0.50727/aspnet_regiis.exe")
-    windows_feature "NetFx3" do
-        action :install
-    end
+windows_feature "NetFx3" do
+  action :install
 end
