635f65beb8b36a9b3ec7d898898fda6df4f89517
Tim Smith <root@chefclient02.devops.dmz>
2012-01-20 20:10:23 +0000

    Add recipes to the CentOS recipe.
    Former-commit-id: 0099f9723acf3681ae5c8b61ddac5af37ee4ea78

--- cookbooks/ondemand_base/recipes/centos.rb
+++ cookbooks/ondemand_base/recipes/centos.rb
@@ -1,6 +1,17 @@
 #Make sure that this recipe only runs on CentOS/RHEL systems
 if platform?("redhat", "centos")
 
+include_recipe "openssh"
+include_recipe "ntp"
+
+#User experience and tools recipes
 include_recipe "vim"
+include_recipe "man"
+include_recipe "networking_basic"
+
+# Install useful tools
+%w{ mtr strace iotop }.each do |pkg|
+  package pkg
+end
 
 end
