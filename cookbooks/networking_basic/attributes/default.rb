011ad165dc1b17af831b1a4c0a725dd0132d9b98
Tim Smith <root@chefclient02.devops.dmz>
2012-01-20 20:09:45 +0000

    Added support for Redhat / CentOS to networking_basic
    Former-commit-id: 76bee271e5a38ccd0245a99a90da303511aaa6d7

---
+++ cookbooks/networking_basic/attributes/default.rb
@@ -0,0 +1,41 @@
+default[:debian][:install_list] = [
+  'lsof',
+  'iptables',
+  'jwhois',
+  'whois',
+  'curl',
+  'wget',
+  'rsync',
+  'jnettop',
+  'nmap',
+  'traceroute',
+  'ethtool',
+  'iproute',
+  'iputils-ping',
+  'netcat-openbsd',
+  'tcptraceroute',
+  'tcputils',
+  'tcpdump',
+  'elinks',
+  'lynx'
+]
+
+default[:redhat][:install_list] = [
+  'lsof',
+  'iptables',
+  'jwhois',
+  'curl',
+  'wget',
+  'rsync',
+  'nmap',
+  'traceroute',
+  'ethtool',
+  'iproute',
+  'iputils',
+  'nc',
+  'tcptraceroute',
+  'tcputils',
+  'tcpdump',
+  'elinks',
+  'lynx'
+]
