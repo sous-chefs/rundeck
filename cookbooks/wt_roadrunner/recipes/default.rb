5411a3ecaac882318ab4c7f19aa4582f769c5912
martink <kendrick.martin@webtrends.com>
2012-01-24 08:39:50 -0800

    Updated wt_roadrunner to only setup firewall if rr_configured flag is not set
    Former-commit-id: f50dd4a7b28b8ba7f6148d477921d8097a0fd7fe

--- cookbooks/wt_roadrunner/recipes/default.rb
+++ cookbooks/wt_roadrunner/recipes/default.rb
@@ -20,11 +20,15 @@ url_extensions = node['url_extensions']
 pod = node.chef_environment
 pod_data = data_bag_item('common', pod)
 master_host = pod_data['master_host']
+user = pod_data['loader_user']
+password = pod_data['loader_pass']
 
-rr_msi_url = "#{build_url}/#{zip_file}"
+
+rr_url = "#{build_url}/#{zip_file}"
 
 gac_cmd = "#{installdir}\\gacutil.exe /i #{installdir}\\Webtrends.RoadRunner.SSISPackageRunner.dll"
-sc_cmd = "\"%WINDIR%\\System32\\sc.exe create WebtrendsRoadRunnerService binPath= #{installdir}\\Webtrends.RoadRunner.Service.exe\""
+sc_cmd = "\"%WINDIR%\\System32\\sc.exe create WebtrendsRoadRunnerService binPath= #{installdir}\\Webtrends.RoadRunner.Service.exe obj= #{user} password= #{password}\""
+netsh_cmd = "netsh http add urlacl url=http://+:8097/ user=#{user}"
 
 windows_feature "NetFx3" do
 	action :install
@@ -34,16 +38,28 @@ http_request "TestingConnection" do
   url "#{base_url}#{url_extensions}"
 end
 
+directory "#{installdir}" do
+	recursive true
+	action :create
+end
+
 windows_zipfile "#{installdir}" do
-	source "#{rr_msi_url}"
+	source "#{rr_url}"
 	action :unzip	
-	not_if {::File.exists?("#{installdir}\\log4net.xml")}
+	not_if {::File.exists?("#{installdir}\\Webtrends.RoadRunner.Service.exe")}
 end
 
 template "#{installdir}\\Webtrends.RoadRunner.Service.exe.config" do
 	source "RRServiceConfig.erb"
 	variables(		
 		:master_host => pod_data['master_host']
+	)
+end
+
+template "#{installdir}\\log4net.config" do
+	source "log4net.erb"
+	variables(		
+		:logdir => node['webtrends']['logdir']
 	)
 end
 
@@ -58,13 +74,19 @@ end
 execute "gac" do
 	command gac_cmd
 	cwd installdir
-	not_if { node[:rr_installed]}
+	not_if { node[:rr_configured]}
 end
 
 execute "sc" do
 	command sc_cmd
 	cwd installdir
 	not_if { node[:rr_installed]}
+end
+
+execute "netsh" do
+	command netsh_cmd
+	cwd installdir
+	not_if { node[:rr_configured]}
 end
 
 service "WebtrendsRoadRunnerService" do
