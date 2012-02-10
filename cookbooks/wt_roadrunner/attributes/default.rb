a05bd58cd88eadf46a23e3eb4342dd12c72680af
Kendrick Martin <kendrick.martin@webtrends.com>
2012-02-10 00:13:35 +0000

    Moving attributes to cookbook. Ading uninstall recipe

---
+++ cookbooks/wt_roadrunner/attributes/default.rb
@@ -0,0 +1,10 @@
+#
+# Author:: Kendrick Martin(<kendrick.martin@webtrends.com>)
+# Cookbook Name:: wt_roadrunner
+# Attribute:: default
+#
+# Copyright 2012, Webtrends Inc.
+#
+
+default['webtrends']['roadrunner']['zip_file'] = "RoadRunner.zip"
+default['webtrends']['roadrunner']['url'] = "http://teamcity.webtrends.corp/guestAuth/repository/download/bt28/.lastSuccessful"
