4652f2218884e92c84532b4987f1e46a0562e4e3
Tim Smith <tim.smith@webtrends.com>
2012-02-10 07:02:37 +0000

    Fix formatting of ms_dotnet4 cookbook, add proper package detection via the windows_package provider

--- cookbooks/ms_dotnet4/recipes/default.rb
+++ cookbooks/ms_dotnet4/recipes/default.rb
@@ -10,21 +10,16 @@
 
 case node['platform']
 when "windows"
-
-    if (win_version.windows_server_2008? || win_version.windows_server_2008_r2? || win_version.windows_7?)
-
-	if !File.exists?("C:/Windows/Microsoft.NET/Framework64/v4.0.30319/")
-            windows_package "Microsoft .NET Framework 4.0" do
-                source node['ms_dotnet4']['http_url']
-                installer_type :custom
-                options "/quiet /norestart"
-                action :install
-	    end
-        end
-
-    elsif (win_version.windows_server_2003_r2? || win_version.windows_server_2003? || win_version.windows_xp?)
-	Chef::Log.warn('The .NET 4.0 Chef recipe currently only supports Windows Vista, 7, 2008, and 2008 R2.')
-    end
+  if (win_version.windows_server_2008? || win_version.windows_server_2008_r2? || win_version.windows_7?)
+    windows_package "Microsoft .NET Framework 4 Client Profile" do
+      source node['ms_dotnet4']['http_url']
+      installer_type :custom
+      options "/quiet /norestart"
+      action :install
+     end
+  elsif (win_version.windows_server_2003_r2? || win_version.windows_server_2003? || win_version.windows_xp?)
+    Chef::Log.warn('The .NET 4.0 Chef recipe currently only supports Windows Vista, 7, 2008, and 2008 R2.')
+  end
 
 else
   Chef::Log.warn('Microsoft .NET 4.0 can only be installed on the Windows platform.')
