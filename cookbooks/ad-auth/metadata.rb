5e4bac83df0b320b06bf143c23f459c5749445b3
Tim Smith <smitht@sops01.(none)>
2012-02-17 22:40:22 +0000

    First round of cleanup to turn ad-likewise into the new ad-auth cookbook
    Former-commit-id: 22ed2b8dc7a8bc937908c9800db9b55e73455574

--- cookbooks/ad-auth/metadata.rb
+++ cookbooks/ad-auth/metadata.rb
@@ -1,7 +1,7 @@
 maintainer       "Peter Crossley"
 maintainer_email "peterc@xley.com"
 license          "Apache 2.0"
-description      "Installs & configures active directory support using Likewise Open 5/6"
+description      "Configures Active Directory authentication support using Likewise Open 5/6"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.4"
+version          "1.0"
 depends          "sudo"
--- cookbooks/ad-auth/recipes/default.rb
+++ cookbooks/ad-auth/recipes/default.rb
@@ -18,10 +18,80 @@
 # limitations under the License.
 #
 
-if node[:lsb][:codename] == "karmic"
-  include_recipe "ad-likewise::source"
-elsif node[:lsb][:codename] == "lucid"
-  include_recipe "ad-likewise::package"
-else
-  include_recipe "ad-likewise::package"
+[ "dcerpcd", "netlogond", "eventlogd", "lwregd", "lwiod", "lsassd" ].each do |svc|
+  service svc do
+    action :nothing
+    only_if { File.exists?("/etc/init.d/${svc}") }
+  end
 end
+
+package "psmisc"
+package "likewise-open"
+
+ad_config = data_bag_item('authorization', node[:authorization][:ad_auth][:ad_network])
+
+execute "initialize-likewise" do
+  command "/usr/bin/domainjoin-cli join #{ad_config['primary_domain']} #{ad_config['auth_domain_user']} \"#{ad_config['auth_domain_password']}\""
+#  only_if "test -n ""`lw-get-status | awk '/Status:.+Unknown/' | tee`"""
+  only_if "which lw-get-current-domain"
+  not_if "lw-get-current-domain"
+end
+
+ad_config['linux_admins'].each do |admin_group|
+    sudoers "linux-admins" do
+    group admin_group
+  end
+end
+
+execute "load-reg" do
+  command "/usr/bin/lwregshell import /etc/likewise-open/lsassd.reg"
+  action :nothing
+end
+
+execute "likewise-config-reload" do
+  command "/usr/bin/lw-refresh-configuration"
+  action :nothing
+  subscribes :run, resources(:execute => "load-reg"), :immediately
+end
+
+execute "clear-cache" do
+  command "/usr/bin/lw-ad-cache --delete-all"
+  ignore_failure true
+  action :nothing
+  subscribes :run, resources(:execute => "likewise-config-reload"), :immediately
+end
+
+### Services (not always started?)
+service "lsassd" do
+  supports :restart => true, :status => true
+  action [ :enable, :start ]
+  notifies :run, resources(:execute => "clear-cache"), :immediately
+end
+
+# eventlogd lwiod lwregd netlogond
+for lwservice in [ "eventlogd", "lwiod", "lwregd", "netlogond"  ] do
+  service lwservice do
+    supports :restart => true, :status => true
+    action [ :enable, :start ]
+  end
+end
+
+### CONFIGS
+template "/etc/likewise-open/lsassd.reg" do
+  source "lsassd.reg.erb"
+  mode "0644"
+  variables(
+    :ad_membership_required => ad_config['membership_required']
+  )
+  notifies :run, resources(:execute => "load-reg"), :immediately
+end
+
+cookbook_file "nsswitch.conf" do
+  source "nsswitch.conf"
+  path "/etc/nsswitch.conf"
+  owner "root"
+  group "root"
+  mode 0644
+end
+
+
