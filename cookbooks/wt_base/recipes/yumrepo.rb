e52280177ff442589c9b817ad51450fd55c76ebf
David Dvorak <david.dvorak@webtrends.com>
2012-02-24 19:41:41 +0000

    added cookbook wt_base to use for providing wt specific resources

---
+++ cookbooks/wt_base/recipes/yumrepo.rb
@@ -0,0 +1,18 @@
+#
+# Author:: David Dvorak (<david.dvorak@webtrends.com>)
+# Cookbook Name:: wt_base
+# Recipe:: yumrepo
+#
+# Copyright 2012, Webtrends Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+node['wt_base']['yum'].each do |yumrepo|
+	yum_repository yumrepo['name'] do
+		repo_name yumrepo['name']
+		description yumrepo['description']
+		url yumrepo['url']
+		action :add
+	end
+end
