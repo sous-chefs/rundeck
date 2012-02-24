1b8532a68d543b1660712f24ac5687ece106a1cf
David Dvorak <david.dvorak@webtrends.com>
2012-02-24 00:38:32 +0000

    add wt_cassandra cookbook
    Former-commit-id: 222131324f1a37e46819203f905838064318e3bc

---
+++ cookbooks/wt_cassandra/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Webtrends Inc."
+maintainer_email "david.dvorak@webtrends.com"
+license          "All rights reserved"
+description      "Installs Cassandra"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+supports         "centos"
---
+++ cookbooks/wt_cassandra/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Author:: David Dvorak (<david.dvorak@webtrends.com>)
+# Cookbook Name:: wt_cassandra
+# Recipe:: default
+#
+# Copyright 2012, Webtrends Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
