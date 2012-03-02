7b19b0202f6b1d95d77709a2e89d0854a554793d
David Dvorak <david.dvorak@webtrends.com>
2012-03-02 20:57:45 +0000

    added LWRP for icacls.exe
    Former-commit-id: 7b6e63f734a383fb5b981fa293fab97230309967 [formerly 4dfdd3dc2318f6239c64b51a9fb2bece4d0b77bc] [formerly 27fede62c341460395f49ab9dfa460acb86f6cbb [formerly 6deda3cb6d3e8099989c70bf6d632cc0ed376c03]]
    Former-commit-id: a56a48af770ab51516c2cc208a9197268a13f7a7 [formerly ee931452f9c4d8b1f5ec62e742ddc62864d8ab56]
    Former-commit-id: b1bfa00e7ae7502e72aa174bf5edac706332d0aa

---
+++ cookbooks/wt_base/providers/icacls.rb
@@ -0,0 +1,46 @@
+#
+# Author:: David Dvorak (<david.dvorak@webtrends.com>)
+# Cookbook Name:: wt_base
+# Provider:: icacls
+#
+# Copyright:: 2012, Webtrends Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include Chef::Mixin::ShellOut
+
+action :grant do
+	Chef::Log.debug("icacls.exe grant #{@new_resource.path} => #{@new_resource.user} => #{@new_resource.perm}")
+	if @new_resource.user == nil
+		raise Chef::Exceptions::AttributeNotFound, "user or group could not be determined"
+	end
+	case @new_resource.perm
+		when :full
+			perm = "(oi)(ci)(f)"
+		when :modify
+			perm = "(oi)(ci)(m)"
+		when :read
+			perm = "(oi)(ci)(rx)"
+		else
+			raise Chef::Exceptions::AttributeNotFound, "perm could not be determined, please use :full, :modify or :read"
+	end
+	shell_out!("icacls \"#{@new_resource.path}\" /grant:r \"#{@new_resource.user}\":#{perm}")
+end
+
+action :remove do
+	Chef::Log.debug("icacls remove #{@new_resource.path} => #{@new_resource.user}")
+	if @new_resource.user == nil
+		raise Chef::Exceptions::AttributeNotFound, "user or group could not be determined"
+	end
+	shell_out!("icacls.exe \"#{@new_resource.path}\" /remove \"#{@new_resource.user}\" /t /c /l /q")
+end
+
+action :run do
+	Chef::Log.debug("icacls remove #{@new_resource.path} => #{@new_resource.user}")
+	if @new_resource.options == nil
+		raise Chef::Exceptions::AttributeNotFound, "options need to be provided"
+	end
+	shell_out!("icacls.exe #{@new_resource.options}")
+end
+
