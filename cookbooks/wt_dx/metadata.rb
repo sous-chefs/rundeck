f35aa4e06c190648eaa949358713371bd7b5d4e3
Kendrick Martin <kendrick.martin@webtrends.com>
2012-03-03 00:26:33 +0000

    Modifying to add node attribute based on build number. Fixed issue in web.config for v3
    Former-commit-id: bcdabd519cbd0bf6b38ce9d2539faee20d72cf54 [formerly dc7d98234b654b2b524c5205d2c2b2b6bb711fc7] [formerly 11e8b90bb1097d0c87f4c4798513867899624301 [formerly 7671b7435a6ffb31ad8bead5b37671d0fd21c82c]]
    Former-commit-id: b9ad981232f7f127ef241d0d1bba24f1136191f1 [formerly 9e4922b5d4b3dbeca5bb781b51b825a9dc1ed3ff]
    Former-commit-id: 865045dabf0c8d7f89538f385b717d4286236521

--- cookbooks/wt_dx/metadata.rb
+++ cookbooks/wt_dx/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "kendrick.martin@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures DX"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.112"
+version          "0.1.116"
 supports         "windows"
 depends          "iis", ">= 1.0.12"
 depends 	 "windows"
