6c321f551cb78efcb609a6a40b92e329d49c8725
martink <kendrick.martin@webtrends.com>
2012-03-06 09:19:21 -0800

    Added install server to C and D pods. Corrected string concat in roadrunner script
    Former-commit-id: 146f7167aeb5cf5d5bde9fa822a2d6666c491e32 [formerly e7ee2680dc5a98f777e2d3517a763f9d44ed4cb2] [formerly a78a057b870672ed4bde995398da74cb6271fd05 [formerly 44fbd8514977c07c777b80180f842be018d188a3 [formerly 6ad2f5cdbef75dfecce696ecde7a27610e31fa58]]]
    Former-commit-id: 71ffeec1d02c35a496af497499a8092083dd06a4 [formerly 82be7dba701108be1be6947d5bca557e49a87b03]
    Former-commit-id: b4be1d034441386b093529343a92227fa368531f
    Former-commit-id: fdfde0c3dd83d08f7b34ada8bbbfb6795e2c0105

--- cookbooks/wt_roadrunner/metadata.rb
+++ cookbooks/wt_roadrunner/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "kendrick.martin@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures Roadrunner service"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.48"
+version          "0.0.50"
 
 supports         "windows"
 
