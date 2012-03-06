efb6e29a98b65e497bde0329564c6850704c7305
vonnagy <ivan.vonnagy@webtrends.com>
2012-03-05 16:38:10 -0800

    Initial check in of Kafka cookbook
    Former-commit-id: 69572647a1d6335b21dc18ba05bf8ce6f6e75169 [formerly 69572647a1d6335b21dc18ba05bf8ce6f6e75169 [formerly da9f9b3107f96a737b13b8fe2dff34c2f0b765f3]]
    Former-commit-id: 070b1267392f4c736318d2dac064e1e74e4ac04b
    Former-commit-id: 47873ff54315ba1ac3e69dd5835d3ca7f61e5154

---
+++ cookbooks/kafka/recipes/default.rb
@@ -0,0 +1,138 @@
+#
+# Cookbook Name::		kafka
+# Description::         Base configuration for Kafka
+# Recipe::				default
+#
+# Copyright 2012, Webtrends, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# == Recipes
+include_recipe "java"
+include_recipe "runit"
+
+# == Users
+
+# Setup kafka user
+user "#{node[:kafka][:user]}" do
+  comment "Kafka user"
+  gid "#{node[:kafka][:group]}"
+  home "/home/#{node[:kafka][:user]}"
+  shell "/bin/bash"
+  supports :manage_home => true
+end
+
+# == Directories
+home_dir = node[:kafka][:home_dir]
+
+# Create a staging home for Kafka
+directory "#{node[:kafka][:stage_dir]}" do
+	mode "0755"
+	action :create
+	owner "root"
+	group "root"
+	not_if { ::File.exists?("#{node[:kafka][:stage_dir]}") }
+end
+
+# Download Kafka to the staging directory. For now the tar is embedded with the cookbook.
+cookbook_file "/usr/local/src/kafka-#{node[:kafka][:version]}.tar.gz" do
+  source 	"kafka-#{node[:kafka][:version]}.tar.gz"
+  owner 	"#{node[:kafka][:user]}"
+  group 	"#{node[:kafka][:group]}"
+  mode 		"0744"
+  not_if 	"test -f /usr/local/src/kafka-#{node[:kafka][:version]}.tar.gz"
+end
+
+# Extract it
+execute 	"extract-kafka" do
+  command 	"tar -zxf /usr/local/src/kafka-#{node[:kafka][:version]}.tar.gz"
+  creates 	"kafka-#{node[:kafka][:version]}"
+  cwd 		"#{node[:kafka][:stage_dir]}"
+  user 		"root"
+  group 	"root"
+end
+
+# link the extracted bits
+link home_dir do
+	to 		"#{node[:kafka][:stage_dir]}"
+	action    	:create
+	link_type 	:symbolic
+	owner 		"#{node[:kafka][:user]}"
+	group 		"#{node[:kafka][:group]}"
+	not_if { ::File.exists?("#{home_dir}") }
+end
+
+# The directories to manage
+kafka_dirs = [
+	node['kafka']['data_dir'],
+	node['kafka']['stage_dir'],
+	node['kafka']['log_dir']
+]
+
+# Create all other directories, if needed
+kafka_dirs.each do |dir|
+	directory dir do
+	mode 	"0755"
+	action 	:create
+	owner 	"root"
+	group 	"root"
+	not_if { ::File.exists?(dir) }
+	end
+end
+
+# grab the zookeeper nodes that are currently available
+zookeeper_pairs = Array.new
+if not Chef::Config.solo
+	search(:node, "role:#{node[:kafka][:zookeeper_role]}").each do |n|
+		zookeeper_pairs << n[:fqdn]
+	end
+end
+
+i = 0
+while i < zookeeper_pairs.size do
+  zookeeper_pairs[i] = zookeeper_pairs[i].concat(":2181")
+  i += 1
+end
+
+directory('/etc/sv/kafka/env'){ owner 'root' ; action :create ; recursive true }
+runit_service "kafka"
+
+# templates
+template "#{home_dir}/bin/kafka-run-class.sh" do
+	source	"kafka-run-class.sh.erb"
+	owner	"root"
+	group   "root"
+	variables({
+		:kafka => node[:kafka],
+		:zookeeper_pairs => zookeeper_pairs
+	})
+end
+
+%w[server.properties consumer.properties producer.properties zookeeper.properties log4j.properties].each do |template_file|
+  template "#{home_dir}/config/#{template_file}" do
+	source	"#{template_file}.erb"
+	owner	"root"
+	group   "root"
+	variables({
+		:kafka => node[:kafka],
+		:zookeeper_pairs => zookeeper_pairs
+	})
+	notifies      :restart, "service[kafka]", :delayed
+  end
+end
+
+service "kafka" do
+	action :start
+end
+
+
