e48e9afe711d9b110b001782b654d33de492ef2b
Tim Smith <tim.smith@webtrends.com>
2012-03-08 18:17:36 +0000

    Add procmail and php-ldap to the default recipe in Nagios XI
    Former-commit-id: d2d3c36057fec39ec7e8d4fef5fadf9c7b1e5df1 [formerly dc1152ea30eceaa2d355bd40d002378234aeca0e] [formerly 5473b8a40885b450ff464aaaba828f23f7d9e6cd [formerly 5af7555570f113539f31478f00f202f751438825]]
    Former-commit-id: c9b16d3222e30bc3383b5f22c9486a7285c8f606 [formerly 0d794b1d84c1a58722235c609babaed7aa1457c5]
    Former-commit-id: 91ada2d86c1a4e7bd8d76ee8d84f2e74979539bd

--- cookbooks/nagios-xi/recipes/default.rb
+++ cookbooks/nagios-xi/recipes/default.rb
@@ -2,7 +2,16 @@
 # Cookbook Name:: nagios-xi
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, Webtrends Inc
 #
 # All rights reserved - Do Not Redistribute
 #
+
+#Install the prereqs for Nagios XI
+package "procmail" do
+  action :install
+end
+
+package "php-ldap" do
+  action :install
+end
