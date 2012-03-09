bc49ba80f3d9c5ae78f9a6f9e176a562108f11cb
Tim Smith <tim.smith@webtrends.com>
2012-03-09 19:21:45 +0000

    Delete the hosts cookbook

--- cookbooks/hosts/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "Jeffrey Hulten"
-maintainer_email "jeffh@automatedlabs.com"
-license          "MIT Licensed"
-description      "Installs/Configures hosts"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.1"
