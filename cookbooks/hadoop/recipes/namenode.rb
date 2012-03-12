41d438d93f2a58c1761648c5cdd29017a6f50928
Tim Smith <tim.smith@webtrends.com>
2012-03-12 16:35:03 -0700

    Add the necessary create for the hadoop name node / backup name node
    Former-commit-id: 60ed7f5ff45e52a13ccac5ce5c33b9e3a95a5c41 [formerly 132ec6d93ebb8fd482edac32f302f850c5cc22da] [formerly 36e641fc5b228b806723af563aedd2957645d10f [formerly 3216c051cb4d23a57d245d70e099965a2e9078a0 [formerly 24682c01439cb77818e3567ec3e691fd9e9659e5]]]
    Former-commit-id: 4c0daa56eb2912940018a18e8588f5e6d5e55b19 [formerly f08fd1f73b1b32095e54597e376ccbb8248e2205]
    Former-commit-id: ca87b1f1c8073aa51ea1b801c084942cd3a24453
    Former-commit-id: 1e61a780041bf2735fa765f6b14eb61ff6df5792

--- cookbooks/hadoop/recipes/namenode.rb
+++ cookbooks/hadoop/recipes/namenode.rb
@@ -17,8 +17,11 @@
 # limitations under the License.
 #
 
-
 include_recipe "hadoop"
 
-
-
+directory "/var/lib/hadoop/hdfs/namenode/current" do
+	owner "hadoop"
+	group "hadoop"
+	mode "0755"
+	action :create
+end
