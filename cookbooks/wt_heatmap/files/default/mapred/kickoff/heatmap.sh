c3b5228ff37ce22e11304d2c99f36d08f60a9acf
seanm <reinvigorate@gmail.com>
2012-03-16 12:26:22 -0600

    improving heatmap kickoff script
    Former-commit-id: 6cf357296b9f733a10ed4f333b024c4f685dfd18 [formerly 12bbf0257da0fbbb60e7d83afa0dbb12772d8146] [formerly cc93a1e9cbb888a84779fc157cebe2da3a81f49e [formerly 64d4deab9ba89d97508b722fd5efe4786876557a]]
    Former-commit-id: 1301b5b4804ea5ca2b0ea870b11ecc0b426be8a1 [formerly f4f14a01f5e251e496df289a440d5da868d6be6e]
    Former-commit-id: bc606224bfe689cc281c8ca650df1ba6f539ef36

---
+++ cookbooks/wt_heatmap/files/default/mapred/kickoff/heatmap.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+# -- usage ----------------------------------------------------------
+#
+#  heatmaps.sh
+#    * computes heatmaps for current day
+#
+#  heatmaps.sh YYYY-MM-DD
+#    * computes heatmaps for YYYY-MM-DD
+#
+#  heatmaps.sh YYYY-MM-DD YYYY-MM-DD
+#    * computes heatmaps for date range
+#
+#  If the last argument is "--", the HiveQL query will be printed
+#  but not executed.
+#
+
+
+# -- variables ------------------------------------------------------
+
+max_clicks_per_block=2000
+
+if [ "$2" ]
+then
+	begin_date=$1
+	end_date=$2
+else
+	if [ "$1" ]
+	then
+		begin_date="$1"
+		end_date="$1"
+	else
+		begin_date=$(date -u -d now +"%Y-%m-%d")
+		end_date=$(date -u -d now +"%Y-%m-%d")
+	fi
+fi
+
+
+# -- query ----------------------------------------------------------
+
+query=$(cat <<HIVEQL
+add FILE /usr/local/mapred/reduce/dynamic_url.py;
+add FILE /usr/local/mapred/reduce/heatmaps.py;
+FROM
+(
+  FROM
+  (
+    FROM
+    (
+      SELECT
+        json
+        , ds
+        , hr
+      FROM logs
+      WHERE
+        ds >= '$begin_date'
+        AND ds <= '$end_date'
+    ) q1 SELECT TRANSFORM (json, ds, hr) USING 'python dynamic_url.py' AS json
+      DISTRIBUTE BY
+        get_json_object(json, '$.page_key')
+        , get_json_object(json, '$.ds')
+        , get_json_object(json, '$.hr')
+        , get_json_object(json, '$.WT_hm_y')
+      SORT BY 
+        get_json_object(json, '$.page_key') ASC
+        , get_json_object(json, '$.ds') ASC
+        , get_json_object(json, '$.hr') ASC
+        , CAST(get_json_object(json, '$.WT_hm_y') AS int) ASC
+  ) q2 SELECT TRANSFORM (json) USING 'python heatmaps.py $max_clicks_per_block' AS (page_key, ds, hr, block_id, clicks)
+) q3 INSERT OVERWRITE TABLE heatmap_reports SELECT CONCAT(page_key,'\;',ds,'\;',hr), map(block_id, clicks)
+;
+HIVEQL
+)
+
+
+# -- job ------------------------------------------------------------
+
+tmp="${@: -1}"
+if [ "$tmp" == "--" ]
+then
+	echo "$query"
+else
+	/usr/local/hive/bin/hive \
+	--auxpath /usr/local/hive/lib/hbase-0.92.0.jar,/usr/local/hive/lib/zookeeper-3.3.1.jar,/usr/local/hive/lib/hive-hbase-handler-0.8.1.jar \
+	-hiveconf hbase.zookeeper.quorum=$(cat /etc/zookeeper | paste -s -d ',') \
+	-e "$query" 2>&1
+fi
+
+exit $?
