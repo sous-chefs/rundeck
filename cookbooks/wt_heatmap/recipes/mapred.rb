c223657e81f3e5e9cb3830bd78eb7a02593d034d
seanm <reinvigorate@gmail.com>
2012-03-16 12:26:22 -0600

    improving heatmap kickoff script
    Former-commit-id: 367738c93cfa3e7759a63c1ebb4a91c824a9fb34 [formerly c3b5228ff37ce22e11304d2c99f36d08f60a9acf] [formerly bc606224bfe689cc281c8ca650df1ba6f539ef36] [formerly 1301b5b4804ea5ca2b0ea870b11ecc0b426be8a1 [formerly f4f14a01f5e251e496df289a440d5da868d6be6e]] [formerly 6cf357296b9f733a10ed4f333b024c4f685dfd18 [formerly 12bbf0257da0fbbb60e7d83afa0dbb12772d8146] [formerly cc93a1e9cbb888a84779fc157cebe2da3a81f49e [formerly a9682aea6964ecda1f0fd82ac05b3556570fb593 [formerly 64d4deab9ba89d97508b722fd5efe4786876557a]]]]
    Former-commit-id: da5ce82e176e3641d1031f4a28b830b87b241236 [formerly 0f972edd7b2de6cebc75a822eae55edf86a0328d] [formerly 1c1eb5c499aac73b4bdb24c95e38761ba99e0f41 [formerly a9682aea6964ecda1f0fd82ac05b3556570fb593]]
    Former-commit-id: b2ddaceb4d5d77970f9e7d2634ffdb117643103f [formerly 42466dcb7a55ca7fbe21bf51f7222d87459b2d03]
    Former-commit-id: c43a3f8bef239146ed0db8f9eb0fe9db44880f33
    Former-commit-id: 979bd4028071036409358e9d9aa0782b22960aaa

--- cookbooks/wt_heatmap/recipes/mapred.rb
+++ cookbooks/wt_heatmap/recipes/mapred.rb
@@ -1,6 +1,18 @@
 
 package "php-cli"
 
+zk_quorum = search(:node, "role:zookeeper AND chef_environment:#{node.chef_environment}")
+
+template "/etc/zookeeper" do
+  source "zookeeper"
+  owner "hadoop"
+  group "hadoop"
+  mode 0755
+  variables(
+    :zk_quorum => zk_quorum
+  )
+end
+
 remote_directory "/usr/local/mapred" do
   source "mapred"
   owner "hadoop"
