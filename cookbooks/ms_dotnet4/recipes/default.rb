25e6cef7085ac7d0eabfd7bac5dcaf2116bc8421
Tim Smith <tim.smith@webtrends.com>
2012-03-16 18:49:56 -0700

    Add support for Windows Vista to the .NET 4.0 cookbook (best commit EVER)
    Former-commit-id: 00bafd3bbe7ff1f27003cfb119c5eb3074efb1b2 [formerly 242944ee9d486e658e2b44f70f9865db9d69ba0b] [formerly fc8d976a824e95585f257000b9bbb5bfb642522d] [formerly 099abcffc4f5630748fff31155f725523156345a [formerly aa2b9bc420392bc76d5c41b51f29740836292a3a]] [formerly bb135e75ffb63ed7011d9e8b9af7e2a57c62531a [formerly 03b2c0538210490e66b376845f557b8d607d9eb1] [formerly df22da0e5760f39586739f9960fcb70edb142d63 [formerly b2c2f1b52cdb32e87bcb1f48db7adb247219ff63 [formerly 624dd5c1a4f06bfc14c91bd13614564a014ec648]]]]
    Former-commit-id: 1a19e9c48b037d87b3306c208c27a8d9e79db167 [formerly 3b5312db4d36978dfd44ec4c8a5d83e883b6aff7] [formerly 38fe7ae0bf0c039e012f88425360d29e83138c81 [formerly b2c2f1b52cdb32e87bcb1f48db7adb247219ff63]]
    Former-commit-id: fd288c7d21c47256c09303d91f9bd40b6deaf59c [formerly 973d505df7703632341ae5aa5b24847ca224fc23]
    Former-commit-id: a5a6dde9b26597e2f4d95894c6df049a6ed0d18b
    Former-commit-id: 5747f7b45610ae0f95ac3fd07c9c52b147dd8f80

--- cookbooks/ms_dotnet4/recipes/default.rb
+++ cookbooks/ms_dotnet4/recipes/default.rb
@@ -9,7 +9,7 @@
 # Code based off the PowerShell cookbook by Seth Chisamore
 
 if platform?("windows")
-  if (win_version.windows_server_2008? || win_version.windows_server_2008_r2? || win_version.windows_7?)
+  if (win_version.windows_server_2008? || win_version.windows_server_2008_r2? || win_version.windows_7? || win_version.windows_vista?)
     windows_package "Microsoft .NET Framework 4 Client Profile" do
       source node['ms_dotnet4']['http_url']
       installer_type :custom
@@ -17,8 +17,8 @@ if platform?("windows")
       action :install
      end
   elsif (win_version.windows_server_2003_r2? || win_version.windows_server_2003? || win_version.windows_xp?)
-    Chef::Log.warn('The .NET 4.0 Chef recipe currently only supports Windows 7, 2008, and 2008 R2.')
+    Chef::Log.warn('The .NET 4.0 Chef recipe currently only supports Windows Vista, 7, 2008, and 2008 R2.')
   end
 else
-  Chef::Log.warn('Microsoft .NET 4.0 can only be installed on the Windows platform.')
+  Chef::Log.warn('Microsoft .NET Framework 4.0 can only be installed on the Windows platform.')
 end
