222d971191f422acced9b45061274c2b3b520136
Tim Smith <tim.smith@webtrends.com>
2012-03-16 19:15:28 -0700

    Fix the versions support for .Net 3.5 and add logic so .Net 4 doesn't try to install on every run
    Former-commit-id: 9a5a656b1011bcfd5609bc62d9e245a3f3e57fe5 [formerly 0fe77c2886db3327ed33c66799d6f1b894c58b9c] [formerly d3a357d6e5f8321e234b7b8bcdec8f571adebd89 [formerly 0f19375fa5037de664bbabf7b70f60d77ea25a51 [formerly 40c8a5b8681c9eed4c4cb16be73673807c586ebe]]]
    Former-commit-id: f473e545338199de442d81c5fa62b013068a060f [formerly 423d8f170b20bbdb05571b456f8f3ad8adfb060e]
    Former-commit-id: 85f4ab7465b02362ac55b0bd2c08a7a68b165419
    Former-commit-id: ee6d2d0a9045657e8447091f63c3ca0aff717630

--- cookbooks/ms_dotnet4/recipes/default.rb
+++ cookbooks/ms_dotnet4/recipes/default.rb
@@ -10,12 +10,14 @@
 
 if platform?("windows")
   if (win_version.windows_server_2008? || win_version.windows_server_2008_r2? || win_version.windows_7? || win_version.windows_vista?)
-    windows_package "Microsoft .NET Framework 4 Client Profile" do
-      source node['ms_dotnet4']['http_url']
-      installer_type :custom
-      options "/quiet /norestart"
-      action :install
-     end
+    if !File.exists?("C:/Windows/Microsoft.NET/Framework/v4.0.30319")
+      windows_package "Microsoft .NET Framework 4 Client Profile" do
+        source node['ms_dotnet4']['http_url']
+        installer_type :custom
+        options "/quiet /norestart"
+        action :install
+       end
+	 end
   elsif (win_version.windows_server_2003_r2? || win_version.windows_server_2003? || win_version.windows_xp?)
     Chef::Log.warn('The .NET 4.0 Chef recipe currently only supports Windows Vista, 7, 2008, and 2008 R2.')
   end
