16f3faba7bf5543c5b0cd93c8f2993c9603aba1c
Tim Smith <tim.smith@webtrends.com>
2012-03-22 18:19:44 +0000

    Add Pete's rundeck recipe to the repo
    Former-commit-id: 6d5d1d14c4f150646817f177166a9ea552dd57fb [formerly 91bc7cd8af8221bd92d8a5456f4d49fc425a07eb] [formerly f515ed5ed0fa81f9cbc7d4323f3125ca858f8316] [formerly fb01760081140a6631ec76fe4ab1111e6ca4b34d [formerly 65690c51dfec90be19b70a88187086a95c8223ec]] [formerly b78435391b2eda103c82926ed7b07de774ba16ed [formerly 408a8d8091edb05c5437a6e60a993aafd118c084] [formerly 211a1b822283add559b35937852233ac024d0392 [formerly 9dd70e47372dea04cb5400af253f9c4121147735 [formerly d07e77f9f5c8be09ba7e91bc2d31f29e28f1e607]]]]
    Former-commit-id: bb251391d65a4647b44cda1f771bf4614306b206 [formerly f8c937865d112db263df62610defc9f9559ec87f] [formerly ea4c86f6f086fff22a02f40fe29bc8278c288e23 [formerly 9dd70e47372dea04cb5400af253f9c4121147735]]
    Former-commit-id: dafd8ca6f28b8e6554f92cd87648a4144a41649b [formerly 9f77484369e8e26cb9ce5d4afe886b7f63f55358]
    Former-commit-id: 4a9dfbc5e3bba96b9791684ffdaa73517c764ffe
    Former-commit-id: 9cdda1067d94b8313971fbd41ac363d0409ec394

---
+++ cookbooks/rundeck/recipes/chef-rundeck.rb
@@ -0,0 +1,80 @@
+#
+# Cookbook Name:: rundeck
+# Recipe:i: chef-rundeck 
+#
+# Copyright 2011, Peter Crossley
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+require 'json'
+
+include_recipe 'rundeck::default'
+
+bags = data_bag('rundeck')
+
+projects = {}
+bags.each do |project|
+  pdata = data_bag_item('rundeck', project) 
+  projects[project] = { 
+    "pattern" => pdata['pattern'],
+    "username" => pdata['username'],
+    "hostname" => pdata['hostname']
+  }
+end
+
+file node[:rundeck][:project_config] do
+  content JSON.pretty_generate(projects)
+  mode "0644"
+  notifies :restart, "service[chef-rundeck]"
+end
+
+cookbook_file "/tmp/chef-rundeck-0.2.0.gem" do
+  source "chef-rundeck-0.2.0.gem"
+  mode "0644"
+end
+
+gem_package "chef-rundeck" do
+  action :install
+  source "/tmp/chef-rundeck-0.2.0.gem"
+end
+
+gem_package "sinatra"
+
+link "/usr/bin/chef-rundeck" do
+  to "/var/lib/gems/1.8/bin/chef-rundeck"
+  only_if do node[:platform_version] >= "10.04" end
+end 
+
+template "/etc/chef/rundeck.rb" do
+  owner node[:rundeck][:user]
+  group node[:rundeck][:user]
+  source "rundeck.rb.erb"
+  variables(
+    :rundeck => node[:rundeck]
+  )
+end
+
+runit_service "chef-rundeck" do
+  run_restart false
+  options(
+    :user => node[:rundeck][:user],
+    :chef_config => node[:rundeck][:chef_config],
+    :chef_webui_url => node[:rundeck][:chef_webui_url],
+    :project_config => node[:rundeck][:project_config]
+  )
+  notifies :restart, "service[chef-rundeck]"
+end
+
+service "chef-rundeck" do
+  action :start
+end
---
+++ cookbooks/rundeck/recipes/server.rb
@@ -0,0 +1,140 @@
+#
+# Cookbook Name:: rundeck
+# Recipe::server
+#
+# Copyright 2011, Peter Crossley
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe 'rundeck::default'
+include_recipe 'java'
+include_recipe "apache2"
+include_recipe "apache2::mod_deflate"
+include_recipe "apache2::mod_headers"
+include_recipe "apache2::mod_ssl"
+include_recipe "apache2::mod_proxy"
+include_recipe "apache2::mod_proxy_http"
+
+
+
+cookbook_file "/tmp/#{node[:rundeck][:deb]}" do
+  source node[:rundeck][:deb]
+  owner node[:rundeck][:user]
+  group node[:rundeck][:user]
+  mode "0644"
+end
+
+package "#{node[:rundeck][:deb]}" do
+  action :install
+  source "/tmp/#{node[:rundeck][:deb]}"
+  provider Chef::Provider::Package::Dpkg
+end
+
+
+directory "#{node[:rundeck][:user_home]}/.ssh" do
+  owner node[:rundeck][:user]
+  group node[:rundeck][:user]
+  recursive true
+  mode "0700"
+end
+
+cookbook_file "#{node[:rundeck][:user_home]}/.ssh/id_rsa" do
+  owner node[:rundeck][:user]
+  group node[:rundeck][:user]
+  mode "0600"
+  backup false
+  source "rundeck"
+end
+
+
+template "#{node[:rundeck][:configdir]}/jaas-activedirectory.conf" do
+  owner node[:rundeck][:user]
+  group node[:rundeck][:user]
+  source "jaas-activedirectory.conf.erb"
+  variables(
+    :ldap => node[:rundeck][:ldap]
+  )
+end
+
+template "#{node[:rundeck][:configdir]}/profile" do
+  owner node[:rundeck][:user]
+  group node[:rundeck][:user]
+  source "profile.erb"
+  variables(
+    :rundeck => node[:rundeck]
+  )
+end
+
+
+file "/etc/init.d/rundeckd" do
+  action :delete
+end
+
+runit_service "rundeck" do
+  run_restart false
+  options(
+    :rundeck => node[:rundeck]
+  )
+end
+
+service "rundeck" do
+  action :start
+end
+
+
+# load up the apache conf
+template "apache-config" do
+  path "#{node[:apache][:dir]}/sites-available/rundeck.conf"
+  source "rundeck.conf.erb"
+  mode 0644
+  owner "root"
+  group "root"
+  notifies :reload, resources(:service => "apache2")
+end
+
+apache_site "rundeck.conf" do
+  notifies :reload, resources(:service => "apache2")
+end
+
+
+
+#load projects
+bags = data_bag('rundeck')
+
+projects = {}
+bags.each do |project|
+  pdata = data_bag_item('rundeck', project) 
+  
+  execute "check-project-#{project}" do
+    user node[:rundeck][:user]
+    command "/usr/bin/rd-project -p #{project} -a create"
+    not_if do
+      File.exists?("#{node[:rundeck][:basedir]}/projects/#{project}/etc/project.properties")
+    end 
+  end
+  
+  template "#{node[:rundeck][:basedir]}/projects/#{project}/etc/project.properties" do
+    source "project.properties.erb"
+    owner node[:rundeck][:user]
+    group node[:rundeck][:user]
+    variables(
+      :project => project,
+      :data => pdata,
+      :rundeck => node[:rundeck]
+    )
+    only_if do
+      File.exists?("#{node[:rundeck][:basedir]}/projects/#{project}/etc")
+    end
+  end
+end
