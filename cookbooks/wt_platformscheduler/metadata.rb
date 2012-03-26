24660392e4bff00d0a0f980c500b8971d3e4224a
Kendrick Martin <kendrick.martin@webtrends.com>
2012-03-26 22:52:59 +0000

    Added cookbook for installing VDM Scheduler Agent
    Former-commit-id: 066a7b2cf140e306e6aa703d5d9cbe32d11d3a07 [formerly e1b12d3b618a8447ca4b93b35c8356283e053807] [formerly 71398c2f62907a3d00dab74dca590c4b5f64dfc9 [formerly 29eb43e5e691798227dbf636913395f84a0fe935 [formerly 9769e4e6781ac65fdacdd55223167dd9b9b43097]]]
    Former-commit-id: eec1d1a04f553c25559d293c8ccefbc438db06d2 [formerly 8f0ccbc56d5066b377cf1c357fe2dbd274da3fef]
    Former-commit-id: cb92fe2cacbdf7eff5dce074adf4f6be957bed60
    Former-commit-id: 1ae1dee60ab7906247fb3f55b2e8ffc9207f9da7

---
+++ cookbooks/wt_platformscheduler/metadata.rb
@@ -0,0 +1,9 @@
+name 'wt_platformscheduler'
+maintainer       "Kendrick"
+maintainer_email "kendrick.martin@webtrends.com"
+license          "All rights reserved"
+description      "Installs/Configures Platform Scheduler"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.78"
+depends          "wt_base"
+depends 		 "windows"
