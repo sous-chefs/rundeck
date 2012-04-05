41706ee700979534b97ef37c3289da3ca1a301f7
Tim Smith <tim.smith@webtrends.com>
2012-04-04 22:00:42 -0700

    Update readme for platform scheduler and rev version to 10.4
    Former-commit-id: 82dcaeb1d742c91a249290f4cd79281ecb34a269 [formerly 3fe6e18ae639430fc01ee9e94952b9ef14abda06] [formerly a65a385a2456a0341e8592ce9dd9b9ae948a1686 [formerly 896fda176c7fa007172f9b7dbdd9f732229126ef]]
    Former-commit-id: 12a22a392c29d6a5e985ff5650f5c8ba9c4b8b00 [formerly 20a6bc46e2db169daeec901d89f2a44da730fe4b]
    Former-commit-id: 15c13f7c9f2009f67de695637c1e6c11da49bd89

--- cookbooks/wt_platformscheduler/metadata.rb
+++ cookbooks/wt_platformscheduler/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "kendrick.martin@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures Platform Scheduler"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.11"
+version          "0.10.4"
 depends          "wt_base"
 depends 		 "windows"
