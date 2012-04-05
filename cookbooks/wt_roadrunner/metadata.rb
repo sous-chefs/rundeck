cee1f68fe6652ec17ff18c7d34ad47e7794e7b33
Tim Alan Smith <tim.smith@webtrends.com>
2012-04-05 17:37:58 +0000

    Rev wt_roadrunner to 0.10.4 while we work out our ruby issues
    Former-commit-id: d714280b018ae0abfb9ce7907a8ac6b33c37d3af [formerly a413e6afd8a5f0603fc7c7ddbab154c8defac9fa] [formerly b467c89a96197ec9bb2ae367444472d9363b3ae5 [formerly f37ace3250736cb2957892ffcf907e25e07b40be]]
    Former-commit-id: 22aa485ded7913571c20a2b2f4d1f9d3717319de [formerly 27af791b9fee0df5c813f97c9a12061a05a77fa0]
    Former-commit-id: d8048609021cbf45a320cdd530f1db6baf2db540

--- cookbooks/wt_roadrunner/metadata.rb
+++ cookbooks/wt_roadrunner/metadata.rb
@@ -4,8 +4,6 @@ maintainer_email "kendrick.martin@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures Roadrunner service"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.10.31"
-
+version          "0.10.4"
 supports         "windows"
-
 depends          "wt_base"
