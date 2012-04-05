5f30a0fe757c99e72bda2b6f982ca0d653e0e602
Kendrick Martin <kendrick.martin@webtrends.com>
2012-04-05 21:27:10 +0000

    Adding new devops auth data bag. Adding UAC and domain joining to windows ondemand_base
    Former-commit-id: 3c9db8b65686a2d95ce19e46d455bbe7bca41497 [formerly 69302acb91f67a35a00b9acaac78ae14f070d374] [formerly b741c7c0b400557481f9a0fe1a7c1d3099e20334 [formerly 62287541955b3d2bca76b203ad0e03b57adbe6f5 [formerly 91b53a9721e51da689231d3380398e9add106653]]]
    Former-commit-id: 42d316560c3a9d4f389cd8c051ab0748e0701b3f [formerly 2037fb01d10d72f72ca9912cf38be4e718379d99]
    Former-commit-id: 2e38e5fd21d38e8ad8975a7cf0d127f0533bc501
    Former-commit-id: 259d5029d4fd7a8ff5a4d414cab818b874d7fc42

--- cookbooks/ondemand_base/recipes/windows.rb
+++ cookbooks/ondemand_base/recipes/windows.rb
@@ -8,12 +8,17 @@
 # All rights reserved - Do Not Redistribute
 #
 
-#Make sure that this recipe only runs on ubuntu systems
+
+domain_to_join = node['authorization']['ad_auth']['ad_network']
+auth_data = data_bag_item('authorization', domain_to_join)
+
+#Make sure that this recipe only runs on Windows systems
 if not platform?("windows")
 	Chef::Log.info("Platform Windows required.")
 	return
 end
 
+include_recipe "windows::reboot_handler"
 #Save the node to prevent empty run lists on failures
 unless Chef::Config[:solo]
 	ruby_block "save node data" do
@@ -36,3 +41,25 @@ execute "powercfg-performance" do
 	action :run
 end
 
+#Install the working version of rubyzip gem
+gem_package("rubyzip") do
+  options("-v 0.9.5")
+end
+
+log "Are you joined to domain? #{node['kernel']['cs_info']['part_of_domain']} Current domain is #{node['kernel']['cs_info']['domain']}"
+
+execute "join domain" do
+  command "NETDOM join /Domain:#{domain_to_join} /userD:#{auth_data['auth_domain_user']} /passwordD:#{auth_data['auth_domain_password']} #{node['hostname']}"
+  not_if {node['kernel']['cs_info']['part_of_domain']}
+  notifies :request, "windows_reboot[60]"
+end
+
+windows_registry 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' do
+  values 'EnableLUA' => 0
+  notifies :request, "windows_reboot[60]"
+end
+
+windows_reboot 60 do
+  reason 'Chef Reboot'
+  action :nothing
+end
