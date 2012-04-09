314fee4ff67acc34cca56cf4a7e6a0d567899824
Tim Smith <tim.smith@webtrends.com>
2012-04-09 22:07:55 +0000

    Import postfix version 1.0.0
    Former-commit-id: 4b1b6c9f376f9a4576567684802b188002e44685 [formerly 11807c22230d6d81388ddfd9319eb2d55baada40] [formerly fa798a086d2a0d0e7ebc6cbf39e834bac18141ce] [formerly c2f6f5b63327648f5c1ac3e56cb557cd1d6372c6 [formerly 93e0b1e6b448897d5298efd7ac478c93be13140e]] [formerly a4468877ed4f6bead12936b2a9f8cd535f783c47 [formerly edcce993d6e02bcfd29c3847720419f90623dcd7] [formerly 5c15a3ae08ccadeb575c98e0940ef4f405ac0360 [formerly 9444182b98e232c3f26ea8dabd115016cd19e088 [formerly 399a8c9ecaad27dfd3053cf3071aa07e295cf889]]]]
    Former-commit-id: 5145e784cb112af710e5bd3215aa68e91fe0911f [formerly e650cb757840a9e883178a4684c09cfd65c7136c] [formerly 204aff731b6625ee48f80192896616daa0080965 [formerly 9444182b98e232c3f26ea8dabd115016cd19e088]]
    Former-commit-id: a23dbce89c7f1163e077fe50ee21394efe605582 [formerly 3a6300372ac0941ebb421e7f303f54ad3d0d03dc]
    Former-commit-id: ce08c94e93b8d9070978ea4ac8d1fa3d62bce7e4
    Former-commit-id: 08d249d10e80d1e7c68a89ec22f3e22ae73adb3c

--- cookbooks/postfix/metadata.rb
+++ cookbooks/postfix/metadata.rb
@@ -2,17 +2,23 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs and configures postfix for client or outbound relayhost, or to do SASL auth"
-version           "0.8.4"
+version           "1.0.0"
 recipe            "postfix", "Installs and configures postfix"
 recipe            "postfix::sasl_auth", "Set up postfix to auth to a server with sasl"
+recipe            "postfix::aliases", "Manages /etc/aliases"
 
-%w{ubuntu debian}.each do |os|
+%w{ubuntu debian redhat centos amazon scientific}.each do |os|
   supports os
 end
 
 attribute "postfix",
   :display_name => "Postfix",
   :description => "Hash of Postfix attributes",
+  :type => "hash"
+
+attribute "postfix/aliases",
+  :display_name => "Postfix Aliases",
+  :description => "Hash of Postfix aliases mapping a name to a value.  Example 'root' => 'operator@example.com'.  See aliases man page for details.",
   :type => "hash"
 
 attribute "postfix/mail_type",
@@ -80,3 +86,7 @@ attribute "postfix/smtp_sasl_passwd",
   :description => "Password for smtp_sasl_user_name",
   :default => ""
 
+attribute "postfix/aliases",
+  :display_name => "Postfix mail aliases",
+  :description => "Hash of mail aliases for /etc/aliases",
+  :default => ""
