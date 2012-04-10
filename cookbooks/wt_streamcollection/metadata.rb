1fb4cd95f56edfd13ebe822e3fea61b54365daf0
Marek Dolgos <marek.dolgos@webtrends.com>
2012-04-10 12:15:51 -0700

    Adding Stream-Collection server to Chef cookbooks.
    Former-commit-id: 0c0de73c5f9879b6c066f3e65d9c060a59c94798 [formerly 63395a6a66758abdce31320fe9c3f63f61ffea30] [formerly 1cd7ed01ca699910382caf372de2a814c36936c9] [formerly 9149f8829f7a08eff7148379907649b50252d149 [formerly a2475c5ddd0d7d566df69087686113b4c3a310a1]] [formerly a4fd1f9ce46fe681db384def896803be7ba40a01 [formerly ce34c6da817c77e18441b6b7f45f022fc16124c4] [formerly edd160e3ce0e07ef8dbb0b2e9f02ca7458132f03 [formerly c0e2dacd90fe0e6c3000daee8b8b600e2f01be23 [formerly db6dc82aa9d12755994aca5342f92246bcd3bef6]]]]
    Former-commit-id: b538d4f15a627dd81923de5aed0c12a40c91b5c3 [formerly ec95a99bc9b8039ccfb16354b951203f845565c2] [formerly bbb014b135631370bc6dd02246ea0e52438361ac [formerly c0e2dacd90fe0e6c3000daee8b8b600e2f01be23]]
    Former-commit-id: 98211ff58299d2ee093fd134322e46db13fdea05 [formerly f3ade539e4fe081fcfdcb9add76e32cfad345c8b]
    Former-commit-id: 940cf72f360c75a1ece440092423e42101c2096f
    Former-commit-id: f23cdf49c1a596b4bbf25c495c2f9a88a6d0ab1f

---
+++ cookbooks/wt_streamcollection/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "YOUR_COMPANY_NAME"
+maintainer_email "YOUR_EMAIL"
+license          "All rights reserved"
+description      "Installs/Configures wt_scs"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+depends "runit"
