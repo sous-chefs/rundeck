f801a2788935ee93b9bd50799dcfedba6eaa9834
Tim Smith <tim.smith@webtrends.com>
2012-04-10 20:41:52 -0700

    Create the directories before we try to download the tar and CD into a directory that might not exist
    Former-commit-id: a7818a44bc3d126edf62a4334fadee8e93da48bf [formerly ce1c074189b88a6d267ce9ab040c398e12b97443] [formerly 86be87f689ae8b0563cf961bb50ce87c51457de8] [formerly 3b0d026df3ee8b46dfa45cd8c121b65bb52c2e8e [formerly 5a3e2b56d4e4a357d4557e79162ca45f61cd9b6d]] [formerly 9adbfba33e9f8ddeeb5eb2c28b58911e56fa92d4 [formerly 57554f6e8a52c05e1060ca88daecbcb79c87f5fe] [formerly 8b8359aacd9cdc7fc214713b5a54ab86d907eed4 [formerly 212196c974e4817df2d9459a71dae74432472916 [formerly 6260726c1877902a350d33dc3b40bdb8dcabb071]]]]
    Former-commit-id: 7fb8c603097e4aaac3c7decfbbf4eb360015a69e [formerly e0a44db955acfd5dc2d1157e1e0437e03ae66b39] [formerly eb5f441294168228159efcd614981d17ebf8e53d [formerly 212196c974e4817df2d9459a71dae74432472916]]
    Former-commit-id: 3036dc1155a4d10330098b65b3cc591a1ea06ef0 [formerly 11e0c4d8da0ab72286d1e539aab2c5fe1a997a16]
    Former-commit-id: fe5a53d7a5a100458e8e5d051125c607a473975f
    Former-commit-id: 96bf46f9179d3458189e879a1d28a13521b61fec

--- cookbooks/wt_streamcollection/recipes/default.rb
+++ cookbooks/wt_streamcollection/recipes/default.rb
@@ -15,17 +15,6 @@ log_dir      = node[:log_dir]
 install_dir  = node[:install_dir]
 download_url = node[:download_url]
 
-remote_file "/tmp/#{tarball}" do
-  source download_url
-  mode "0644"
-end
-
-execute "tar" do
-  user  "root"
-  group "root"
-  cwd install_dir
-  command "tar zxf /tmp/#{tarball}"
-end
 
 directory "#{log_dir}" do
   owner "root"
@@ -41,6 +30,18 @@ directory "#{install_dir}/#{name}/bin" do
   mode "0755"
   recursive true
   action :create
+end
+
+remote_file "/tmp/#{tarball}" do
+  source download_url
+  mode "0644"
+end
+
+execute "tar" do
+  user  "root"
+  group "root"
+  cwd install_dir
+  command "tar zxf /tmp/#{tarball}"
 end
 
 template "#{install_dir}/#{name}/bin/#{name}" do
