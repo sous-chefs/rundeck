711f8510103ac0f92e889215be22d7338571efe9
vonnagy <ivan.vonnagy@webtrends.com>
2012-04-11 08:56:41 -0700

    Added Streaming API cookbook
    Former-commit-id: 6af47cb5a86e3a7186c5e25d2e82095b54d09420 [formerly 83e107b805257cbe2f3f2f78e152a6fd12052447] [formerly e329c9fee5a649e7d8de801e90909ca08066a01d [formerly 8fe8eb1b13e0b2deec7003ebf9f5cccbc3e7e331]]
    Former-commit-id: 254446ccc4a03b81074672f17285109fc67fefd6 [formerly 7c1f15cba21ee8f31aab32a6d847b01dba19b97a]
    Former-commit-id: 578311a252866918ff9eb070b6146f2be075b291

---
+++ cookbooks/wt_streamapi/metadata.rb
@@ -0,0 +1,9 @@
+maintainer        "Webtrends, Inc."
+maintainer_email  "ivan.vonnagy@webtrends.com"
+license          "All rights reserved"
+description      "Installs/Configures wt_sapi"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version           "1.0.0"
+
+depends	"java"
+depends	"runit"
---
+++ cookbooks/wt_streamapi/recipes/default.rb
@@ -0,0 +1,53 @@
+#
+# Cookbook Name:: wt_sapi
+# Recipe:: default
+#
+# Copyright 2012, Webtrends
+#
+# All rights reserved - Do Not Redistribute
+#
+
+name         = node[:name]
+user         = node[:user]
+group        = node[:group]
+tarball      = node[:tarball]
+log_dir      = node[:log_dir]
+install_dir  = node[:install_dir]
+download_url = node[:download_url]
+
+remote_file "/tmp/#{tarball}" do
+  source download_url
+  mode "0644"
+end
+
+execute "tar" do
+  user  "root"
+  group "root"
+  cwd install_dir
+  command "tar zxf /tmp/#{tarball}"
+end
+
+directory "#{log_dir}" do
+  owner "root"
+  group "root"
+  mode "0755"
+  action :create
+end
+
+directory "#{install_dir}/#{name}/bin" do
+  owner "root"
+  group "root"
+  mode "0755"
+  action :create
+end
+
+template "#{install_dir}/#{name}/bin/#{name}" do
+  source "#{name}.erb"
+  owner "root"
+  group "root"
+  mode  "0755"
+end
+
+runit_service "#{name}" do
+  action :start
+end
