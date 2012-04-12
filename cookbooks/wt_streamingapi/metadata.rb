813f3c6ff7c407f3a6db487bf6fca0c0937b4b31
Tim Smith <tim.smith@webtrends.com>
2012-04-12 15:38:46 -0700

    Rename the wt_streamapi cookbook to wt_streamingapi to match the service name
    Former-commit-id: 10eb0717127f98d00aa358efa36b0e0082e449a4 [formerly ab7eafb0ae6c07a30cf362a36c65636e101452f0] [formerly 42413c6abdee242448904beeb228a082daccaf14 [formerly 65575752d365f7e9edcd93c149868ab211ec4df1 [formerly 6bc39e6db99f154724dfce7f463851d7c158200a]]]
    Former-commit-id: b4aa5df26613318b904923daf51f6377f1a91f38 [formerly 52977b01124e73cef4e543c87cad4a4fb862a071]
    Former-commit-id: aa1ba762fc8171ae2d062266a2506c30dfbb3005
    Former-commit-id: 59218519a36b488a9a9390260ab62796adaa99c4

--- cookbooks/wt_streamingapi/metadata.rb
+++ cookbooks/wt_streamingapi/metadata.rb
@@ -1,7 +1,7 @@
 maintainer        "Webtrends, Inc."
 maintainer_email  "ivan.vonnagy@webtrends.com"
 license          "All rights reserved"
-description      "Installs/Configures wt_sapi"
+description      "Installs/Configures wt_streamingapi"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version           "1.0.0"
 
