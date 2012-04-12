a232435808f52b48c04df66973936174f401dbb8
Tim Smith <tim.smith@webtrends.com>
2012-04-12 14:50:28 -0700

    wt_streamingcollection: Remove the name and logdir variables.  Build paths for install_dir and log_dir variables from the wt_common linux values.  Misc fixups to the metadata as well
    Former-commit-id: 0af759ff1d5aee5d3e8e76568f8b4e92b8f2baef [formerly fbf7e0110830b38d5b6b1c8f198aa3d2d0ef3107] [formerly 10a1314f3b32564a8ab788b55f18b85ddcb1092e] [formerly 855b690a6bf2d363f70b8bdde07b1321b809b21e [formerly a14de91038e5d33035eeb9dea5c8765cd3ddcfb9]] [formerly 4a980d79974ed2f807df33af94269fe215f7f478 [formerly c1b10f8aee2a17da0958d4db329814eb41cc74f3] [formerly 879b01216cf63a0deb9101a46052cf9cd67b8b1f [formerly c7bae6bbe60063e975cf1b32bd4cb29c252a8b4b [formerly 2793c54e8c473e4d9f4916a4995d4f127db99459]]]]
    Former-commit-id: baa1b5c4eec8cbd911878642a4a29043dea40662 [formerly 27e36efdd4f9abf56ec11b09e5df81788dffcea7] [formerly e148a6d0548f1ceafa3d9a767e46d457bb8efb05 [formerly c7bae6bbe60063e975cf1b32bd4cb29c252a8b4b]]
    Former-commit-id: 9290aa9e3a35812f319fe857f05dcdaba53ece0c [formerly b5b4e6ad87cb1517f26ff8e522c77a57a60a715a]
    Former-commit-id: 77d968412425eec8b0916ded9532edfa3b9b767c
    Former-commit-id: 0afe830307cbbc042f8327af2de9fcbc3347f4f3

--- cookbooks/wt_streamcollection/metadata.rb
+++ cookbooks/wt_streamcollection/metadata.rb
@@ -1,7 +1,7 @@
 maintainer       "Webtrends, Inc"
 maintainer_email "marek.dolgos@webtrends.com"
 license          "All rights reserved"
-description      "Installs/Configures wt_scs"
+description      "Installs/Configures the Webtrends Streaming Collection Service"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
