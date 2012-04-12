1e033874ebe5617f1e047890898b5ee9bf4a3113
Tim Smith <tim.smith@webtrends.com>
2012-04-12 16:01:34 -0700

    Update wt_platformscheduler to use the windows specific installdir variable
    Former-commit-id: 388d1cae3042c4e8d5426ab379248a66416cefc8 [formerly 0486d925a16627862505b1f4ad7863619dedc1c9] [formerly 4555c46457d11379496fe9ec93e962d33607f49a] [formerly bf3538e4ca6295b4d309448e8172d8e5a858a84f [formerly 54d77c65d0fc0f1acb7a0cfb7d2d67cbd4cc3632]] [formerly 9808480762a3907964b698a5a1a92127eb03364a [formerly 2ba9691e767666e4a63f714c1462b6a2c3890bdb] [formerly ff59506f59f328668c78fb26377f44ece5733d81 [formerly 2d1ff770ed2851012b3f3d83328682787f865f7d [formerly 1715a55b6fab0423fe475e970d1cfe6426b4716a]]]]
    Former-commit-id: ad207c82b14c12f068d836ef998e8b96a8efcb2d [formerly 8e8b90e72f1f6969a9744ddee487b1112af76ade] [formerly 10934c50c9eeaf14e9ce5b089c8b9675194b79fb [formerly 2d1ff770ed2851012b3f3d83328682787f865f7d]]
    Former-commit-id: 3aef52d83bfe04d78ea1ee335c083fa200c55eda [formerly 54ee487f859711dd4501a332494fd471d0245422]
    Former-commit-id: 62087fe6ecab463d4aa22c62128c2997c3235531
    Former-commit-id: 2e1a8442ba0ba6b9d8c7623e6a21b98dbe33dec9

--- cookbooks/wt_platformscheduler/metadata.rb
+++ cookbooks/wt_platformscheduler/metadata.rb
@@ -2,7 +2,7 @@ name 'wt_platformscheduler'
 maintainer       "Kendrick"
 maintainer_email "kendrick.martin@webtrends.com"
 license          "All rights reserved"
-description      "Installs/Configures Platform Scheduler"
+description      "Installs/Configures Webtrends Platform Scheduler"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.10.4"
 depends          "wt_base"
