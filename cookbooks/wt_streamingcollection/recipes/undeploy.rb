9c02a3198ee66a6a6bd4ce745d76bb067fe6aa52
Tim Smith <tim.smith@webtrends.com>
2012-04-12 14:59:35 -0700

    Add an undeploy recipe for wt_streamingcollection
    Former-commit-id: c8370882d5c271ae48d3f1033c4d5a0e8eb1b80d [formerly d3a14a1ca0fdcecff7eb3a63f8c3234fdc2041b7] [formerly 3488192e5a23078470bcb1a1ee3f2f53a6d9bfa1 [formerly 1ac0dcce9047a9b3e24779000eb223d4836c788e]]
    Former-commit-id: 061341401da84dbbad5797f3a123fd032b19bf8f [formerly 71bd7452df52365565db1af17a1e9537c611512b]
    Former-commit-id: e977575c1e8b1423ac102b79a9d90ff3ca730215

---
+++ cookbooks/wt_streamingcollection/recipes/undeploy.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook Name:: wt_streamingcollection
+# Recipe:: undeploy
+#
+# Copyright 2012, Webtrends
+#
+# All rights reserved - Do Not Redistribute
+#
+
+log_dir      = "#{node['wt_common']['log_dir_linux']}/streamingcollection"
+install_dir  = "#{node['wt_common']['install_dir_linux']}/streamingcollection"
+
+service "streamingcollection" do
+  action [:stop, :disable]
+end
+
+directory "#{log_dir}" do
+  action :delete
+end
+
+directory "#{install_dir}" do
+  action :delete
+end
