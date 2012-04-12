8adf6dbdacb333f9aecde30f82db171b4681ebaa
Tim Smith <tim.smith@webtrends.com>
2012-04-12 16:55:23 -0700

    wt_heatmaps: Cookbooks should match product names.  Renamed wt_heatmap to wt_heatmaps and formatted recipes.  Updated all roles to point to the correct cookbook

---
+++ cookbooks/wt_monitoring/recipes/opsmon_scheduledhits.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: wt_monitoring
+# Recipe:: opsmon_scheduledhits
+#
+# Copyright 2012, Webtrends
+#
+# All rights reserved - Do Not Redistribute
+#
+# This recipe is used to add test traffic to the Optimize opsmon accounts.
+# We use this test data to confirm that data processing is functioning as expected.
+#
+
+
