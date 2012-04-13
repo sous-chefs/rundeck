dabf125d134f4ccffdec7c44b8824d188100a542
David Dvorak <david.dvorak@webtrends.com>
2012-04-13 00:38:36 +0000

    changed ref to README.rdoc to README.md.  Mismatched names causes cookbook upload error.

--- cookbooks/wt_streamingapi/metadata.rb
+++ cookbooks/wt_streamingapi/metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Webtrends, Inc."
 maintainer_email  "ivan.vonnagy@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures wt_streamingapi"
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.0.0"
 
 depends	"java"
