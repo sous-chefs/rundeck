624392eef17fb1c13820c5ace51107a420f335ca
reinvigorate <sean@reinvigorate.net>
2012-04-16 14:32:55 -0600

    changing zookeeper journal dir

--- cookbooks/zookeeper/attributes/default.rb
+++ cookbooks/zookeeper/attributes/default.rb
@@ -8,5 +8,5 @@ default[:zookeeper][:tickTime] = 2000
 default[:zookeeper][:initLimit] = 10
 default[:zookeeper][:syncLimit] = 5
 default[:zookeeper][:clientPort] = 2181
-default[:zookeeper][:dataDir] = "/var/run/zookeeper"
+default[:zookeeper][:dataDir] = "/var/lib/zookeeper"
 default[:zookeeper][:quorum] = ["localhost"]
