4e208fe0203bc588a61911b5a991268b9e6079d3
vonnagy <ivan.vonnagy@webtrends.com>
2012-04-17 10:16:35 -0700

    General upgrades
    Former-commit-id: 62be2df07baa48aa28a47a6012dcf5619aa1cb24 [formerly e75552723020224a8d64dffb5eaf7863bfbf7cfd] [formerly 6a4a9be4b1eb14122d1f6df05b97b6ec4d86f9b4 [formerly 5a04c79def9f41496962778b10fca804fe8213da [formerly efcda43af3b6c9d2f4ec383e226380308be834d2]]]
    Former-commit-id: 5ee226254d2b17a2eeb7cc6ed2553c1199033fe6 [formerly ca7e8e1a56da3e943a3f404419bda9015ea486dc]
    Former-commit-id: 26f9a241f78d6b2b630fa48a2675732c8ca7bc5f
    Former-commit-id: 9ad9a16c2612640bdec4ffa6b274086125732ee2

--- cookbooks/wt_streamingapi/attributes/default.rb
+++ cookbooks/wt_streamingapi/attributes/default.rb
@@ -3,3 +3,4 @@ default[:group]        = "nogroup"
 default[:tarball]      = "streamingapi-bin.tar.gz"
 default[:java_home]    = "/usr/lib/jvm/jre"
 default[:download_url] = "http://teamcity.webtrends.corp/guestAuth/repository/download/bt76/.lastSuccessful/streamingapi-bin.tar.gz"
+
