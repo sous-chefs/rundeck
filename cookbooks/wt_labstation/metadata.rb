3a5c6ca54f0b01f6a2847b73d8763bd0455ea62e
Kendrick Martin <kendrick.martin@webtrends.com>
2012-04-17 16:38:01 +0000

    Adding wt_labstation to use for initial setup of machines for use.

---
+++ cookbooks/wt_labstation/metadata.rb
@@ -0,0 +1,8 @@
+name 'wt_labstation'
+maintainer       "Kendrick"
+maintainer_email "kendrick.martin@webtrends.com"
+license          "All rights reserved"
+description      "Sets up the machine initial settings."
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+supports         "windows"
