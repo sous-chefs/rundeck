e5c7cb83a3caec1a101f179bdc93ca5fc65302f8
Kendrick Martin <kendrick.martin@webtrends.com>
2012-04-17 16:38:01 +0000

    Adding wt_labstation to use for initial setup of machines for use.
    Former-commit-id: be2e04a9f84d442fd912e398a1b943fa751cf5ff [formerly 17d99839ce565118b714e7b1da8fa1288edf6cce] [formerly f25a32def800c7460aa6fc931172457102a3e862 [formerly 379cdac151ea41d27cf838e65ed45645daef891b [formerly 3a5c6ca54f0b01f6a2847b73d8763bd0455ea62e]]]
    Former-commit-id: 5e0c2705f3c5a55480d974f42907e0c7da1c4f4b [formerly d189318be7b481be20f121ad662d147b815c706f]
    Former-commit-id: bc31b5f297062972accdc0c01def0649a5494e54
    Former-commit-id: 40c434a8dea56e5559627e9dee00012c1890172b

---
+++ cookbooks/wt_labstation/attributes/default.rb
@@ -0,0 +1,9 @@
+#
+# Author:: Kendrick Martin(<kendrick.martin@webtrends.com>)
+# Cookbook Name:: wt_labstation
+# Attribute:: default
+#
+# Copyright 2012, Webtrends Inc.
+#
+
+default['wt_labstation']['editor'] = "notepad"
