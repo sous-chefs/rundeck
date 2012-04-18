ac9f4632703690921f8f277d1d044ffac4846990
reinvigorate <sean@reinvigorate.net>
2012-04-18 11:49:37 -0600

    zookeeper maintenance fixes
    Former-commit-id: 68f8372a0524882f6266dc24016e114200ff2957 [formerly 608d376ef9d52c8ad0cc596c183a93c53a1a011b] [formerly 7b382132ed69249120e7904f968d864f664dfca5 [formerly 07fb1324ab937e35b0a39c6620847583dfa69e1e]]
    Former-commit-id: 62951dad4cdea6d30c75dfa6530b56300a01be4d [formerly 695cc59a51b11b26111ba7ea9ca9fe92073f5fd0]
    Former-commit-id: 41602c084106d50dc1c67d35776788a0c52efe04

--- cookbooks/zookeeper/attributes/default.rb
+++ cookbooks/zookeeper/attributes/default.rb
@@ -9,4 +9,6 @@ default[:zookeeper][:initLimit] = 10
 default[:zookeeper][:syncLimit] = 5
 default[:zookeeper][:clientPort] = 2181
 default[:zookeeper][:dataDir] = "/var/lib/zookeeper"
+default[:zookeeper][:snapshotDir] = "/var/lib/zookeeper/snapshots"
+default[:zookeeper][:snapshotNum] = 3
 default[:zookeeper][:quorum] = ["localhost"]
