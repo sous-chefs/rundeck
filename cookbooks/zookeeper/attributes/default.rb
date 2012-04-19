7be0edfdeb7d53c9e6e377a0d8198137891ee40c
Tim Smith <tim.smith@webtrends.com>
2012-04-19 13:45:36 -0700

    Use the standard Zookeeper data dir.  http://zookeeper.apache.org/doc/r3.2.2/zookeeperStarted.html

--- cookbooks/zookeeper/attributes/default.rb
+++ cookbooks/zookeeper/attributes/default.rb
@@ -2,7 +2,7 @@ default[:zookeeper][:version] = "3.3.4"
 
 default[:zookeeper][:installDir] = "/usr/local/share/zookeeper"
 default[:zookeeper][:logDir] = '/var/log/zookeeper'
-default[:zookeeper][:dataDir] = "/var/lib/zookeeper"
+default[:zookeeper][:dataDir] = "/var/zookeeper"
 default[:zookeeper][:snapshotDir] = "/var/lib/zookeeper/snapshots"
 
 default[:zookeeper][:tickTime] = 2000
