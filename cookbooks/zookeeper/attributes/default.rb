497fd61a258b8d29d3f42821595c90514e1f8199
Tim Smith <tim.smith@webtrends.com>
2012-04-19 13:43:49 -0700

    Arrange attributes by type (version, directories, app properties), rename install_stage_dir attribute to installDir to be consistent with other attribute names

--- cookbooks/zookeeper/attributes/default.rb
+++ cookbooks/zookeeper/attributes/default.rb
@@ -1,14 +1,13 @@
-
 default[:zookeeper][:version] = "3.3.4"
-default[:zookeeper][:install_stage_dir] = "/usr/local/share/zookeeper"
 
-
+default[:zookeeper][:installDir] = "/usr/local/share/zookeeper"
+default[:zookeeper][:logDir] = '/var/log/zookeeper'
+default[:zookeeper][:dataDir] = "/var/lib/zookeeper"
+default[:zookeeper][:snapshotDir] = "/var/lib/zookeeper/snapshots"
 
 default[:zookeeper][:tickTime] = 2000
 default[:zookeeper][:initLimit] = 10
 default[:zookeeper][:syncLimit] = 5
 default[:zookeeper][:clientPort] = 2181
-default[:zookeeper][:dataDir] = "/var/lib/zookeeper"
-default[:zookeeper][:snapshotDir] = "/var/lib/zookeeper/snapshots"
 default[:zookeeper][:snapshotNum] = 3
 default[:zookeeper][:quorum] = ["localhost"]
