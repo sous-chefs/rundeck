5fcc011a04041277d8db698842211d4db0e8c96b
Tim Smith <tim.smith@webtrends.com>
2012-04-19 13:40:09 -0700

    Bump zookeeper to 1.0 and require Java cookbook >= 1.5 so that Java actually installs
    Former-commit-id: 9d4656611ccec52c75d09972656504c59f90b3bf [formerly 87ec90819d5c6d9aeb69ee43bb73d87645b0d25f] [formerly c2f227ef70004d317645481ade4be66b19f16490 [formerly 71f4ca5b4477804f61a8e51524dbae82cf3e2f01]]
    Former-commit-id: a24eca527b27d4ac453d11be51af70ad4a6c5a2e [formerly 2242846339b2f06409d95f6f3467a0258a97df41]
    Former-commit-id: 2a425fe6ed705ecd0e732017623370853253fe0c

--- cookbooks/zookeeper/metadata.rb
+++ cookbooks/zookeeper/metadata.rb
@@ -3,8 +3,8 @@ maintainer_email  "sean.mcnamara@webtrends.com"
 license           "Apache 2.0"
 description       "Installs Zookeeper"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version           "0.0.1"
-depends           "java"
+version           "1.0.0"
+depends           "java" ">= 1.5" 
 
 recipe "Zookeeper", "Installs Apache Zookeeper"
 
