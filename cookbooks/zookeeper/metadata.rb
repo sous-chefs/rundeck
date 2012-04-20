bbe59b1527eb6243d1bd903d7016b5a3bb5358dc
Tim Smith <tim.smith@webtrends.com>
2012-04-20 08:48:05 -0700

    Add runit to the zookeeper metadata and include the runit recipe

--- cookbooks/zookeeper/metadata.rb
+++ cookbooks/zookeeper/metadata.rb
@@ -5,6 +5,7 @@ description       "Installs Zookeeper"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version           "1.0.0"
 depends           "java" ">= 1.5" 
+depends           "runit"
 
 recipe "Zookeeper", "Installs Apache Zookeeper"
 
