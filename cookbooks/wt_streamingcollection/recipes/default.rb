f3b7e81e2fb51f52ca66d289673bd9aeb2cf68e9
Marek Dolgos <marek.dolgos@webtrends.com>
2012-04-23 15:54:08 -0700

    Added config and Kafka settings.

--- cookbooks/wt_streamingcollection/recipes/default.rb
+++ cookbooks/wt_streamingcollection/recipes/default.rb
@@ -7,21 +7,17 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "runit"
-
-execute "install-java" do
-  user "root"
-  group "root"
-  command "sudo add-apt-repository ppa:webupd8team/java;sudo apt-get update;sudo apt-get install oracle-java7-installer"
-  action :run
-end
-
 log_dir      = File.join("#{node['wt_common']['log_dir_linux']}", "streamingcollection")
 install_dir  = File.join("#{node['wt_common']['install_dir_linux']}", "streamingcollection")
 
+port         = node['wt_streamingcollection']['port']
 tarball      = node[:tarball]
 java_home    = node[:java_home]
 download_url = node[:download_url]
+
+zk_host      = node['zookeeper']['quorum'][0]
+zk_port      = node['zookeeper']['clientPort']
+zk_connect   = "#{zk_host}:#{zk_port}"
 
 log "Install dir: #{install_dir}"
 log "Java home: #{java_home}"
@@ -56,7 +52,7 @@ execute "tar" do
 end
 
 template "#{install_dir}/bin/streamingcollection.sh" do
-  source  "streamingcollection.erb"
+  source  "streamingcollection.sh.erb"
   owner "root"
   group "root"
   mode  "0755"
@@ -73,7 +69,7 @@ template "#{install_dir}/conf/netty.properties" do
   group   "root"
   mode    "0644"
   variables({
-              :port => node[:wt_streamingcollection][:port]
+              :port => port
             })
 end
 
@@ -83,7 +79,17 @@ template "#{install_dir}/conf/kafka.properties" do
   group   "root"
   mode    "0644"
   variables({
-              :zk_connect => node[:zookeeper][:quorum][0]
+              :zk_connect => zk_connect
+            })
+end
+
+template "#{install_dir}/conf/config.properties" do
+  source  "config.properties.erb"
+  owner   "root"
+  group   "root"
+  mode    "0644"
+  variables({
+              :server_url => "file://#{install_dir}/conf/dcsid.whitelist"
             })
 end
 
@@ -93,11 +99,3 @@ execute "delete_install_source" do
     command "rm -f /tmp/#{tarball}"
     action :run
 end
-
-runit_service "streamingcollection" do
-    options({
-        :log_dir => log_dir,
-        :install_dir => install_dir,
-        :java_home => java_home
-      }) 
-end 
