572f06b8d65361d45aecd4ed8c1a460aa8be08ec
Tim Smith <tim.smith@webtrends.com>
2012-04-24 07:38:41 -0700

    Bump Rundeck cookbook to version 1.0 and add a basic Readme
    Former-commit-id: 5dd9a1444891b8eadf33a0b79498667c54830ceb [formerly e6c7be26e953adb581da3f536752297d9c1505a1] [formerly b8b01ac2452a47e68bb3c165e990ea74cc4bd491 [formerly 86bfb76d7ff11af31c11da9a6dd5c46be9c2b5c4]]
    Former-commit-id: 0d44f4b7bf25311b56410ca5aba6d18a96a3a9db [formerly 502352214ecfdbaa45f2ad54c622592ec276dcdf]
    Former-commit-id: fa075b72619f7121c5afdf0899e15135416c4df0

--- cookbooks/rundeck/metadata.rb
+++ cookbooks/rundeck/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "hostedops@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures rundeck"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+version          "1.0.0"
 depends          "runit"
 depends          "sudo"
 depends          "java"
