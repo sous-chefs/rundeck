150a0f66a44946f96f9162ab293be4b2dc701932
vonnagy <ivan.vonnagy@webtrends.com>
2012-04-24 21:32:14 -0700

    Externalized the port
    Former-commit-id: 830974832e2ff35882ed1e79fe4aefb074fa2c4d [formerly a98e938b73b62766e4f53dab8d13561e185742c2] [formerly 1918e815d6f08fa6f1fd9076533c2f34ada2f107 [formerly ebcc9a9daf9292afc231de4d0782bf55d5c53c37]]
    Former-commit-id: 9432cdd96ae2525220ecee3cdc41a52efe8cc6d8 [formerly 3965992cade84f1e079390b85db457bbee7b5021]
    Former-commit-id: ab57fdfa1320f7aef6c14cc910cfc311bd84a776

--- cookbooks/wt_streamingapi/attributes/default.rb
+++ cookbooks/wt_streamingapi/attributes/default.rb
@@ -4,4 +4,5 @@ default[:java_home]    = "/usr/lib/jvm/java-7-oracle/jre"
 default[:download_url] = "http://teamcity.webtrends.corp/guestAuth/repository/download/bt76/.lastSuccessful/streamingapi-bin.tar.gz"
 default[:tarball]      = "streamingapi-bin.tar.gz"
 default[:authentication_url] = "http://ec2-174-129-84-113.compute-1.amazonaws.com/camservice"
+default[:port] = "8080"
 
