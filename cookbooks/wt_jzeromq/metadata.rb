fbf6f8d7a2869e23b7c139a222186118ad6add95
Marek Dolgos <marek.dolgos@webtrends.com>
2012-04-26 13:32:56 -0700

    Added correct file.

--- cookbooks/wt_jzeromq/metadata.rb
+++ cookbooks/wt_jzeromq/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "marek.dolgos@webtrends.com"
 license          "All rights reserved"
 description      "Installs ZeroMQ"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.9"
+version          "1.0.10"
 
 depends "java"
