0ce0555029846c77b64dba610210ef9a7e992aa7
Tim Smith <tim.smith@webtrends.com>
2012-04-26 00:28:24 +0000

    Add support for install NRPE via package on CentOS and similar systems

--- cookbooks/nagios/recipes/client_package.rb
+++ cookbooks/nagios/recipes/client_package.rb
@@ -18,11 +18,21 @@
 # limitations under the License.
 #
 
-%w{
-  nagios-nrpe-server
-  nagios-plugins
-  nagios-plugins-basic
-  nagios-plugins-standard
-}.each do |pkg|
-  package pkg
+case platform
+when "centos" "redhat" "fedora" "amazon" "scientific"
+  %w{
+    nrpe
+    nagios-plugins
+  }.each do |pkg|
+    package pkg
+  end
+else
+  %w{
+    nagios-nrpe-server
+    nagios-plugins
+    nagios-plugins-basic
+    nagios-plugins-standard
+  }.each do |pkg|
+    package pkg
+  end
 end
