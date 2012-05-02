d2c8096124f38e57807ee40ed2ebb007c0817616
Tim Smith <tim.smith@webtrends.com>
2012-05-02 10:21:15 -0700

    Bump Cassandra to 1.0.9, add JNA jar that is recommended by Datastax, and mx4j jar that allows you to browse the JMX counters on each node

--- cookbooks/cassandra/recipes/default.rb
+++ cookbooks/cassandra/recipes/default.rb
@@ -34,3 +34,30 @@ file "apache-cassandra1" do
 	path "#{Chef::Config[:file_cache_path]}/#{rpmfile}"
 	action :delete
 end
+
+# Drop the JNA Jar.
+bash "installJNA" do
+user "root"
+  code <<-EOH
+  if [ ! -e /usr/share/cassandra/lib/jna.jar ]; then
+    wget #{node['cassandra']['jna_url']} -O /usr/share/cassandra/lib/jna.jar
+    chown cassandra /usr/share/cassandra/lib/jna.jar
+    chmod 755 /usr/share/cassandra/lib/jna.jar
+  fi
+  EOH
+end
+
+# Drop MX4J jar.
+bash "installMX4J" do
+user "root"
+  cwd "#{Chef::Config[:file_cache_path]}"
+  code <<-EOH
+  if [ ! -e /usr/share/cassandra/lib/mx4j-tools.jar ]; then
+    wget #{node['cassandra']['mx4j_url']}
+    tar zxvf mx4j-3.0.2.tar.gz mx4j-3.0.2/lib/mx4j-tools.jar
+    cp mx4j-3.0.2/lib/mx4j-tools.jar /usr/share/cassandra/lib/
+    chown cassandra:cassandra /usr/share/cassandra/lib/mx4j-tools.jar
+    chmod 755 /usr/share/cassandra/lib/mx4j-tools.jar
+  fi
+  EOH
+end
