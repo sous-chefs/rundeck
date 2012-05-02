011c7bcab979c6447e343754a4d8ae5fea3ff063
Tim Smith <tim.smith@webtrends.com>
2012-05-01 22:08:12 -0700

    Change mode declaration an fix spacing for readability

--- cookbooks/wt_streamingcollection/metadata.rb
+++ cookbooks/wt_streamingcollection/metadata.rb
@@ -4,5 +4,4 @@ license          "All rights reserved"
 description      "Installs/Configures the Webtrends Streaming Collection Service"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.4"
-
-depends	"runit"
+depends          "runit"
