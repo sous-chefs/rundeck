2f53b7fc5735d7e912e830121f111154fa440201
Tim Smith <tim.smith@webtrends.com>
2012-05-02 09:19:36 -0700

    Move Nginx out of the heat maps recipe so that it can be applied at the role level.  This will allow us to set attributes at some point and correctly install Nginx (avoiding the crappy template)
    Former-commit-id: f49e5a7e9e037d55fd15006efe2a429b7e6b8115 [formerly 57fa1ecc6692e3996f08d0974e312904a0c8fc87] [formerly 4f6713485dc6d2da44361af8b6be1ede5d4612a8 [formerly 4b8c5334a828ed39ae20ecfb67dc9a500b1891e9]]
    Former-commit-id: be9ca6182c11e0f3f37c48ace5f2652de87fc17c [formerly 6ebd91c04ad9747348f750661ba66fcc331ed3e6]
    Former-commit-id: 16bb828db975eeb73110e3fe0a0831803a7d6a7f

--- cookbooks/wt_heatmaps/metadata.rb
+++ cookbooks/wt_heatmaps/metadata.rb
@@ -4,7 +4,6 @@ license           "Apache 2.0"
 description       "Installs heatmaps 0.8.0"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version           "0.10.4"
-depends           "nginx"
 
 recipe "apiserver", "Installs heatmaps apiserver"
 recipe "mapred", "Installs heatmaps mapreduce scripts"
