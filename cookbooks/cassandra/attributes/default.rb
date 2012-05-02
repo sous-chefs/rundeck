76792ab8c4b64e2ad05277ce73e9c7392f4e0659
Tim Smith <tim.smith@webtrends.com>
2012-05-02 10:21:15 -0700

    Bump Cassandra to 1.0.9, add JNA jar that is recommended by Datastax, and mx4j jar that allows you to browse the JMX counters on each node
    Former-commit-id: da50c853fcb1a1c579327a2011f1d728ec5cf6f9 [formerly ce0ce19b4fd28460b30d60c0449fc621e9440315] [formerly a1a2eb5977eb77e0e5cd2e8af3c1f69d30d96a2c [formerly aaaee97769a59e96f953b06e56c511063ee5c1f9 [formerly d2c8096124f38e57807ee40ed2ebb007c0817616]]]
    Former-commit-id: 405bf2732421fa281e49df4c252331db45218997 [formerly fb930e46e06b463c7accc62e3cc56280d11e5f62]
    Former-commit-id: b19cd59204027f730b6c0693cd9e15813e5efa6e
    Former-commit-id: 2152e5cbaed4734e227e4eb4fd2245d29f57f414

--- cookbooks/cassandra/attributes/default.rb
+++ cookbooks/cassandra/attributes/default.rb
@@ -7,4 +7,6 @@
 #
 
 # Cassandra
-default['cassandra']['build_uri'] = 'http://rpm.datastax.com/community/noarch/apache-cassandra1-1.0.7-1.noarch.rpm'
+default['cassandra']['build_uri'] = 'http://rpm.datastax.com/community/noarch/apache-cassandra1-1.0.9-1.noarch.rpm'
+default['cassandra']['mx4j_url'] = 'http://sourceforge.net/projects/mx4j/files/MX4J%20Binary/3.0.2/mx4j-3.0.2.tar.gz/download'
+default['cassandra']['jna_url'] = 'http://java.net/projects/jna/sources/svn/content/tags/3.3.0/jnalib/dist/jna.jar?rev=1208'
--- cookbooks/cassandra/metadata.rb
+++ cookbooks/cassandra/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "david.dvorak@webtrends.com"
 license          "All rights reserved"
 description      "Installs Cassandra"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.7"
+version          "1.0.9"
 
 supports	"centos", ">= 6.0"
