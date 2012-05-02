ad537a8969307ee1ead54d916ebdf300b6940b47
Tim Smith <tim.smith@webtrends.com>
2012-05-02 09:01:03 -0700

    Change source_url to download_url to match other cookbooks
    Former-commit-id: 2e8c82e7fc6c7fb1dc8d43a5146d7fe846e45dc6 [formerly 11f6efd9c0b7844180509646f1b202970b73d57c] [formerly 609ec3f36027e1c8c0e10f7be8a3499106765767 [formerly db7e9f84890863b88a3bb6bb3e6be1cfb2493943]]
    Former-commit-id: 3ecf2d04d5328c84ae8e80df4efc1247982ea9e5 [formerly bf860ffa186bec4b8a4a554c54bd8ee6e727ebc3]
    Former-commit-id: d88aaaef1c827c4ec9f2ee3ffaad5dd86756cd05

--- cookbooks/zeromq/attributes/default.rb
+++ cookbooks/zeromq/attributes/default.rb
@@ -1,3 +1,3 @@
 # Version of ZeroMQ used in this cookbook.
 default[:zeromq][:version] = "2.1.7"
-default[:zeromq][:source_url] = "http://download.zeromq.org/historic"
+default[:zeromq][:download_url] = "http://download.zeromq.org/historic"
