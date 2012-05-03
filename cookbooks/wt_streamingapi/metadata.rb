274f67d2a9005891fff9baa3ba3d807d97c2a55e
vonnagy <ivan.vonnagy@webtrends.com>
2012-05-03 15:51:07 -0700

    Changed how we use java
    Former-commit-id: 1ec0c8f283029f680f383662bc413ec3c3c0cfab [formerly 7f5ddc29950aa36109891ec31b4b1f7450dcf999] [formerly 566ca853c68288719a4c10c6ded1a3b2801d3688 [formerly 7541714550fd085847e61490bc17588fdd271e8d]]
    Former-commit-id: b69adb8a4ea54fa9b5261629c0cc10df849a9d9a [formerly 57aef16f5f96d694da3ef957827e09f640c4cbca]
    Former-commit-id: 79122fae03f1cb5405f8c0c0a844e201b4442da8

--- cookbooks/wt_streamingapi/metadata.rb
+++ cookbooks/wt_streamingapi/metadata.rb
@@ -5,5 +5,4 @@ description      "Installs/Configures wt_streamingapi"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.0.1"
 
-depends	"java"
 depends	"runit"
