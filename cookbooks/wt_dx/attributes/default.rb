afeef025c1e5991da63d2edc9327711186c450ea
Kendrick Martin <kendrick.martin@webtrends.com>
2012-05-03 23:13:47 +0000

    Updating dx cookbook to only use one recipe and require deploy_build flag
    Former-commit-id: 0f1bb426837dc16bb86ed35601b68e9106d06011 [formerly 4fdcee15c45de169b662c728c1904703978dd787] [formerly f97fd9c21d4731f89cd95fa10bb5bcb2b218c7ad] [formerly c98a7c284345579eec42b7b8edbdb2245fe2d4a0 [formerly 2ec63b4162d3bfefae151c145593eb8a3566d484]] [formerly c44a2056caee818139fddf4616d25eeb64e57025 [formerly 848090d3bbaf449fb6673bbdc87a1677183dd227] [formerly f5a667f9977fc0f9f77b7fd8a35ecac470ed539f [formerly 52a9d3ccd9f05a437b2eb29aa512bca73fe19fe5 [formerly 62236b0602dfe472a1fc57249e35d3183d789117]]]]
    Former-commit-id: 09d238e78fa891df778612dfce8bb4cd2ad02dd3 [formerly f9ec8e1c27e95c1c501666c4718839f6dda39d62] [formerly afb55ecd30263dc878583f1ed7716a61b8da8751 [formerly 52a9d3ccd9f05a437b2eb29aa512bca73fe19fe5]]
    Former-commit-id: 51d2aa1f9a41ec403d90ee2fe8cd173242b657b7 [formerly 92b98bc0be15bc8483e5308beadee1112ad8ebab]
    Former-commit-id: a0a6faf2d51d02df4771806a775b8d97cf4534ac
    Former-commit-id: eecfaf0c8321cc378f476eb000ce9f7ebf318af2

--- cookbooks/wt_dx/attributes/default.rb
+++ cookbooks/wt_dx/attributes/default.rb
@@ -8,17 +8,9 @@
 
 default['wt_dx']['cfg_cmd'] = ["/section:staticContent /clientCache.cacheControlMode:UseMaxAge /clientCache.cacheControlMaxAge:04:00:00","/section:urlCompression /doStaticCompression:True","/section:urlCompression /doDynamicCompression:True","/section:httpCompression -[name='gzip'].dynamicCompressionLevel:4","/section:httpCompression -[name='gzip'].staticCompressionLevel:9","/section:httpCompression -[name='deflate'].dynamicCompressionLevel:4","/section:httpCompression -[name='deflate'].staticCompressionLevel:9","/section:system.webServer/httpErrors /errorMode:Detailed","/section:system.applicationHost/sites /siteDefaults.logfile.directory:\"D:\\wrs\\logs\"","/section:system.applicationHost/sites /siteDefaults.logfile.logExtFileFlags:BytesRecv,BytesSent,ClientIP,ComputerName,Cookie,Date,Host,HttpStatus,HttpSubStatus,Method,ProtocolVersion,Referer,ServerIP,ServerPort,SiteName,Time,TimeTaken,UriQuery,UriStem,UserAgent,UserName,Win32Status","/section:system.webServer/httpErrors /errorMode:DetailedLocalOnly","/section:system.webServer/httpLogging /SelectiveLogging:LogAll"]
 
-default['wt_dx']['v2']['app_pool'] = "Webtrends_WebServices_v2"
-default['wt_dx']['v2']['dir'] = "\\Data Extraction API\\v2"
-default['wt_dx']['v2']['file_name'] = "v2.zip"
-
 default['wt_dx']['v2_1']['app_pool'] = "Webtrends_WebServices_v2_1"
 default['wt_dx']['v2_1']['dir'] = "\\Data Extraction API\\v2_1"
 default['wt_dx']['v2_1']['file_name'] = "v2_1.zip"
-
-default['wt_dx']['v2_2']['app_pool'] = "Webtrends_WebServices_v2_2"
-default['wt_dx']['v2_2']['dir'] = "\\OEM Data Extraction API\\v2_2"
-default['wt_dx']['v2_2']['file_name'] = "v2_2.zip"
 
 default['wt_dx']['v3']['dir'] = "\\Data Extraction API\\v3"
 default['wt_dx']['v3']['file_name'] = "v3.zip"
