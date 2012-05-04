7c7ab5de9b68bfc12e56c9eaff4b88bd24dcf984
vonnagy <ivan.vonnagy@webtrends.com>
2012-05-04 14:49:09 -0700

    Added version numbers and pinned them for the V pod
    Former-commit-id: 75c1a53f2464a6bd65e283fcd1e3c977c91c3895 [formerly c388b4bd9d21744d5535f433eff516778460ab56] [formerly fb46f0187c65eadf19b1054bdb5124458753a5c7 [formerly 91f47e61edca3ed977ab52569b0ff6ce051503d3]]
    Former-commit-id: 5407cdfadaf7daa4ba5ee3d62f027d8ad64c5852 [formerly 82a8c999287f5c2a5cc96c2eb89ebc127ad22f5c]
    Former-commit-id: 0f6282730c6d28ba57d996190d2b78ec91ebbe02

--- cookbooks/wt_realtimeapi/metadata.rb
+++ cookbooks/wt_realtimeapi/metadata.rb
@@ -1,8 +1,8 @@
-maintainer        "Webtrends, Inc."
-maintainer_email  "ivan.vonnagy@webtrends.com"
-license          "All rights reserved"
-description      "Installs/Configures wt_realtimeapi"
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.0.1"
-
-depends	"runit"
+maintainer          "Webtrends, Inc."
+maintainer_email    "ivan.vonnagy@webtrends.com"
+license             "All rights reserved"
+description         "Installs/Configures the Webtrends Realtime API Service"
+long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version             "0.0.5"
+depends             "java"
+depends             "runit"
