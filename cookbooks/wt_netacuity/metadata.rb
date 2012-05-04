aa220b361fdaca629d6d40c0d78003852b5bec72
Tim Smith <tim.smith@webtrends.com>
2012-05-03 17:39:31 -0700

    New cookbook wt_netacuity, with a non edited dump of the net acuity recipe from Optimize.  Attributes are created and NetAcuity has been placed in the repo so we don't have to use a cookbook file.

---
+++ cookbooks/wt_netacuity/metadata.rb
@@ -0,0 +1,8 @@
+maintainer        "Webtrends, Inc."
+maintainer_email  "tim.smith@webtrends.com"
+license          "All rights reserved"
+description      "Installs/Configures NetAcuity with a Webtrends license"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "1.0.0"
+
+depends	"runit"
