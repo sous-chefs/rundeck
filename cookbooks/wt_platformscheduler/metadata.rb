233e57ecc11bcb31e5ae36c16c51926ccda1ee5d
Tim Smith <tim.smith@webtrends.com>
2012-05-04 17:04:34 -0700

    Bump platform scheduler and roadrunner to 10.5

--- cookbooks/wt_platformscheduler/metadata.rb
+++ cookbooks/wt_platformscheduler/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "kendrick.martin@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures Webtrends Platform Scheduler"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.10.4"
+version          "0.10.5"
 depends          "wt_base"
 depends          "windows"
