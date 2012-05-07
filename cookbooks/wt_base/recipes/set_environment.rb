9150593a3afce3fa8e24635575eb95aef0260e34
David Dvorak <david.dvorak@webtrends.com>
2012-05-07 21:27:03 +0000

    Add support for single lettered chef environments (works around chef-client -E bug)
    Former-commit-id: 06d10be077385fec9cff8b8dd8e3f681482b87a9 [formerly 0ea6a53ffd104656826e7421ed7e29c69f59ee29] [formerly 8d0442991d13fe6335ee9f1fab3d2d7dfeac0dfc] [formerly 30653ca1e34ac4d0cb9e18797e1953176ff748f4 [formerly 7dd73d81558af8bd4a9af0c932ad04de21efade4]] [formerly ffe72426b441a66d0c5d6eeafdae2e317e9645ed [formerly 07d02eee561a2714985d26901465950520cd4942] [formerly 915d3d53150532973346aac0cf20f36a6d12849d [formerly 8dbeabd6d0d57b6ac69e934b6ee2568d5a71f734 [formerly 5cf7a32b25ef31522af7499915649fe9dc1d2329]]]]
    Former-commit-id: 0764895be7b445db8e210626a8f9104cef67b46e [formerly 2e75656879b17dcb2d0d1df5945a72adc648e3b3] [formerly 8d7a5c665f0adaf129fbec8bb11c8ae4165d2a4f [formerly 8dbeabd6d0d57b6ac69e934b6ee2568d5a71f734]]
    Former-commit-id: 0a4d25c1eddaf0f90e5ff1b598801f5b5d42a000 [formerly cfde95eba433c278f75e14aee8675286569aac43]
    Former-commit-id: a082207e88c66da27082bb5fa71dbca383a5ac82
    Former-commit-id: 678c307ebdc8249ddda68d897f7adf2012c8856b

---
+++ cookbooks/wt_base/recipes/set_environment.rb
@@ -0,0 +1,15 @@
+#
+# Author:: David Dvorak (<david.dvorak@webtrends.com>)
+# Cookbook Name:: wt_base
+# Recipe:: set_environment
+#
+# Copyright 2012, Webtrends Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+unless ENV['CHEF_ENVIRONMENT'].nil?
+	log "changing environment #{node.chef_environment} to #{ENV['CHEF_ENVIRONMENT']}"
+	node.chef_environment(ENV['CHEF_ENVIRONMENT'])
+	node.save
+end
