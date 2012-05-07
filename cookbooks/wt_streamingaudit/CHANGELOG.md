85fa74ea86cb2513aed4495559fdcbf8281d0e56
vonnagy <ivan.vonnagy@webtrends.com>
2012-05-07 16:19:09 -0700

    Initial auditing cookbook
    Former-commit-id: 2958648eac413278edbce3bcefdd07fe502deb17 [formerly 0ca5667c6c2638761a35dd9ede13db51f60b37f5] [formerly f0b2054f4bee9cc7eeee715dc4db1a43945b23e7] [formerly 2718c483fb851e3a5a649e45d4b6ed8bc3ee366d [formerly 1f7ac5adf848ad5db6ebd1a4c14bf79fdef8c77e]] [formerly fec58a62a5cd8bce344c1449be8c85daea1e1b03 [formerly 96d38dfff27d693a7ea9383790ada986f628152a] [formerly 1b9ab4e9e958a0438cb77ce9a80bb0191b259b60 [formerly aa81844c5f1200febf5877bc35127ed31a4de932 [formerly c703c8cb0b893ef430974bffd6a38c48c0502385]]]]
    Former-commit-id: d4113e090071a81ef9a789a8a093823c92013968 [formerly c7c54ce6464534f3168d930876256271f15d9a1e] [formerly ad320190c914317375f2d0f38cceab9b5a70a71f [formerly aa81844c5f1200febf5877bc35127ed31a4de932]]
    Former-commit-id: 90d91bb9763882d038cea3df2f1bd098fd4feb57 [formerly aee7ee7d220970d784e5b40403ed11453518d358]
    Former-commit-id: 4625085a8be7aa52bce0f58cb3205e1672c87f59
    Former-commit-id: 9d3d0d2f005e22c48a89e27696d59da6732bd8b2

---
+++ cookbooks/wt_streamingaudit/CHANGELOG.md
@@ -0,0 +1,6 @@
+## Future
+
+* Unknown
+
+## 0.0.1:
+* Initial release with a changelog
---
+++ cookbooks/wt_streamingaudit/recipes/undeploy.rb
@@ -0,0 +1,27 @@
+#
+# Cookbook Name:: wt_streamingaudit
+# Recipe:: undeploy
+#
+# Copyright 2012, Webtrends
+#
+# All rights reserved - Do Not Redistribute
+#
+
+log_dir      = "#{node['wt_common']['log_dir_linux']}/streamingaudit"
+install_dir  = "#{node['wt_common']['install_dir_linux']}/streamingaudit"
+
+runit_service "streamingaudit" do
+    action :disable
+end
+
+service "streamingaudit" do
+  action [:stop, :disable]
+end
+
+directory "#{log_dir}" do
+  action :delete
+end
+
+directory "#{install_dir}" do
+  action :delete
+end
