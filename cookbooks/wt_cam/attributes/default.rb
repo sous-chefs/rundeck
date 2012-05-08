a467d275137ea6e93310cdf04a5126a15340a266
Kendrick Martin <kendrick.martin@webtrends.com>
2012-05-08 22:20:48 +0000

    Created wt_cam cookbook
    Former-commit-id: f4effc47f90cb2991d289b089cb26f0206b22aa1 [formerly d381503e099d8f0d72020ab38d869add2d309848] [formerly 1ee61e4b65e3374e779cbfffa3d0c3d27b1f0476 [formerly 553b53516670aead9dcc26f244218b18c5bfd051]]
    Former-commit-id: 682bce1724a81df0a7b0eef39129edec0de7e222 [formerly f917a134536d0e1d0c04313c0aa36d412638f399]
    Former-commit-id: a49f39e77bd0505a184e9652d8eb58157125e9fd

---
+++ cookbooks/wt_cam/attributes/default.rb
@@ -0,0 +1,12 @@
+#
+# Author:: Kendrick Martin(<kendrick.martin@webtrends.com>)
+# Cookbook Name:: wt_cam
+# Attribute:: default
+#
+# Copyright 2011, Webtrends Inc.
+#
+
+
+default['wt_cam']['app_pool'] = "CAMService"
+default['wt_cam']['url'] = "http://pdxteamcitys01.webtrends.corp/guestAuth/repository/download/bt122/.lastSuccessful/"
+default['wt_cam']['zip_file'] = "CAM.zip"
