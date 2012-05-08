9e10b627ac97eb162c461692b9e925184c78c7f6
Tim Smith <tim.smith@webtrends.com>
2012-05-08 15:13:40 -0700

    Make heat maps recipe version 0.10.4 so we can apply it in production
    Former-commit-id: 57240918363bd80ad2430d683ec67b1936327204 [formerly 694fa2c0e5a9147eb1ec80539f0941f82f97c77d] [formerly d25fc4811b912751cf6a4a12478e41bcec6407f3] [formerly 5b9ba16bc0cbc0d199b7bf9067339d61f6d0c80b [formerly 07caca461061c4b8feb6c4b72f1ff351778c8f40]] [formerly c6b9ee0d107d7fbed63c1c6bb95879de297697eb [formerly fe758180d49f32049942d79387278b1da2ede8e2] [formerly 7c5c3e4b194fa805d926e91e91b568c482e9191a [formerly 3ccb1d36a66d6913ef59de74ee86f14caaff54ce [formerly a537ba0d9de7b3486690452d9f3d57539f78afaf]]]]
    Former-commit-id: ea80ed0cb230e1144fff736d72a13d88a4904616 [formerly 8901f2edae62db9b52a46abf1d5891574edaee05] [formerly 2ec866f8d48aa1ded63f9a4f95ee368e00832cdd [formerly 3ccb1d36a66d6913ef59de74ee86f14caaff54ce]]
    Former-commit-id: 174446cbf5af461dfec09e45f821ffeded1956eb [formerly dc89538b4a55e02a350360644ab5b5f06b26951f]
    Former-commit-id: 70a0f89adeda543787db814be254f466247e8c5d
    Former-commit-id: a6c221f98b4aeb82db1a9380f3f11a7183d939ce

--- cookbooks/wt_heatmaps/metadata.rb
+++ cookbooks/wt_heatmaps/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "sean.mcnamara@webtrends.com"
 license           "Apache 2.0"
 description       "Installs heatmaps 0.8.0"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version           "0.10.5"
+version           "0.10.4"
 
 recipe "apiserver", "Installs heatmaps apiserver"
 recipe "mapred", "Installs heatmaps mapreduce scripts"
