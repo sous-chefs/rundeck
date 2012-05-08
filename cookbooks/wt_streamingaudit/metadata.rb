26f8f3711c60268a4d9a92353dc67ee17afff23f
vonnagy <ivan.vonnagy@webtrends.com>
2012-05-08 12:59:23 -0700

    Added zookeeper search change to streaming auditor
    Former-commit-id: 01ef8b6adaf7732068bee55542aabca2dda8ffdc [formerly 9e6606f52adb56daa9d7f3f70460cf3e2d20da35] [formerly 5f6c28279542324b6993ddc03c126441bef53024 [formerly 99f2b32a10ebf688932413b5761759dc7ddb8e7a [formerly de116451c5ebf077beb95348bb30e4e9906e1002]]]
    Former-commit-id: 9129ba1dff3d8a62b9da3b389a0518c7ba4cc16e [formerly bb863274a9298f6b144465333f04189e315fa3ff]
    Former-commit-id: 7c6690a58cb6df04fc3a20dfc94030b91e567f01
    Former-commit-id: 9599b32b50f3217072b1fed77da599f6634f2e7a

--- cookbooks/wt_streamingaudit/metadata.rb
+++ cookbooks/wt_streamingaudit/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email    "ivan.vonnagy@webtrends.com"
 license             "All rights reserved"
 description         "Installs/Configures the Webtrends Streaming Auditor Service"
 long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version             "0.0.1"
+version             "0.0.2"
 depends             "java"
 depends             "runit"
