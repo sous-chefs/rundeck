d62e5e3f4e85f3ac4d3cc2333b177249ca32aa5f
vonnagy <ivan.vonnagy@webtrends.com>
2012-05-08 08:53:39 -0700

    Change tarball url for the Streaming Auditor
    Former-commit-id: 40b2972cea5baa90efc8ad3317afb5b3995fb4a0 [formerly f675eb234f7b0f23c49a57a7ef984d456b2b0c5b] [formerly 4541bffb492d40a4fbda4116bea872f856450c18 [formerly e61705262b2456a25251f9cd9b98fecb4033d220 [formerly e494ead3b9c72ef278436d27d723bd6a0c006913]]]
    Former-commit-id: 69433cdb93d7df120616cd55328d2879ad91e2b2 [formerly ae72c8e857c0dca4cc1bfa22abd5784d76bd4c5b]
    Former-commit-id: 44b508431fa022938bdeb3ca65e5f3ce0bf1407a
    Former-commit-id: dbe0faa37732119ee82d5508ed8f3230884c2478

--- cookbooks/wt_streamingaudit/attributes/default.rb
+++ cookbooks/wt_streamingaudit/attributes/default.rb
@@ -1,5 +1,5 @@
 default['wt_streamingaudit']['user']         = "nobody"
 default['wt_streamingaudit']['group']        = "nogroup"
-default['wt_streamingaudit']['download_url'] = "http://teamcity.webtrends.corp/guestAuth/repository/download/bt75/.lastSuccessful/streamingaudit-bin.tar.gz"
+default['wt_streamingaudit']['download_url'] = "http://teamcity.webtrends.corp/guestAuth/repository/download/bt123/.lastSuccessful/streamingaudit-bin.tar.gz"
 default['wt_streamingaudit']['tarball']      = "streamingaudit-bin.tar.gz"
 default['wt_streamingaudit']['listener_threads'] = 4
