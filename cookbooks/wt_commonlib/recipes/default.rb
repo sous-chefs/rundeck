9e652c3a895cd75aafdf3a04f051e0dc47e13493
Kendrick Martin <kendrick.martin@webtrends.com>
2012-05-09 23:49:38 +0000

    Adding deploy_build flag to common lib cookbook.
    Former-commit-id: 8f0586a43c50fceb1c691f1aac0b1985bfa0a332 [formerly 248a40769dbd031f0f1075115c7fc2b2a0847f3c] [formerly d5e1999d4273d070578c16e1b287a7c583ed3fbc [formerly ee94c1ec9e4530f77dd562167c91722505a14b5a [formerly 1f843181bb77bee80b0024da52097f597df909de]]]
    Former-commit-id: c36384e4b0f1246cb7fce5ef0d332944292fd05b [formerly 8bb5846318b1bcd9c40ff501c2cb14cf0a443dbe]
    Former-commit-id: 05388b9ca286d336ae9ac784d4ebea9cf626ad27
    Former-commit-id: 0d1a70af0a8b01474d85c52d60cdaef0cdfd9b15

--- cookbooks/wt_commonlib/recipes/default.rb
+++ cookbooks/wt_commonlib/recipes/default.rb
@@ -20,23 +20,15 @@ directory logdir do
 	recursive true
 end
 
-remote_file "#{Chef::Config[:file_cache_path]}\\#{msi_name}" do
-        source "#{archive_url}#{common_install_url}"
-        action :nothing
-end
+if ENV["deploy_build"] == "true" then 
+	remote_file "#{Chef::Config[:file_cache_path]}\\#{msi_name}" do
+		source "#{archive_url}#{common_install_url}"
+		action :nothing
+	end
 
-http_request "HEAD #{archive_url}" do
-  message ""
-  url archive_url
-  action :head
-  if File.exists?("#{Chef::Config[:file_cache_path]}\\#{msi_name}")
-    headers "If-Modified-Since" => File.mtime("#{Chef::Config[:file_cache_path]}\\#{msi_name}").httpdate
-  end
-  notifies :create, resources(:remote_file => "#{Chef::Config[:file_cache_path]}\\#{msi_name}"), :immediately
-end
-
-windows_package "WebTrends Common Lib" do
-        source "#{Chef::Config[:file_cache_path]}\\#{msi_name}"
-        options "/l*v \"#{logdir}\\#{msi_name}-Install.log\" INSTALLDIR=\"#{installdir}\" SQL_SERVER_NAME=\"#{master_host}\" WTMASTER=\"wtMaster\"  WTSCHED=\"wt_Sched\""
-        action :install
+	windows_package "WebTrends Common Lib" do
+		source "#{Chef::Config[:file_cache_path]}\\#{msi_name}"
+		options "/l*v \"#{logdir}\\#{msi_name}-Install.log\" INSTALLDIR=\"#{installdir}\" SQL_SERVER_NAME=\"#{master_host}\" WTMASTER=\"wtMaster\"  WTSCHED=\"wt_Sched\""
+		action :install
+	end
 end
