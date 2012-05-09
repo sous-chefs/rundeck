2b2e3fc8166494b45ee6f5d0ddbd9ba79d153ae9
Tim Smith <tim.smith@webtrends.com>
2012-05-08 18:55:23 -0700

    Setup proxy support in the template/attributes
    Former-commit-id: 73f8def68ff8eeffb6c87d6cae0b72af3d567050 [formerly bcbfab94f79a9248af390b01a56f075ec4b5b7ad] [formerly ae9ec66f1e69a287fce4fef1f216bc4e18681bec] [formerly 0cd07a56cca1e86a23871fffc9a9b307bdaa2c17 [formerly 31152d9152dc845296a51170a04c12e320f800a5]] [formerly 2b81ac725e37e8f27684dd3bc1f960cf78a977b3 [formerly 38f9ea4a045ed4c1420dc80b2c5e58bd43016ca4] [formerly 1b3f932c7848a1ac5f1535ddc963b4c05cae7933 [formerly ea7967e52432f76e189768ada6b0579cceaa3203 [formerly 85cee85f4e898b22f7c81fbb4c7dca8b57ff8c30]]]]
    Former-commit-id: 9c43247f39ad0ac5b766b5d99f8c60850bb5d6f7 [formerly 35b2c0de9f964f8ff8788791e6cd2d35777b0164] [formerly d4b12a8e5d11ae4411abb9c25bb0e93879c02af7 [formerly ea7967e52432f76e189768ada6b0579cceaa3203]]
    Former-commit-id: b65a119a17815845ace65deb7abf52781cd80448 [formerly 90e7005f764e507c19c29c0ff43f8935693d727b]
    Former-commit-id: 89e38bae3c7f519a9da3639b41040909c5d5d677
    Former-commit-id: 8905f4d60bb75ed4a5f8fe1539e9a148d52d0849

--- cookbooks/wt_netacuity/attributes/default.rb
+++ cookbooks/wt_netacuity/attributes/default.rb
@@ -1,3 +1,4 @@
 default['wt_netacuity']['version'] = "4.6"
 default['wt_netacuity']['download_url'] = ""
 default['wt_netacuity']['install_dir'] = "/opt/netacuity"
+default['wt_netacuity']['use_proxy'] = false
