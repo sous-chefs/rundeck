745b6ebf1e927c73a3e431c738905c3892941abc
reinvigorate <sean@reinvigorate.net>
2012-05-10 15:52:01 -0700

    nuking jzeromq and zeromq
    Former-commit-id: 5d590f675a7a591b5dbf09097a2430e8ae48e891 [formerly 308365978f793cd8c3d0737be6a02ac13d9d5442] [formerly c93bae3278fac903538233cfc99489ae7e23a9f0 [formerly 85a5e74c5c4abd70baadf4fe661feb34034755e4]]
    Former-commit-id: f0f268b2c29ff76bcc36da0570c45a306f8dea1d [formerly e2cb7fac8ef4ee003e7ba8377b47acfb7aa15702]
    Former-commit-id: 62c2fb6004dd051f6018f23c0dae2eaea0b58292

--- cookbooks/zeromq/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "Webtrends, Inc"
-maintainer_email "marek.dolgos@webtrends.com"
-license          "All rights reserved"
-description      "Installs ZeroMQ"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
