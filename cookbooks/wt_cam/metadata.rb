70766c04f25c4dfe134943a593722bc34f82f7e7
Kendrick Martin <kendrick.martin@webtrends.com>
2012-05-10 00:33:13 +0000

    Adding dependencies to metadata and aspnet_regiis
    Former-commit-id: a846800bee5e843a7998208f18d0c875e841aa2d [formerly c677b131285b2ad9bb9a3e393fa11061d5466ba2] [formerly 167702e732f78de23d72fcc40611a4ec454529c3 [formerly fd68dccb30b920c2fb1317a28afcaca1fa015ea4 [formerly 05432c18dd42e62b0eca3b254e3eb335922d4073]]]
    Former-commit-id: 1f12faeefe9c6e8250452c84e3ecb15e82bdc245 [formerly d426d69044ed696a8becc2b610e7a05b32534ca2]
    Former-commit-id: c81cbff0165d11a0ead37668c12cf5dcdff1c5d4
    Former-commit-id: 5cdeed8b207e93a0fb9ba5cdf73ddedbd7506d4b

--- cookbooks/wt_cam/metadata.rb
+++ cookbooks/wt_cam/metadata.rb
@@ -4,3 +4,6 @@ license          "All rights reserved"
 description      "Installs/Configures CAM IIS site"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.10.4"
+supports 	"windows"
+depends		"iis"
+depends		"windows"
