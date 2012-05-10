2a6c1761ebfb4e082e9eefeafa3abf40c882b8f7
Marek Dolgos <marek.dolgos@webtrends.com>
2012-05-10 12:20:40 -0700

    Added metadata back.
    Former-commit-id: 47d604bf1c9710a0d5686b2bd2f86db8a85d9a83 [formerly cadf35c9464401bb19be36e4e077c025bef47331] [formerly cd02e4a027e8242a894eb2dd54d96c2fc1266c29] [formerly e9fcad462a70539c52845c99bb7d904f81125406 [formerly a38999f9262462ce78d7f0142fe5eafe15b56cb4]] [formerly eb44b4a27d2870c426d44919a91858e2e913e80b [formerly d8c9a3e0eb5aa7229ca1a61a70e09662c59ec8d9] [formerly ffcffd97688ae313b0f4edd242bb5d5ab6b826b8 [formerly f62a23bcf8ddecd8318e37ec8dd23825b557e23d [formerly 7d753815c650c6a415cd4704bfc3799a3ba15413]]]]
    Former-commit-id: 3b7b1ec709913ff2f1487383989d9b14d4a9a2a9 [formerly 114cc386199477a991d8775ce1fb69cc98c95eed] [formerly e7dafce3f6fd55c0fcdeabc666cce332a65cd5ac [formerly f62a23bcf8ddecd8318e37ec8dd23825b557e23d]]
    Former-commit-id: d581d964a633e84641a0a16fdf2f567997bcae3f [formerly 418014dc68f33335336cd38b8959719b38024fa8]
    Former-commit-id: c26247914d9d60afe45e6e97fdefb376855f53d1
    Former-commit-id: 45c86633817d80e8b098067b3aee4053e0da03e8

---
+++ cookbooks/wt_streamingcollection/metadata.rb
@@ -0,0 +1,8 @@
+maintainer          "Webtrends, Inc"
+maintainer_email    "marek.dolgos@webtrends.com"
+license             "All rights reserved"
+description         "Installs/Configures the Webtrends Streaming Collection"
+long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version             "0.0.10"
+depends             "java"
+depends             "runit"
