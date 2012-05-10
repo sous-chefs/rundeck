228f3f493d35a14ce8937abebe502ac2a98d8172
Marek Dolgos <marek.dolgos@webtrends.com>
2012-05-10 12:09:13 -0700

    Added new logreplay cookbook.

---
+++ cookbooks/wt_streaminglogreplay/attributes/default.rb
@@ -0,0 +1,4 @@
+default['wt_streaminglogreplay']['user']         = "nobody"
+default['wt_streaminglogreplay']['group']        = "nogroup"
+default['wt_streaminglogreplay']['download_url'] = "http://teamcity.webtrends.corp/guestAuth/repository/download/bt127/.lastSuccessful/streaminglogreplay-bin.tar.gz"
+default['wt_streaminglogreplay']['tarball']      = "streaminglogreplay-bin.tar.gz"
