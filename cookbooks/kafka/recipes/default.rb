0c7d8a8b5dd7eb03e03e06e9cde7d805365de7e4
vonnagy <ivan.vonnagy@webtrends.com>
2012-05-10 10:42:01 -0700

    Re-write of the kafka cookbook
    Former-commit-id: 4fcaf738dcce193428ebda75f251a4785397f4c1 [formerly 95f334adad962d71df0196439a50df32b16ec5b3] [formerly b5e596ce7defb57a0d6ea85936284472935f0245] [formerly eaaada3e9aea2875da48e136e9a29b43c2d32dc9 [formerly 9a05aaffad2927961e23cfe3303f8534d7c62565]] [formerly ab71d666a47f38114e41019aa928e089a10d81ce [formerly ae6ce9f7f990651ce2296f2c3747fc284af20197] [formerly 92b22cf146d2d9eff18d7ddabe7a07ab2846c9b8 [formerly 7cc3d4854be8b5269fee8c3ee829c830af9950d8 [formerly 68c4b69e295015f56afd41cde2394fabc0b99065]]]]
    Former-commit-id: 3f4e79c27fc254facb78ec3313f84f2081b9d00b [formerly 0443169f02b974c4aad36c37b2a04805a4fb4aac] [formerly 812377ade4f406b1a82ae060a6a20c95780afc71 [formerly 7cc3d4854be8b5269fee8c3ee829c830af9950d8]]
    Former-commit-id: 20dacfa939db8dd7366c84911aa28b1246679e07 [formerly f23d8a8bdfc405a884cb05762f98c00581c4e1d5]
    Former-commit-id: 3a32f5033eeab820bce2a69a5fefaed64d3ed17a
    Former-commit-id: 57830b18ebf998e0aff89e306cba63c0147ee858

--- cookbooks/kafka/recipes/default.rb
+++ cookbooks/kafka/recipes/default.rb
@@ -21,79 +21,89 @@
 include_recipe "java"
 include_recipe "runit"
 
+java_home   = node['java']['java_home']
+user = "kafka"
+group = "kafka"
+
 # == Users
 
-# Setup kafka user
-user "#{node[:kafka][:user]}" do
+# setup kafka group
+group group do
+end
+
+# setup kafka user
+user user do
   comment "Kafka user"
-  gid "#{node[:kafka][:group]}"
-  home "/home/#{node[:kafka][:user]}"
-  shell "/bin/bash"
-  supports :manage_home => true
+  gid "kafka"
+  home "/home/kafka"
+  shell "/bin/noshell"
+  supports :manage_home => false
 end
 
-# == Directories
-home_dir = node[:kafka][:home_dir]
+# create the install directory
+install_dir = node[:kafka][:install_dir]
 
-# Create a staging home for Kafka
-directory "#{node[:kafka][:stage_dir]}" do
-	mode "0755"
-	action :create
-	owner "root"
-	group "root"
-	not_if { ::File.exists?("#{node[:kafka][:stage_dir]}") }
+directory "#{install_dir}/bin" do
+  owner "root"
+  group "root"
+  mode 00755
+  recursive true
+  action :create
 end
 
-# Download Kafka to the staging directory. For now the tar is embedded with the cookbook.
-cookbook_file "/usr/local/src/kafka-#{node[:kafka][:version]}.tar.gz" do
-  source 	"kafka-#{node[:kafka][:version]}.tar.gz"
-  owner 	"#{node[:kafka][:user]}"
-  group 	"#{node[:kafka][:group]}"
-  mode 		"0744"
-  not_if 	"test -f /usr/local/src/kafka-#{node[:kafka][:version]}.tar.gz"
+# create the log directory
+directory "#{node[:kafka][:log_dir]}" do
+  owner   user
+  group   group
+  mode    00755
+  recursive true
+  action :create
 end
 
-# Extract it
-execute 	"extract-kafka" do
-  command	"tar -zxf /usr/local/src/kafka-#{node[:kafka][:version]}.tar.gz"
-  creates	"kafka-#{node[:kafka][:version]}"
-  cwd 		"#{node[:kafka][:stage_dir]}"
-  user		"root"
-  group 	"root"
+# create the data directory
+directory "#{node[:kafka][:data_dir]}" do
+  owner   user
+  group   group
+  mode    00755
+  recursive true
+  action :create
 end
 
-# link the extracted bits
-link home_dir do
-	to          "#{node[:kafka][:stage_dir]}"
-	action    	:create
-	link_type 	:symbolic
-	owner 		"#{node[:kafka][:user]}"
-	group 		"#{node[:kafka][:group]}"
-	not_if { ::File.exists?("#{home_dir}") }
+# pull the remote file only if we create the directory
+tarball = "kafka-#{node[:kafka][:version]}.tar.gz"
+download_file = "#{node[:kafka][:download_url]}/#{tarball}"
+
+remote_file "#{Chef::Config[:file_cache_path]}/#{tarball}" do
+    source download_file
+    mode 00644
 end
 
-# The directories to manage
-kafka_dirs = [
-	node['kafka']['data_dir'],
-	node['kafka']['stage_dir'],
-	node['kafka']['log_dir']
-]
+execute "tar" do
+  user  "root"
+  group "root" 
+  cwd install_dir
+  command "tar zxf #{Chef::Config[:file_cache_path]}/#{tarball}"
+end
 
-# Create all other directories, if needed
-kafka_dirs.each do |dir|
-        directory dir do
-        mode        "0744"
-        action      :create
-        owner       "#{node[:kafka][:user]}"
-        group       "#{node[:kafka][:group]}"
-        recursive   true
-    end
+template "#{install_dir}/bin/service-control" do
+  source  "service-control.erb"
+  owner "root"
+  group "root"
+  mode  00755
+  variables({
+        :install_dir => install_dir,
+		:log_dir => node[:kafka][:log_dir], 
+        :java_home => java_home,
+        :java_jmx_port => 9999,
+        :java_class => "kafka.Kafka",
+        :user => user
+  })
 end
 
 # grab the zookeeper nodes that are currently available
 zookeeper_pairs = Array.new
 if not Chef::Config.solo
-    search(:node, "recipe:#{node[:kafka][:zookeeper_recipe]} AND chef_environment:#{node.chef_environment}").each do |n|
+    search(:node, "role:zookeeper AND chef_environment:#{node.chef_environment}").each do |n|
 		zookeeper_pairs << n[:fqdn]
 	end
 end
@@ -101,31 +111,38 @@ end
 # append the zookeeper client port (defaults to 2181)
 i = 0
 while i < zookeeper_pairs.size do
-  zookeeper_pairs[i] = zookeeper_pairs[i].concat(":#{node[:kafka][:zookeeper_client_port]}")
+  zookeeper_pairs[i] = zookeeper_pairs[i].concat(":#{node[:zookeeper][:clientPort]}")
   i += 1
 end
 
-# templates
-%w[kafka-server-start.sh kafka-run-class.sh sv-kafka-run sv-kafka-log-run].each do |template_file|
-  template "#{home_dir}/bin/#{template_file}" do
-	source	"#{template_file}.erb"
-	mode 		"0755"
-	variables({
-		:kafka => node[:kafka]
-	})
-	end
-end
-
 %w[server.properties consumer.properties producer.properties zookeeper.properties log4j.properties].each do |template_file|
-  template "#{home_dir}/config/#{template_file}" do
+  template "#{install_dir}/config/#{template_file}" do
         source	"#{template_file}.erb"
-        mode 		"0755"
-        owner		"root"
+        owner "root"
+        group "root"
+        mode  00644
         variables({ 
             :kafka => node[:kafka],
-            :zookeeper_pairs => zookeeper_pairs
+            :zookeeper_pairs => zookeeper_pairs,
+            :client_port => node[:zookeeper][:clientPort]
         })
     end
 end
 
-runit_service "kafka"
+# delete the application tarball
+execute "delete_install_source" do
+    user "root"
+    group "root"
+    command "rm -f #{Chef::Config[:file_cache_path]}/#{tarball}"
+    action :run
+end
+
+# create the runit service
+runit_service "kafka" do
+    options({
+        :log_dir => node[:kafka][:log_dir],
+        :install_dir => install_dir,
+        :java_home => java_home,
+        :user => user
+      }) 
+end
