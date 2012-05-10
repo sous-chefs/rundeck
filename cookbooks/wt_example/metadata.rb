2f848c296f33fe7872f9e5ceb4e72d4c313b4e90
David Dvorak <david.dvorak@webtrends.com>
2012-05-10 18:27:07 +0000

    experimental cookbook
    Former-commit-id: 65ec84420e51a30d9174215b8fd4e135883b60c1 [formerly 4509e674bb74c77afc2dc648f4202870e9cdf047] [formerly e381dc164edb9b03728e08fe115d69b7fe72ed1e [formerly 08d50a018699c3ac23b6fff38b8b38d52b710fb4 [formerly 167208dba2e66b4c714f224c7b85142745f1d5b7]]]
    Former-commit-id: 80826e21a428eb6f8f0546f6177bc3caa21b6d26 [formerly a7f4a3bb17b601e5f32dcc461b6f637f205ca7b6]
    Former-commit-id: 1f4b21ab8c9220ca5742c1d858fa1ba1537bf13e
    Former-commit-id: e2c9d4b9df2549e3fc8c407999294f5bc2889129

---
+++ cookbooks/wt_example/metadata.rb
@@ -0,0 +1,11 @@
+maintainer       "Webtrends Inc."
+maintainer_email "david.dvorak@webtrends.com"
+license          "All rights reserved"
+description      "Installs/Configures wt_example"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+supports	"windows", ">= 6.1"
+
+depends		"windows"
+depends		"wt_base"
