69992f79c15a4c1a2a7a5d1ae75f95424c03aacf
vonnagy <ivan.vonnagy@webtrends.com>
2012-05-10 12:33:47 -0700

    Externalized standard monitoring
    Former-commit-id: d456eee112e9e87d780033bfb56463526296353a [formerly 12adf682fbcd6e24f55cae9c0cb4f472bfdeea0e] [formerly 2d94b741daff6242d06d03ac5504153f54478791] [formerly ff18e522a129b29ccd04d33b372ab56e9bb1d6f6 [formerly f82e57f186bef109e3be03ddb9a691ddcee7d39a]] [formerly 309f3b75d6af30cf696a09143ab78cd99e6fe83e [formerly 7ffc6e0b8001acd0765036018e2304e9d640344b] [formerly 2808eb03404a13b4b392a8039b1b7b845760af3d [formerly d92df195282946f7b4abee10db404eacf566dd2d [formerly ca9a963a02ddedd2dcf50859078416b4c35562ac]]]]
    Former-commit-id: fab3de902521dda7ed07ced8fe5c5ba1706c372b [formerly e4d79c364fe5ef19d5f62e54d4583d04407acd21] [formerly e905e083508ca22f09f39f53bdc7dae3425916f9 [formerly d92df195282946f7b4abee10db404eacf566dd2d]]
    Former-commit-id: dd7c8de39a02b439d09720d46192d243797e27a2 [formerly dbf5719e624ca2b81e995d8c093c3392412170c3]
    Former-commit-id: 98bb61387238a610ec8b402541c7e839e0a9dd5c
    Former-commit-id: 8369e56f6fe80d4373067c122453da01ca6d4171

--- cookbooks/wt_streamingcollection/metadata.rb
+++ cookbooks/wt_streamingcollection/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email    "marek.dolgos@webtrends.com"
 license             "All rights reserved"
 description         "Installs/Configures the Webtrends Streaming Collection Service"
 long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version             "0.0.10"
+version             "0.0.11"
 depends             "java"
 depends             "runit"
