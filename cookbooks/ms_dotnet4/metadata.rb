80672f87a2188b068f051cc4d033d40cfb11f983
Kendrick Martin <kendrick.martin@webtrends.com>
2012-05-10 17:52:09 +0000

    Adding recipe to reset iis to add ISAPI filters

--- cookbooks/ms_dotnet4/metadata.rb
+++ cookbooks/ms_dotnet4/metadata.rb
@@ -4,6 +4,6 @@ license           "Apache 2.0"
 license          "All rights reserved"
 description      "Installs/Configures Microsoft .NET 4.0"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.1"
 supports	 "windows"
 depends          "windows"
