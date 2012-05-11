b6db7ef9be3910940134a6126e10ce1458650458
Josh Behrends <josh.behrends@webtrends.com>
2012-05-11 22:05:28 +0000

    Added a collectd plugin to the cassandra recipe along with a .conf.erb template. Also updated wt_monitoring changelog.
    Former-commit-id: f6e9fb93e619bb094906fe8b780e159c749d1294 [formerly 89e6ef2edbe1ff04fd0fbdf9e0a929d60f6e44c5] [formerly 8a41cc0c340b31eb75aa65750b05ce432c540f2d] [formerly 2796b792c16c5353891a04220cb29c23e975d4de [formerly 18d4c0fdf66b145cbab714a8990969784ba72054]] [formerly 788b4de0387c5d8a9ee00d64f03ca55471954790 [formerly 7a46a21c94fdfac2d5a615003f9ad6e4ff29a01c] [formerly b00fd687418e4b38e7373e6d6eab34c3685b2900 [formerly a4afd337abd78f46418910d0e63a9a22dfcc37cb [formerly 0026866cd1bbd49fed4d8ed94913f816b75f29e0]]]]
    Former-commit-id: 28a3d650a0360a5be888f7410f50afb0bcef3463 [formerly 47d46962ac8490a6607584847f972b861ae13a41] [formerly 3efbd8a9d888e313056d30baea8abbf74da8b3b9 [formerly a4afd337abd78f46418910d0e63a9a22dfcc37cb]]
    Former-commit-id: 32b78164691b52e561884f7a28a9955c4dc1317a [formerly 516a1c386501713e785bd759980fcf33eb391475]
    Former-commit-id: 0b1214d43ad791c442ce4776e7cc20fcbfdc590e
    Former-commit-id: f42c59d01903e3e711812156b8f4891ea82f8540

--- cookbooks/wt_monitoring/metadata.rb
+++ cookbooks/wt_monitoring/metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "tim.smith@webtrends.com"
 license          "All rights reserved"
 description      "Contains multiple recipes for use in monitoring particular components of the service"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
