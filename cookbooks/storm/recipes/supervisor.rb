d70b87298dd798ce324228127aa245cd315c78a5
reinvigorate <sean@reinvigorate.net>
2012-05-11 11:51:22 -0700

    initial rewrite of storm cookbook
    Former-commit-id: efc59552825a989ce7072c966f6cf50eca5c9651 [formerly 4eacafd81ab393b7701fbefc84e58279b0f05c41] [formerly 80aea46806dd7d2e14271da4e9ccb8f936d7cf57 [formerly a2f40b9f89dd779768abbfbebcfa5b6cbe3590d3 [formerly b89156d561b9d74c4124bff40c3c0f6f910f2ef6]]]
    Former-commit-id: 6b2df5dfe506cd56c93bc3c1fe878e7b0fea51c9 [formerly 1dd8b94b98371515a574a77d0af4ade428ab0cc1]
    Former-commit-id: 416ab8c8782bb242c0bddfbb144eed68bc822872
    Former-commit-id: ac46fda7e784ba49b8da3abe0ea9d5103a46964d

---
+++ cookbooks/storm/recipes/supervisor.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: storm
+# Recipe:: default
+#
+# Copyright 2012, Webtrends
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "storm"
+
+# do supervisor specific bits go here
