52994f1899d22832b4431ce1a6094ec20805620b
Tim Smith <tim.smith@webtrends.com>
2012-05-14 17:08:54 -0700

    Add the future attributes to the Cassandra recipe and change it to only install from the repo.  There's no need to install it any other way the recipe is crashing chef on our centos boxes in its current state
    Former-commit-id: 6aaa97e90b8b8e97b839528d7ab028e13b0078dc [formerly f37e98103fde9d4324cc3bc61218916c19a1d683] [formerly c2f1ab65bd394c65aa0c159509f1981f527ebcb5] [formerly d94b8c226faa834df25dd5bb241216790c8df407 [formerly fc1739969d2b1ef164110edb250c50071ff7a6ae]] [formerly 53e3e18bce378ed8b5ec884bfa61d4f58d9a8b21 [formerly 9211cfa076b9f3bd5893fe03ef35bbb99af3cee3] [formerly cec40cad10053d2ca41d8fdc44cbfe415316d2d1 [formerly 8a591e53a0b8da374ad0a8e1198acaec87ffc096 [formerly 9e5b71cdf0036468e4ee1dcd26126eaf1a6dd8d7]]]]
    Former-commit-id: 82fe68447f2b9f02a20645069f8084efb2b72d79 [formerly e866fc6fca7bf2bf01cb27435cae75666f7678d8] [formerly de34cc19f84d2df7f7fe6fa06201db01ee9e8422 [formerly 8a591e53a0b8da374ad0a8e1198acaec87ffc096]]
    Former-commit-id: 8d251c36bd563d3cf7e5a7cfcfd30eb17ee3fa05 [formerly a6261d24115d60392f8d611f7d4d8e55b2d4886e]
    Former-commit-id: 2ae7b7d78b176daf64ccf5395e23141e625b68f7
    Former-commit-id: 2fb5e0de271287dfda3037a0b0ef1ba5a9c5d0cc

--- cookbooks/cassandra/attributes/default.rb
+++ cookbooks/cassandra/attributes/default.rb
@@ -6,7 +6,57 @@
 # Copyright 2012, Webtrends Inc.
 #
 
-# Cassandra
-default['cassandra']['build_uri'] = 'http://rpm.datastax.com/community/noarch/apache-cassandra1-1.0.9-1.noarch.rpm'
-default['cassandra']['mx4j_url'] = 'http://sourceforge.net/projects/mx4j/files/MX4J%20Binary/3.0.2/mx4j-3.0.2.tar.gz/download'
-default['cassandra']['jna_url'] = 'http://java.net/projects/jna/sources/svn/content/tags/3.3.0/jnalib/dist/jna.jar?rev=1208'
+# download paths for jna and mx4j
+default['cassandra']['mx4j_url'] = "http://sourceforge.net/projects/mx4j/files/MX4J%20Binary/3.0.2/mx4j-3.0.2.tar.gz/download"
+default['cassandra']['jna_url'] = "http://java.net/projects/jna/sources/svn/content/tags/3.3.0/jnalib/dist/jna.jar?rev=1208"
+
+default['cassandra']['version'] = "1.0.7"
+
+# Currently unused attributes that will be part of the new full configuration recipe
+default['cassandra']['auto_bootstrap'] = false
+default['cassandra']['hinted_handoff_enabled'] = true 
+default['cassandra']['max_hint_window_in_ms'] = 3600000
+default['cassandra']['hinted_handoff_throttle_delay_in_ms'] = 50
+default['cassandra']['authenticator'] = "org.apache.cassandra.auth.AllowAllAuthenticator"
+default['cassandra']['authority'] = "org.apache.cassandra.auth.AllowAllAuthority"
+default['cassandra']['partitioner'] = "org.apache.cassandra.dht.RandomPartitioner"
+default['cassandra']['data_file_directories'] =  "/var/lib/cassandra/data"
+default['cassandra']['commitlog_directory'] = "/var/lib/cassandra/commitlog"
+default['cassandra']['saved_caches_directory'] = "/var/lib/cassandra/saved_caches"
+default['cassandra']['commitlog_rotation_threshold_in_mb'] = 128
+default['cassandra']['commitlog_sync'] = "periodic"
+default['cassandra']['commitlog_sync_period_in_ms'] = 10000
+default['cassandra']['flush_largest_memtables_at'] = 0.75
+default['cassandra']['reduce_cache_sizes_at'] = 0.85
+default['cassandra']['reduce_cache_capacity_to'] = 0.6
+default['cassandra']['seed_provider_class_name'] = "org.apache.cassandra.locator.SimpleSeedProvider"
+default['cassandra']['disk_access_mode'] = "auto"
+default['cassandra']['concurrent_reads'] = 80 
+default['cassandra']['concurrent_writes'] = 64
+default['cassandra']['memtable_flush_queue_size'] = 4
+default['cassandra']['memtable_flush_writers'] = 1
+default['cassandra']['sliced_buffer_size_in_kb'] = 64
+default['cassandra']['storage_port'] = 7000
+default['cassandra']['rpc_port'] = 9160
+default['cassandra']['rpc_keepalive'] = true
+default['cassandra']['rpc_server_type'] = "sync"
+default['cassandra']['thrift_framed_transport_size_in_mb'] = 15
+default['cassandra']['thrift_max_message_length_in_mb'] = 16
+default['cassandra']['incremental_backups'] = false
+default['cassandra']['snapshot_before_compaction'] = false
+default['cassandra']['column_index_size_in_kb'] = 64
+default['cassandra']['in_memory_compaction_limit_in_mb'] = 64
+default['cassandra']['compaction_throughput_mb_per_sec'] = 16
+default['cassandra']['compaction_preheat_key_cache'] = true
+default['cassandra']['rpc_timeout_in_ms'] = 10000
+default['cassandra']['phi_convict_threshold'] = 8
+default['cassandra']['endpoint_snitch'] = "org.apache.cassandra.locator.PropertyFileSnitch"
+default['cassandra']['dynamic_snitch_badness_threshold'] = 0.0
+default['cassandra']['request_scheduler'] = "org.apache.cassandra.scheduler.NoScheduler"
+default['cassandra']['index_interval'] = 128
+default['cassandra']['memtable_total_space_in_mb'] = 4096
+default['cassandra']['multithreaded_compaction'] = false
+default['cassandra']['commitlog_total_space_in_mb'] = 3072
+default['cassandra']['MAX_HEAP_SIZE'] = "10G"
+default['cassandra']['HEAP_NEWSIZE'] = "800M"
+default['cassandra']['jna_location'] = "800M"
