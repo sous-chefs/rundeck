f37e98103fde9d4324cc3bc61218916c19a1d683
Tim Smith <tim.smith@webtrends.com>
2012-05-14 17:08:54 -0700

    Add the future attributes to the Cassandra recipe and change it to only install from the repo.  There's no need to install it any other way the recipe is crashing chef on our centos boxes in its current state
    Former-commit-id: 53e3e18bce378ed8b5ec884bfa61d4f58d9a8b21 [formerly 9211cfa076b9f3bd5893fe03ef35bbb99af3cee3] [formerly cec40cad10053d2ca41d8fdc44cbfe415316d2d1 [formerly 9e5b71cdf0036468e4ee1dcd26126eaf1a6dd8d7]]
    Former-commit-id: d94b8c226faa834df25dd5bb241216790c8df407 [formerly fc1739969d2b1ef164110edb250c50071ff7a6ae]
    Former-commit-id: c2f1ab65bd394c65aa0c159509f1981f527ebcb5

--- cookbooks/cassandra/recipes/default.rb
+++ cookbooks/cassandra/recipes/default.rb
@@ -8,31 +8,10 @@
 # All rights reserved - Do Not Redistribute
 #
 
-build_uri = node['cassandra']['build_uri']
-if (build_uri =~ /\/([^\/]*)$/)
-	rpmfile = $1
-end
-
+# Install the package.
 package "apache-cassandra1" do
-	action :install
-	ignore_failure true
-end
-
-remote_file "#{Chef::Config[:file_cache_path]}/#{rpmfile}" do
-	source build_uri
-	not_if "rpm -qa | egrep -q apache-cassandra1"
-	notifies :install, "rpm_package[apache-cassandra1]", :immediately
-end
-
-rpm_package "apache-cassandra1" do
-	source "#{Chef::Config[:file_cache_path]}/#{rpmfile}"
-	only_if {::File.exists?("#{Chef::Config[:file_cache_path]}/#{rpmfile}")}
-	action :nothing
-end
-
-file "apache-cassandra1" do
-	path "#{Chef::Config[:file_cache_path]}/#{rpmfile}"
-	action :delete
+  action :install
+  version node['cassandra']['version']
 end
 
 # Drop the JNA Jar.
