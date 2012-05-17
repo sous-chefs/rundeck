bdf3d51f6676fa721cedfc50d67df58db980900a
vonnagy <ivan.vonnagy@webtrends.com>
2012-05-17 13:08:35 -0700

    Moved jmx_port to wt_monitoring
    Former-commit-id: 55f4e32bec88eee15577a0b38f604d6547fa9b1d [formerly 1ee387bd8471de8614d56d4847a5163056a484fb] [formerly f7c1618d86b843906b29bc5c03cf4cd4b923608b] [formerly d3bf204f17f81df2880a8e3839a470fcfc77cdcb [formerly 9008790b150e387b76f605707758012f4d182677]] [formerly 4070355aa0894d5e9af85600ed38fa2f9fbb83d8 [formerly e0efdf1aa48dd1a4adf849fb253554b1e9f9a329] [formerly afa5d0ed36ab7c2ea2a6ef891f64a4169335b737 [formerly 055b4936ee5082c7f0949a9430ac83a089168d03 [formerly 839bc46ba6cb03cccb0773452a1067727a1ef6f3]]]]
    Former-commit-id: 8dd469c03e99a9190a4d82e03fe31f5c6cd8c981 [formerly 4596b40fbf1cd8ab2be5d2364ca7c6197d7a5c63] [formerly 568e0a423a50f22100ba2e771b0ece01b61a9c05 [formerly 055b4936ee5082c7f0949a9430ac83a089168d03]]
    Former-commit-id: f37d22e123d9f8129bd6fd96c814a6ae6fdfbdb1 [formerly d9c354a3036f8e2eedf4ffcb4f13c533c65670c8]
    Former-commit-id: 267457df4e3a3baae900fe7eb2e5e9d561a86924
    Former-commit-id: 7e120ed5cdf09be6cd7326eb78c9bff7eac39bd1

--- cookbooks/wt_streamingapi/recipes/default.rb
+++ cookbooks/wt_streamingapi/recipes/default.rb
@@ -69,7 +69,7 @@ template "#{install_dir}/bin/service-control" do
         :java_home => java_home,
         :user => user,
         :java_class => "com.webtrends.streaming.websocket.StreamingAPIDaemon",
-        :java_jmx_port => node['wt_streamingapi']['jmx_port'],
+        :java_jmx_port => node['wt_monitoring']['jmx_port'],
         :java_opts => java_opts
     })
 end
