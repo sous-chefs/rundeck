880aa0f6d9c4094207cd53bcc2b97d750d6225c4
vonnagy <ivan.vonnagy@webtrends.com>
2012-05-18 13:36:36 -0700

    Fixed version issues
    Former-commit-id: cc176568e656010c3ae6ec19e0dadfb84552b04a [formerly 4b76837aebb0bc01b4a011229b70be2f704cee37] [formerly 770d7f083afd29a57a239d192df3c428a0e69f0f [formerly 17a41a1fa1f8f781b6b4597c67c556f03897920f]]
    Former-commit-id: 956d35c9a9d688c876a9810974e56589d85a5851 [formerly 9a6011bf4cee53de7197c49e8082156c9d42b7b1]
    Former-commit-id: 72b5d12a63cec9590caf6e201190edf5e594f284

--- cookbooks/wt_streamingaudit/metadata.rb
+++ cookbooks/wt_streamingaudit/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email    "ivan.vonnagy@webtrends.com"
 license             "All rights reserved"
 description         "Installs/Configures the Webtrends Streaming Auditor Service"
 long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version             "0.0.3"
+version             "0.0.4"
 depends             "java"
 depends             "runit"
