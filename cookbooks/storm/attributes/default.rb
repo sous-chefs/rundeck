4d412aa8ef2b52465af65500822572373abf4c82
reinvigorate <sean@reinvigorate.net>
2012-05-24 18:13:08 -0600

    adding search + config + daemon supervision to storm cookbook
    Former-commit-id: 2015731333c60b8479d5f8f3e3638bf37bc3e0ec [formerly 8a923a9b82f7e3ab73536f596a252b799da12ad7] [formerly aed3ee89863d98ebe6e3b2813c2839a3f91ad5e2 [formerly b8c256d0b92b00da3179bf516b1657e14ffdf850]]
    Former-commit-id: 9ed2eed23c59a81272b45a1b0a7e9d56f81fd3df [formerly 39cc64db9a8c1a26f69b778ba0dd6afd9fe4296e]
    Former-commit-id: ce41c87b7c3cdb7e229182755ab48ea5ee73917d

--- cookbooks/storm/attributes/default.rb
+++ cookbooks/storm/attributes/default.rb
@@ -6,78 +6,31 @@
 # Copyright 2012, Webtrends Inc.
 #
 
+# cluster identifier
+default['storm']['cluster_role'] = ""
+
 # installation attributes
-default['storm']['download_url'] = ""
-default['storm']['tarball'] = ""
+default['storm']['version'] = "0.7.1"
+default['storm']['download_url'] = "https://github.com/downloads/nathanmarz/storm/storm-0.7.1.zip"
 default['storm']['install_dir'] = "/opt/storm"
+
+
+
+######################################################
+# attributes for storm.yaml below here
+
 
 # general storm attributes
 default['storm']['localdir'] = "/mnt/storm"
-default['storm']['zk_port'] = 2181
-default['storm']['zk_session_timeout'] = 20000
-default['storm']['sk_retry_attempts'] = 5
-default['storm']['sk_retry_interval'] = 1000
-default['storm']['cluster_mode'] = "distributed"
-default['storm']['local_mode_zmq'] = "false"
+default['storm']['logdir'] = "/var/log/storm"
 
-# nimbus attributes
-default['storm']['nimbus_thrift_port'] = 6627
-default['storm']['nimbus_childopts'] = "-Xmx1024m"
-default['storm']['nimbus_task_timeout'] = 30
-default['storm']['nimbus_supervisor_timeout_secs'] = 60
-default['storm']['nimbus_monitor_freq_secs'] = 10
-default['storm']['nimbus_cleanup_inbox_freq_secs'] = 600
-default['storm']['nimbus_inbox_jar_expiration_secs'] = 3600
-default['storm']['nimbus_task_launch_secs'] = 120
-default['storm']['nimbus_reassign'] = "true"
-default['storm']['nimbus_file_copy_expiration_secs'] = 600
+# supervisor attributes
+default['storm']['supervisor']['workers'] = 4
 
-# ui attributes
-default['storm']['ui_port'] = 8080
-default['storm']['ui_childopts'] = "-Xmx768m"
+# zookeeper
+default['storm']['zookeeper']['root'] = "/storm"
 
-# drpc attributes
-default['storm']['drpc_port'] = 3772
-default['storm']['drpc_ivocations_port'] = 3773
-default['storm']['drpc_request_timeout_secs'] = 600
 
-# transactional options
-default['storm']['transactional_zookeeper_root'] = "/transactional"
-default['storm']['transactional_zookeeper_servers'] = "null"
-default['storm']['transactional_zookeeper_port'] = "null"
 
-# supervisor options
-default['storm']['supervisor_childopts'] = "-Xmx1024m"
-default['storm']['supervisor_worker_start_timeout_secs'] = 120
-default['storm']['supervisor_worker_timeout_secs'] = 30
-default['storm']['supervisor_monitor_frequency_secs'] = 3
-default['storm']['supervisor_heartbeat_frequency_secs'] = 5
-default['storm']['supervisor_enable'] = "true"
+# TODO: Add in all the attributes for storm.yaml
 
-# worker options
-default['storm']['woker_childopts'] = "-Xmx768m"
-default['storm']['heartbeat_frequency_secs'] = 1
-
-# task attributes
-default['storm']['task_heartbeat_frequency_secs'] = 3
-default['storm']['task_refresh_poll_secs'] = 10
-
-# zeromq attributes
-default['storm']['zmq_threads'] = 1
-default['storm']['zmq_linger_millis'] = 5000
-
-# topology options
-default['storm']['topology_debug'] = "false"
-default['storm']['topology_optimize'] = "true"
-default['storm']['topology_workers'] = 1
-default['storm']['topology_acker_executers'] = 1
-default['storm']['topology_acker_tasks'] = "null"
-default['storm']['topology_tasks'] = "null"
-default['storm']['topology_message_timeout_secs'] = 30
-default['storm']['topology_skip_missing_kryo_registrations'] = "false"
-default['storm']['topology_max_task_parallelism'] = "null"
-default['storm']['topology_max_spout_pending'] = "null"
-default['storm']['topology_state_synchronization_timeout_secs'] = 60
-default['storm']['topology_stats_sample_rate'] = 0.05
-default['storm']['topology_fall_back_on_java_serialization'] = "true"
-default['storm']['topology_worker_childopts'] = "null"
