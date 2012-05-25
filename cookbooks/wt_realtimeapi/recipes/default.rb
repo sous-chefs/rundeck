cf92fb81fb282e40c63eaa17650c784d747ed561
vonnagy <ivan.vonnagy@webtrends.com>
2012-05-25 11:30:27 -0700

    Added gate keeper for deploy using the deploy_build environment variable for the following cookbooks: wt_realtime wt_streamingapi wt_streamingaudit wt_streamingcollection wt_streaminglogreplayer
    Former-commit-id: 7537a56567868789046bd5d1efd20a26613eaedc [formerly 00521649f3acea5e13ecda880a1fa2cd58d69d36] [formerly 6ffabf9ed959e46a7d4f328309cdaef28d4214a5 [formerly 898303602bee395325f322155bdc91f7eba5c941]]
    Former-commit-id: 723eb34b67c8e591c3db80bdcdd21d54ee23538c [formerly f5ff1bf90b2747f599f67a2d05ed4f01e3698fa2]
    Former-commit-id: c7a4726c4971fa146eb14d7b7cc3f6b57d121c25

--- cookbooks/wt_realtimeapi/recipes/default.rb
+++ cookbooks/wt_realtimeapi/recipes/default.rb
@@ -10,101 +10,109 @@
 # include runit so we can create a runit service
 include_recipe "runit"
 
-log_dir     = File.join("#{node['wt_common']['log_dir_linux']}", "realtimeapi")
-install_dir = File.join("#{node['wt_common']['install_dir_linux']}", "realtimeapi")
-tarball     = "realtimeapi-bin.tar.gz"
-download_url = node['wt_realtimeapi']['download_url']
-java_home   = node['java']['java_home']
-port = node['wt_realtimeapi']['port']
-cam_url = node['wt_camservice']['url']
-user = node['wt_realtimeapi']['user']
-group = node['wt_realtimeapi']['group']
-java_opts = node['wt_realtimeapi']['java_opts']
+log "Deploy build is #{ENV["deploy_build"]}"
+if ENV["deploy_build"] != "true" then
+    log "The deploy_build value is not set or is false so exit here"
+else
+    log "The deploy_build value is true so un-deploy first"
+    include_recipe "wt_realtimeapi::undeploy"
 
-log "Install dir: #{install_dir}"
-log "Log dir: #{log_dir}"
-log "Java home: #{java_home}"
+    log_dir     = File.join("#{node['wt_common']['log_dir_linux']}", "realtimeapi")
+    install_dir = File.join("#{node['wt_common']['install_dir_linux']}", "realtimeapi")
+    tarball     = "realtimeapi-bin.tar.gz"
+    download_url = node['wt_realtimeapi']['download_url']
+    java_home   = node['java']['java_home']
+    port = node['wt_realtimeapi']['port']
+    cam_url = node['wt_camservice']['url']
+    user = node['wt_realtimeapi']['user']
+    group = node['wt_realtimeapi']['group']
+    java_opts = node['wt_realtimeapi']['java_opts']
 
-# create the log dir
-directory "#{log_dir}" do
-    owner   user
-    group   group
-    mode    00755
-    recursive true
-    action :create
-end
+    log "Install dir: #{install_dir}"
+    log "Log dir: #{log_dir}"
+    log "Java home: #{java_home}"
 
-# create the install dir 
-directory "#{install_dir}/bin" do
-   owner "root"
-   group "root"
-   mode 00755
-   recursive true
-   action :create
-end
+    # create the log dir
+    directory "#{log_dir}" do
+        owner   user
+        group   group
+        mode    00755
+        recursive true
+        action :create
+    end
 
-# grab the source file
-remote_file "#{Chef::Config[:file_cache_path]}/#{tarball}" do
-  source download_url
-  mode 00644
-end
-
-# extract the source file
-execute "tar" do
-  user  "root"
-  group "root" 
-  cwd install_dir
-  command "tar zxf #{Chef::Config[:file_cache_path]}/#{tarball}"
-end
-
-#templates
-template "#{install_dir}/bin/service-control" do
-    source  "service-control.erb"
+    # create the install dir 
+    directory "#{install_dir}/bin" do
     owner "root"
     group "root"
-    mode  00755
-    variables({
-        :log_dir => log_dir,
-        :install_dir => install_dir,
-        :java_home => java_home,
-        :user => user,
-        :java_class => "com.webtrends.realtime.server.RealtimeApiDaemon",
-        # node['wt_monitoring']['jmx_port']
-        :java_jmx_port => 9998,
-        :java_opts => java_opts
-    })
-end
+    mode 00755
+    recursive true
+    action :create
+    end
+
+    # grab the source file
+    remote_file "#{Chef::Config[:file_cache_path]}/#{tarball}" do
+    source download_url
+    mode 00644
+    end
+
+    # extract the source file
+    execute "tar" do
+    user  "root"
+    group "root" 
+    cwd install_dir
+    command "tar zxf #{Chef::Config[:file_cache_path]}/#{tarball}"
+    end
+
+    #templates
+    template "#{install_dir}/bin/service-control" do
+        source  "service-control.erb"
+        owner "root"
+        group "root"
+        mode  00755
+        variables({
+            :log_dir => log_dir,
+            :install_dir => install_dir,
+            :java_home => java_home,
+            :user => user,
+            :java_class => "com.webtrends.realtime.server.RealtimeApiDaemon",
+            # node['wt_monitoring']['jmx_port']
+            :java_jmx_port => 9998,
+            :java_opts => java_opts
+        })
+    end
 
 
-%w[monitoring.properties config.properties netty.properties].each do | template_file|
-  template "#{install_dir}/conf/#{template_file}" do
-	source	"#{template_file}.erb"
-	owner "root"
-	group "root"
-	mode  00644
-	variables({
-        :cam_url => cam_url,
-        :install_dir => install_dir,
-        :port => port,
-        :wt_monitoring => node[:wt_monitoring]
-    })
-	end 
-end 
+    %w[monitoring.properties config.properties netty.properties].each do | template_file|
+    template "#{install_dir}/conf/#{template_file}" do
+        source	"#{template_file}.erb"
+        owner "root"
+        group "root"
+        mode  00644
+        variables({
+            :cam_url => cam_url,
+            :install_dir => install_dir,
+            :port => port,
+            :wt_monitoring => node[:wt_monitoring]
+        })
+        end 
+    end 
 
-# delete the source file
-execute "delete_install_source" do
-    user "root"
-    group "root"
-    command "rm -f #{Chef::Config[:file_cache_path]}/#{tarball}"
-    action :run
-end
+    # delete the source file
+    execute "delete_install_source" do
+        user "root"
+        group "root"
+        command "rm -f #{Chef::Config[:file_cache_path]}/#{tarball}"
+        action :run
+    end
 
-# create the runit service
-runit_service "realtimeapi" do
-    options({
-        :log_dir => log_dir,
-        :install_dir => install_dir,
-        :java_home => java_home,
-        :user => user
-      }) 
+    # create the runit service
+    runit_service "realtimeapi" do
+        options({
+            :log_dir => log_dir,
+            :install_dir => install_dir,
+            :java_home => java_home,
+            :user => user
+        }) 
+    end
 end
