2faed2b535a134687fbe64b162b52c5041157db8
reinvigorate <sean@reinvigorate.net>
2012-05-25 16:26:06 -0600

    adding all storm attributes
    Former-commit-id: 90268dd87bdfc0282d6218045bdf1995e6fa2cff [formerly 83f64de370a7c51b7144db29477e616fcccca111] [formerly e2cafb9052ca803f2dfda46aa5791db16c09bb35] [formerly 67ab53e0dffbac96ed874fc8ede7f4492c11207a [formerly 932cfbd91305fddd0c41b494c91a8228587491f9]] [formerly 96a484a65cf9f1f5ed85897b99b9ece30a2d363d [formerly 79334b0b5ccd9ebef5b65723d397961af90482f4] [formerly 2e73f689cb3fef05722b10dbd40ce388d6c4d26d [formerly 9e80789f21ae8efff1322e349a59d8ed0d809068 [formerly ec9b6b4375cfc279a39c91b403148e230865d187]]]]
    Former-commit-id: 0b46bae7dc0a50d6e42b30d8e5929fb0d5543a7f [formerly 256dc2230be7a594354f0c5f5c946cd6d29ab62f] [formerly 2b2d42e8ad0b6e5a4efe53a0e7dc8e2f5ae370e8 [formerly 9e80789f21ae8efff1322e349a59d8ed0d809068]]
    Former-commit-id: d7d997c78c48703a1bc0c35fed0ec8218595d726 [formerly abace21bf209bdf6a65f9128d65bc9b610ebde10]
    Former-commit-id: 022c11aba24f1da25b019a041301b6d5deb8e4d2
    Former-commit-id: c118419059ea162e96d8eab51417b45d3e579b16

--- cookbooks/storm/attributes/default.rb
+++ cookbooks/storm/attributes/default.rb
@@ -13,6 +13,7 @@ default['storm']['cluster_role'] = ""
 default['storm']['version'] = "0.7.1"
 default['storm']['download_url'] = "https://github.com/downloads/nathanmarz/storm/storm-0.7.1.zip"
 default['storm']['install_dir'] = "/opt/storm"
+default['storm']['log_dir'] = "/var/log/storm"
 
 
 
@@ -20,17 +21,84 @@ default['storm']['install_dir'] = "/opt/storm"
 # attributes for storm.yaml below here
 
 
+
 # general storm attributes
-default['storm']['localdir'] = "/mnt/storm"
-default['storm']['logdir'] = "/var/log/storm"
+default['storm']['java_lib_path'] = "/usr/local/lib:/opt/local/lib:/usr/lib"
+default['storm']['local_dir'] = "/mnt/storm"
+default['storm']['local_mode_zmq'] = "false"
+default['storm']['cluster_mode'] = "distributed"
+
+
+# zookeeper attributes
+default['storm']['zookeeper']['port'] = 2181
+default['storm']['zookeeper']['root'] = "/storm"
+default['storm']['zookeeper']['session_timeout'] = 20000
+default['storm']['zookeeper']['retry_times'] = 5
+default['storm']['zookeeper']['retry_interval'] = 1000
+
 
 # supervisor attributes
 default['storm']['supervisor']['workers'] = 4
+default['storm']['supervisor']['childopts'] = "-Xmx1024m"
+default['storm']['supervisor']['worker_start_timeout'] = 120
+default['storm']['supervisor']['worker_timeout_secs'] = 30
+default['storm']['supervisor']['monitor_frequecy_secs'] = 3
+default['storm']['supervisor']['heartbeat_frequency_secs'] = 5
+default['storm']['supervisor']['enable'] = true
 
-# zookeeper
-default['storm']['zookeeper']['root'] = "/storm"
+
+# worker attributes
+default['storm']['worker']['childopts'] = "-Xmx768m"
+default['storm']['worker']['heartbeat_frequency_secs'] = 1
+default['storm']['task']['heartbeat_frequency_secs'] = 3
+default['storm']['task']['refresh_poll_secs'] = 10
+default['storm']['zmq']['threads'] = 1
+default['storm']['zmq']['longer_millis'] = 5000
 
 
+# nimbus attributes
+default['storm']['nimbus']['thrift_port'] = 6627
+default['storm']['nimbus']['childopts'] = "-Xmx1024m"
+default['storm']['nimbus']['task_timeout_secs'] = 30
+default['storm']['nimbus']['supervisor_timeout_secs'] = 60
+default['storm']['nimbus']['monitor_freq_secs'] = 10
+default['storm']['nimbus']['cleanup_inbox_freq_secs'] = 600
+default['storm']['nimbus']['inbox_jar_expiration_secs'] = 3600
+default['storm']['nimbus']['task_launch_secs'] = 120
+default['storm']['nimbus']['reassign'] = true
+default['storm']['nimbus']['file_copy_expiration_secs'] = 600
 
-# TODO: Add in all the attributes for storm.yaml
+
+# ui attributes
+default['storm']['ui']['port'] = 8080
+default['storm']['ui']['childopts'] = "-Xmx768m"
+
+
+# drpc attributes
+default['storm']['drpc']['port'] = 3772
+default['storm']['drpc']['invocations_port'] = 3773
+default['storm']['drpc']['request_timeout_secs'] = 600
+
+
+# transactional attributes
+default['storm']['transactional']['zookeeper']['root'] = "/transactional"
+default['storm']['transactional']['zookeeper']['servers'] = "null"
+default['storm']['transactional']['zookeeper']['port'] = "null"
+
+
+# topology attributes
+default['storm']['topology']['debug'] = false
+default['storm']['topology']['optimize'] = true
+default['storm']['topology']['workers'] = 1
+default['storm']['topology']['acker_executors'] = 1
+default['storm']['topology']['acker_tasks'] = "null"
+default['storm']['topology']['tasks'] = "null"
+default['storm']['topology']['message_timeout_secs'] = 30
+default['storm']['topology']['skip_missing_kryo_registrations'] = false
+default['storm']['topology']['max_task_parallelism'] = "null"
+default['storm']['topology']['max_spout_pending'] = "null"
+default['storm']['topology']['state_synchronization_timeout_secs'] = 60
+default['storm']['topology']['stats_sample_rate'] = 0.05
+default['storm']['topology']['fall_back_on_java_serialization'] = true
+default['storm']['topology']['worker_childopts'] = "null"
 
