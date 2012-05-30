f698a3d036acf7fa95ad48eb302a6e0d095861c9
Tim Smith <tim.smith@webtrends.com>
2012-05-30 12:45:05 -0700

    Verison 0.0.2 of the wt_static_tag_host cookbook.  Configure apache, include an uninstall, wrap it all in the deploy_build flag.

--- cookbooks/wt_static_tag_host/recipes/default.rb
+++ cookbooks/wt_static_tag_host/recipes/default.rb
@@ -16,3 +16,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# disable the default apache site
+apache_site "000-default" do
+  enable false
+end
+
+log "Deploy build is #{ENV["deploy_build"]}"
+if ENV["deploy_build"] != "true" then
+    log "The deploy_build value is not set or is false so exit here"
+else
+    log "The deploy_build value is true so un-deploy first"
+    include_recipe "wt_static_tag_host::undeploy"
+
+		# template the apache config for the repo site
+		template "#{node['apache']['dir']}/sites-available/static_tag_host.conf" do
+			source "apache2.conf.erb"
+			mode 00644
+			variables(:docroot => "/var/www")
+			if ::File.symlink?("#{node['apache']['dir']}/sites-enabled/static_tag_host.conf")
+				notifies :reload, resources(:service => "apache2")
+			end
+		end
+		
+		# create the apache site
+		apache_site "static_tag_host" do
+			ignore_failure true
+			action :create
+		end
+
+end
