82ff3ad05b99d8d2e4a2be072ec4772062e7bf59
Tim Smith <tim.smith@webtrends.com>
2012-05-30 13:04:01 -0700

    Revert "Add nfs-common to the multirepo install"
    This reverts commit 2782fa10085c38d06cafc4a63191ef1451e5999e.

--- cookbooks/hp-tools/metadata.rb
+++ cookbooks/hp-tools/metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "hostedops@webtrends.com"
 license          "Apache 2.0"
 description      "Installs/Configures various HP tools"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.2"
+version          "0.1"
