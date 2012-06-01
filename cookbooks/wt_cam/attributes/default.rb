dc381e7ec51b4ce6f712be0ff1f5f2f29b18118f
Kendrick Martin <kendrick.martin@webtrends.com>
2012-06-01 17:24:04 +0000

    Adding new dependencies for cam. Changed attribute for download_url to be full url.

--- cookbooks/wt_cam/attributes/default.rb
+++ cookbooks/wt_cam/attributes/default.rb
@@ -8,6 +8,5 @@
 
 
 default['wt_cam']['app_pool'] = "CAMService"
-default['wt_cam']['download_url'] = "http://pdxteamcitys01.webtrends.corp/guestAuth/repository/download/bt122/.lastSuccessful/"
-default['wt_cam']['zip_file'] = "CAM.zip"
+default['wt_cam']['download_url'] = "http://pdxteamcitys01.webtrends.corp/guestAuth/repository/download/bt122/.lastSuccessful/CAM.zip"
 default['wt_cam']['db_server'] = "(local)"
