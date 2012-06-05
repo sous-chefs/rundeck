b88c772014bbcadd76cd2f2b3977e780fe42518f
Kendrick Martin <kendrick.martin@webtrends.com>
2012-06-05 21:34:57 +0000

    Initial commit of Search service cookbook.
    Former-commit-id: 747a4a323cb8183f2faee7456f280b100265fd4e [formerly 15cdc9325b1f3feb4f054760439ba99af3f28aad] [formerly 931eb00b6b1cfb7419c875488103b4746a05c6e7] [formerly 0f29d43bd467bd2bbb8fd7a4d7602ea880d7ad9d [formerly 3dfd6346d85913ed33c0924ec3708cdedffd8b59]] [formerly 81c903b1892147280dfb658d34d5c34aae179a03 [formerly 8aa73158ed24b5532041370f26ecbdffd8a51e59] [formerly 936e7f733af3688d8e68e673d061bc27d66e1df1 [formerly 03102ff66a2e24b7ea04518d256025560a163f74 [formerly d6bd133ac5f741377a315f285a11b94e828ab112]]]]
    Former-commit-id: 8ff7aa04ac81e9df56994524cb0593333bcee523 [formerly 1d93c1feabe72a4a9aada343efc10c07c78ac9bb] [formerly c3e89132287e13b2a9f1434ef48462e198cf6da8 [formerly 03102ff66a2e24b7ea04518d256025560a163f74]]
    Former-commit-id: a6b35ae66b0137698d532f384a0a26d9ca824d4c [formerly 963d471de35798f8e9b07862d2e8d6163586993c]
    Former-commit-id: 287fad3f247939386d0de007c093c59f54efc688
    Former-commit-id: 4ec8edcdad849d4e3a505b256ec36a0692e8975d

---
+++ cookbooks/wt_search/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Webtrends"
+maintainer_email "kendrick.martin@webtrends.com"
+license          "All rights reserved"
+description      "Installs/Configures Webtrends Search Service"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
