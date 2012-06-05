1c5d8bd1a8cf435567f42d57748b7bad9ed58390
Kendrick Martin <kendrick.martin@webtrends.com>
2012-06-05 22:52:04 +0000

    Correcting template usage in search. Adding uninstall recipe.
    Former-commit-id: 56ef43e34a8729e8f31fc0dcf1654456d451d7fd [formerly be12ddd6fe390ade8406f8574701bf35f282d0a8] [formerly c42eaeb68e0d9e5e4fefdb5427c90a5d4e3ea400 [formerly b3f205954ee0b67f727ed0e9b990c3a56bfc7a6b]]
    Former-commit-id: b557577e697d7f4dccfa9ce0f81ffc48a8742c20 [formerly 387e99b064f9f84b6fcc9f9533971823f514f7e2]
    Former-commit-id: 3775de98593c7db543557f6d1cac769e6b1281b3

--- cookbooks/wt_search/attributes/default.rb
+++ cookbooks/wt_search/attributes/default.rb
@@ -9,5 +9,5 @@
 default['wt_search']['artifact'] = "Search.zip"
 #default['wt_search']['build_url'] = "http://teamcity.webtrends.corp/guestAuth/repository/download/bt67/.lastSuccessful/"
 default['wt_search']['service_name'] = "Webtrends Search Service"
-default['wt_search']['install_dir'] = "\\Search"
-#default['wt_search']['log_dir'] = "\\logs"
+default['wt_search']['install_dir'] = "\\Search\\bin"
+default['wt_search']['log_dir'] = "\\logs"
