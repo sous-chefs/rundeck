747a4a323cb8183f2faee7456f280b100265fd4e
Kendrick Martin <kendrick.martin@webtrends.com>
2012-06-05 21:34:57 +0000

    Initial commit of Search service cookbook.
    Former-commit-id: 81c903b1892147280dfb658d34d5c34aae179a03 [formerly 8aa73158ed24b5532041370f26ecbdffd8a51e59] [formerly 936e7f733af3688d8e68e673d061bc27d66e1df1 [formerly 03102ff66a2e24b7ea04518d256025560a163f74 [formerly d6bd133ac5f741377a315f285a11b94e828ab112]]]
    Former-commit-id: 0f29d43bd467bd2bbb8fd7a4d7602ea880d7ad9d [formerly 3dfd6346d85913ed33c0924ec3708cdedffd8b59]
    Former-commit-id: 931eb00b6b1cfb7419c875488103b4746a05c6e7
    Former-commit-id: 15cdc9325b1f3feb4f054760439ba99af3f28aad

---
+++ cookbooks/wt_search/attributes/default.rb
@@ -0,0 +1,13 @@
+#
+# Author:: Kendrick Martin(<kendrick.martin@webtrends.com>)
+# Cookbook Name:: wt_search
+# Attribute:: default
+#
+# Copyright 2012, Webtrends Inc.
+#
+
+default['wt_search']['artifact'] = "Search.zip"
+#default['wt_search']['build_url'] = "http://teamcity.webtrends.corp/guestAuth/repository/download/bt67/.lastSuccessful/"
+default['wt_search']['service_name'] = "Webtrends Search Service"
+default['wt_search']['install_dir'] = "\\Search"
+#default['wt_search']['log_dir'] = "\\logs"
