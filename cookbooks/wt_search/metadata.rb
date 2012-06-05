d5ca5f406e3e350187de804fe8fe27555bfa8546
Kendrick Martin <kendrick.martin@webtrends.com>
2012-06-05 22:59:44 +0000

    Fixed deployment directory.

--- cookbooks/wt_search/metadata.rb
+++ cookbooks/wt_search/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "kendrick.martin@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures Webtrends Search Service"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.9"
+version          "0.0.11"
 depends			 "wt_base"
 depends			 "powershell"
