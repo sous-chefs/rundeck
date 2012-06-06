a60ba5d95e964b9288bf6078459f89cd921a20d3
Kendrick Martin <kendrick.martin@webtrends.com>
2012-06-06 17:38:54 +0000

    Moving properties from hardcoding. Fixing uninstall recipe.

--- cookbooks/wt_search/attributes/default.rb
+++ cookbooks/wt_search/attributes/default.rb
@@ -7,7 +7,8 @@
 #
 
 default['wt_search']['artifact'] = "Search.zip"
-#default['wt_search']['build_url'] = "http://teamcity.webtrends.corp/guestAuth/repository/download/bt67/.lastSuccessful/"
+default['wt_search']['tc_proj'] = "Search"
 default['wt_search']['service_name'] = "Webtrends Search Service"
+default['wt_search']['service_binary'] = "Webtrends.Search.Service.exe"
 default['wt_search']['install_dir'] = "\\Search\\bin"
 default['wt_search']['log_dir'] = "\\Search\\logs"
