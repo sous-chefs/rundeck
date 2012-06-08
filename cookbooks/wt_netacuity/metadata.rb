e83437ff69a3ecc6b6950bf5997d9849ed7d1a78
Tim Smith <tim.smith@webtrends.com>
2012-06-07 18:25:51 -0700

    Change to the gating method for NetAcuity
    Former-commit-id: 6621e1c2aeee54840c9d985abadcff700b8700df [formerly e2fe75b489a905eef2bec89b89345253ff3c69b4] [formerly ea0e2eb2b838f9e22260d0d11ec971c89ab051fd [formerly dfacbffae5195b06126b83f58c8a1066a2109c32 [formerly 6f90c6261205c4b359a67677be5509e93a42f626]]]
    Former-commit-id: cae24fb4a25388c983fe49347522a46e63180198 [formerly 7f6923bb311430f9142ff4a36e82c28d22a11c74]
    Former-commit-id: 2bdf1459f681197f429703c5bbd6e1e24da93468
    Former-commit-id: d3a779fa7e0d810142d2e2d0ba7a4d595e1e0535

--- cookbooks/wt_netacuity/metadata.rb
+++ cookbooks/wt_netacuity/metadata.rb
@@ -3,5 +3,5 @@ maintainer_email  "tim.smith@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures NetAcuity with a Webtrends license"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.0"
+version           "1.0.1"
 depends           "java"
