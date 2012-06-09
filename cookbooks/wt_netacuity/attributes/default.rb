1c45c7ba01d9fb749ec1f5c55b7576c4ebbe5d8a
Tim Smith <tim.smith@webtrends.com>
2012-06-09 10:58:39 -0700

    Set the default NetAcuity version to 5.0.  We originally pinned to 4.6 in Optimize since we had never tested with 5.0.  5.0 support ipv6, which we'll want at some point.  We should start out using this version
    Former-commit-id: 1a7b77f4828129800ee1eb1ce52b70291cd8ed73 [formerly 3192384b1207f127d3a515dc5c0127a0d858ffe1] [formerly 661087963958e3c4e7240083b3ba06b87dafce64 [formerly d3e7eccd4c128aa2f29a10649d18435182dd523f]]
    Former-commit-id: ceff75ab27d0ce2cf94a5ea15bc60b8516d1df35 [formerly f27182597430dc7817a6f64e1db70509c02a11cb]
    Former-commit-id: 54f328531e90a01ea07957d0170b0aa328d48a22

--- cookbooks/wt_netacuity/attributes/default.rb
+++ cookbooks/wt_netacuity/attributes/default.rb
@@ -1,4 +1,4 @@
-default['wt_netacuity']['version'] = "4.6"
+default['wt_netacuity']['version'] = "5.0"
 default['wt_netacuity']['download_url'] = ""
 default['wt_netacuity']['install_dir'] = "/opt/netacuity"
 default['wt_netacuity']['use_proxy'] = false
