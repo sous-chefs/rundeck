ac6b3d69ffffae6778656c4fedbde2d70c914976
vonnagy <ivan.vonnagy@webtrends.com>
2012-06-12 10:40:46 -0700

    Added support for deploying config and directory changes without having to nuke the existing deploy.
    Former-commit-id: 38d664b49f6452ebeb4fc3e03fe4cd1cff4cf071 [formerly 60afc16b7c12c0cd13e82801d165e0967a569994] [formerly c9185e5afd43c1037936b15485b700093b2a7d97 [formerly 2026522736000ac8c2eb1177f1146780b4d2faa6]]
    Former-commit-id: 1868c88308ce23cf84316f1888967e25e965ba70 [formerly bccb7ffce5eefb5b3dce84b307012587f810e5c3]
    Former-commit-id: 8480ff646d224813918b9c98a7a2a5bca1c5f468

--- cookbooks/wt_streaminglogreplayer/recipes/default.rb
+++ cookbooks/wt_streaminglogreplayer/recipes/default.rb
@@ -14,59 +14,108 @@ include_recipe "runit"
 package "nfs-common"
 
 log "Deploy build is #{ENV["deploy_build"]}"
-if ENV["deploy_build"] != "true" then 
-    log "The deploy_build value is not set or is false so exit here"
-else
+if ENV["deploy_build"] == "true" then 
     log "The deploy_build value is true so un-deploy first"
     include_recipe "wt_streaminglogreplayer::undeploy"
+else
+    log "The deploy_build value is not set or is false so we will only update the configuration"
+end
 
-    log_dir      = File.join("#{node['wt_common']['log_dir_linux']}", "streaminglogreplayer")
-    install_dir  = File.join("#{node['wt_common']['install_dir_linux']}", "streaminglogreplayer")
+log_dir      = File.join("#{node['wt_common']['log_dir_linux']}", "streaminglogreplayer")
+install_dir  = File.join("#{node['wt_common']['install_dir_linux']}", "streaminglogreplayer")
 
-    java_home    = node['java']['java_home']
-    download_url = node['wt_streaminglogreplayer']['download_url']
-    user = node['wt_streaminglogreplayer']['user']
-    group = node['wt_streaminglogreplayer']['group']
-    java_opts = node['wt_streaminglogreplayer']['java_opts']
-    tarball = "streaminglogreplayer-bin.tar.gz"
-    zookeeper_port = node['zookeeper']['clientPort']
+java_home    = node['java']['java_home']
+download_url = node['wt_streaminglogreplayer']['download_url']
+user = node['wt_streaminglogreplayer']['user']
+group = node['wt_streaminglogreplayer']['group']
+java_opts = node['wt_streaminglogreplayer']['java_opts']
+tarball = "streaminglogreplayer-bin.tar.gz"
+zookeeper_port = node['zookeeper']['clientPort']
 
-    # get the correct environment for the zookeeper nodes
-    zookeeper_env = "#{node.chef_environment}"
-    unless node[:wt_streaminglogreplayer][:zookeeper_env].nil? || node[:wt_streaminglogreplayer][:zookeeper_env].empty?
-        zookeeper_env = node['wt_streaminglogreplayer']['zookeeper_env']
-    end
+log "Install dir: #{install_dir}"
+log "Log dir: #{log_dir}"
+log "Java home: #{java_home}"
 
-    log "Install dir: #{install_dir}"
-    log "Log dir: #{log_dir}"
-    log "Java home: #{java_home}"
+# create the log directory
+directory "#{log_dir}" do
+owner   user
+group   group
+mode    00755
+recursive true
+action :create
+end
 
-    # create the log directory
-    directory "#{log_dir}" do
+# create the bin directory
+directory "#{install_dir}/bin" do
+owner "root"
+group "root"
+mode 00755
+recursive true
+action :create
+end
+
+# create the conf directory
+directory "#{install_dir}/conf" do
+owner "root"
+group "root"
+mode 00755
+recursive true
+action :create
+end
+
+# create the share mount dir
+directory "#{node['wt_streaminglogreplayer']['share_mount_dir']}" do
+    action :create
     owner   user
     group   group
     mode    00755
-    recursive true
-    action :create
-    end
+end
 
-    # create the bin directory
-    directory "#{install_dir}/bin" do
-    owner "root"
-    group "root"
-    mode 00755
-    recursive true
-    action :create
-    end
+# mount the NFS mount and add to /etc/fstab
+mount "#{node['wt_streaminglogreplayer']['share_mount_dir']}" do
+    device "#{node['wt_streaminglogreplayer']['log_share_mount']}"
+    fstype "nfs"
+    options "rw"
+    action [:mount, :enable]
+end
 
-    # create the conf directory
-    directory "#{install_dir}/conf" do
-    owner "root"
-    group "root"
-    mode 00755
-    recursive true
-    action :create
+# get the correct environment for the zookeeper nodes
+zookeeper_env = "#{node.chef_environment}"
+unless node[:wt_streaminglogreplayer][:zookeeper_env].nil? || node[:wt_streaminglogreplayer][:zookeeper_env].empty?
+    zookeeper_env = node['wt_streaminglogreplayer']['zookeeper_env']
+end
+
+# grab the zookeeper nodes that are currently available
+zookeeper_pairs = Array.new
+if not Chef::Config.solo
+    search(:node, "role:zookeeper AND chef_environment:#{zookeeper_env}").each do |n|
+        zookeeper_pairs << n[:fqdn]
     end
+end
+
+# append the zookeeper client port (defaults to 2181)
+i = 0
+while i < zookeeper_pairs.size do
+zookeeper_pairs[i] = zookeeper_pairs[i].concat(":#{zookeeper_port}")
+i += 1
+end
+
+%w[monitoring.properties producer.properties logconverter.properties].each do |template_file|
+template "#{install_dir}/conf/#{template_file}" do
+        source	"#{template_file}.erb"
+        owner user
+        group group
+        mode  00755
+        variables({ 
+            :wt_streaminglogreplayer => node[:wt_streaminglogreplayer],
+            :zookeeper_pairs => zookeeper_pairs,
+            :wt_monitoring => node[:wt_monitoring]
+        })
+    end
+end
+
+if ENV["deploy_build"] == "true" then 
+    log "The deploy_build value is true so we will grab the tar ball and install"
 
     # download the application tarball
     remote_file "#{Chef::Config[:file_cache_path]}/#{tarball}" do
@@ -74,7 +123,7 @@ else
     mode 00644
     end
 
-    # uncompress the application tarbarll into the install dir
+    # uncompress the application tarball into the install dir
     execute "tar" do
     user  "root"
     group "root" 
@@ -83,48 +132,19 @@ else
     end
 
     template "#{install_dir}/bin/service-control" do
-    source  "service-control.erb"
-    owner "root"
-    group "root"
-    mode  00755
-    variables({
-        :log_dir => log_dir,
-        :install_dir => install_dir,
-        :java_home => java_home,
-        :user => user,
-        :java_class => "com.webtrends.streaming.LogReplayer",
-        :java_jmx_port => node['wt_monitoring']['jmx_port'],
-        :java_opts => java_opts
-    })
-    end
-
-    # grab the zookeeper nodes that are currently available
-    zookeeper_pairs = Array.new
-    if not Chef::Config.solo
-        search(:node, "role:zookeeper AND chef_environment:#{zookeeper_env}").each do |n|
-            zookeeper_pairs << n[:fqdn]
-        end
-    end
-
-    # append the zookeeper client port (defaults to 2181)
-    i = 0
-    while i < zookeeper_pairs.size do
-    zookeeper_pairs[i] = zookeeper_pairs[i].concat(":#{zookeeper_port}")
-    i += 1
-    end
-
-    %w[monitoring.properties producer.properties logconverter.properties].each do |template_file|
-    template "#{install_dir}/conf/#{template_file}" do
-            source	"#{template_file}.erb"
-            owner user
-            group group
-            mode  00755
-            variables({ 
-                :wt_streaminglogreplayer => node[:wt_streaminglogreplayer],
-                :zookeeper_pairs => zookeeper_pairs,
-                :wt_monitoring => node[:wt_monitoring]
-            })
-        end
+        source  "service-control.erb"
+        owner "root"
+        group "root"
+        mode  00755
+        variables({
+            :log_dir => log_dir,
+            :install_dir => install_dir,
+            :java_home => java_home,
+            :user => user,
+            :java_class => "com.webtrends.streaming.LogReplayer",
+            :java_jmx_port => node['wt_monitoring']['jmx_port'],
+            :java_opts => java_opts
+        })
     end
 
     # delete the application tarball
@@ -133,22 +153,6 @@ else
         group "root"
         command "rm -f #{Chef::Config[:file_cache_path]}/#{tarball}"
         action :run
-    end
-
-    # create the share mount dir
-    directory "#{node['wt_streaminglogreplayer']['share_mount_dir']}" do
-      action :create
-      owner   user
-      group   group
-      mode    00755
-    end
-
-    # mount the NFS mount and add to /etc/fstab
-    mount "#{node['wt_streaminglogreplayer']['share_mount_dir']}" do
-      device "#{node['wt_streaminglogreplayer']['log_share_mount']}"
-      fstype "nfs"
-      options "rw"
-      action [:mount, :enable]
     end
 
     # create a runit service
