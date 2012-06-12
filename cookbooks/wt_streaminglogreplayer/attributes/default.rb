6f3bb8618bb42b9b5cb04e88e3e7196f979caa0c
vonnagy <ivan.vonnagy@webtrends.com>
2012-06-12 10:40:46 -0700

    Added support for deploying config and directory changes without having to nuke the existing deploy.
    Former-commit-id: 38d664b49f6452ebeb4fc3e03fe4cd1cff4cf071 [formerly 60afc16b7c12c0cd13e82801d165e0967a569994] [formerly c9185e5afd43c1037936b15485b700093b2a7d97 [formerly c0a1d935be5cc65d65e6bc790192e27de5a5ac9c [formerly 2026522736000ac8c2eb1177f1146780b4d2faa6]]]
    Former-commit-id: 1868c88308ce23cf84316f1888967e25e965ba70 [formerly bccb7ffce5eefb5b3dce84b307012587f810e5c3]
    Former-commit-id: 8480ff646d224813918b9c98a7a2a5bca1c5f468
    Former-commit-id: ac6b3d69ffffae6778656c4fedbde2d70c914976

--- cookbooks/wt_streaminglogreplayer/attributes/default.rb
+++ cookbooks/wt_streaminglogreplayer/attributes/default.rb
@@ -2,12 +2,12 @@ default['wt_streaminglogreplayer']['user']         = "webtrends"
 default['wt_streaminglogreplayer']['group']        = "webtrends"
 default['wt_streaminglogreplayer']['download_url'] = "http://teamcity.webtrends.corp/guestAuth/repository/download/bt127/.lastSuccessful/streaminglogreplayer-bin.tar.gz"
  
-default['wt_streaminglogreplayer']['dcsid_whitelist'] = ""
-default['wt_streaminglogreplayer']['log_extension']   = ".gz"
-default['wt_streaminglogreplayer']['log_share_mount'] = ""
-default['wt_streaminglogreplayer']['zookeeper_env'] = ""
-default['wt_streaminglogreplayer']['share_mount_dir'] = "/srv/logsharedir"
-default['wt_streaminglogreplayer']['kafka_topic']     = "rawHits"
-default['wt_streaminglogreplayer']['delete_logs']     = "true"
-default['wt_streaminglogreplayer']['java_opts']       = "-Xms1024m -Djava.net.preferIPv4Stack=true"
-
+default['wt_streaminglogreplayer']['dcsid_whitelist']   = ""
+default['wt_streaminglogreplayer']['log_extension']     = ".gz"
+default['wt_streaminglogreplayer']['log_share_mount']   = ""
+default['wt_streaminglogreplayer']['zookeeper_env']     = nil
+default['wt_streaminglogreplayer']['share_mount_dir']   = "/srv/logsharedir"
+default['wt_streaminglogreplayer']['kafka_topic']       = "rawHits"
+default['wt_streaminglogreplayer']['delete_logs']       = "true"
+default['wt_streaminglogreplayer']['java_opts']         = "-Xms1024m -Djava.net.preferIPv4Stack=true"
+default['wt_streaminglogreplayer']['kafka_broker_list'] = nil
