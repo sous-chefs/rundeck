f6b79444a987ad46b1bd302a9ea485c058c3f6a7
Tim Smith <tim.smith@webtrends.com>
2012-06-12 19:23:57 -0700

    Set default version back to 4.6 and use the correct version string format
    Former-commit-id: ff68fc6658c887b00d5d6b9b18762fbcbb2edbba [formerly 556cc1ac345b791c2ee564f2f838eac652f5347c] [formerly ebecb4d2977827ab30c31a8ad548ff1f5adcbd8a [formerly cfc9155379fc6d39d4a81b3cccb081a91def8acc [formerly 5fb71dbeedc026ecb38fe6cdbdf70f2070fa66fe]]]
    Former-commit-id: 5b6b747baf85319eb6db60e58c4d0667ae6170ab [formerly 97e40ffa75b151bde29d2549a6b03e22a5203126]
    Former-commit-id: 998067cd2189eac9e80915af1070f896cdf0eb6d
    Former-commit-id: 9283c8f2c0c1350c276c8e292b506877d36b22e4

--- cookbooks/wt_netacuity/attributes/default.rb
+++ cookbooks/wt_netacuity/attributes/default.rb
@@ -1,4 +1,4 @@
-default['wt_netacuity']['version'] = "5.0"
+default['wt_netacuity']['version'] = "460"
 default['wt_netacuity']['download_url'] = ""
 default['wt_netacuity']['install_dir'] = "/opt/netacuity"
 default['wt_netacuity']['use_proxy'] = false
