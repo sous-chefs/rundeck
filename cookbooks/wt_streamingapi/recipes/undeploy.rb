dbf6b42e51024ae2129c34ae1e20f3e68f4b9dd3
vonnagy <ivan.vonnagy@webtrends.com>
2012-06-12 18:09:57 -0700

    Added support for deploying changes to attributes that write to templates without having to re-deploy all the bits.
    Former-commit-id: 2fbf78f0a3653de8962adce98a2962a0af61386d [formerly 134b51e57fbf5452c04dce1701a1fcbc7bcd5ce6] [formerly a8f10c6ef85791b3524c4e02d8822ff18c7ed1ae] [formerly dacef44607e7799718592ebab4253baf03a53365 [formerly d7c0eefa454bae87bebb5a3949a1c397e44b1ea6]] [formerly 14d57664951c05840c63e84374a10391bbc615e8 [formerly 79ae9beaecf996c2719eff45d9e7228f4db89826] [formerly b3793e46f7c38ac4d475f1461dbceda8e94157b6 [formerly fbf05b09292f9fe3255dc24d2e147833f99c7973 [formerly f361ec21eba67fd130fb1938170c8601a4f36d4e]]]]
    Former-commit-id: ca680b87612f8f98fd68021743dda7d9a14ceb08 [formerly 8cf9b54fb563f6cbd66a69e6658b666ae71efccd] [formerly d75efd45f146f2e9eb14be46e421d0fda89d526d [formerly fbf05b09292f9fe3255dc24d2e147833f99c7973]]
    Former-commit-id: f7ca08ce2f3f776a8ce50f3ba9fa5070b95ea1f8 [formerly 60e7bc341210f0a63b5fdc13a1de523066c4f60c]
    Former-commit-id: d46a6b3ae5e54332d961ba266c15a124d452a8ec
    Former-commit-id: 78b3a841468c95bf725b5d3b502da73b4aef8275

--- cookbooks/wt_streamingapi/recipes/undeploy.rb
+++ cookbooks/wt_streamingapi/recipes/undeploy.rb
@@ -13,10 +13,20 @@ install_dir  = "#{node['wt_common']['install_dir_linux']}/streamingapi"
 
 runit_service "streamingapi" do
     action :disable
+    run_restart false
 end 
 
+# try to stop the service, but allow a failure without printing the error
 service "streamingapi" do
   action [:stop, :disable]
+  ignore_failure true
+end
+
+# force stop the service in case the stop failed
+service "streamingapi" do
+  action [:stop]
+  stop_command "force-stop"
+  ignore_failure true
 end
 
 directory "#{log_dir}" do
