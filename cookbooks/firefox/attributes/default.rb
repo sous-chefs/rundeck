c80d58e59be03084a574bdd069e76c0e26e59b3b
Tim Smith <tim.smith@webtrends.com>
2012-06-16 16:07:56 -0700

    Update Firefox cookbook to install 13.0 not 11.0
    Former-commit-id: 3ea584b3d8a148da2d1d964c7e4a845379defae2 [formerly 1639ca3c63775c600008882c4dcb28ab5cee0f6e] [formerly a31bf0b0f87676e8494ed876beba8dd6f1dcdb8b [formerly 2fef5083ab5fb030e85b82a658e310ee27077511]]
    Former-commit-id: 2b5de04eb87643ff99ff1fee797a06580b1ccdb0 [formerly f4c0acc32fa49faf3d318bf53a59aebc8c542197]
    Former-commit-id: 6ccb21db9c31f4bf39d14a90e75038d8f4f9441f

--- cookbooks/firefox/attributes/default.rb
+++ cookbooks/firefox/attributes/default.rb
@@ -18,4 +18,4 @@
 # limitations under the License.
 #
 
-default['firefox']['http_url'] = "http://mozilla.cdn.leaseweb.com/firefox/releases/11.0/win32/en-US/Firefox%20Setup%2011.0.exe"
+default['firefox']['http_url'] = "http://mozilla.cdn.leaseweb.com/firefox/releases/13.0/win32/en-US/Firefox%20Setup%2013.0.exe"
