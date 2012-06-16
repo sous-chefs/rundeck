d0765d483a143d120ea7560a44d148022771d825
Tim Smith <tim.smith@webtrends.com>
2012-06-16 16:45:30 -0700

    remove wt_dbmaint and wt_indexmaint.  These aren't being done with Chef now.
    Former-commit-id: 0a61d9d53f32b14dac1fcb3bad53f35ede9e6e87 [formerly ae05bc44e3f2852bd4bee2879527aedbddae405e] [formerly b7813173ef2209d26112e6d5f374208478cdac55 [formerly 1fb1a9675979ab173ecdb9aa1efacc0a27528632]]
    Former-commit-id: 4ed43b5f597fa3ae72c8615014ad73511edfd991 [formerly 4fb8abfcc053bb4d0e7a72cef6ed7907523772c2]
    Former-commit-id: eb1ede4c33424a149e4cfd59a4d1ba3f8ef94d9c

--- cookbooks/wt_dbmaint/metadata.rb
+++
@@ -1,9 +0,0 @@
-name 'wt_platformscheduler'
-maintainer       "Kendrick"
-maintainer_email "kendrick.martin@webtrends.com"
-license          "All rights reserved"
-description      "Installs/Configures Platform Scheduler"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.10.5"
-depends          "wt_base"
-depends          "windows"
