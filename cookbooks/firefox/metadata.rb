2fef5083ab5fb030e85b82a658e310ee27077511
Tim Smith <tim.smith@webtrends.com>
2012-06-16 16:07:56 -0700

    Update Firefox cookbook to install 13.0 not 11.0

--- cookbooks/firefox/metadata.rb
+++ cookbooks/firefox/metadata.rb
@@ -4,6 +4,6 @@ license           "Apache 2.0"
 license          "All rights reserved"
 description      "Installs/Configures Firefox 10.0"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.3"
+version          "0.0.4"
 supports         "windows"
 depends          "windows"
