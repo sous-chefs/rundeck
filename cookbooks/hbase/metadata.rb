bb28f823f8e6219e1ff0315088d2668619a9476f
Tim Smith <tim.smith@webtrends.com>
2012-06-16 20:09:42 -0700

    Minor hbase cleanup and use install_dir from hadoop not install_stage_dir
    Former-commit-id: 88a4310bd3ed6c669424e7c68f980a4f5d05ffb5 [formerly 38198dc0107f602f93d55b60522987a4f6c4b6fc] [formerly 29b9d88815139f3b7be1b7b98a09f54c7a7382bd] [formerly 8089ffa553d7e1cbfd1b453c74d124bd8f83f58a [formerly a05dd384a91fb4992a0b8a3d01e55ca585ff0b4b]] [formerly 9cf9d0145a802a7b285799d9361aa1822da21133 [formerly 637b7c787c5066212dbc8b4b7139de32f21c24bf] [formerly a890da280e3e3c0a4daec7580c360d2b2f9d61ad [formerly 9454e9203cb11855d5ab908dac69b39871598550 [formerly ee416401a85a34d7df734ece0623cff800d8cb32]]]]
    Former-commit-id: 9cae677d560b0063c651f583d49190c512f24678 [formerly 1fb917306439f4a4738a038b7b215f9c09a43e79] [formerly f72c6457152ef5aaf760db9512d8a8f079ae1726 [formerly 9454e9203cb11855d5ab908dac69b39871598550]]
    Former-commit-id: a548333345c864e17d7cea5f4b5a9c2017514d14 [formerly dd48afdfaf779cc23fe6b3989fe460ec6ab687fa]
    Former-commit-id: 860194b2a6f0dd932e95051088890ab1a02d69e5
    Former-commit-id: bbe20a6880a934441e05eebf89189a19c6f4a33e

--- cookbooks/hbase/metadata.rb
+++ cookbooks/hbase/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "sean.mcnamara@webtrends.com"
 license           "Apache 2.0"
 description       "Installs hbase 0.92.0"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version           "1.0.2"
+version           "1.0.3"
 depends           "java"
 depends           "hadoop"
 
