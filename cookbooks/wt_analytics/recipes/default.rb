85985ab9cc71c5aec53acaa57af34c20fccf153e
Kendrick Martin <kendrick.martin@webtrends.com>
2012-06-18 17:54:16 +0000

    Adding recipe for install analytics.
    Former-commit-id: a5398244abd1fbcb62309e1e18182ce6a10068ca [formerly b785e1877b3842adfdacdbd2e279dc0824c92565] [formerly 4eee99c1f88b92747a404fb0756045fb105a2b4a [formerly 9f64f8860a4304a53f04b1c62ca69dfe19421561]]
    Former-commit-id: 08a62baf8561824164215c6bf990e20d1db238df [formerly a0a9961d663a5de6603ca1de6861e761b3485538]
    Former-commit-id: 6321e85ff02bd79fa4e794a0370a76150e20f9e7

--- cookbooks/wt_analytics/recipes/default.rb
+++ cookbooks/wt_analytics/recipes/default.rb
@@ -6,3 +6,63 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+if deploy_mode? 
+  include_recipe "wt_analytics::uninstall" 
+  include_recipe "ms_dotnet4::resetiis"
+  # source build
+	build_data = data_bag_item('wt_builds', node.chef_environment)
+	build_id = build_data[node['wt_search']['tc_proj'] ]
+	base_url = 'http://teamcity.webtrends.corp/guestAuth/app/rest/builds/' + build_id
+
+	response = RestClient.get base_url
+	btID = nil
+	build_doc = REXML::Document.new(response.body)
+	build_doc.elements.each('//buildType') do |type|
+		btID = type.attributes["id"]
+	end
+	install_url = "http://teamcity.webtrends.corp/guestAuth/repository/download/#{btID}/#{build_id}:id/#{node['wt_analytics']['artifact']}"
+	log install_url
+end
+
+#Properties
+install_dir = node['wt_common']['install_dir_windows']node['wt_analytics']['install_dir']
+install_logdir = node['wt_common']['install_log_dir_windows']
+pod = node.chef_environment
+user_data = data_bag_item('authorization', pod)
+ui_user = user_data['wt_common']['ui_user']
+ui_password = user_data['wt_common']['ui_pass']
+
+directory install_logdir do
+	action :create
+end
+
+iis_site 'Default Web Site' do
+	action [:stop, :delete]
+end
+
+iis_site 'Analytics' do
+	protocol :http
+    port 80
+    path "#{node['wt_common']['install_dir_windows']}\\Insight"
+	action [:add,:start]
+end
+
+wt_base_firewall 'A10WS' do
+	protocol "TCP"
+	port 80
+	action [:open_port]
+end
+
+if deploy_mode?
+	windows_zipfile install_dir do
+		source install_url
+		action :unzip
+	end
+	
+	iis_pool "Webtrends_Analytics" do
+  	  pipeline_mode :Integrated
+  	  runtime_version "4.0"
+      action [:add, :config]
+    end
+end
