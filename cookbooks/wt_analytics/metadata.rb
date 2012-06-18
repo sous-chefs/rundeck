1191c908cf84e4f52fc69d9bdc55abebd657ce5c
martink <kendrick.martin@webtrends.com>
2012-06-18 10:37:57 -0700

    Initial checkin of wt_analytics.
    Former-commit-id: 06af3ab31505c74ad32f984633b1f75fbf84d0c1 [formerly 08ccb577f2cd2a34b172f05beb48b90cddbac15e] [formerly b4cdc0a5d790ede160931a46bf1e62ec2b2ed3ee] [formerly 71837a230a150e2ab18466cb5764f359944e0d75 [formerly 8d53e50dab2af5fd8862907cecde0964b05adbd1]] [formerly e71805e4664f52a2ccf9b3503b08cebe23118ed5 [formerly 7b8dcc7de66c788f667af342a10b021f0e422154] [formerly eb7bb7a1c9aa5abc4eaed1a9e60ef5e9f9587fae [formerly a652d62d558bc0577d689f616a8bbe6409e302d5 [formerly e72cc19b6713487ca03057e9088fe442e0efcb2c]]]]
    Former-commit-id: 6f001ae075029fc08fbcf5d0754518cbcae6c6f2 [formerly 0c8c87de37b041cc9665e305dc0f83f9fbca453b] [formerly e7b40f56559b1988f6dab8d74e968510b167d707 [formerly a652d62d558bc0577d689f616a8bbe6409e302d5]]
    Former-commit-id: 836b0de373b8cd909244524a1cf43fb444124be6 [formerly 06211a05a0ed6e7aafae64eb2f78ff30452d79b3]
    Former-commit-id: a782d810868b8bce99f1a48cea4fcc6e9376a20c
    Former-commit-id: 58d10039ebb8e274813fcd933877b173c92dbeab

---
+++ cookbooks/wt_analytics/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Webtrends"
+maintainer_email "kendrick.martin@webtrends.com"
+license          "All rights reserved"
+description      "Installs and configures A10"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+depends			 "iis"
+depends 		 "windows"
+supports		 "windows"
