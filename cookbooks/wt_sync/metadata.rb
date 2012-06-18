f49b9e74e8e0ea4054b8b31e406990a815f39920
martink <kendrick.martin@webtrends.com>
2012-06-18 10:12:10 -0700

    Adding corrected icacls for sync.

--- cookbooks/wt_sync/metadata.rb
+++ cookbooks/wt_sync/metadata.rb
@@ -1,8 +1,8 @@
 maintainer       "Webtrends"
 maintainer_email "kendrick.martin@webtrends.com"
 license          "All rights reserved"
-description      "Installs/Configures Webtrends Search Service"
+description      "Installs/Configures Webtrends Sync Service"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.16"
+version          "0.0.18"
 depends			 "wt_base"
 depends			 "powershell"
