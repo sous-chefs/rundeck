96decb743d3b2a6526b6fe3abff8f7328207dbda
martink <kendrick.martin@webtrends.com>
2012-06-20 10:11:59 -0700

    Fixing version numbers to match what chef server uses.
    Former-commit-id: f6a82cdb064873a68a25f98a47b37e83e8e95ab2 [formerly 3a6dff42e208e291fe7ec3cac1061f796f3a5731] [formerly c760fa9eae8a3d78e2bd19d94fc278a1a950061a] [formerly e566991551aac7f024f92d15e713010c76fe90d5 [formerly fe9ac33af9ec83120301f0dc07d8206f02b87bc4]] [formerly 345832ca276d838020edfcf626c44633f60b37d3 [formerly 1b3a912155d2970058ee88c82bf67389036c49ad] [formerly 06d31096747231d6a5ebe3599a6186530e1bee5e [formerly 0e503322e24db600752cf87febd30f7ae4cd73b8 [formerly 468a642578057389945ce615335d41cf85bfeb11]]]]
    Former-commit-id: f1df95fb74c67f377c4a44707e6a39d142539e6b [formerly 4132ea42bc2ce2a7f6423ba1c0eb13d55c883447] [formerly 11e88cf4a275240a779d3ea7b31f0d4c9ab80810 [formerly 0e503322e24db600752cf87febd30f7ae4cd73b8]]
    Former-commit-id: 256b65107d3553c9e73b143ba2f10c3c17f3d8cd [formerly 24344262dacb49015824c27ce6d5a06149363804]
    Former-commit-id: a2b8b96942f72e5541440651e7fb42a20c36c6c2
    Former-commit-id: cb5b62d242c1f49e2164d3d74a70de8ca2fe7c85

--- cookbooks/wt_viracocha/metadata.rb
+++ cookbooks/wt_viracocha/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "hostedops@webtrends.com"
 license          "Apache 2.0"
 description      "Installs/Configures buildvm"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0"
+version          "1.0.0"
 depends          "rbvmomi"
 depends          "ruby"
 depends          "build-essential"
