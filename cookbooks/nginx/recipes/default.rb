a2232a8bbaa2fc7be6c564c05f93121f2c5149eb
Tim Smith <tim.smith@webtrends.com>
2012-06-21 15:34:04 +0000

    Import nginx version 0.101.6
    Former-commit-id: 9b5346652bbe09d0c9f8586687b5d46d932ffbb2 [formerly 5f5cff0b4083ce8d80dfe7be2d37416207372999] [formerly 9422b2d581796b2e32899a73f1c21a5d37201764 [formerly 931131a600d309fcc96f677f4cb8d6efb778f8a8 [formerly 8a817cbfc01df8b0c991d3eb9254006601324cb1]]]
    Former-commit-id: 23e470a41704ca1264e426cb72023b880f1271bf [formerly e6d12363729de501e9900d976201d352920b3bdd]
    Former-commit-id: e09bc5e0a2f8f3397f770202071f6832ef2f77f6
    Former-commit-id: ee9a7e8062ea5f65768f2903b9d7f28742289e20

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -18,47 +18,25 @@
 # limitations under the License.
 #
 
-include_recipe "nginx::ohai_plugin"
+include_recipe 'nginx::ohai_plugin'
 
-package "nginx"
-
-service "nginx" do
-  supports :status => true, :restart => true, :reload => true
-  action :enable
-end
-
-directory node[:nginx][:log_dir] do
-  mode 0755
-  owner node[:nginx][:user]
-  action :create
-end
-
-%w{nxensite nxdissite}.each do |nxscript|
-  template "/usr/sbin/#{nxscript}" do
-    source "#{nxscript}.erb"
-    mode 0755
-    owner "root"
-    group "root"
+case node['nginx']['install_method']
+when 'source'
+  include_recipe 'nginx::source'
+when 'package'
+  case node['platform']
+  when 'redhat','centos','scientific','amazon','oracle'
+    include_recipe 'yum::epel'
   end
+  package 'nginx'
+  service 'nginx' do
+    supports :status => true, :restart => true, :reload => true
+    action :enable
+  end
+  include_recipe 'nginx::commons'
 end
 
-template "nginx.conf" do
-  path "#{node[:nginx][:dir]}/nginx.conf"
-  source "nginx.conf.erb"
-  owner "root"
-  group "root"
-  mode 0644
-  notifies :reload, "service[nginx]"
-end
-
-template "#{node[:nginx][:dir]}/sites-available/default" do
-  source "default-site.erb"
-  owner "root"
-  group "root"
-  mode 0644
-  notifies :reload, "service[nginx]"
-end
-
-service "nginx" do
+service 'nginx' do
+  supports :status => true, :restart => true, :reload => true
   action :start
 end
