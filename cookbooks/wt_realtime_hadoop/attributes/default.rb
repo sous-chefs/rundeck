a8760d6bc43a29d98962e7da671a8d1edc330de1
reinvigorate <sean@reinvigorate.net>
2012-06-20 21:33:58 -0600

    forgot to add attributes file

---
+++ cookbooks/wt_realtime_hadoop/attributes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: zookeeper
+# Attributes:: default
+#
+# Copyright 2012, Webtrends Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+default[:wt_realltime_hadoop][:datacenter] = nil
+default[:wt_realltime_hadoop][:pod] = nil
+default[:wt_realltime_hadoop][:environment] = nil
