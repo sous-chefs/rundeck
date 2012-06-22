79987c97db10eee1145f1b1b130b2b883b21f4cf
Marek Dolgos <marek.dolgos@webtrends.com>
2012-06-22 14:23:34 -0700

    Added new topology parameter.
    Former-commit-id: 3349089dd9f83a3307db38d7a283b88355edc698 [formerly 2f60b71edd864f68d0fe150837b0b07b7dbb607b] [formerly 5b1c4dba86e58d8c6b4336f5da4223a0404bd4c7] [formerly 25a05e5b67c7905c4213deec3d12bd11caac77b3 [formerly 9180a474d85a95ab1c9c9eb6ec59cb52eace1082]] [formerly eb2146e7603e6373c9b46e037c42ab8958e04323 [formerly 07a936d940c46d55995cbfd59b54fd88c0b0695a] [formerly b1515a985f478ddcdad5f830e4131d35e26e3703 [formerly 9deb9f76043673ce6676a4bd86f85319dd2329b6 [formerly f0df8126a1bac5b7984803b89a34aceb957dacfb]]]]
    Former-commit-id: 50f0613bbbdac0ec9d52d7023fdced3f7f247f6d [formerly b6e03512eead444294de7791b409c290e6fa0dd3] [formerly f61e912ac43fc6a95b90861d68af6c7536aeb7a3 [formerly 9deb9f76043673ce6676a4bd86f85319dd2329b6]]
    Former-commit-id: 5093d5cf73a07a7c58dc0c77150145e4d55ae461 [formerly 79516e922d456aae0d4e70cfcb4758fed35bc00b]
    Former-commit-id: c722705132e2810973a2a50c1e71cb3809b69099
    Former-commit-id: e3dc5a4e59ecfe0f7f1debdc7f810384176d655d

--- cookbooks/wt_storm/recipes/realtime_deploy.rb
+++ cookbooks/wt_storm/recipes/realtime_deploy.rb
@@ -7,3 +7,28 @@
 # in the future this will pull builds from teamcity
 
 include_recipe "wt_storm"
+
+zk_quorum = search(:node, "role:zookeeper AND chef_environment:#{node['wt_realtime_hadoop']['environment']}") # Find zk quorum in the *declared* environment
+sapi = search(:node, "role:wt_streaming_api_server AND chef_environment:#{node.chef_environment}").first
+netacuity = search(:node, "role:wt_netacuity AND chef_environment:#{node.chef_environment}").first
+streaming_data_mover = search(:node, "role:streaming_data_mover AND chef_environment:#{node.chef_environment}").first
+
+template "#{node['storm']['install_dir']}/storm-#{node['storm']['version']}/conf/config.properties" do
+  source "config.properties.erb"
+  owner  "storm"
+  group  "storm"
+  mode   "00644"
+  variables(
+	:topology             => "realtime-topology"
+    :zk_quorum            => zk_quorum.map { |server| server[:fqdn] } * ",",
+    :cam                  => node[:wt_cam][:cam_server_url],
+    :sapi                 => sapi[:fqdn],
+    :config_distrib       => node[:wt_configdistrib][:dcsid_url],
+    :netacuity            => netacuity[:fqdn],
+    :streaming_data_mover => streaming_data_mover[:fqdn],
+    :pod                  => node[:wt_realtime_hadoop][:pod],
+    :datacenter           => node[:wt_realtime_hadoop][:datacenter],
+    :dcsid_whitelist      => node[:wt_storm][:dcsid_whitelist],
+    :debug                => node[:wt_storm][:debug]
+  )
+end
