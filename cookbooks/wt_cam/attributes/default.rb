37a1a898d7330852b66e827ad3f6918f08c8d89c
David Dvorak <david.dvorak@webtrends.com>
2012-06-22 00:15:22 +0000

    several changes, but most significant is switch to WinAuth

--- cookbooks/wt_cam/attributes/default.rb
+++ cookbooks/wt_cam/attributes/default.rb
@@ -10,3 +10,4 @@
 default['wt_cam']['app_pool'] = "CAMService"
 default['wt_cam']['download_url'] = "http://pdxteamcitys01.webtrends.corp/guestAuth/repository/download/bt122/.lastSuccessful/CAM.zip"
 default['wt_cam']['db_server'] = "(local)"
+default['wt_cam']['db_name'] = "wt_CamLite"
