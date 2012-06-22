3607bb30ca899fea37b9c4753ece37f4ca84e1ca
Tim Smith <tim.smith@webtrends.com>
2012-06-22 14:48:27 -0700

    Update rundeck from the Lab

--- cookbooks/rundeck/metadata.rb
+++ cookbooks/rundeck/metadata.rb
@@ -1,5 +1,5 @@
 maintainer       "Webtrends"
-maintainer_email "hostedops@webtrends.com"
+maintainer_email "Peter Crossley <peter.crossley@webtrends.com>"
 license          "All rights reserved"
 description      "Installs/Configures rundeck"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
