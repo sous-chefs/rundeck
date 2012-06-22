b68bd12f07afc0531418076f4f3a48a3a8d220a6
Kendrick Martin <kendrick.martin@webtrends.com>
2012-06-22 17:00:04 +0000

    Forcing version to upgrade.

--- cookbooks/ondemand_base/recipes/updatechef.rb
+++ cookbooks/ondemand_base/recipes/updatechef.rb
@@ -1,11 +1,13 @@
+new_version = "10.12.0"
 if node['upgrade_chef'] == true
   node.set['upgrade_chef'] = false
-  gem "chef" do
+  gem_package("chef") do
+    version new_version
     action :install
   end
 
   if node.platform == "windows" then
-    gem "ffi" do
+    gem_package("ffi") do
       action :install
     end
   end
