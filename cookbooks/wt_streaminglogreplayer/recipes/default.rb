5aece1e5aedd1c1542e4a40cf843ff87fd935ef3
Tim Smith <tim.smith@webtrends.com>
2012-06-24 11:07:29 -0700

    Define a NRPE check for old file on the streaming log replayer share
    Former-commit-id: 9da574f83cb9d72fa82cfc8391986ec2277d9e52 [formerly 3e88a6567e44fc07e88c7c1de5bdb243e9d04cd1] [formerly e127abd0810ab1c174a7c8f96dad9e61ffca310b [formerly 9443bb7318b6059cfda2dec5849e78bfc0b18c1b]]
    Former-commit-id: 05f0aabb979d9947da24c82a6ab9d15d9dbdb984 [formerly db1b1dbd3f04d34f2220300d0a838d3c239e6371]
    Former-commit-id: 5bdea80143577ea341da0029884330bd96c4b8f1

--- cookbooks/wt_streaminglogreplayer/recipes/default.rb
+++ cookbooks/wt_streaminglogreplayer/recipes/default.rb
@@ -189,3 +189,13 @@ if node.attribute?("collectd")
     notifies :restart, resources(:service => "collectd")
   end
 end
+
+#Create a nagios nrpe check for old files in the logshare
+if node.attribute?("nagios")
+	nagios_nrpecheck "wt_streaming_logreplayer_oldfiles" do
+		command "#{node['nagios']['plugin_dir']}/check_file_ages_in_dirs"
+		warning_condition "15"
+		critical_condition "20"
+		parameters "-a '-d #{node['wt_streaminglogreplayer']['share_mount_dir']} -t minutes'"
+		action :add
+	end
