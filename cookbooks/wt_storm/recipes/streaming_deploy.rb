1526e260aed72b2a1cd38d9e0cbda51372db1ac0
Marek Dolgos <marek.dolgos@webtrends.com>
2012-06-28 16:42:13 -0700

    Adding new audit logic.
    Former-commit-id: f59ebce8c1fc906ed40d702348185b184d4a7b23 [formerly e19a73292cb5815db8fa7f0cde7d18af680f216f] [formerly 7459e91eb31df7abeaa229172d9d461662468870 [formerly bdcd731ef3fa822a4c4b6f467a370090ea8c21ff]]
    Former-commit-id: 25d4d1413431dc64cf46a79c2b586cdf55d5e49a [formerly cc12e16ce64394b7c7e0e2d31e35cf5ea2dc74ce]
    Former-commit-id: c743199f541776d05b39122b0c1c518051ab7ac4

--- cookbooks/wt_storm/recipes/streaming_deploy.rb
+++ cookbooks/wt_storm/recipes/streaming_deploy.rb
@@ -13,6 +13,37 @@ sapi = search(:node, "role:wt_streaming_api_server AND chef_environment:#{node.c
 netacuity = search(:node, "role:wt_netacuity AND chef_environment:#{node.chef_environment}").first
 streaming_data_mover = search(:node, "role:kafka AND chef_environment:#{node.chef_environment}").first
 
+log "Updating the template files"
+
+# get the correct environment for the zookeeper nodes
+zookeeper_port = node['zookeeper']['client_port']
+zookeeper_env = "#{node.chef_environment}"
+unless node[:wt_streaminglogreplayer][:zookeeper_env].nil? || node[:wt_streaminglogreplayer][:zookeeper_env].empty?
+    zookeeper_env = node['wt_streaminglogreplayer']['zookeeper_env']
+end
+
+# grab the zookeeper nodes that are currently available
+zookeeper_pairs = Array.new
+if not Chef::Config.solo
+    search(:node, "role:zookeeper AND chef_environment:#{zookeeper_env}").each do |n|
+        zookeeper_pairs << n[:fqdn]
+    end
+end
+
+# fall back to attribs if search doesn't come up with any zookeeper roles
+if zookeeper_pairs.count == 0
+    node[:zookeeper][:quorum].each do |i|
+        zookeeper_pairs << i
+    end
+end
+
+# append the zookeeper client port (defaults to 2181)
+i = 0
+while i < zookeeper_pairs.size do
+    zookeeper_pairs[i] = zookeeper_pairs[i].concat(":#{zookeeper_port}")
+    i += 1
+end
+
 template "#{node['storm']['install_dir']}/storm-#{node['storm']['version']}/conf/config.properties" do
   source "config.properties.erb"
   owner  "storm"
@@ -29,6 +60,7 @@ template "#{node['storm']['install_dir']}/storm-#{node['storm']['version']}/conf
     :pod                  => node[:wt_realtime_hadoop][:pod],
     :datacenter           => node[:wt_realtime_hadoop][:datacenter],
     :dcsid_whitelist      => node[:wt_storm][:dcsid_whitelist],
-    :debug                => node[:wt_storm][:debug]
+    :debug                => node[:wt_storm][:debug],
+	:zookeeper_pairs      => zookeeper_pairs
   )
 end
