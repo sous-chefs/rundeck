c71aa6455983c28e608c54ba37c8a11dbe103023
Marek Dolgos <marek.dolgos@webtrends.com>
2012-07-05 14:37:53 -0700

    Cookbook clean up.
    Former-commit-id: a714c6f62cd776ed43ebfc7a70d915d10bf1bbed [formerly eb10fee35d7cc1ef0db0526502fa60d70239fdd7] [formerly c14dc8ca2d6208917013094e798c6d0300110390] [formerly 437335281d974bda7267e9d1c03f90e9ee03deda [formerly 64591f3d66211ecc6f6427a15a628a022663bc8f]] [formerly fc11bb1c809dc97bb3aa366bdf529f8a4b2935be [formerly 76780347e53f0d6a0b33d536f438bcbc9b0ed00d] [formerly ff056113214be6bb25f0464d5df1084c10618f11 [formerly cbb9b8261789d3af2aa3220168551836802f7d99 [formerly 2a0844866ca39eee1ca47ac1b7e6ac0a8499f052]]]]
    Former-commit-id: cc1733c73667de9fbb78f52c2606e5596d7280b2 [formerly d6b85af8eee37bbf63eb1ef331f04ff71fd613b3] [formerly e01d052bf812753e99072a3e7f212d52aad48858 [formerly cbb9b8261789d3af2aa3220168551836802f7d99]]
    Former-commit-id: 711e4f5971a07605d01a142839b4daa9d7619b73 [formerly 06bd11f3d96e366450047c6a2424062a48a01b56]
    Former-commit-id: 2f8db3847503c2095dd1a99f4178cbae8ee37abf
    Former-commit-id: de4608391d00607822ac39d07391c78a92481f7a

--- cookbooks/wt_storm/metadata.rb
+++ cookbooks/wt_storm/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "sean.mcnamara@webtrends.com"
 license          "All rights reserved"
 description      "Installs Webtrends specific storm bits"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.14"
+version          "1.0.15"
 
 depends "storm"
 
