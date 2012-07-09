ef60640b28d77422edea9f3119d80563a6aee035
Marek Dolgos <marek.dolgos@webtrends.com>
2012-07-09 11:54:22 -0700

    Added change to not fail with method call to Nil object because a list comes back empty.
    Former-commit-id: c170632588e86b8c0846cfd135569df038e58698 [formerly 083874c2f9a782e1d730ff3f223b6431fb89c53c] [formerly 15855f3c3a036ca92c05367ec075381d3c46b711] [formerly 04e5d5dc51d7c33dff21fdbaf95c18e96cd4499b [formerly 1649d96e7b92ed337297c31cb561ee76c9da2b92]] [formerly e07248b9237b0b2991fd2983e971803b265626e6 [formerly 7aed6b9a3164bfe989a24e15bda12de2f4eec03e] [formerly 3aa97f22bd7769b9618a658a0d83ef485b691e0f [formerly cd3a3d78d36939e477ddd24bc7a7995e09f4b4d1 [formerly d34234a0fb9d94c6c59b822b8831018098c1d7d3]]]]
    Former-commit-id: a3e89b765058586e310153322a02f69bb5556df3 [formerly 6e4a81dccb92f341c91d490ee7be03f04bf6f0cc] [formerly a6a682909a04813d12531d149d99bdd0443c7eef [formerly cd3a3d78d36939e477ddd24bc7a7995e09f4b4d1]]
    Former-commit-id: 306c8509f6fb5516b98649bbcb4800e3a629f5bd [formerly c49cbf41ac22233b28631f1ec6aba2082823cf29]
    Former-commit-id: 106b5d9a660fa7d274346d164cc773cdcdf987a4
    Former-commit-id: 8e0486b4ad7db20606538bcb28cad8a5718ecf5f

--- cookbooks/wt_storm/metadata.rb
+++ cookbooks/wt_storm/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "sean.mcnamara@webtrends.com"
 license          "All rights reserved"
 description      "Installs Webtrends specific storm bits"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.18"
+version          "1.0.19"
 
 depends "storm"
 
