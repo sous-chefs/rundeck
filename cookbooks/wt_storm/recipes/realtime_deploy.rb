fd5f2fbd81473b044642f4279641e620719c1e90
Marek Dolgos <marek.dolgos@webtrends.com>
2012-07-09 11:47:39 -0700

    Adding fallback for find zookeepers needed in other staging env's.

--- cookbooks/wt_storm/recipes/realtime_deploy.rb
+++ cookbooks/wt_storm/recipes/realtime_deploy.rb
@@ -10,6 +10,14 @@ include_recipe "wt_storm"
 
 zookeeper_quorum = search(:node, "role:zookeeper AND chef_environment:#{node.chef_environment}")
 zookeeper_clientport = search(:node, "role:zookeeper AND chef_environment:#{node.chef_environment}").first[:client_port]
+
+# fall back to attribs if search doesn't come up with any zookeeper nodes
+if zookeeper_quorum.count == 0
+    node[:zookeeper][:quorum].each do |i|
+        zookeeper_quorum << i
+    end
+end
+
 sapi = search(:node, "role:wt_streaming_api_server AND chef_environment:#{node.chef_environment}").first
 netacuity = search(:node, "role:wt_netacuity AND chef_environment:#{node.chef_environment}").first
 kafka = search(:node, "role:kafka AND chef_environment:#{node.chef_environment}").first
@@ -24,7 +32,7 @@ template "#{node['storm']['install_dir']}/storm-#{node['storm']['version']}/conf
     :topology => "realtime-topology",
     :realtime_topology_parsing_bolt_count    => 3,
     :realtime_topology_row_key_bolt_count    => 12,
-    :realtime_topology_writing_bolt_count    => 24,
+    :realtime_topology_writing_bolt_count    => 48,
     :realtime_topology_dimensions_bolt_count => 3,
     :topology_override_max_spout_pending => 3000,
     :topology_override_msg_timeout_seconds => 120,
