d0ca049482c35a7326d7fc92475ea7a52a13087d
Kendrick Martin <kendrick.martin@webtrends.com>
2012-07-10 23:12:32 +0000

    Fixing slash in app pool command. Changing wt_analytics to use pool vs app_pool for var name.

--- cookbooks/wt_analytics/attributes/default.rb
+++ cookbooks/wt_analytics/attributes/default.rb
@@ -9,5 +9,5 @@
 default['wt_analytics']['artifact'] = "AnalyticsUI.zip"
 default['wt_analytics']['tc_proj'] = "Analytics"
 default['wt_analytics']['install_dir'] = "\\Insight"
-default['wt_analytics']['app_pool'] = "Webtrends_Analytics"
+default['wt_analytics']['pool'] = "Webtrends_Analytics"
 default['wt_analytics']['download_url'] = "http://teamcity.webtrends.corp/guestAuth/repository/download/bt15/.lastSuccessful/AnalyticsUI.zip"
--- cookbooks/wt_analytics/metadata.rb
+++ cookbooks/wt_analytics/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "kendrick.martin@webtrends.com"
 license          "All rights reserved"
 description      "Installs and configures A10"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.14"
+version          "0.0.15"
 depends			 "iis"
 depends 		 "windows"
 depends			 "wt_base"
