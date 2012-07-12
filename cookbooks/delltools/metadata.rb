7ae8aed7980ccaec7e992d2428497d242f750358
Tim Smith <tim.smith@webtrends.com>
2012-07-12 12:50:43 -0700

    Add ipmitool to the dell tools install.
    Former-commit-id: 6f8bf28ec4a39e35ca3f09f9987fab0da9a5550b [formerly e6a31a53476b277226ca5776c8ae9fce2ebb6df1] [formerly 39524c0fc8d3d429560a2e3f874aa2f6731a3cb9 [formerly 3d5e04da0d7b4877e8bfcb0b089dae0b4cf7ae39]]
    Former-commit-id: 2872597e4391cf320597d0950e94dc6cfec59f70 [formerly 1f0253adc3964c771edf0859da49497be3168b96]
    Former-commit-id: c95baae638bb90fc66bf1e5ed1f42c6a466f93b9

--- cookbooks/delltools/metadata.rb
+++ cookbooks/delltools/metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "nathan@outbrain.com"
 license          "All rights reserved"
 description      "Installs/Configures delltools"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.2"
+version          "0.0.3"
