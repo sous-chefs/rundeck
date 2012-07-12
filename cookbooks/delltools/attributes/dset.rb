5f5aa7717f41a0c598139f1170afc50558cfaeb1
Tim Smith <tim.smith@webtrends.com>
2012-07-12 10:50:43 -0700

    Update the dell tools cookbook to match best practices.
    Lowercase attributes
    Attributes that match the cookbook name
    Simplified package install loops
    Use chef resources to pull / execute the dset install not a bash script
    Download temp files to the chef temp dir not /tmp
    Exit all recipes if the system manufacturer is not Dell so these can be applied on all nodes without worry
    
    
    Former-commit-id: 2f2e6412f75195b56f40cc89d048491ab4e8f4d3 [formerly a111abb842b6569013825d7b4eec9a49b1f9880e] [formerly 75e4cd77a2a209671a418f4b3c38f9d665b2c3af] [formerly 1ee9d1cc4555641b0c2f71af60287976dd02ecd5 [formerly 83da7e3b8f539c56cb0bee3bc84d5e8f1614e0f0]] [formerly 291dae6ce2df826e0c6da64e7d94c10e9b7f7b11 [formerly adadbb66dfb8c822d25392eb3a8c358903fad4a2] [formerly 08d016629892de73398e3b0b243edc91e0c9e27a [formerly dc5ca8cd43167491bc5b50f7c93786dcb32bd635 [formerly 9819d295d5dbdef0913bc15b846f56ab08ee96cc]]]]
    Former-commit-id: f042261be97a804733c7a00efe1a50640574a5e0 [formerly 4fe773ee5e4db3b94b43909ad17b94e16947d411] [formerly e8cd57739a1b7085b9a55b2f06fed6579b32a844 [formerly dc5ca8cd43167491bc5b50f7c93786dcb32bd635]]
    Former-commit-id: 11bef87d4804e3cefbd926ce72bae1002d8a84a6 [formerly 0886056fa1d1b02d3544d7b42ec5eba80974153a]
    Former-commit-id: 0a73bb8cf68ad4ce06de94e759956edee5acc33f
    Former-commit-id: 5c1b318cf5adc1821f5315c5a8a623fc00b6197e

--- cookbooks/delltools/attributes/dset.rb
+++ cookbooks/delltools/attributes/dset.rb
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-default[:Dell][:DSET][:Version] = "2.2.0.125_x64_A01"
+default[:delltools][:dset][:version] = "2.2.0.125_x64_A01"
