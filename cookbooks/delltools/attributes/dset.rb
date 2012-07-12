f3a901589a843ba5304ef2110e8089f40a969773
Tim Smith <tim.smith@webtrends.com>
2012-07-12 11:51:54 -0700

    fix dset version

--- cookbooks/delltools/attributes/dset.rb
+++ cookbooks/delltools/attributes/dset.rb
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-default[:delltools][:dset][:version] = "2.2.0.125_x64_A01"
+default[:delltools][:dset][:version] = "2.2.125_x64_a01"
