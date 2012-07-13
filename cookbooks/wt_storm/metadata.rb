f2339783627f86aa026ccf687eef7009f8c0bec6
Andrew Psaltis <andrew.psaltis@webtrends.com>
2012-07-12 22:43:47 -0600

    Changes to support the log smoothing feature in the streaming topology
    Former-commit-id: 162051d6038dbdabc42821fbd3454209c1f706d8 [formerly 5b2a05aa042843dc0d27809ad263d5879167a76d] [formerly f914b824de5976662e5961db47fea592884548c6 [formerly 538777f3bb66312e66c16b8ba0cb2fbb0adbef6e]]
    Former-commit-id: bb36e3c726d26d2028054418efca65d5393122f1 [formerly ae4c4ce61d9f4cd5ea0050a4db10f2dd106aaa6f]
    Former-commit-id: 3e3aff47e6ff9836fc94989a9ad23d5db6d708f1

--- cookbooks/wt_storm/metadata.rb
+++ cookbooks/wt_storm/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "sean.mcnamara@webtrends.com"
 license          "All rights reserved"
 description      "Installs Webtrends specific storm bits"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.24"
+version          "1.0.25"
 
 depends "storm"
 
--- cookbooks/wt_streaminglogreplayer/attributes/default.rb
+++ cookbooks/wt_streaminglogreplayer/attributes/default.rb
@@ -7,7 +7,7 @@ default['wt_streaminglogreplayer']['log_extension']     = ".gz"
 default['wt_streaminglogreplayer']['log_share_mount']   = ""
 default['wt_streaminglogreplayer']['zookeeper_env']     = nil
 default['wt_streaminglogreplayer']['share_mount_dir']   = "/srv/logsharedir"
-default['wt_streaminglogreplayer']['kafka_topic']       = "rawHits"
+default['wt_streaminglogreplayer']['kafka_topic']       = "lrRawHits"
 default['wt_streaminglogreplayer']['delete_logs']       = "true"
 default['wt_streaminglogreplayer']['java_opts']         = "-Xms1024m -Djava.net.preferIPv4Stack=true"
 default['wt_streaminglogreplayer']['kafka_broker_list'] = nil
