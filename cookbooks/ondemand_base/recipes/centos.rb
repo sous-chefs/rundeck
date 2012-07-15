e36520f3907da5f2b88963e16fb92ed8e3476d0a
Tim Smith <tim.smith@webtrends.com>
2012-07-15 15:47:43 -0700

    Use RAID cookbook not megaraid
    Former-commit-id: 9dd403b460ddbd9e26bc67d5847b2b4625c8b6a5 [formerly b53fb67b43238b36b1950472e243cb3987cdd2d9] [formerly c6a06f886ec81bbde635fb319fb9011aba9d19ab [formerly b0b37a55c4b6b2bddfae988950dfc2f9aa4b3657 [formerly 8b93edd423d612ffad976ae29e89211bdefb6e01]]]
    Former-commit-id: 6b74ecdcee74e91d242c5c3a0f685d38e08385aa [formerly 3bd5795be8b268e259861b52239b05058201720e]
    Former-commit-id: 815adf3e021a1f122516b8110e49d00a6e8d649c
    Former-commit-id: e1e5ec220f561081001495e5dfde5797f59c45fa

--- cookbooks/ondemand_base/recipes/centos.rb
+++ cookbooks/ondemand_base/recipes/centos.rb
@@ -174,7 +174,7 @@ include_recipe "hp-tools"
 #Dell Systems only: Install Dell System E-Support Tool
 include_recipe "delltools::default"
 include_recipe "delltools::dset"
-include_recipe "delltools::megaraid"
+include_recipe "delltools::raid"
 
 #VMware Systems only: Install VMware tools since we have CentOS boxes in PP/Staging on VMs
 include_recipe "vmware-tools"
