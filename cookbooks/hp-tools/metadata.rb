0b98bd3dfc8c8902af2e7d372c081869e0b95eda
Tim Smith <tim.smith@webtrends.com>
2012-07-15 22:51:25 -0700

    Add the HP nagios monitor to the hp-tools cookbook.
    Former-commit-id: f9c8e06854d29e7c35de6ab8d3800d9736db2750 [formerly 2b55eb71b87facd578f890c9ea8136a379e3daa4] [formerly 7e07ed11d880eeb91d5d488b06f560ac849888aa] [formerly dbbd68a5811b78cf657ff38087b72b2e76a7663e [formerly 04d354bf69e040226b2ffe2c666f57df2f0d5741]] [formerly dd0b9cc48e0329760ed722b75966f0a4a08f87d6 [formerly b3e0fce34d332c530101bb83c1f3c97788ddaf58] [formerly ee8585da2905e2d210cc1017604417046efe36e6 [formerly 228f191531b12d6156f01f4fe3ed089588196603 [formerly 9b88c13208ccd1464b3104db5489d770d2fa84cd]]]]
    Former-commit-id: 828a338c8239f17aaddb659d9ad0b2690b97bab5 [formerly 3dc69626e37f69c546bbebb5a3784882e76f7055] [formerly 5cae6b0b35835aa1f6f20d86416dc5d9c8866668 [formerly 228f191531b12d6156f01f4fe3ed089588196603]]
    Former-commit-id: a39920661a95fadba3c2309ce27d295961c6f00b [formerly 709b51db2bedf009b3469faa254ff428c0d5abd1]
    Former-commit-id: 1329092775089a59a66b6200f005860027f34e46
    Former-commit-id: 9a9819e3f9be93e483fe42f1bd1add814d97f341

--- cookbooks/hp-tools/metadata.rb
+++ cookbooks/hp-tools/metadata.rb
@@ -3,8 +3,8 @@ maintainer_email "hostedops@webtrends.com"
 license          "Apache 2.0"
 description      "Installs/Configures various HP tools"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.2.0"
+version          "0.3.0"
 
-%w{ centos redhat }.each do |os|
+%w{ centos redhat fedora amazon scientific }.each do |os|
   supports os
 end
