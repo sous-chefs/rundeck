1fd65ba6db270769ebe763ebd2b74ac84799252c
Tim Smith <tim.smith@webtrends.com>
2012-07-15 18:56:11 -0700

    Remove quotes that prevented data bag based search from functioning
    Former-commit-id: d940267ef3592592f985cfdd2dba2e2128c1540c [formerly 996aa1137f407c383555e9f2a61e7547174a6357] [formerly 72d7e3f52edeb4878ec6e165e395189786c24cb8 [formerly 38128cb8808653bdaafcf55e4e98a238caf58e60]]
    Former-commit-id: 8d8212f8f1b165d28a69335f0a53f8bd1a1df89a [formerly acbdc5195f7bccad5294e7feac89ec686a73da2a]
    Former-commit-id: a37512a8933c3efd7d92117cdd50abe4677cb799

--- cookbooks/nagios/recipes/server.rb
+++ cookbooks/nagios/recipes/server.rb
@@ -82,7 +82,7 @@ begin
   hostgroup_list = Array.new
   search(:nagios_hostgroups, '*:*') do |hg|
     hostgroup_list << hg['hostgroup_name']
-    search("#{hg['search_query']}") do |n|
+    search(#{hg['search_query']}) do |n|
       hostgroup_nodes[hg['hostgroup_name']] = n['hostname']
     end
   end
