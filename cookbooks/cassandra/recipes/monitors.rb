60611f7b7248342fb077716aee9416931934f7fd
Tim Smith <tim.smith@webtrends.com>
2012-07-17 06:54:39 -0700

    Move the monitors to an external file.  Add the NRPE def for checking the ring.  Figure out the number of nodes to alert on based on a chef search.
    Former-commit-id: 3d2ae14c57419f8a49f125f6ea1fbdcc3479d346 [formerly 73fdd485ae010a05a9d33a4f8c66dce71bf81aea] [formerly c0eb23e7db20f020611fa8796009771ab47ddaae] [formerly 988d188f747cc5e7ab0e2ffa2daccdcf815c49d4 [formerly a10306f64f7d864acb67b0787af94ff60f56801b]] [formerly 1953f21b86cf114983fdf75443538c9696845b4b [formerly 15eadba245a84d16816ed1a5401856aba84255d4] [formerly 55be269a5959b2613d0dcbcd41e3d3a1384cf926 [formerly f51b2ebba4318264dc39ec4a71fb8dbe8a29ea1c [formerly 844a873889c56f3658abc4267e8f2bae5bc2e2b3]]]]
    Former-commit-id: c3ba882b9b89b5e231173c05547d20622916c70c [formerly 5c74e9a514db6cb1c7b83058772b717505ef9beb] [formerly 80a64b1df6f1292059d88cbf90ca4c164118e3fc [formerly f51b2ebba4318264dc39ec4a71fb8dbe8a29ea1c]]
    Former-commit-id: ec1ae84150b1dbb5b415148515cb77012f0eaeba [formerly 5ae8cd1a28b2ddc1a6912b118e129bff4df22a22]
    Former-commit-id: 3938fba0d34c8a94e246c65a2ae4e32e1e7cba63
    Former-commit-id: f5c01eb1ea5db92b3abd84a380b2176deb818c7e

---
+++ cookbooks/cassandra/recipes/monitors.rb
@@ -0,0 +1,40 @@
+#
+# Author:: Timothy Smith (<tim.smith@webtrends.com>)
+# Cookbook Name:: cassandra
+# Recipe:: monitors
+#
+# Copyright 2012, Webtrends Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+#Create collectd plugin for cassandra if collectd has been applied.
+if node.attribute?("collectd")
+  template "#{node[:collectd][:plugin_conf_dir]}/collectd_cassandra.conf" do
+    source "collectd_cassandra.conf.erb"
+    owner "root"
+    group "root"
+    mode 00644
+    notifies :restart, resources(:service => "collectd")
+  end
+end
+
+# Add nagios checks if nagios is applied on the node
+if node.attribute?("nagios")
+	# find the number of Cassandra nodes in the environment
+	node_count=search(:node, 'recipes:cassandra\:\:default')
+  node_count.count
+	cookbook_file "#{node['nagios']['plugin_dir']}/cassandra_ring" do
+		source "cassandra_ring"
+		mode 00755
+	end
+
+	nodes=search(:node, "recipes:cassandra\:\:default")
+	alert_threshold=nodes.count - 1
+	nagios_nrpecheck "check_cassandra_ring" do
+		command "sudo #{node['nagios']['plugin_dir']}/cassandra_ring"
+		warning_condition "#{alert_threshold}"
+    critical_condition "#{alert_threshold}"
+		action :add
+	end
+end
