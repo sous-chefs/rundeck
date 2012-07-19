b48271dd56ebf9c24decf08a6c25964dee409a9c
Adam Keene <adam.keene@webtrends.com>
2012-07-19 21:10:07 +0000

    Add PatentsLink app settings in wt_analytics web.config template.
    Former-commit-id: 5262b17220857421d5f80afb74aebe11a6c02a6c [formerly c35051bd66183aecdafd47351b982cabf6abebbe] [formerly df13e5d7efffe71f07789a5c393fa3e32055a3d4 [formerly 696716516d5ecb4ba57aca3d824ed8226a6be51b [formerly e618ccd74c454f334161a2cba010ef0641745d9e]]]
    Former-commit-id: b69d9e95fccaa3981589193710830b297eb90247 [formerly f01f1f52a8182f721fbc7bb08b77413d7a4b777c]
    Former-commit-id: a5e2e37f7b8a623d8bd48756ba47c7befee4ce11
    Former-commit-id: 5d641e0dd1227ec00f0dc2d27f09b423a4a8023e

--- cookbooks/wt_analytics/metadata.rb
+++ cookbooks/wt_analytics/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "kendrick.martin@webtrends.com"
 license          "All rights reserved"
 description      "Installs and configures A10"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.20"
+version          "0.0.21"
 depends			 "iis"
 depends 		 "windows"
 depends			 "wt_base"
