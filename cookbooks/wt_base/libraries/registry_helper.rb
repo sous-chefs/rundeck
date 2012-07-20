1309b6b809c86d2bf9105d6538fd618b6bd1985c
David Dvorak <david.dvorak@webtrends.com>
2012-07-20 17:58:50 +0000

    added function in_reg_uninstall?

---
+++ cookbooks/wt_base/libraries/registry_helper.rb
@@ -0,0 +1,31 @@
+#
+# Author:: David Dvorak (<david.dvorak@webtrends.com>)
+# Cookbook Name:: wt_base
+# Library:: registry_helper
+#
+# Copyright 2012, Webtrends Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+#
+# search registry uninstallation data for DisplayName
+#
+def in_reg_uninstall?(this_display_name)
+	found = false
+	uninstall_subkey = 'Software\Microsoft\Windows\CurrentVersion\Uninstall'
+	Win32::Registry::HKEY_LOCAL_MACHINE.open(uninstall_subkey) do |reg|
+		reg.each_key do |key|
+			begin
+				k = reg.open(key)
+				display_name = k["DisplayName"] rescue nil
+			rescue Win32::Registry::Error
+			end
+			if display_name =~ /#{Regexp.quote(this_display_name)}/
+				found = true
+				break
+			end
+		end
+	end
+	return found
+end
