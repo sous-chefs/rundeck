4d719d58762f31d1f41c3864b678def2e297e228
David Dvorak <david.dvorak@webtrends.com>
2012-07-20 19:23:07 +0000

    removed 2 attributes that don't need to be exposed
    Former-commit-id: 709b83ba3a60da490699e62c2d8599cc91ee23bd [formerly b40569000224b13a4d8561d2dc48a59f64bed196] [formerly e8506e89a39d6e311ef8bf06a9fc207065d80373 [formerly acc9e027fcc990826653a210fa2a202f2adfbdca [formerly db960f90b9624a2293f8a4249f719586a4b9e56b]]]
    Former-commit-id: 067603c500197260ffcfb8b13521091396669ed0 [formerly c8e26a50132b027d7c7f43955645b8ecbbac76d6]
    Former-commit-id: c81d69162849538b39d295a3fa43aeed6cf7927b
    Former-commit-id: e6832aee4cb30a7e89fbb79e138d5173b3006cb6

--- cookbooks/beyond_compare/attributes/default.rb
+++ cookbooks/beyond_compare/attributes/default.rb
@@ -9,7 +9,5 @@
 # display name
 default['beyond_compare']['display_name'] = 'Beyond Compare Version 3.3.5'
 
-# setup files
+# setup executable
 default['beyond_compare']['download_url'] = 'http://repo.staging.dmz/repo/windows/beyond_compare/BCompare-3.3.5.15075.exe'
-default['beyond_compare']['key_file']     = 'BC3Key.txt'
-default['beyond_compare']['inf_file']     = 'bc3inf.txt'
