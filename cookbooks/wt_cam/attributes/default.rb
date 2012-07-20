8c70bc68d62dc19d2edbaf39d1030e56d3b768a0
alexanderm <alexanderm@sops01.(none)>
2012-07-20 19:59:42 +0000

    Changing the database name again for the new cam to keep it in sync
    Former-commit-id: 15e893e84d57d322e1c22c01f68e6a3a4326d409 [formerly 34c399cc9178238b84ad5c499c0955d326c32793] [formerly 26e9e915b3c5944b927c9957954777200cf651fb [formerly 326437c616c323c616ce7ea7db8055c1fce1a0d3 [formerly 332e7e545facb8fdbc150ca38184c8e77cb699d3]]]
    Former-commit-id: 6e53d2e65d5210812995ae1767584513f1fa70b6 [formerly f26daa4f4333f50db8fe9fc26bdf7ccab3028774]
    Former-commit-id: 597c9045106661e1bbbb41dfa4bad313e7266b22
    Former-commit-id: 5293ff50badfc9d92228070c09490e85ce7e6ee1

--- cookbooks/wt_cam/attributes/default.rb
+++ cookbooks/wt_cam/attributes/default.rb
@@ -12,6 +12,6 @@ default['wt_cam']['camlite_app_pool'] = "CAMService"
 default['wt_cam']['download_url'] = "http://pdxteamcitys01.webtrends.corp/guestAuth/repository/download/bt137/.lastSuccessful/CAM.zip"
 default['wt_cam']['camlite_download_url'] = "http://pdxteamcitys01.webtrends.corp/guestAuth/repository/download/bt122/.lastSuccessful/CAM.zip"
 default['wt_cam']['db_server'] = "(local)"
-default['wt_cam']['db_name'] = "wtCam"
+default['wt_cam']['db_name'] = "Cam"
 default['wt_cam']['camlite_db_name'] = "wtCamLite"
 default['wt_cam']['tokenExpirationMinutes'] = 60
