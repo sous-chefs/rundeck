9d770cfcbfd142b2b0cd36d631327055baf78316
Tim Smith <tim.smith@webtrends.com>
2012-07-22 14:26:18 -0700

    Install PowerGUI on Windows systems.  This is a GUI PowerShell editor.

---
+++ cookbooks/powergui/recipes/default.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: powergui
+# Recipe:: default
+#
+# Copyright 2012, Webtrends Inc.
+#
+# All rights reserved
+#
+# Code based off the PowerShell cookbook by Seth Chisamore
+
+if platform?("windows")
+    windows_package "Quest PowerGUI® 3.2" do
+        source node['powergui']['http_url']
+        installer_type :msi
+        action :install
+    end
+else
+  Chef::Log.warn('PowerGUI can only be installed on the Windows platform using this cookbook.')
+end
