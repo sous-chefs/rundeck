2bb21177ee8a84aa9feadc9443432095125e6cfb
David Dvorak <david.dvorak@webtrends.com>
2012-07-24 12:40:11 -0700

    Removed installlogdir (only for msi's). Changed method for config app_pool identity.
    Former-commit-id: 7643fb7923aef1ce64cc2ca0c65d0c63b39ea9ac [formerly c2de3dae6cbe931462d19c550d7d7989a0726d11] [formerly d86f2a94f3aa1eed2a7719de860d638a57ffd46c [formerly 46959eb6f6e56874647cf5093de13660efe1c20f [formerly bf0be8f4b615025f1eb67792957a28c62f1b45b9]]]
    Former-commit-id: cfa2ce5ad0e8aed9c0640f9a91218435a8928e52 [formerly 622698a37d64ddd46d9edf1020ce98f754e82bd9]
    Former-commit-id: 3e3993e3e243ca4051bdadab2dffe42d63049411
    Former-commit-id: 2a6f9ea841fe104b77ba7e7a31e6276d484bc17e

--- cookbooks/wt_analytics/recipes/default.rb
+++ cookbooks/wt_analytics/recipes/default.rb
@@ -11,29 +11,22 @@
 # require 'rexml/document'
 # require 'json'
 
-if deploy_mode? 
-	include_recipe "wt_analytics::uninstall" 
-	include_recipe "ms_dotnet4::regiis" 
+if deploy_mode?
+	include_recipe "wt_analytics::uninstall"
+	include_recipe "ms_dotnet4::regiis"
 end
 
 # properties
 install_dir    = File.join(node['wt_common']['install_dir_windows'], node['wt_analytics']['install_dir']).gsub(/[\\\/]+/,"\\")
 install_url    = node['wt_analytics']['download_url']
-install_logdir = node['wt_common']['install_log_dir_windows']
 
 user_data = data_bag_item('authorization', node.chef_environment)
 ui_user   = user_data['wt_common']['ui_user']
 ui_pass   = user_data['wt_common']['ui_pass']
 
-pool     = node['wt_analytics']['app_pool_name']
-auth_cmd = "/section:applicationPools /[name='#{pool}'].processModel.identityType:SpecificUser /[name='#{pool}'].processModel.userName:#{ui_user} /[name='#{pool}'].processModel.password:#{ui_pass}"
+app_pool_name = node['wt_analytics']['app_pool_name']
 
 directory install_dir do
-	action :create
-	recursive true
-end
-
-directory install_logdir do
 	action :create
 	recursive true
 end
@@ -42,18 +35,21 @@ iis_site 'Default Web Site' do
 	action [:stop, :delete]
 end
 
-iis_pool pool do
+iis_pool app_pool_name do
 	pipeline_mode :Integrated
 	runtime_version "4.0"
+	private_mem node['wt_analytics']['app_pool_private_memory']
+	pool_username ui_user
+	pool_password ui_pass
 	action [:add, :config]
 end
 
 iis_site 'Analytics' do
-    protocol :http
-    port 80
-    path install_dir
-    app_pool pool
-    action [:add,:start]
+	protocol :http
+	port node['wt_analytics']['website_port']
+	path install_dir
+	app_pool app_pool_name
+	action [:add, :start]
 end
 
 wt_base_icacls install_dir do
@@ -69,17 +65,15 @@ wt_base_icacls install_dir do
 end
 
 if deploy_mode?
+
 	windows_zipfile install_dir do
 		source install_url
 		action :unzip
-	end	
-	
-	search_server = search(:node, "chef_environment:#{node.chef_environment} AND role:wt_search")
-	search_host = "#{search_server[0][:fqdn]}"
-	
-	template "#{install_dir}\\web.config" do
-	  source "webConfig.erb"
-	   variables(
+	end
+
+	template "#{install_dir}\\Web.config" do
+		source "webConfig.erb"
+		 variables(
 			# master database host
 			:master_host => node['wt_common']['master_host'],
 
@@ -120,7 +114,7 @@ if deploy_mode?
 			:custom_errors        => node['wt_analytics']['custom_errors'],
 			:search_host          => node['wt_search']['search_hostname'],
 			:monitor_service_addr => node['wt_common']['monitor_service_addr']
-	  )
+		)
 	end
 
 	template "#{install_dir}\\App_Data\\brands\\mapping.xml" do
@@ -133,12 +127,9 @@ if deploy_mode?
 	template "#{install_dir}\\App_Data\\brands\\webtrends\\brand.xml" do
 		source "webtrends-brand.xml.erb"
 		variables(
-			:help_link      => node['wt_analytics']['help_link'],
-			:termsofservice => "http://" + node['wt_analytics']['ondemand_base_domain'] + "/licenseagreement.asp"
+			:help_link            => node['wt_analytics']['help_link'],
+			:ondemand_base_domain => node['wt_analytics']['ondemand_base_domain']
 		)
 	end
-	
-	iis_config auth_cmd do
-  		action :config
-	end
+
 end
