29d1ad32545dbeb480625ac71e5df58e254eeec2
Tim Smith <tim.smith@webtrends.com>
2012-07-23 18:18:10 -0700

    Bump wt_netacuity version.  Update read me and changelog
    Former-commit-id: 469d0c9f6611730ab319070107b96fe285f0f045 [formerly 1eb2a89a2a33413760a9ca8afbe2e24bf35353c8] [formerly 200f16749cb4062da4740dab7c2f88ed029a3414 [formerly 14bdb689b1228d0520e1b0600e7552599fc21b3b]]
    Former-commit-id: cf1a0a5fd485ecb819f722454134645669129d7c [formerly c41218f7d377880c0a5f582556b91e948464d29a]
    Former-commit-id: 5370788e51c53f2d2f08a60df103ca753dc93e48

--- cookbooks/wt_netacuity/metadata.rb
+++ cookbooks/wt_netacuity/metadata.rb
@@ -3,6 +3,5 @@ maintainer_email  "tim.smith@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures NetAcuity with a Webtrends license"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.4"
+version           "1.0.5"
 depends           "java"
-depends           "wt_base"
