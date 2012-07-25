3d80163ac66ba626d36fd0bff0c55dff5a7c37dc
vonnagy <ivan.vonnagy@webtrends.com>
2012-07-25 15:38:09 -0700

    Documented streaming auditor and added ASOS DCSIDs to log replayer chef logic
    Former-commit-id: 2bbaa88f6796aed271a8fb0e1f34cdff731312a6 [formerly f77048e43598489496aee2a781056e2dc7bf366e] [formerly 72ad3ff0c493be4e19a513b18be275cbe8665bb2] [formerly 0b2b65f1b682a4949e26fa07f80a7d68300cf8b9 [formerly c46e4d6fe906e8fe6ec4e41d85af1b463e66a84a]] [formerly df973e0f40d6d639df4d8f87c512642791d5e6ee [formerly 330c22db6ef113c6a9b864a2cb11afe84bbc1e41] [formerly c4d6828c9f9246ae139bfd8e3a436d2d634a1971 [formerly 4d7ba3c9df639facf313c321553170b2fe4ea578 [formerly 79479f98806d9f30d6f5dcc66062aa57937c34e2]]]]
    Former-commit-id: c9c352b9b5c1dd8c270016b8d7b03889f5571517 [formerly c9c9d3fb76e8e36a616c345f3d7b88bd5985eb94] [formerly 002f298d86b4ae8594edad761a40da9eae274d6a [formerly 4d7ba3c9df639facf313c321553170b2fe4ea578]]
    Former-commit-id: 1bf54e8d12265b137f7ce515509ae94148d6a0f5 [formerly 14da75d81e8afceea39b437dc8417d69fd6e6578]
    Former-commit-id: 908b661333e1b96845f16ae84129bbde672f2a10
    Former-commit-id: 83f32354eb9f5f9e0ee15a13ba873cb345f4682d

--- cookbooks/wt_streamingauditor/metadata.rb
+++ cookbooks/wt_streamingauditor/metadata.rb
@@ -6,3 +6,5 @@ long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version             "0.0.7"
 depends             "java"
 depends             "runit"
+depends             "nagios"
+depends             "collectd"
