b6b9af9515bb9c598bb05b8f17a68ce2ac2d9aeb
Tim Smith <tim.smith@webtrends.com>
2012-07-25 21:25:32 +0000

    Import vsftpd version 0.0.1
    Former-commit-id: 1d340bac24e899e2b64eea54ea5e4e21d3f20f19 [formerly 78c4bd78392e4eeca5c6243912f24455e75e9ada] [formerly 87b5c957f5d71c78dc31a0c1522d21be0558794e] [formerly c38a08792ba501c370e1fdc78d2a309e83deee9e [formerly 3d7459e863eb275933f92e9ea9bcc73ce63f78e5]] [formerly 2ee293ecb32f30b9af7339de3603b6ad820c5988 [formerly e2c6008589b0e44677c7166fb843a4932db281b7] [formerly e79556c245876e5f2cb8445ac8909c4fb79793c3 [formerly 8eb9438b7dfb4f01dbb6b50483bb6f9ae6c75041 [formerly c8623f82bf3261d9e0c5b01769fcb5b15ed6a36e]]]]
    Former-commit-id: bfb8f77812f2237b6791376253f011deda5600fe [formerly fbbcbec05f6690be3c487417d26d4e304d883d7c] [formerly e88444fea2d1c9fabfcc99d437dadb270f297aa4 [formerly 8eb9438b7dfb4f01dbb6b50483bb6f9ae6c75041]]
    Former-commit-id: ee9c852000fab1d68c4642ab09319a2094326100 [formerly df34157e9ef151209faf062d5df733e418764d75]
    Former-commit-id: 952fbfb322e8b94aa1985269a01705d7b91b8d8e
    Former-commit-id: 409474f80dbc8b9f922b58078778d56e2910662e

---
+++ cookbooks/vsftpd/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Robert J. Berger"
+maintainer_email "rberger+maintainer@ibd.com"
+license          "Apache License, Version 2.0"
+description      "Installs/Configures vsftpd for Secure SSL SFTP"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
