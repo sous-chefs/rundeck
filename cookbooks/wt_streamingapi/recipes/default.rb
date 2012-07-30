ed5eabf69adadc9006b03a1cd8267745a85aa798
seanm <sean.mcnamara@webtrends.com>
2012-07-30 12:50:33 -0600

    stepping up nofile
    Former-commit-id: 813b20d81a470585438f1aab33cc76a0c825da43 [formerly 10631f815af9af00842a43c44108462687a6e78a] [formerly a62c7631335f14414fea69de8470a8f7e8275348 [formerly d50fb2e6276305f4b2f8524b3ea777d59828c09f [formerly ea6f06395deafbb9a7bc5949cdc9616885c40270]]]
    Former-commit-id: 627edf8aa40f06dcfa07a5a7a19cb4ae8ce3dff3 [formerly b176882dc2b7a1dca15b86f5b4592cd0248c3dff]
    Former-commit-id: a513579456f5895a8c2ebd6a54956c4a9a17c8b0
    Former-commit-id: d5a14917f5a384f049e174c659e49aa0a48086b9

--- cookbooks/wt_streamingapi/recipes/default.rb
+++ cookbooks/wt_streamingapi/recipes/default.rb
@@ -57,6 +57,20 @@ log "Install dir: #{install_dir}"
 log "Log dir: #{log_dir}"
 log "Java home: #{java_home}"
 
+
+# step up nofile
+cookbook_file "/etc/security/limits.conf" do
+  source "limits.conf"
+  mode 00644
+end
+
+# enable pam_limits.so
+cookbook_file "/etc/pam.d/common-session" do
+  source "common-session"
+  mode 00644
+end
+
+
 # create the log directory
 directory "#{log_dir}" do
     owner   user
