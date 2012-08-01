65adcc1cb353faea12934cda3b63c54fdc9ab36b
David Dvorak <david.dvorak@webtrends.com>
2012-08-01 01:29:32 +0000

    added header

--- cookbooks/wt_xd/recipes/schema.rb
+++ cookbooks/wt_xd/recipes/schema.rb
@@ -1,18 +1,26 @@
+#
+# Cookbook Name:: wt_xd
+# Recipe:: schema
+#
+# Copyright 2012, Webtrends Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
 
 # create dir
 directory "/opt/webtrends/wt_xd" do
-  mode 00755
-  recursive true
+	mode 00755
+	recursive true
 end
 
 
 # drop in templates
 %w[hbasetable.py schema.py].each do |file|
-  cookbook_file "/opt/webtrends/wt_xd/#{file}" do
-    source "#{file}"
-    owner "hadoop"
-    group "hadoop"
-    mode 00550
-  end
+	cookbook_file "/opt/webtrends/wt_xd/#{file}" do
+		source "#{file}"
+		owner "hadoop"
+		group "hadoop"
+		mode 00550
+	end
 end
 
