ce64d313e776ff27c780b572ef62978ab049f973
Mark Alexander <mark.alexander@webtrends.com>
2012-08-03 21:28:15 +0000

    Initial deposit wt_streamingconfigservice cookbook

---
+++ cookbooks/wt_streamingconfigservice/attributes/default.rb
@@ -0,0 +1,7 @@
+default['wt_streamingconfigservice']['camConnString'] = ""
+default['wt_streamingconfigservice']['masterConnString'] = ""
+default['wt_streamingconfigservice']['includeUnmappedAnalyticsIds'] = ""
+default['wt_streamingconfigservice']['port'] = ""
+default['wt_streamingconfigservice']['download_url'] = ""
+default['wt_streamingconfigservice']['user'] = "webtrends"
+default['wt_streamingconfigservice']['group'] = "webtrends"
