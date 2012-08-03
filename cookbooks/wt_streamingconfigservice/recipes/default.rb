322a8a7b7d82a2ed5430a28ac93b3064b36bc914
Mark Alexander <mark.alexander@webtrends.com>
2012-08-03 21:28:15 +0000

    Initial deposit wt_streamingconfigservice cookbook
    Former-commit-id: df1a54aed8277255ec5406029fab70b106a6729e [formerly 8c988d35ecef2fd2664d872e42db94c5a3080e4c] [formerly 60fe144a740bede440bbbb5e3b020e8b0e2607ab] [formerly 78ff4987a747d2250320477bc3cd795e2d9fb619 [formerly 58ead061a60110279f2d806f6a7b12f1d87971fa]] [formerly 77b43938c4cee32c824240eaecb900ffccda4eaf [formerly ef64da6f169b888db1efe9eb604fee390d4fc472] [formerly 52046d402cedc536fa44d013f40a9487e99f0841 [formerly 3c00c02490a7368a003e2b5cad52d1af869cb18e [formerly ce64d313e776ff27c780b572ef62978ab049f973]]]]
    Former-commit-id: 343844faede0defac2ee480dfc7f2001d9a9e442 [formerly 7792eb4565b2ec2e677d3f59885b957a47bbf5e6] [formerly 1f9eaf75f6fbe1f564a282ce4d136376bb0f3539 [formerly 3c00c02490a7368a003e2b5cad52d1af869cb18e]]
    Former-commit-id: c0d22e2b1f6f65e4749e7dfa369b04723764d12a [formerly 63238e10895ea320549a7e90558f5357df864cd8]
    Former-commit-id: 88760f9341d575899b96914f81261079904b0026
    Former-commit-id: e4c54b1bc447d588bff8af6fec63494a284c82ad

---
+++ cookbooks/wt_streamingconfigservice/recipes/default.rb
@@ -0,0 +1,122 @@
+#
+# Cookbook Name:: wt_streamingconfigservice
+# Recipe:: default
+#
+# Copyright 2012, Webtrends
+#
+# All rights reserved - Do Not Redistribute
+#
+
+log "Deploy build is #{ENV["deploy_build"]}"
+if ENV["deploy_build"] == "true" then 
+    log "The deploy_build value is true so un-deploy first"
+    include_recipe "wt_streamingconfigservice::undeploy"
+else
+    log "The deploy_build value is not set or is false so we will only update the configuration"
+end
+
+   
+log_dir      = File.join("#{node['wt_common']['log_dir_linux']}", "streamingconfigservice")
+install_dir  = File.join("#{node['wt_common']['install_dir_linux']}", "streamingconfigservice")
+
+tarball      = "streaming-configservice-bin.tar.gz"
+java_home    = node['java']['java_home']
+download_url = node['wt_streamingconfigservice']['download_url']
+user = node['wt_streamingconfigservice']['user']
+group = node['wt_streamingconfigservice']['group']
+
+log "Install dir: #{install_dir}"
+log "Log dir: #{log_dir}"
+log "Java home: #{java_home}"
+
+# create the log directory
+directory "#{log_dir}" do
+owner   user
+group   group
+mode    00755
+recursive true
+action :create
+end
+
+# create the install directory
+directory "#{install_dir}/bin" do
+owner "root"
+group "root"
+mode 00755
+recursive true
+action :create
+end
+
+def updateTemplates (install_dir, node)
+
+    log "Updating the template file"
+
+    template "#{install_dir}/conf/config.properties" do
+        source  "config.properties.erb"
+        owner   "root"
+        group   "root"
+        mode    00644
+        variables({
+            :port => node['wt_streamingconfigservice']['port'],
+            :camConnString => node['wt_streamingconfigservice']['camConnString'],
+            :masterConnString => node['wt_streamingconfigservice']['masterConnString'],
+            :includeUnmappedAnalyticsIds => node['wt_streamingconfigservice']['includeUnmappedAnalyticsIds']
+        })
+    end
+
+    template "#{install_dir}/bin/service-control" do
+        source  "service-control.erb"
+        owner "root"
+        group "root"
+        mode  00755
+        variables({
+            :log_dir => log_dir,
+            :install_dir => install_dir,
+            :java_home => java_home,
+            :user => user,
+            :java_class => "com.webtrends.streaming.configservice.ConfigServiceDaemon",
+            :java_jmx_port => "",
+            :java_opts => ""
+    })
+    end	
+end
+
+if ENV["deploy_build"] == "true" then 
+    log "The deploy_build value is true so we will grab the tar ball and install"
+
+    # download the application tarball
+    remote_file "#{Chef::Config[:file_cache_path]}/#{tarball}" do
+    source download_url
+    mode 00644
+    end
+
+    # uncompress the application tarball into the install directory
+    execute "tar" do
+    user  "root"
+    group "root" 
+    cwd install_dir
+    command "tar zxf #{Chef::Config[:file_cache_path]}/#{tarball}"
+    end
+
+    updateTemplates(install_dir, node)
+
+    # delete the application tarball
+    execute "delete_install_source" do
+        user "root"
+        group "root"
+        command "rm -f #{Chef::Config[:file_cache_path]}/#{tarball}"
+        action :run
+    end
+
+    # create a runit service
+    runit_service "streamingconfigservice" do
+    options({
+        :log_dir => log_dir,
+        :install_dir => install_dir,
+        :java_home => java_home,
+        :user => user
+    })
+    end
+else
+    updateTemplates(install_dir, node)
+end
