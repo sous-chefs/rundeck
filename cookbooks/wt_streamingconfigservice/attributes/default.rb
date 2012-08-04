65f9c8e0bd7d244f51e4f34d11964d68c088c567
Mark Alexander <mark.alexander@webtrends.com>
2012-08-04 00:37:53 +0000

    updates to recipes to fix typos
    Former-commit-id: 79025c52da89be035a0abfe6fde94165ea14707c [formerly 18db6aac6cbb8d4f9fe79f83c199c0341e1aa20a] [formerly 7225cfa1130ad2f192722474728d7f3eb8ecd7f2 [formerly d81e2d03c11ed03001b025aebcbff3ec3f9ade17]]
    Former-commit-id: a8099e50fec29dada9c769f6fb87d0786b4eb39c [formerly 5cf3b026f5641128e85ac3e35b92720374f84150]
    Former-commit-id: b14e95678b76b35fff983d40ac3ddf85e22878f4

--- cookbooks/wt_streamingconfigservice/attributes/default.rb
+++ cookbooks/wt_streamingconfigservice/attributes/default.rb
@@ -1,7 +1,2 @@
-default['wt_streamingconfigservice']['camConnString'] = ""
-default['wt_streamingconfigservice']['masterConnString'] = ""
-default['wt_streamingconfigservice']['includeUnmappedAnalyticsIds'] = ""
-default['wt_streamingconfigservice']['port'] = ""
-default['wt_streamingconfigservice']['download_url'] = ""
 default['wt_streamingconfigservice']['user'] = "webtrends"
 default['wt_streamingconfigservice']['group'] = "webtrends"
