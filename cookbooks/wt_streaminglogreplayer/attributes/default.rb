1078d923422de7796f9f33eb4a41f41feeb1aad6
vonnagy <ivan.vonnagy@webtrends.com>
2012-08-07 14:17:29 -0700

    Added 'logtime_catchup' attribute.

--- cookbooks/wt_streaminglogreplayer/attributes/default.rb
+++ cookbooks/wt_streaminglogreplayer/attributes/default.rb
@@ -13,5 +13,6 @@ default['wt_streaminglogreplayer']['java_opts']         = "-Xms1024m -Djava.net.
 default['wt_streaminglogreplayer']['kafka_broker_list'] = nil
 default['wt_streaminglogreplayer']['lock_check_period'] = 15
 default['wt_streaminglogreplayer']['thread_pool_size'] = 100
+default['wt_streaminglogreplayer']['logtime_catchup'] = 200
 default['wt_streaminglogreplayer']['eventtime_log_regex'] = "^.*(\\d{4}-\\d{2}-\\d{2}-\\d{2}-0\\d{4}).*$"
 default['wt_streaminglogreplayer']['znode_root'] = "/LogReplayer"
