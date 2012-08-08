bade528d4da61ba6ab2822472dc6844499e13a2f
David Dvorak <david.dvorak@webtrends.com>
2012-08-08 00:34:02 +0000

    initial deploy recipe for xd mapreduce
    Former-commit-id: 4fde21a5e4618923d51632b83492ecec5fe79d77 [formerly 2d4b59d9afbcc66af47e766f96ce167d3364a527] [formerly c897de4bc41dca7ced386d650899cd6bf66d9a48] [formerly 1770fba10fa0a2efb3deabd9e4ea59436885d783 [formerly 633e566719bf624b42e9e4e70a47ccfffc0f1d01]] [formerly d91db6f6d33d03749d5b8cc952166c026b4ad372 [formerly 8dedd7c769267ffbce86baf41db2184afe99933a] [formerly 4513356ee8e7bb1e07b5ed77ccab506174d80f82 [formerly 1328e70b15f941bd0ea6f7a9341e300bab31d21f [formerly 7d81d863687c8ac0245e068caf283b9d847237df]]]]
    Former-commit-id: 7612fc2f0f4deaacb6eb261213f96bc764b135a6 [formerly f576ac469061a3353635f7df7abb643e7c3ff35f] [formerly 8d0a5522fea577b1a9865ebd217f8af662cde369 [formerly 1328e70b15f941bd0ea6f7a9341e300bab31d21f]]
    Former-commit-id: 36c71c5fccc7eeefc14ff5dd41574830e6f6bc73 [formerly 429756fd427d2bd33f580b1911b354a72e9190de]
    Former-commit-id: 09f76b5775641a7907ebcbda17be82804c21f5ed
    Former-commit-id: d60151f519583c704fae77ca1e3567d64c493902

--- cookbooks/wt_xd/recipes/mapred.rb
+++ cookbooks/wt_xd/recipes/mapred.rb
@@ -2,7 +2,115 @@
 # Cookbook Name:: wt_xd
 # Recipe:: mapred
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, Webtrends Inc.
 #
 # All rights reserved - Do Not Redistribute
 #
+
+# sources
+source_zipfile  = node['wt_xd']['download_url'][/\/([^\/\?]+)(\?.*)?$/, 1]
+source_fullpath = File.join(Chef::Config[:file_cache_path], source_zipfile)
+
+# destinations
+install_dir = File.join(node['wt_common']['install_dir_linux'], 'wt_xd')
+log_dir     = node['wt_common']['log_dir_linux']
+
+# clean up old deploy
+include_recipe 'wt_xd::mapred_undeploy' if deploy_mode?
+
+# create directories
+[install_dir, log_dir].each do |dir|
+	directory dir do
+		owner 'webtrends'
+		group 'webtrends'
+		recursive true
+	end
+end
+
+# deploy build
+if deploy_mode?
+	package 'unzip'
+	remote_file source_fullpath do
+		source node['wt_xd']['download_url']
+		mode 00644
+	end
+	execute 'unzip artifacts' do
+		command "unzip -o #{source_fullpath} -d #{install_dir}"
+		action :run
+	end
+	file source_fullpath do
+		action :delete
+	end
+end
+
+# get zookeeper nodes
+zookeeper_quorum = Array.new
+if not Chef::Config.solo
+	search(:node, "role:zookeeper AND chef_environment:#{node.chef_environment}").each do |n|
+		zookeeper_quorum << n[:fqdn]
+	end
+end
+# fall back to attribs if search doesn't come up with any zookeeper nodes
+if zookeeper_quorum.count == 0
+	node[:zookeeper][:quorum].each do |i|
+		zookeeper_quorum << i
+	end
+end
+
+# configure templates
+%w[environment.properties hbase.properties log4j.mapreduce.fb.xml log4j.mapreduce.tw.xml log4j.xml].each do |template_file|
+	template "#{install_dir}/conf/#{template_file}" do
+		source "#{template_file}.erb"
+		owner 'webtrends'
+		group 'webtrends'
+		mode  00644
+		variables ({
+			# hbase config
+			:hbase_dc_id  => node['hbase']['data_center_id'],
+			:hbase_pod_id => node['hbase']['pod_id'],
+
+			# zookeeper config
+			:zookeeper_quorum     => zookeeper_quorum * ',',
+			:zookeeper_clientport => node['zookeeper']['client_port'],
+
+			# log level
+			:log_level => node['wt_xd']['log_level']
+		})
+	end
+end
+
+# configure scripts
+%w[MapReduceFB.sh MapReduceTW.sh].each do |template_file|
+	template "#{install_dir}/#{template_file}" do
+		source "#{template_file}.erb"
+		owner 'webtrends'
+		group 'webtrends'
+		mode  00755
+		variables ({
+			# locations
+			:install_dir => install_dir,
+			:log_dir     => log_dir,
+			:java_home   => node['java']['java_home']
+		})
+	end
+end
+
+# fix ownership
+[install_dir, log_dir].each do |dir|
+	execute dir do
+		command "chown -R webtrends:webtrends \"#{dir}\""
+		action :run
+	end
+end
+
+# setup cron jobs
+cron 'MapReduceFB' do
+	user 'webtrends'
+	minute '*/15'
+	command File.join(install_dir, 'MapReduceFB.sh')
+end
+cron 'MapReduceTW' do
+	user 'webtrends'
+	minute '*/30'
+	command File.join(install_dir, 'MapReduceTW.sh')
+end
