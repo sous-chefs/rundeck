eb67fdf7b2b6145417df043999a5dbeb11ef4610
Tim Smith <tim.smith@webtrends.com>
2012-08-10 07:51:08 -0700

    Add support for amazon linux, fix mode def, define supported platforms in the metadata
    Former-commit-id: 6965cd41cd5815724e9d70ac87ae63b403c08fca [formerly 783d31e980d156a086e0bdd86ae67c4af006aa1e] [formerly 076a136f07c341a9b48e45888d7290d11de4072a [formerly 32de363d8c371002b9d068e893658c4f8beccc84]]
    Former-commit-id: da5ac623fe88173e6ed711b716eaab3e47b4b14e [formerly cb45030f229021dd106037f3788db01e5528e1f5]
    Former-commit-id: ed7255dcfa2fd002396b8e4924102e6e1abd1968

--- cookbooks/hosts/recipes/default.rb
+++ cookbooks/hosts/recipes/default.rb
@@ -2,15 +2,15 @@
 # Cookbook Name:: hosts
 # Recipe:: default
 #
-# Copyright 2012, Webtrends Inc 
+# Copyright 2012, Webtrends Inc
 #
 # All rights reserved - Do Not Redistribute
 #
 
-if platform?("centos","rhel","scientific")
+if platform?("centos","rhel","scientific","amazon")
   template "/etc/hosts" do
     source "hosts.erb"
-    mode 0644
+    mode 00644
     owner "root"
     group "root"
   end
