cf24f5aabd2d53e088a1c1e328ab96003e8186a5
Tim Smith <tim.smith@webtrends.com>
2012-08-10 07:34:45 -0700

    Fix some odd inconsistent platform support in the collectd cookbook.  Everything now supports amazon, scientific, and fedora where as before there was a mix with metadata supporting certain platforms and the actual recipe supporting others
    Former-commit-id: 0f7ed8a07a84110097ff4499b83a8f9bc9b4157d [formerly ba6e633211f6fa14f7655e943c9e6160414a8e19] [formerly d3a54ed6672a26cbb94b185e93615743bbb95762 [formerly b56069cf934f86f754866ca9edbeec0384219c92]]
    Former-commit-id: 56c6bdbfe71dffd1fe481434583d5b63ef20f588 [formerly 40585473556915ea360e49b8cdb1a0b854999d33]
    Former-commit-id: c4d5a740712fdae014e07c9765511cdbb75716eb

--- cookbooks/collectd/metadata.rb
+++ cookbooks/collectd/metadata.rb
@@ -5,6 +5,6 @@ description      "Install and configure the collectd monitoring daemon"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "1.0.1"
 
-%w{ debian ubuntu centos redhat }.each do |os|
+%w{ debian ubuntu centos redhat amazon scientific fedora}.each do |os|
   supports os
 end
