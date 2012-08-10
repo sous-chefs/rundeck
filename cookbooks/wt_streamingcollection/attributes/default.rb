e05bade57785c673ee8a6c76f40e4e239539b418
vonnagy <ivan.vonnagy@webtrends.com>
2012-08-10 12:35:25 -0700

    Fixed gz file names
    Former-commit-id: 517a2c7262eb2d8e32aa49dc34bf4ed54cc77c7a [formerly 73ecb9cba9d56e95429a3080efa3582fb11f5dd2] [formerly e9d9710a148bf401094f77bdf5107da5798d7d93 [formerly 6527fec784c801fe17de9a67ab24fc3509728de9]]
    Former-commit-id: eaef0b087ae1db320e66afecc1fff395930d5f01 [formerly 703cd32f3f0589f1b1843fe17be2ba431fecf64e]
    Former-commit-id: 35445268203e9f2c5cad5b03466161d3e9d5d512

--- cookbooks/wt_streamingcollection/attributes/default.rb
+++ cookbooks/wt_streamingcollection/attributes/default.rb
@@ -1,5 +1,5 @@
 default['wt_streamingcollection']['user']         = "webtrends"
 default['wt_streamingcollection']['group']        = "webtrends"
-default['wt_streamingcollection']['download_url'] = "http://teamcity.webtrends.corp/guestAuth/repository/download/bt75/.lastSuccessful/streamingcollection-bin.tar.gz"
+default['wt_streamingcollection']['download_url'] = "http://teamcity.webtrends.corp/guestAuth/repository/download/bt75/.lastSuccessful/webtrends-streamingcollection-bin.tar.gz"
 default['wt_streamingcollection']['port']         = "8080"
 default['wt_streamingcollection']['java_opts']    = "-Xms1024m -Djava.net.preferIPv4Stack=true"
