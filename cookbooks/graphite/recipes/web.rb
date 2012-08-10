ae80510dc3100fbdfefc12bbd35b41663fd64a49
Tim Smith <tim.smith@webtrends.com>
2012-08-10 07:46:36 -0700

    Minor read me update, add support for amazon/fedora/scientific, fix mode definitions, add recipe/attribute header blocks

--- cookbooks/graphite/recipes/web.rb
+++ cookbooks/graphite/recipes/web.rb
@@ -1,3 +1,22 @@
+#
+# Cookbook Name:: graphite
+# Recipe:: web
+#
+# Copyright 2011, Heavy Water Software Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 version = node[:graphite][:version]
 pyver = node[:graphite][:python_version]
 
@@ -5,7 +24,7 @@ include_recipe "apache2"
 include_recipe "memcached::default"
 
 # CentOS/RH prerequisites
-if platform?("redhat", "centos")
+if platform?("redhat", "centos", "amazon", "scientific", "fedora")
   %w{ Django django-tagging mod_wsgi pycairo python-ldap }.each do |pkg|
     package pkg
   end
@@ -56,7 +75,7 @@ end
 # Setup the apache site for Graphite
 
 # CentOS/RH
-if platform?("redhat", "centos")
+if platform?("redhat", "centos", "amazon", "scientific", "fedora")
   template "/etc/httpd/sites-available/graphite" do
     source "graphite-vhost.conf.erb"
     mode 0644
@@ -100,7 +119,7 @@ directory "/opt/graphite/storage/whisper" do
 end
 
 cookbook_file "/opt/graphite/bin/set_admin_passwd.py" do
-  mode "755"
+  mode 00755
 end
 
 cookbook_file "/opt/graphite/storage/graphite.db" do
@@ -116,7 +135,7 @@ end
 file "/opt/graphite/storage/graphite.db" do
   owner node['apache']['user']
   group node['apache']['group']
-  mode "644"
+  mode 00644
 end
 
 service "memcached" do
