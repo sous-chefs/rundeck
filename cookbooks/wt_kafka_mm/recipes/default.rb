c250e2d3cf104cd590551e60286c11df9dc59e11
Jeff Berger <Jeff.Berger@webtrends.com>
2012-08-13 11:25:33 -0700

    Adding mirrormaker role and cookbook
    Former-commit-id: ba078f376bc9fca9635b8c80eef300d9683bfe5c [formerly 648b57d736dbcc1eca70071cb43824fbddd582e9] [formerly 9f89d5062bb8604f29f6d140519d5617e3c30733] [formerly 908599479910fc48a6c4c35bf3d5839ced8b0292 [formerly 4486310dfd85b669c773da338b664df5298e59ee]] [formerly 135894374c6cf032a2cb367ec68587d9dd742b15 [formerly 0737c41f47f69981b0ac0afdc274b49759fc2089] [formerly 936ae4593cdeab5e2d4a448398ff1ea53d06eed5 [formerly d799c6d714e4df35ab992e32cbc1ac4c9364c17b [formerly 83aa2f1aa5ec396c51a45d16f8d0e8b6ee9e2d81]]]]
    Former-commit-id: 6610436613b69cb3293d47d9b46892d9f34df687 [formerly f0e6b63be48ed79c26c44f1abe85f1a2fcc95d8f] [formerly 12a725ab1fef453591e5e267a26723b7c2dbc9a5 [formerly d799c6d714e4df35ab992e32cbc1ac4c9364c17b]]
    Former-commit-id: 5de715b493de7b66a05264372794c17effd5a30e [formerly ddccab667e5a14ca164289aa3180e3d3a4f090ec]
    Former-commit-id: 7cb3834ec4c007fbea63bd86bbe3d1ac7615c0fa
    Former-commit-id: a2e8845802997b75c2fb8f0f8a95afd1aa061c3b

---
+++ cookbooks/wt_kafka_mm/recipes/default.rb
@@ -0,0 +1,234 @@
+#
+# Cookbook Name:: wt_streamingcollection
+# Recipe:: default
+#
+# Copyright 2012, Webtrends
+#
+# All rights reserved - Do Not Redistribute
+#
+
+log "Deploy build is #{ENV["deploy_build"]}"
+if ENV["deploy_build"] == "true" then 
+    log "The deploy_build value is true so un-deploy first"
+else
+    log "The deploy_build value is not set or is false so we will only update the configuration"
+end
+   
+log_dir      	= File.join("#{node['wt_common']['log_dir_linux']}", "mirrormaker")
+install_dir	= File.join("#{node['wt_common']['install_dir_linux']}", "mirrormaker")
+
+user = node['wt_mirrormaker']['user']
+group = node['wt_mirrormaker']['group']
+
+java_home    = node['java']['java_home']
+java_opts = node['wt_mirrormaker']['java_opts']
+
+#Get the zookeeper instances in the specified environment
+def getZookeeperPairs(node, env)
+	
+	# get the correct environment for the zookeeper nodes
+	zookeeper_port = node['zookeeper']['client_port']
+	  
+	# grab the zookeeper nodes that are currently available
+	zookeeper_pairs = Array.new
+	if not Chef::Config.solo
+		search(:node, "role:zookeeper AND chef_environment:#{env}").each do |n|
+			zookeeper_pairs << n[:fqdn]
+		end
+	end
+	
+	# fall back to attribs if search doesn't come up with any zookeeper roles
+	# if zookeeper_pairs.count == 0
+	#	node[:zookeeper][:quorum].each do |i|
+	#		zookeeper_pairs << i
+	#	end
+	# end
+
+	# append the zookeeper client port (defaults to 2181)
+	i = 0
+	while i < zookeeper_pairs.size do
+		zookeeper_pairs[i] = zookeeper_pairs[i].concat(":#{zookeeper_port}")
+		i += 1
+	end
+
+	return zookeeper_pairs
+end
+
+#update the config files
+def processConfTemplates (install_dir, node, log_dir)
+
+    	log "Updating the template files"
+ 
+    	# grab the zookeeper nodes that are currently available
+    	zookeeper_pairs_src = getZookeeperPairs(node, node['mirror_source_env'])
+    	zookeeper_pairs_target = getZookeeperPairs(node, node.chef_environment)
+
+    	# Set up the consumer config - The zookeepers in the source environment
+    	template "#{install_dir}/conf/#{node['mirror_source_env']}/consumer.config" do
+    		source  "consumer.config.erb"
+    		owner   "root"
+		group   "root"
+		mode    00644
+		variables({
+			#:zkconnect => zookeeper_pairs_src,
+			#hard coding until environments are ready
+			:zkconnect => ["szoo01.staging.dmz:2181", "szoo02.staging.dmz:2181", "szoo03.staging.dmz:2181"], 
+			:conn_timeout => "10000",
+			:groupid => "mm_#{node['mirror_source_env']}"
+    		})
+    	end
+
+    	# Set up the producer config - The zookeepers in the target environment
+    	template "#{install_dir}/conf/#{node['mirror_source_env']}/producer.config" do
+		source  "producer.config.erb"
+		owner   "root"
+		group   "root"
+		mode    00644
+		variables({
+			:zkconnect => zookeeper_pairs_target 
+		})
+    	end
+
+    	# Set up the topic whitelist
+    	template "#{install_dir}/conf/#{node['mirror_source_env']}/whitelist" do
+    		source  "whitelist.erb"
+		owner   "root"
+		group   "root"
+		mode    00644
+		variables({
+    		})
+    	end
+
+	# log4j
+    	template "#{install_dir}/conf/#{node['mirror_source_env']}/log4j.properties" do
+    		source  "log4j.properties.erb"
+		owner   "root"
+		group   "root"
+		mode    00644
+		variables({
+			:log_file => "#{log_dir}/mirrormaker_#{node['mirror_source_env']}.log"
+    		})
+    	end
+end
+
+#Pull Kafka from the repository and copy necessary files to lib directory
+def getLib(lib_dir)
+
+	tarball = "kafka-#{node[:kafka][:version]}.tar.gz"
+	download_file = "#{node[:kafka][:download_url]}/#{tarball}"
+	install_tmp = "#{Chef::Config[:file_cache_path]}/kafka_mm_install"
+	
+	#download the tar to temp directory
+	remote_file "#{Chef::Config[:file_cache_path]}/#{tarball}" do
+		source download_file
+		mode 00644
+		action :create_if_missing
+	end
+
+	# create the install TEMP dirctory
+	directory install_tmp do
+		owner "root"
+		group "root"
+		mode 00755
+		recursive true
+    	end
+
+    	# extract the source file into TEMP directory
+    	execute "tar" do
+      		user  "root"
+     		group "root"
+      		cwd install_tmp
+      		creates "#{install_tmp}/lib"
+      		command "tar zxvf #{Chef::Config[:file_cache_path]}/#{tarball}"
+    	end
+
+	#copy necessary files to /lib
+    	execute "mv" do
+      		user  "root"
+      		group "root"
+      		command "mv #{install_tmp}/lib/*.jar #{lib_dir}"
+    	end
+
+	#Clean up
+	execute "delete_install_source" do
+    		user "root"
+    		group "root"
+    		command "rm -rf #{Chef::Config[:file_cache_path]}/#{tarball} #{install_tmp}"
+    		action :run
+	end
+end
+
+if ENV["deploy_build"] == "true" then 
+
+	# create the log directory
+	directory "#{log_dir}" do
+		owner   user
+		group   group
+		mode    00755
+		recursive true
+		action :create
+	end
+
+	# create the install directory
+	directory "#{install_dir}/bin" do
+		owner "root"
+		group "root"
+		mode 00755
+		recursive true
+		action :create
+	end
+	
+	# create the conf directory
+	directory "#{install_dir}/conf/#{node['mirror_source_env']}" do
+		owner "root"
+		group "root"
+		mode 00755
+		recursive true
+		action :create
+	end
+
+	# create the lib directory
+	directory "#{install_dir}/lib" do
+		owner "root"
+		group "root"
+		mode 00755
+		recursive true
+		action :create
+	end
+
+	#pull down the mirror maker dependencies and copy to /lib
+	getLib("#{install_dir}/lib")	
+
+	#Set up the control script
+	template "#{install_dir}/bin/service-control" do
+	    	source  "service-control.erb"
+		owner "root"
+		group "root"
+		mode  00755
+		variables({
+			:log_dir => log_dir,
+			:install_dir => install_dir,
+			:java_home => java_home,
+			:user => user,
+			:java_class => "kafka.tools.MirrorMaker",
+			:java_jmx_port => node['wt_monitoring']['jmx_port'],
+			:java_opts => java_opts
+		})
+	end
+
+	#create the run it service for this mirror maker instance 
+	runit_service "mirrormaker_#{node['mirror_source_env']}" do
+		template_name "mirrormaker"	#/templates/sv-mirrormaker-run.erb
+	    	options({
+			:install_dir => install_dir,
+			:user => user,
+			:src_env => node['mirror_source_env']
+	    	})
+    	end
+
+end
+
+#Do this in all situations
+processConfTemplates(install_dir, node, log_dir)
+
+
