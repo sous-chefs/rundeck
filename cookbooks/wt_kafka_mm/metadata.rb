83aa2f1aa5ec396c51a45d16f8d0e8b6ee9e2d81
Jeff Berger <Jeff.Berger@webtrends.com>
2012-08-13 11:25:33 -0700

    Adding mirrormaker role and cookbook

---
+++ cookbooks/wt_kafka_mm/metadata.rb
@@ -0,0 +1,8 @@
+maintainer          "Webtrends, Inc"
+maintainer_email    "Jeff.Berger@webtrends.com"
+license             "All rights reserved"
+description         "Installs/Configures the Kafka mirror maker"
+long_description    IO.read(File.join(File.dirname(__FILE__), 'CHANGELOG.md'))
+version             "0.0.2"
+depends             "java"
+depends             "runit"
