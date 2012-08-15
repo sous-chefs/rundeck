218a25bfeb922e015321db18828aa6ce548b93df
Josh Behrends <josh.behrends@webtrends.com>
2012-08-15 12:45:37 -0700

    Adding cookbook to install and setup gdash on ubuntu
    Former-commit-id: d66ea8c5411a87d1667f84033d8745fdfd25ca6b [formerly 074b714624aaabb8e1dd672b01aef5e81ecb9469] [formerly 3dd9cc44764dea00eb533f22dfe77ae13fa44f11] [formerly 3d621c67d87cbd4d8ecd5ec7e283379e265b2344 [formerly 4d854714a752fc50ec8aa05c74246e80298d8d65]] [formerly 1b86f4c6fca2d493618e47f81ed67a8404ece53e [formerly 195b397e421b5eb99b140d529254e2020e454bd5] [formerly 6d44c9c1bcc574c2afe650cde841d14c1822bdc1 [formerly e301c7d5a7fed5a87e64fbf25ffb7d430d163d88 [formerly 0c6eba5a33747b33e88e9fde9662ccf9aeeb3009]]]]
    Former-commit-id: 5e8ca75e10f1c17a30cee02e4e3a80ece7a15709 [formerly 97573d181bca70c35c44a66488f40a70e911f100] [formerly 36eab9c04d08109114889ead81e2123e01fb27ff [formerly e301c7d5a7fed5a87e64fbf25ffb7d430d163d88]]
    Former-commit-id: 38b0109e207b1625590be2060de0227bb366eaae [formerly 0ecc096e3288125dcb82a9b03acd5818f31dd5fb]
    Former-commit-id: 344610f55b6fc7606c407fbc84e51a50f95751c0
    Former-commit-id: e5d13074a5453b6367da4c634fb803ecdbf22ee5

---
+++ cookbooks/gdash/README.md
@@ -0,0 +1,18 @@
+Description
+===========
+
+Cookbook to automatically deploy the Gdash web interface for
+Graphite using apache2 and Phusion Passenger
+
+Requirements
+============
+
+## Platform:
+
+ * Debian/Ubuntu
+
+## Cookbooks
+
+ * apache2
+
+
---
+++ cookbooks/gdash/attributes/gdash.rb
@@ -0,0 +1,16 @@
+default.gdash.tarfile = "/usr/src/gdash.tgz"
+default.gdash.base = "/opt/gdash"
+default.gdash.url = "https://github.com/ripienaar/gdash/tarball/master"
+
+####
+default.gdash.graphite_url = "http://graph.netiq.dmz"
+####
+
+default.gdash.templatedir = "/var/lib/gdash_templates"
+default.gdash.owner = "www-data"
+default.gdash.group = "www-data"
+default.gdash.basic_auth = false
+#default.gdash.username = "gdash"
+#default.gdash.password = "gdash"
+default.gdash.title = "WT Dashboard"
+default.gdash.refresh_rate = 60
