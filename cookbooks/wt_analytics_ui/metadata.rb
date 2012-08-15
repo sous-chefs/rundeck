ae80600f85ba142b013ee35d3b71b2d6be247352
Kendrick Martin <kendrick.martin@webtrends.com>
2012-08-15 19:06:44 +0000

    Fixing space in metadata.
    Former-commit-id: 6a5f00da599197ebc6fcf543282c8703163faaee [formerly 468e6d155aa2abf22c326112b1533205b02f2cad] [formerly 46301dc621745289d4379dea833f8c56bd105678 [formerly edb9dea7e4d48b36a3dc0c464ef45a0a28ad36c8]]
    Former-commit-id: 89b1e35f907a23e8f9af245e322f0415be7d8b5b [formerly 683e1f7d67b1e052dc42389258535056449ba6f9]
    Former-commit-id: bbc679da424783ddc45b429263d55b3af7c1435d

--- cookbooks/wt_analytics_ui/metadata.rb
+++ cookbooks/wt_analytics_ui/metadata.rb
@@ -4,7 +4,7 @@ license          "All rights reserved"
 description      "Installs and configures A10"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.29"
-depends          "webpi", ">=1.0.3"
+depends          "webpi", ">= 1.0.3"
 depends          "iis"
 depends          "windows"
 depends          "wt_base"
