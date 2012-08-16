233d04b5b0e739ce20b10fc41ceb26aa6de2e7bb
Jeff <bergerj@bergerj-VirtualBox.(none)>
2012-08-15 17:06:40 -0700

    Modifying mirror maker recipe - make source environments a recipe attribute instead of a role attribute and have each machine with the role set up to mirror all remote data centers
    Former-commit-id: 72b047ceb698e17f8082f7b4b21a7fc8d5922efd [formerly 0df87e0d5fe3d34c7c23a590d02c8fcce9e800b6] [formerly 08d82f081a38e8e957abac20f0da56f2d18dbf8b] [formerly a5c4fdc90336f85d782c272e0f0c9332f92ae2d3 [formerly 881bfa7c1e7a5685f48a5ef7a14314a36b7496a0]] [formerly 060670fa9aefa99455ab053164ee032078250124 [formerly c610e6ad76217ffea459b30b2e5a2c356e818dac] [formerly 94c00b0f857a8f451badc55a82e89e539a382c5c [formerly d860a1d803e4dcd3fdff656fe6a05d262237ca1c [formerly 9f2676c21d333f7f7e3362732a77cf92f0e1c4d6]]]]
    Former-commit-id: 1b3cbcaf698acc01d0b5f18b4bc3b6893250fe00 [formerly d377cf7fba50b0d78a20fcd6583bb0ef253dd19e] [formerly 5864effd02a30ea63b2a090df1ce46da0f96ef4e [formerly d860a1d803e4dcd3fdff656fe6a05d262237ca1c]]
    Former-commit-id: 20f5281bf754f1c907dba4e44870dce4e9b3060c [formerly 8a8f3043b19a8fa9768518a5cccefc18f0099dde]
    Former-commit-id: 0a457497f0f5a7a89473510285f7f397d7cdbc52
    Former-commit-id: b12c7aeb8b07055668a02ab5375420f951b66854

--- cookbooks/wt_kafka_mm/attributes/default.rb
+++ cookbooks/wt_kafka_mm/attributes/default.rb
@@ -2,3 +2,6 @@ default['wt_mirrormaker']['user']         = "kafka"
 default['wt_mirrormaker']['group']        = "kafka"
 default['wt_mirrormaker']['java_opts']    = "-Xms1024m -Djava.net.preferIPv4Stack=true"
 
+default['wt_mirrormaker']['src_envs'] = ["H"]
+default['wt_mirrormaker']['topic_white_list'] = "*RawHits"
+
