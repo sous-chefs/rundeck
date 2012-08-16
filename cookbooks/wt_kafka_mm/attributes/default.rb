87437b2c3056b27c2511a24557d45bed6d407ff8
Jeff Berger <Jeff.Berger@webtrends.com>
2012-08-16 14:49:56 -0700

    Add pod prefix logic to zk connect strings in mirrormaker recipe

--- cookbooks/wt_kafka_mm/attributes/default.rb
+++ cookbooks/wt_kafka_mm/attributes/default.rb
@@ -3,6 +3,7 @@ default['wt_mirrormaker']['group']        = "webtrends"
 default['wt_mirrormaker']['java_opts']    = "-Xms1024m -Djava.net.preferIPv4Stack=true"
 default['wt_mirrormaker']['jmx_port']    = "10000"
 
-default['wt_mirrormaker']['src_envs'] = ["G"]
+default['wt_mirrormaker']['src_envs'] = [ ["G", "Lab"] ]
+
 default['wt_mirrormaker']['topic_white_list'] = ".*RawHits"
 
