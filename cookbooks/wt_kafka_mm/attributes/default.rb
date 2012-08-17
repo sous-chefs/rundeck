ee18276e3c6ab26ea5699cfcf11e929b9af3c902
Tim Smith <tim.smith@webtrends.com>
2012-08-16 18:35:28 -0700

    Fix formatting on wt_kafka_mm
    Former-commit-id: ee373ef98f2e7bb859549692590697dda0f889fe [formerly e075e8d68c6d223b74456f5ac5d7c7ad7c455357] [formerly 2eeaf6ee3c437d1c354aa69f6faf9aca9a461cfa] [formerly b34a7d815db650a2d40fc1d579992d95109a458b [formerly abb177406cffc159f248fea763eb8ce9d3e57680]] [formerly 7f89856e1bb53a0a3600080bffd634ed8cde1e89 [formerly 078171e8941132956b7fc9cf0c79be4c6177c6fe] [formerly c8f0f182d780a3de8f5be4e50e12a81f1e140787 [formerly f81590e73ab37b3662bbbe248adfd2294b5c4311 [formerly 00df88f398c74a91891038b72cfd92ac276e78a3]]]]
    Former-commit-id: 02b4598cf021bbfbb93176ee4b31614720bf6ef5 [formerly b5f2bee34b60f144a6c5e428feb229fd261914ba] [formerly 69d13d03557b4849c3f2812b0be811d971bab101 [formerly f81590e73ab37b3662bbbe248adfd2294b5c4311]]
    Former-commit-id: 51d24e14f53b2a9fee34d16af0061e3521a1c8dd [formerly 57d76a815ffff244ec2db22b90734430f76d86af]
    Former-commit-id: 5e72d5cc4d4b45fcb239c38753c6cf52e3c114b9
    Former-commit-id: 32a1b5c38be49372468f5b11613fbaeb1ff98866

--- cookbooks/wt_kafka_mm/attributes/default.rb
+++ cookbooks/wt_kafka_mm/attributes/default.rb
@@ -1,8 +1,17 @@
+#
+# Author:: Jeff Berger (<jeff.berger@webtrends.com>)
+# Cookbook Name:: wt_kafka_mm
+# Attribute:: default
+#
+# Copyright 2012, Webtrends Inc.
+#
+
 default['wt_mirrormaker']['user']         = "webtrends"
 default['wt_mirrormaker']['group']        = "webtrends"
 default['wt_mirrormaker']['java_opts']    = "-Xms1024m -Djava.net.preferIPv4Stack=true"
 default['wt_mirrormaker']['jmx_port']    = "10000"
 
+# This is the environment that mirror maker pull Kafka data from
 default['wt_mirrormaker']['src_envs'] = [ ["G", "Lab"] ]
 
 default['wt_mirrormaker']['topic_white_list'] = ".*RawHits"
