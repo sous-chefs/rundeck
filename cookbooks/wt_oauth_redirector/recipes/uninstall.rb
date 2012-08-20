3ab8b4bcaa64e24802467da5b3808e7dd02000ae
martink <kendrick.martin@webtrends.com>
2012-08-20 15:50:24 -0700

    Added uninstall recipe, and the ignore_failure to service start command.
    Former-commit-id: cb52c23ac60e76b2109a0a372623549910423f99 [formerly d6deed7b7f8fa6c3f060ae3683bb0f6f26aa07e9] [formerly 546b62b09a364a466f8d3ac6b5b4a80844a0ac30] [formerly 67b233637cc612a1b6b429931e21ed369f55c4f3 [formerly 0ce2eef45b9a8d0a08c31d06cc96f1efc1412fa0]] [formerly 0c6c1f8d5eccc9cd3dd3be9fc813712a76e22b4d [formerly b6c372e875100a8c0a34f3e1a44e5c5ab6a63195] [formerly d7cc199432dcd26e2906bd610dac37464d178273 [formerly 897ce75a514004ece61fa167b0b35458ff032628 [formerly 1b070fbf1f92de86d144c2bae72e89701a448bd3]]]]
    Former-commit-id: a18b88ca63cf15e65bc1c746413cef74ae8e570e [formerly b4015004a0f318ae2fa34e7a39446cd14dca40e1] [formerly cff3daf27581be92312a10ddf273bda03f02ce6d [formerly 897ce75a514004ece61fa167b0b35458ff032628]]
    Former-commit-id: 2df19118260eef4e1ca0281175d4d302e963a578 [formerly 3d1787fe4b6c35b61a77a762c62473e15253e5c3]
    Former-commit-id: 67459a9c17758b5313b56ce6d15074f17d6804b4
    Former-commit-id: c3a2437eb77f6c29411a51b9cb06ac84ced1e47b

---
+++ cookbooks/wt_oauth_redirector/recipes/uninstall.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook Name:: wt_oauth_redirector
+# Recipe:: uninstall
+#
+# Copyright 2012, Webtrends
+#
+# All rights reserved - Do Not Redistribute
+#
+
+log_dir     = File.join("#{node['wt_common']['log_dir_linux']}", "oauth_redirector")
+install_dir = File.join("#{node['wt_common']['install_dir_linux']}", "oauth_redirector")
+stop_cmd = "thin stop -C #{install_dir}/oard.thin.yml"
+
+execute "stop application" do
+  command stop_cmd
+  action :run
+  ignore_failure
+end
+
+directory install_dir do
+	action :delete
+end
