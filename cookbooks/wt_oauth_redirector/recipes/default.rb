861fcb2ca3325d50372b25bd3726b55ee1deb202
martink <kendrick.martin@webtrends.com>
2012-08-20 11:26:22 -0700

    Updating recipe for oauth deployment
    Former-commit-id: 08affee093706bdb31db60e6b7c6ffdae85fddc0 [formerly 74c0761a74993ce92273dfddde0e62ae2f746051] [formerly 5ff8392336811ba177f5b543ea9cb5241f4caff4 [formerly 7293ff94c8d1eed1ed1f1c0b0fe1b35f33a7f689 [formerly 76e3c69ff4e2b94cb985137fee0636967c7b9d3a]]]
    Former-commit-id: ef59aeedd207b59dcaa4aca7dab7de574b26e737 [formerly c8bbeec0366eb6bb3f418505ce9bc8a7db7516b9]
    Former-commit-id: 782ade161015009efb41ca359c675efc8ff73e98
    Former-commit-id: e78dc0fdf58b26dc004ee8d5a605b5c7dd651f1f

--- cookbooks/wt_oauth_redirector/recipes/default.rb
+++ cookbooks/wt_oauth_redirector/recipes/default.rb
@@ -7,9 +7,39 @@
 # All rights reserved - Do Not Redistribute
 #
 
+log_dir     = File.join("#{node['wt_common']['log_dir_linux']}", "oauth_redirector")
+install_dir = File.join("#{node['wt_common']['install_dir_linux']}", "oauth_redirector")
+tarball      = node['wt_oauth_redirector']['download_url'].split("/")[-1]
+download_url = node['wt_oauth_redirector']['download_url']
+
+
+start_cmd = "thin start -C #{install_dir}/oard.thin.yml"
+stop_cmd = "thin stop -C #{install_dir}/oard.thin.yml"
+
 # Install gems
 
+gem_package "sinatra"
+gem_package "thin"
+
 # Unpack app
+if ENV["deploy_build"] == "true" then 
+    log "The deploy_build value is true so we will grab the tar ball and install"
+
+    # download the application tarball
+    remote_file "#{Chef::Config[:file_cache_path]}/#{tarball}" do
+    source download_url
+    mode 00644
+    end
+
+    # uncompress the application tarbarll into the install dir
+    execute "tar" do
+    user  "root"
+    group "root" 
+    cwd install_dir
+    command "tar zxf #{Chef::Config[:file_cache_path]}/#{tarball}"
+    end
+end
+
 
 # Set up config templates
 
@@ -28,7 +58,8 @@ template "#{install_dir}/oard.thin.yml" do
 	)
 end
 
-# Start?
+execute "start application" do
+  command start_cmd
+  action :run
+end
 
-start_cmd = "thin start -C #{install_dir}/oard.thin.yml"
-stop_cmd = "thin stop -C #{install_dir}/oard.thin.yml"
