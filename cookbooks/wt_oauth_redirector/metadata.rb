e78dc0fdf58b26dc004ee8d5a605b5c7dd651f1f
martink <kendrick.martin@webtrends.com>
2012-08-20 11:26:22 -0700

    Updating recipe for oauth deployment
    Former-commit-id: 08affee093706bdb31db60e6b7c6ffdae85fddc0 [formerly 74c0761a74993ce92273dfddde0e62ae2f746051] [formerly 5ff8392336811ba177f5b543ea9cb5241f4caff4 [formerly 76e3c69ff4e2b94cb985137fee0636967c7b9d3a]]
    Former-commit-id: ef59aeedd207b59dcaa4aca7dab7de574b26e737 [formerly c8bbeec0366eb6bb3f418505ce9bc8a7db7516b9]
    Former-commit-id: 782ade161015009efb41ca359c675efc8ff73e98

--- cookbooks/wt_oauth_redirector/metadata.rb
+++ cookbooks/wt_oauth_redirector/metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "kendrick.martin@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures OAuth Redirector"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 depends          "ruby", ">= 1.8.7"
