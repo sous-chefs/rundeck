ad063591310f76832612305afe17cd7aa2ebbd87
martink <kendrick.martin@webtrends.com>
2012-08-20 22:05:41 -0700

    Fixing CHANGELOG's and bumping version to 1.0.1 to signify merging 10.5 hot fixes to master.

--- cookbooks/wt_search/metadata.rb
+++ cookbooks/wt_search/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "kendrick.martin@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures Webtrends Search Service"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.1"
 depends          "wt_base"
 depends          "powershell"
