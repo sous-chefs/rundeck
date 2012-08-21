b1313d3d3494bca57d4b53dafb52676d632bd4e0
Kendrick Martin <kendrick.martin@webtrends.corp>
2012-08-21 20:18:19 +0000

    Updating wt_oauth_redirector to allow better attribute control and use of runit.

--- cookbooks/wt_oauth_redirector/metadata.rb
+++ cookbooks/wt_oauth_redirector/metadata.rb
@@ -3,4 +3,5 @@ maintainer_email "kendrick.martin@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures OAuth Redirector"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.3"
+version          "0.0.6"
+depends 		 "runit"
