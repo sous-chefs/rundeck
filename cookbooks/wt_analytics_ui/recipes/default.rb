53f490d049fad979660b26829e2be6c5efaf852e
martink <kendrick.martin@webtrends.com>
2012-08-20 21:45:44 -0700

    Syncing up HBase attribute moves.
    Former-commit-id: 65f37147a5c72b9025271f5b6c7955462530fedd [formerly 1c8f610300b20d7d8840b57c65865647c0984b00] [formerly f07fa2d511106d8d4ee56e1ba4605bf75fd4e9c3 [formerly 0bf28131afcadaa02b03e80c701cf7f41c3f04c7 [formerly 02b0aa5182a775d484f8555e80207275a025f7f4]]]
    Former-commit-id: b03abebc00f53617163fd769d17ba25f3d2d0ecb [formerly c166d4587f4250e3617a79c77c485d8f18c3ae88]
    Former-commit-id: 10e7e69116441e432f33dcc8921111a6535db5e6
    Former-commit-id: 3a4235806607ac6abe6accf3bb60fe1e1d521d7d

--- cookbooks/wt_analytics_ui/recipes/default.rb
+++ cookbooks/wt_analytics_ui/recipes/default.rb
@@ -168,9 +168,8 @@ if deploy_mode?
 
 			# hbase config
 			:hbase_location    => node['hbase']['location'],
-			:hbase_thrift_port => node['hbase']['thrift_port'],
-			:hbase_dc_id       => node['hbase']['data_center_id'],
-			:hbase_pod_id      => node['hbase']['pod_id'],
+			:hbase_dc_id       => node['wt_analytics_ui']['fb_data_center_id'],
+			:hbase_pod_id      => node['wt_common']['pod_id'],
 
 			# cassandra config
 			:cass_host            => node['cassandra']['cassandra_host'],
