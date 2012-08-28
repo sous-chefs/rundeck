3a313e48c198d9427c7d9a9e234db6428e7cc22a
Robert Towne <robert.towne@webtrends.com>
2012-08-28 03:33:11 +0000

    #       modified:   environment.properties.erb

--- cookbooks/wt_xd/metadata.rb
+++ cookbooks/wt_xd/metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "david.dvorak@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures wt_xd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
 depends		"wt_base"
