273d4b914d5944e4d01c159a756d7c5b73579931
vonnagy <ivan.vonnagy@webtrends.com>
2012-08-29 10:43:54 -0700

    Added broker chroot prefix for audit.zk.connect string
    Former-commit-id: 1cbc67843b9140151453ccde3bf6c36ab4b6c1dd [formerly 0247c6f50b5b08ae6e4fa0304b55d30f58080257] [formerly 5b3ae837f778af222fc072ae2699d0aea427d7d3 [formerly a06dbdaf06a70b87916c2f48e8dec2afb8cc3e3b]]
    Former-commit-id: 4ec29cb873b45a00de1212d8f606634ecb0e72d2 [formerly 2a73f6715b6fda6364fdeb3ade0831b31707d1e7]
    Former-commit-id: cc227fe3fe4ac0e488c87e4b55cf544a9db59d1c

--- cookbooks/wt_storm_realtime/metadata.rb
+++ cookbooks/wt_storm_realtime/metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "sean.mcnamara@webtrends.com"
 license          "All rights reserved"
 description      "Installs Webtrends specific storm bits for the Realtime cluster"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.0.6"
+version          "2.0.7"
 depends          "storm"
