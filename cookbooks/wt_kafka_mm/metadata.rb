1364426fd177770be7b423c69efde93661a1b7b6
Jeff Berger <Jeff.Berger@webtrends.com>
2012-08-29 15:57:43 -0700

    Updating mirrormaker to work with production zk prefixing

--- cookbooks/wt_kafka_mm/metadata.rb
+++ cookbooks/wt_kafka_mm/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email    "Jeff.Berger@webtrends.com"
 license             "All rights reserved"
 description         "Installs/Configures the Kafka mirror maker"
 long_description    IO.read(File.join(File.dirname(__FILE__), 'CHANGELOG.md'))
-version             "0.0.11"
+version             "0.0.12"
 depends             "java"
 depends             "runit"
