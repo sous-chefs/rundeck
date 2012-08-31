6ba834b7b792f3e9d5e7a2e121e3ed40e56dc513
vonnagy <ivan.vonnagy@webtrends.com>
2012-08-31 14:28:01 -0700

    Fixed user data issue
    Former-commit-id: 6e09a1f0cf40f33a9558386ec9d6041294df21b3 [formerly d9f349ab9e2efc13457d340b3ea0c31bbf60ff83] [formerly 1ed7b32a1f4924f928d420909d580b9024d1a279 [formerly 07dbf9e7bad80bcee50e13225dcdce053e57bbdf [formerly f9384b2d2dfb4735c388a3c60dc617d69d8a44dd]]]
    Former-commit-id: 1abdc6630522e733726a0c46aa5989806e6089a4 [formerly 4450e6a4e4f5eb2a66393e4554679adbd7603696]
    Former-commit-id: 3bc4729cd52a65e3cc206c9110a2d224038b4cf2
    Former-commit-id: 635230e5746bbb6a1bc0986b5b08df4a90506660

--- cookbooks/wt_cam/recipes/cam_plugins.rb
+++ cookbooks/wt_cam/recipes/cam_plugins.rb
@@ -9,6 +9,8 @@
 # This recipe installs the plugins into the CAM site
 
 #Properties
+pod = node.chef_environment
+user_data = data_bag_item('authorization', pod)
 plugin_install_dir = "#{node['wt_common']['install_dir_windows']}\\Webtrends.Cam\\Plugins"
 
 # Make sure the plugin directory exists
