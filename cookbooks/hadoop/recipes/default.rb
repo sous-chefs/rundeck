44e05833953712b8fb72b8b30dc5d5750c68c1c7
David Dvorak <david.dvorak@webtrends.com>
2012-09-04 22:46:08 +0000

    add support for multiple hadoop/hbase/hive/zookeeper clusters in the same chef environment

--- cookbooks/hadoop/recipes/default.rb
+++ cookbooks/hadoop/recipes/default.rb
@@ -17,104 +17,95 @@
 # limitations under the License.
 #
 
-include_recipe "java"
+include_recipe 'java'
 
 %w[snappy-devel python-simplejson python-cjson].each do |pkg|
-  package pkg do
-    action :install
-  end
+	package pkg do
+		action :install
+	end
 end
 
-hadoop_namenode = search(:node, "role:hadoop_primarynamenode AND chef_environment:#{node.chef_environment}")
-hadoop_namenode = hadoop_namenode.length == 1 ? hadoop_namenode.first[:fqdn] : "localhost"
+hadoop_namenode       = hadoop_search('hadoop_primarynamenode', 1)
+hadoop_backupnamenode = hadoop_search('hadoop_backupnamenode', 1)
+hadoop_jobtracker     = hadoop_search('hadoop_jobtracker', 1)
+hadoop_datanodes      = hadoop_search('hadoop_datanode').sort
 
-hadoop_backupnamenode = search(:node, "role:hadoop_backupnamenode AND chef_environment:#{node.chef_environment}")
-hadoop_backupnamenode = hadoop_backupnamenode.length == 1 ? hadoop_backupnamenode.first[:fqdn] : "localhost"
-
-hadoop_jobtracker = search(:node, "role:hadoop_jobtracker AND chef_environment:#{node.chef_environment}")
-hadoop_jobtracker = hadoop_jobtracker.length == 1 ? hadoop_jobtracker.first[:fqdn] : "localhost"
-
-hadoop_datanodes = Array.new
-search(:node, "role:hadoop_datanode AND chef_environment:#{node.chef_environment}").each do |n|
-  hadoop_datanodes << n[:fqdn]
-end
-
-# determine local_dir (datanodes often have multiples disks, while namenode/jotrackers don't)
-local_dir = search(:node, "name:#{node.name} AND role:hadoop_datanode").length == 1 ? node[:hadoop][:mapred][:local_dir] : node[:hadoop][:mapred][:non_datanode_local_dir]
-
+# determine local_dir (datanodes often have multiples disks, while namenode/jobtrackers don't)
+query = "name:#{node.name} AND role:hadoop_datanode"
+local_dir = search(:node, query).length == 1 ? node.hadoop_attrib(:mapred, :local_dir) : node.hadoop_attrib(:mapred, :non_datanode_local_dir)
 
 # setup hadoop group
-group "hadoop"
+group 'hadoop'
 
 # setup hadoop user
-user "hadoop" do
-  comment "Hadoop user"
-  gid "hadoop"
-  home "/home/hadoop"
-  shell "/bin/bash"
-  supports :manage_home => true
+user 'hadoop' do
+	comment 'Hadoop user'
+	gid 'hadoop'
+	home '/home/hadoop'
+	shell '/bin/bash'
+	supports :manage_home => true
 end
 
 # create the bashrc file for the hadoop user
-cookbook_file "/home/hadoop/.bashrc" do
-  source "bashrc"
-  owner "hadoop"
-  group "hadoop"
-  mode 00644
+cookbook_file '/home/hadoop/.bashrc' do
+	source 'bashrc'
+	owner 'hadoop'
+	group 'hadoop'
+	mode 00644
 end
 
 # setup ssh keys so we can use the easy cluster start/stop scripts
-remote_directory "/home/hadoop/.ssh" do
-  source "ssh"
-  owner "hadoop"
-  group "hadoop"
-  files_owner "hadoop"
-  files_group "hadoop"
-  files_mode 00600
-  mode 00700
+remote_directory '/home/hadoop/.ssh' do
+	source 'ssh'
+	owner 'hadoop'
+	group 'hadoop'
+	files_owner 'hadoop'
+	files_group 'hadoop'
+	files_mode 00600
+	mode 00700
 end
 
 # create the install dir
-directory node[:hadoop][:install_dir] do
-  owner "hadoop"
-  group "hadoop"
-  mode 00744
-  recursive true
+directory node.hadoop_attrib(:install_dir) do
+	owner 'hadoop'
+	group 'hadoop'
+	mode 00744
+	recursive true
 end
 
 # install the hadoop rpm from our repo
-package "hadoop" do
-  action :install
-  version "1.0.3-1"
+package 'hadoop' do
+	action :install
+	version node.hadoop_attrib(:version)
 end
 
 # manage hadoop configs
 %w[core-site.xml log4j.properties fair-scheduler.xml hadoop-env.sh hadoop-policy.xml hdfs-site.xml mapred-site.xml masters slaves taskcontroller.cfg].each do |template_file|
-  template "/etc/hadoop/#{template_file}" do
-    source "hadoop-conf/#{template_file}"
-    mode 00755
-    variables(
-      :namenode => hadoop_namenode,
-      :jobtracker => hadoop_jobtracker,
-      :backupnamenode => hadoop_backupnamenode,
-      :datanodes => hadoop_datanodes,
-      :local_dir => local_dir
-    )
-  end
+	template "/etc/hadoop/#{template_file}" do
+		source "hadoop-conf/#{template_file}"
+		mode 00755
+		variables(
+			:namenode       => hadoop_namenode,
+			:jobtracker     => hadoop_jobtracker,
+			:backupnamenode => hadoop_backupnamenode,
+			:datanodes      => hadoop_datanodes,
+			:local_dir      => local_dir
+		)
+	end
 end
 
 # set perms on hadoop startup scripts since the rpm has them wrong
 %w[start-dfs.sh stop-dfs.sh start-mapred.sh stop-mapred.sh slaves.sh].each do |file_name|
-  file "/usr/sbin/#{file_name}" do
-    mode 00555
-  end
+	file "/usr/sbin/#{file_name}" do
+		mode 00555
+	end
 end
 
 # increase the file limits for the hadoop user
-file "/etc/security/limits.d/123hadoop.conf" do
-  owner "root"
-  group "root"
-  mode 00644
-  content "hadoop  -       nofile  32768"
-  action :create
+file '/etc/security/limits.d/123hadoop.conf' do
+	owner 'root'
+	group 'root'
+	mode 00644
+	content 'hadoop  -       nofile  32768'
+	action :create
 end
