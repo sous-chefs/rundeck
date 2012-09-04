331c573835acb87b5dc66e4dcd0ddbc1e8780e63
Adam Sinnett <sinneta@webtrends.com>
2012-09-04 16:48:26 -0700

    Updated metadata and readme
    Former-commit-id: 29025d0f1c216848282fb1eb4d987bd2e380c8a7 [formerly c881e2e0e7728c7b148c5179b91b1a7ce27a3e0e] [formerly 48d81890535aca59148d97cbe74bcb806455b671 [formerly f7b907d6fd8dec6dd0b08400fa63457f25399912]]
    Former-commit-id: 3ab14e3879770887ca362519885b908b63220b9b [formerly d844036e00b9ff493e9f63edd49607bce17396d7]
    Former-commit-id: 223749133bb5f1774eca5ac6580ffab29aa95726

---
+++ cookbooks/wt_cam_db/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Webtrends, Inc"
+maintainer_email "adam.sinnet@webtrends.com"
+license          "All rights reserved"
+description      "Installs/Configures wt_cam_db"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.2"
