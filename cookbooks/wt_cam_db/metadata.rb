4b4f8dd19e017959f39e1ee9b0545ee8704db9d2
Adam Sinnett <sinneta@webtrends.com>
2012-09-04 16:51:10 -0700

    Added database cookbook dependancy

--- cookbooks/wt_cam_db/metadata.rb
+++ cookbooks/wt_cam_db/metadata.rb
@@ -4,3 +4,4 @@ license          "All rights reserved"
 description      "Installs/Configures wt_cam_db"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.2"
+depends          "database"
