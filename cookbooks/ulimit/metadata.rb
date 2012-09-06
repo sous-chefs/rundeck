9701d6bfc5fdc3d84e4f5b27f59fbc9d2668928c
Tim Smith <tim.smith@webtrends.com>
2012-09-06 12:22:37 -0700

    add ulimit cookbook from Optimize
    Former-commit-id: 542b55b246cd6697be555a6f69b68ef3943748fb [formerly 91fc6d6404aa991c28c8937857b77ad0a7642d16] [formerly d8cd11d0889b9a11162255d6aca9511250a16ca5 [formerly 8d97baa1bf656b2896acfbbeb83c1fc49e0cc7fc]]
    Former-commit-id: 44c42ecba5bd549ef4f5876c597cbe0af30822a1 [formerly 27f3f26b1b16e03af53ffd626b3544ab08c13600]
    Former-commit-id: 804049200289457a52556af864ea6da6db40c83a

---
+++ cookbooks/ulimit/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Webtrends"
+maintainer_email "david.andrew@webtrends.com"
+license          "Apache 2.0"
+description      "Configures ulimit"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
