526e0f89466f8f831a7a190946350e51aa45dc04
Tim Smith <tim.smith@webtrends.com>
2012-09-06 14:53:11 -0700

    Don't create cookbook attributes for kafka_topic and configservice_url when they just get overridden anyways.  Grab configservice_url
    Former-commit-id: 6d484065800225280c9cb510ce6a45c6c12feb03 [formerly 22af02adb7de9331434d538014c63ddd5c319de8] [formerly 02676abec9ed14d84a749797b7a9c7fd0bf5a81c [formerly 86f78f1ca04a0012fd0e98338f92acdc646f83ab]]
    Former-commit-id: 9d63e2b252c0c83e0823154480674906c09beedc [formerly 9fde86445871336c8ff110ab0a1e59abd5024b04]
    Former-commit-id: 85dce358ab6a59b13f66b3d45395f420b23764aa

--- cookbooks/wt_streaminglogreplayer/CHANGELOG.md
+++ cookbooks/wt_streaminglogreplayer/CHANGELOG.md
@@ -1,5 +1,6 @@
 ## 0.0.25
 * Add an JMX port attribute and use that attribute vs the wt_monitoring jmx_port attribute
+* Don't use attributes for kafka_topic and configservice_url that get overridden by the cookbook anyways
 
 ## 0.0.24
 * Replaced static whitelist with url to config service endpoint
--- cookbooks/wt_streaminglogreplayer/attributes/default.rb
+++ cookbooks/wt_streaminglogreplayer/attributes/default.rb
@@ -9,11 +9,9 @@ default['wt_streaminglogreplayer']['user']         = "webtrends"
 default['wt_streaminglogreplayer']['group']        = "webtrends"
 default['wt_streaminglogreplayer']['download_url'] = "http://teamcity.webtrends.corp/guestAuth/repository/download/bt127/.lastSuccessful/webtrends-streaminglogreplayer-bin.tar.gz"
 
-default['wt_streaminglogreplayer']['configservice.url'] = nil 
 default['wt_streaminglogreplayer']['log_extension']     = ".gz"
 default['wt_streaminglogreplayer']['log_share_mount']   = ""
 default['wt_streaminglogreplayer']['share_mount_dir']   = "/srv/logsharedir"
-default['wt_streaminglogreplayer']['kafka_topic']       = "lrRawHits"
 default['wt_streaminglogreplayer']['delete_logs']       = "true"
 default['wt_streaminglogreplayer']['java_opts']         = "-Xms1024m -Djava.net.preferIPv4Stack=true"
 default['wt_streaminglogreplayer']['kafka_broker_list'] = nil
