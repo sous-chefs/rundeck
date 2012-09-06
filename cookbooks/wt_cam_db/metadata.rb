78689edbf8061c5b8e2a7e13b67881e620a40010
Adam Sinnett <sinneta@webtrends.com>
2012-09-06 13:15:18 -0700

    Working version of the wt_cam_db cookbook
    Former-commit-id: e482ff6020568a269e22d1bb3b3b2a2f0b370081 [formerly 8444ac721cf9d4ac39896975d94fec959cbdee00] [formerly 3b3ab694033934a0e04d679956fcf74983be1e0f [formerly 934bc773398dc28da4a72aea6969b3f9cf4587c2]]
    Former-commit-id: 4611cff43865f4bbb93308f185090fa52b15daa7 [formerly 4954dee964a3b3b7053b4b2c29f9619ac4685557]
    Former-commit-id: fa71908b6650e38735dfd7c9beb548d4c17a42b5

--- cookbooks/wt_cam_db/metadata.rb
+++ cookbooks/wt_cam_db/metadata.rb
@@ -3,5 +3,6 @@ maintainer_email "adam.sinnet@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures wt_cam_db"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.1.0"
 depends          "database"
+depends          "windows"
