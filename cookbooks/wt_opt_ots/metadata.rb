0f478cbf2461131c328841d98b9a86a137920bf1
Tim Smith <tim.smith@webtrends.com>
2012-09-09 16:06:31 -0700

    Project Hadden

--- cookbooks/wt_opt_ots/metadata.rb
+++ cookbooks/wt_opt_ots/metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Tim Smith - Webtrends, Inc."
+maintainer_email "hostedops@webtrends.com"
 license          "All rights reserved"
-description      "Installs/Configures wt_opt_ots"
+description      "Installs/Configures Optimize OTS Data Collection"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
