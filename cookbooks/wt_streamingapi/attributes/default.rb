4db50b93da4671dfe65794a18c73aaa0f7a43e79
Tim Smith <tim.smith@webtrends.com>
2012-09-11 17:46:39 -0700

    Make SAPI not fallback to a valid TC url
    Former-commit-id: 65ec13d9294b71a49958e346d189420214039d6d [formerly 151b89a7a1f71b7c7ceb1727727d5a868057e200] [formerly 705393bb2d19d9f581db70642b4b07d263a5ddc6 [formerly 193e28447b732c4c964a73b9ba9fad5c564e350a]]
    Former-commit-id: 08a00049a545389b3c785fbb542c9a18866ce3df [formerly 6fe72f5ecde5786f7d8f93e9ac6d0afef69751a0]
    Former-commit-id: 10c331cb6f17b5c7c7ec2f9133e772c430478224

--- cookbooks/wt_streamingapi/attributes/default.rb
+++ cookbooks/wt_streamingapi/attributes/default.rb
@@ -9,7 +9,7 @@
 
 default['wt_streamingapi']['user']                     = "webtrends"
 default['wt_streamingapi']['group']                    = "webtrends"
-default['wt_streamingapi']['download_url']             = "http://teamcity.webtrends.corp/guestAuth/repository/download/bt76/.lastSuccessful/webtrends-streamingapi-bin.tar.gz"
+default['wt_streamingapi']['download_url']             = ""
 default['wt_streamingapi']['port']                     = "8080"
 default['wt_streamingapi']['java_opts']                = "-Xms2048m -XX:+UseG1GC -Djava.net.preferIPv4Stack=true"
 default['wt_streamingapi']['writeBufferHighWaterMark'] = 1048576
