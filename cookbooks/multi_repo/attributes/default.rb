d90cb8d07017744e1cac451642a936159579e2c7
Tim Smith <tim.smith@webtrends.com>
2012-09-12 18:49:28 -0700

    Fix the path to the repo to match the cookbook that was actually running in prod and fix the link to create the correct link
    Former-commit-id: 7a88f64af5c3f4025f37316080d00057b11fd5a8 [formerly 8bee4588f2660ebdc0e589b3bfe7fe6336d97ba1] [formerly 00fb2f6e1d38731c5e44fdd55c0bf6a94d675d61 [formerly f7d27ae272b932c5322d1a570384234a1879644a]]
    Former-commit-id: 42304604b9bfd743787c2bc3f044106745af1136 [formerly a1ecf34ddd43b9f1bca8ddd85b2cf186af57949a]
    Former-commit-id: ab5a73b028eb30956370f3858121d7602d24cab0

--- cookbooks/multi_repo/attributes/default.rb
+++ cookbooks/multi_repo/attributes/default.rb
@@ -1,4 +1,4 @@
-default['multi_repo']['repo_path'] = "/var/repo"
+default['multi_repo']['repo_path'] = "/srv/repo"
 default['multi_repo']['repo_mount'] = "pdxstore10.netiq.dmz:/ifs/data/osdeploy/repo"
 default['multi_repo']['sysadmin_email'] = ""
 default['multi_repo']['chef_repo_path'] = ""
