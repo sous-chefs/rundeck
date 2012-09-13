dff04ab0e6b88b528f83e1e794f3fbde556b875a
Robert Towne <robert.towne@webtrends.com>
2012-09-13 16:46:21 +0000

    Fix to JobTracker to the twitter/fb mapreduce
    Former-commit-id: 9b5785fd5e8171db893047f50fbdde8c0a644e98 [formerly 2166ced5853217e30b5719472a1377f1e81648b2] [formerly 525cbad4eaa331c288f5f023641588185e2195da [formerly 779877bf98cbf0e58f8625cf653d1baa13e92e44 [formerly cac6edbeacf3a3370ef1ed6c596f9c1c638f968f]]]
    Former-commit-id: 142f77543a720449316c6462b53fd4659537ad1e [formerly 7fee14065cd63f63ea03ba8188982738af2770cd]
    Former-commit-id: a4aa47af9c1aad2698b3c7abaacaf8ce955e4e03
    Former-commit-id: ad7a83810664e567cf9ac912f2e57d32a6a6d083

--- cookbooks/wt_xd/metadata.rb
+++ cookbooks/wt_xd/metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "david.dvorak@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures wt_xd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.3"
+version          "0.0.4"
 depends		"wt_base"
