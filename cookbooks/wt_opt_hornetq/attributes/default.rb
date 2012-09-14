f2a4ae864a0b2007cfdeb307e82f16e487f63b04
Tim Smith <tim.smith@webtrends.com>
2012-09-13 17:37:10 -0700

    Project Hadden
    Former-commit-id: f77a2aec31e3bb731f7f2b3e10298d9c9a5bd673 [formerly 73653aee042bfef9d968da6d406d2fef7c9a645f] [formerly 09e60d04d94e16141b3c360ecd74e5899b6b00e2 [formerly 13119994c8d6d832dec690feace72ca944ce4832]]
    Former-commit-id: bcdfe652d54e7f9f28b796deea99a22ddbc789ac [formerly bf3a2eed04665940e579a10a2b1c960f02cb4bae]
    Former-commit-id: 75aba1335ded3fea65a59807323040dc53094bfb

--- cookbooks/wt_opt_hornetq/attributes/default.rb
+++ cookbooks/wt_opt_hornetq/attributes/default.rb
@@ -12,3 +12,4 @@ default['hornetq']['hornetq_version'] = "hornetq-2.2.5"
 default['hornetq']['type'] = "main"
 default['hornetq']['jboss_gid'] = 1001
 default['hornetq']['jboss_uid'] = 1001
+default['hornetq']['pods'] = {}
