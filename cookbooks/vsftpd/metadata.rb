021818a0de125e2b48bb64dc6e846f57ac1712f7
David Dvorak <david.dvorak@webtrends.com>
2012-09-14 00:52:40 +0000

    using vsftpd as the source ifr for englab pods; minor update to conf file
    Former-commit-id: 1bd9aa93413218611f12e9d871b3a05228d0973a [formerly baf62816b96b8aa8822778a365fc5eb7b017d03e] [formerly 9d49e43d33d57c5f949aeddda0ca5cb5f28b767f [formerly faf6ab2f4c0dfb4787f6c59056b18ebc7e4368f0]]
    Former-commit-id: 47471a440f229b5cdb07c5c81e14befa8cc10472 [formerly 750aacee34b2320d479d547debd810f4de366d44]
    Former-commit-id: 660bfb8b9fab53bd4fbf991af9ebd6a9f0bef891

--- cookbooks/vsftpd/metadata.rb
+++ cookbooks/vsftpd/metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "rberger+maintainer@ibd.com"
 license          "Apache License, Version 2.0"
 description      "Installs/Configures vsftpd for Secure SSL SFTP"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+version          "0.0.2"
