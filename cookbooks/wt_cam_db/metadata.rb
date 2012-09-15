7c82f0472bf4ddbfe2225cddf4934522b486fe3e
Tim Smith <tim.smith@webtrends.com>
2012-09-15 12:42:29 -0700

    Remove the use of the last successful build attribute in the wt_cam_db cookbook
    Former-commit-id: 5276267c792f32db8642d780d311d70d85ef3b74 [formerly 9f085f2c68aae1169bbebf9804eb0facce5769f2] [formerly a32a6ab78337d7852cbf699f1936c40c43215911 [formerly b1c7335887a9b7c608a15d3338c4595f2ac462c9 [formerly f816d96d0b1c957a003097ace2829d00b1980be1]]]
    Former-commit-id: f46ebf796cffaa84d395877b89f913068cb22d19 [formerly 3eb1937de00663dd74e982c1752531efcbfb947c]
    Former-commit-id: 8b5f72b8d614d70a19579799c27cadc9cc0227ac
    Former-commit-id: dd56b24b22ae32a0cd07e5cb516217bb655386f3

--- cookbooks/wt_cam_db/metadata.rb
+++ cookbooks/wt_cam_db/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "adam.sinnet@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures wt_cam_db"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 depends          "database"
 depends          "windows"
