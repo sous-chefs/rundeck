a91791c472971b1f840d5d8562f097226ddc1f2a
Tim Smith <tim.smith@webtrends.com>
2012-09-15 12:42:29 -0700

    Remove the use of the last successful build attribute in the wt_cam_db cookbook
    Former-commit-id: 7c82f0472bf4ddbfe2225cddf4934522b486fe3e [formerly dd56b24b22ae32a0cd07e5cb516217bb655386f3] [formerly 8b5f72b8d614d70a19579799c27cadc9cc0227ac] [formerly f46ebf796cffaa84d395877b89f913068cb22d19 [formerly 3eb1937de00663dd74e982c1752531efcbfb947c]] [formerly 5276267c792f32db8642d780d311d70d85ef3b74 [formerly 9f085f2c68aae1169bbebf9804eb0facce5769f2] [formerly a32a6ab78337d7852cbf699f1936c40c43215911 [formerly b1c7335887a9b7c608a15d3338c4595f2ac462c9 [formerly f816d96d0b1c957a003097ace2829d00b1980be1]]]]
    Former-commit-id: bfe5b0e33155a5cd44201b6aae42dbaeb7e27d24 [formerly 8467473155d6b7e1776ab8ca6501d5584305035c] [formerly c043a52e30c146e7c72dbc647ccf064761985dad [formerly b1c7335887a9b7c608a15d3338c4595f2ac462c9]]
    Former-commit-id: 2b45aae2be61559cfeb3277bcf0c7e18948d8a87 [formerly 6e0a974b0af8d96f6468d3e84ef6a6cbbc16f400]
    Former-commit-id: 41702f4724af3d8116b981240ad51b372503a7a3
    Former-commit-id: 4f0162880562a5e1543b0b3a3d31f2102d800906

--- cookbooks/wt_cam_db/attributes/default.rb
+++ cookbooks/wt_cam_db/attributes/default.rb
@@ -9,4 +9,4 @@
 default['wt_cam_db']['port'] = 1433
 default['wt_cam_db']['major_version'] = 1
 default['wt_cam_db']['minor_version'] = 0
-default['wt_cam_db']['download_url'] = "http://teamcity.webtrends.corp/guestAuth/repository/downloadAll/bt29/.lastSuccessful/artifacts.zip"
+default['wt_cam_db']['download_url'] = ""
