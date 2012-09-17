6a3add77bd5d54ba8c3b304bd100063ff0b5a5c4
Tim Smith <tim.smith@webtrends.com>
2012-09-17 11:20:14 -0700

    Revert "Import webpi version 1.0.2"
    This reverts commit ce2f855f2751dc721cf2032f4b6dac9b091da3cd [formerly 0a027a31178c621f2dc342b0862740b205d21bd0] [formerly 9b0fb5853b9c0e935e46320edee24712e3387d69] [formerly 7d8622cabad4dbc55ebc12e3fd5ee8453391e65e [formerly 4dc1f6fcded3a1a9e5b50da2a7d4d5faf25e9b32]] [formerly 83e99637676bc82a666cc3d5f7cdddbc2e028e6c [formerly e93cd1d85a42ebcb73d98c8568ddd36ea1735143] [formerly 738383fc90c5a10a2d213354977676237b2015fa [formerly a223b5f0a21b47918e07ee02c5060a758fc30709]]] [formerly fc0bd22c7f15ab5783ed48ee3c07ecb5bf7bf54c [formerly 840ee7bb2d6bfbfbfbb7b613d2b9a0301901d04b] [formerly 200c0e6cdfdd76f9a52d01a95c472de1f25e4fd3] [formerly b7eb08ed84cb78d2f1bacba26f0cf2fa453666c7 [formerly 20696be39d1291f1cb195759549fa3c8e4e3310c]] [formerly 34a91e1711ce1327f7c4beb9a0766ebc0abfaf60 [formerly f3ac8cb495cadb1ce870382e6ac979d7bf141131] [formerly 232842f028fa31a99decc176fba32496589dffa6 [formerly a223b5f0a21b47918e07ee02c5060a758fc30709 [formerly a2fd095a00ee1cff3fb35f9c8002ff6463f36f5a]]]]].
    
    
    Former-commit-id: 01e4f90ff2ffd4cde520342be368dcb74705dd00 [formerly 5f79fe7c58b4cf51aa01148ebaba5d92102cb568] [formerly 9f5a8c3e1ee7e46d2b0ab679a4b555db4d982997] [formerly 46870e4034cf92beb89c733c49230d20c23add0a [formerly 7b3bf96749ced91eb4a32172c5c471ce69de0561]] [formerly 4bb01105a594d76636a87af5c59f8f167e79dda5 [formerly 25d9671e50e9be98e19f82c9e6908768ff0174e8] [formerly 7c6d3c0522be7df07269e8e2ceff887db0cd49ca [formerly 18ddc32e89e4c553b545cd91debbdf009c828287 [formerly b9b0c938e6feed65e1bd2102cce1b929f6b5fb39]]]]
    Former-commit-id: 471b5919fa27e6e973e1e5b62c2dc8bbafebad50 [formerly ee09c09341056c348cf5ad30c3f71bb9a71a27af] [formerly 74d8e84248bb97040c8f3e803d29ecbd4051153c [formerly 18ddc32e89e4c553b545cd91debbdf009c828287]]
    Former-commit-id: 5b2c233ebe45e5ba6fc8a33f9fd5373f2203f84d [formerly 2457f18c29c75f788b7fe96bdb5f7b3069e3e4bd]
    Former-commit-id: f5ba427d95f94a8d2a82126c5468394aac163dd3
    Former-commit-id: 4cc6bc489a02a6598e795d555fca22a83ddf8ada

--- cookbooks/webpi/providers/product.rb
+++ cookbooks/webpi/providers/product.rb
@@ -27,6 +27,7 @@ action :install do
   unless installed?
     cmd = "#{webpicmdline} /products:#{@new_resource.product_id} /suppressreboot"
     cmd << " /accepteula" if @new_resource.accept_eula
+	cmd << " /XML:#{node['webpi']['xmlpath']}" if node['webpi']['xmlpath']
     shell_out!(cmd, {:returns => [0,42]})
     @new_resource.updated_by_last_action(true)
     Chef::Log.info("#{@new_resource} added new product '#{@new_resource.product_id}'")
