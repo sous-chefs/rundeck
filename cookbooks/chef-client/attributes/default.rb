9fba725544d14b8d20026911da2f5787b49166d2
Tim Smith <tim.smith@webtrends.com>
2012-09-17 19:02:13 +0000

    Import chef-client version 1.2.0

--- cookbooks/chef-client/attributes/default.rb
+++ cookbooks/chef-client/attributes/default.rb
@@ -23,11 +23,16 @@ require 'rbconfig'
 default["chef_client"]["interval"]    = "1800"
 default["chef_client"]["splay"]       = "20"
 default["chef_client"]["log_dir"]     = "/var/log/chef"
+default["chef_client"]["log_file"]    = nil
+default["chef_client"]["log_level"]   = :info
+default["chef_client"]["verbose_logging"] = true
 default["chef_client"]["conf_dir"]    = "/etc/chef"
 default["chef_client"]["bin"]         = "/usr/bin/chef-client"
 default["chef_client"]["server_url"]  = "http://localhost:4000"
 default["chef_client"]["validation_client_name"] = "chef-validator"
-default["chef_client"]["cron"] = { "minute" => "0", "hour" => "*/4" }
+default["chef_client"]["cron"] = { "minute" => "0", "hour" => "*/4", "path" => nil}
+default["chef_client"]["environment"] = nil
+default["chef_client"]["load_gems"] = {}
 
 case platform
 when "arch"
@@ -35,7 +40,7 @@ when "arch"
   default["chef_client"]["run_path"]    = "/var/run/chef"
   default["chef_client"]["cache_path"]  = "/var/cache/chef"
   default["chef_client"]["backup_path"] = "/var/lib/chef"
-when "debian","ubuntu","redhat","centos","fedora"
+when "debian","ubuntu","redhat","centos","fedora","suse"
   default["chef_client"]["init_style"]  = "init"
   default["chef_client"]["run_path"]    = "/var/run/chef"
   default["chef_client"]["cache_path"]  = "/var/cache/chef"
@@ -52,14 +57,15 @@ when "openindiana","opensolaris","nexentacore","solaris2"
   default["chef_client"]["backup_path"] = "/var/chef/backup"
 when "windows"
   default["chef_client"]["init_style"]  = "winsw"
-  default["chef_client"]["run_path"]    = "C:/var/run/chef"
-  default["chef_client"]["cache_path"]  = "C:/var/chef/cache"
-  default["chef_client"]["backup_path"] = "C:/var/chef/backup"
   default["chef_client"]["conf_dir"]    = "C:/chef"
-  default["chef_client"]["bin"]         = File.join(Gem.bindir, "chef-client")
+  default["chef_client"]["run_path"]    = "#{node["chef_client"]["conf_dir"]}/run"
+  default["chef_client"]["cache_path"]  = "#{node["chef_client"]["conf_dir"]}/cache"
+  default["chef_client"]["backup_path"] = "#{node["chef_client"]["conf_dir"]}/backup"
+  default["chef_client"]["log_dir"]     = "#{node["chef_client"]["conf_dir"]}/log"
+  default["chef_client"]["bin"]         = "C:/opscode/chef/bin/chef-client"
   #Required for minsw wrapper
   default["chef_client"]["ruby_bin"]    = File.join(RbConfig::CONFIG['bindir'], "ruby.exe")
-  default["chef_client"]["winsw_url"]   = "http://maven.dyndns.org/2/com/sun/winsw/winsw/1.8/winsw-1.8-bin.exe"
+  default["chef_client"]["winsw_url"]   = "http://repo1.maven.org/maven2/com/sun/winsw/winsw/1.9/winsw-1.9-bin.exe"
   default["chef_client"]["winsw_dir"]   = "C:/chef/bin"
   default["chef_client"]["winsw_exe"]   = "chef-client.exe"
 else
