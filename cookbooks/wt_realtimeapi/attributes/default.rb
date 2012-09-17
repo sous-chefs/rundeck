2b0daedf0084c1ecb9e0bc18247ccc9049fa39f9
Tim Smith <tim.smith@webtrends.com>
2012-09-16 20:26:46 -0700

    realtimeapi version 0.0.24:  Find Zookeeper using search, use a cookbook attribute for the JMX port, resolve food critic warnings and generally clean things up a bit
    Former-commit-id: cd273dd7dbdfd35432f5e6c0ce7253b7167eccef [formerly b64261b6a875bbdbadcc1868eeb02b1c2d7a6652] [formerly 39e954f4bfe5f12ed583d593e189c7fd0eae6276 [formerly 273779acf0f639d890f52761e956518dcffb8777 [formerly f2c5fd564831376178a8a76fd408b56c7589eecf]]]
    Former-commit-id: ad0af3bcf5b6c208f5171f6934747c78ef557f1e [formerly 858193b64da0053096307349261ec031f6fec924]
    Former-commit-id: b475bedf11c18ca8a26d6316b3ed7c8f834edb8b
    Former-commit-id: 5a36f246da3acddd9e597f20c5f13f84fa444460

--- cookbooks/wt_realtimeapi/attributes/default.rb
+++ cookbooks/wt_realtimeapi/attributes/default.rb
@@ -12,3 +12,4 @@ default['wt_realtimeapi']['group']        = "webtrends"
 default['wt_realtimeapi']['port']         = "8080"
 default['wt_realtimeapi']['java_opts']    = "-Xms1024m -Djava.net.preferIPv4Stack=true"
 default['wt_realtimeapi']['download_url'] = ""
+default['wt_realtimeapi']['jmx_port'] = 9999
