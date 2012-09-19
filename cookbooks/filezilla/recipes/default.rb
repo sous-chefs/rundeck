92bb27e7d6f7a598dab7895f4e9c63f738d92a90
Tim Smith <tim.smith@webtrends.com>
2012-09-18 18:56:43 -0700

    Missed one of the open sourced cookbooks
    Former-commit-id: d2e052ab8d4f601b2d7687385412e343bfd00acb [formerly d664dd8b1c50d7c66d6cca111b8b8b6eaf17cad2] [formerly 10aeb14812714197e43db398ebd434000dc1e46a [formerly 812dafa3f6424ca7b7c6cbef57bd5d7268b8baed]]
    Former-commit-id: b77ae9f7717d6c6356811e68b91d75497a751bbf [formerly faed7b135d565490ab7933c148bf37b4268213b5]
    Former-commit-id: 6de159e8f49fcf51c9a919a00f8ce6fd5422f619

--- cookbooks/filezilla/recipes/default.rb
+++ cookbooks/filezilla/recipes/default.rb
@@ -2,11 +2,19 @@
 # Cookbook Name:: filezilla
 # Recipe:: default
 #
-# Copyright 2012, Webtrends Inc.
+# Copyright 2012, Webtrends, Inc.
 #
-# All rights reserved
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# Code based off the PowerShell cookbook by Seth Chisamore
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 if platform?("windows")
     windows_package "FileZilla Client 3.5.3" do
