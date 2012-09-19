e62e2d40fc3253efadc0e93982fdbbe61facf9c9
Tim Smith <tim.smith@webtrends.com>
2012-09-18 18:55:55 -0700

    Our open sourced cookbooks are open source.  Take out the do not distribute warning and replace it with the Apache license
    Former-commit-id: a1a98818345db1044100e531a3048941891e354a [formerly 937f04cafe0eed28a4705a1b26a3eed232db0830] [formerly e51e5e477961a1be288fc7ff391e5315d8ddc91f] [formerly 15cdb0e5a051a42f2f1290cd30a34f88a18d9758 [formerly b446360429f8d0662893863b62db9da0f9bbf781]] [formerly 28ba33858815867bbc7456cfcaee3ce730dcddec [formerly 03ec660969dd0bed308dc6974aceb0e31944fcce] [formerly f10d11e6c34f6d9dd9dcf6d6e491c2b13315bc96 [formerly 8eafca51fd534e922b2d18d678ccaf985318337d [formerly 707b0695bd0ad48b53cec2c29aa9c9bc48999c43]]]]
    Former-commit-id: 60f8a631cc3f6791bb13ffe988fca6c6b995704b [formerly 0f4f47cbf674e08a909e8ca0eb133bd5a5f30836] [formerly ac3166869cd917951f18d2380fe69d341deb8b6e [formerly 8eafca51fd534e922b2d18d678ccaf985318337d]]
    Former-commit-id: ae2f965b3276d5837a2d0bb78024b67736c97c6a [formerly eb0104a2b159614ab09ce132a94f3554232951c2]
    Former-commit-id: 51559ee19392b0fc1fe5d7fbbdec28d99dac248f
    Former-commit-id: 72cf1dc20ba143df14b5d022c3d96ef7fe81d1a9

--- cookbooks/storm/recipes/undeploy-supervisor.rb
+++ cookbooks/storm/recipes/undeploy-supervisor.rb
@@ -2,10 +2,19 @@
 # Cookbook Name:: storm
 # Recipe:: undeploy
 #
-# Copyright 2012, Webtrends
+# Copyright 2012, Webtrends, Inc.
 #
-# All rights reserved - Do Not Redistribute
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 runit_service "supervisor" do
     action :disable
@@ -23,7 +32,7 @@ service "supervisor" do
   action [:stop]
   stop_command "force-stop"
   ignore_failure true
-end 
+end
 
 # and just in case that did not work, we do a kill on all storm user processes
 execute "kill" do
