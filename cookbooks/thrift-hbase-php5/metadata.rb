2d9dd1ad2c6a9277c813daa14cd7c7f53ff6eeee
Tim Smith <tim.smith@webtrends.com>
2012-09-18 18:51:59 -0700

    License the cookbook, provide a header in the recipe, fix mode declaration.  Bump version to 0.0.2

--- cookbooks/thrift-hbase-php5/metadata.rb
+++ cookbooks/thrift-hbase-php5/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email  "sean.mcnamara@webtrends.com"
 license           "Apache 2.0"
 description       "Installs thrift hbase pgp5"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version           "0.0.1"
+version           "0.0.2"
 
 
