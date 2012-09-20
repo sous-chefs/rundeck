64c2744372edc401bd8b742b69c8e37f3b2e21bb
Tim Smith <tim.smith@webtrends.com>
2012-09-20 22:55:13 +0000

    Import nginx version 1.0.2

--- cookbooks/nginx/attributes/passenger.rb
+++ cookbooks/nginx/attributes/passenger.rb
@@ -3,5 +3,5 @@
 #
 node.default["nginx"]["passenger"]["version"] = "3.0.12"
 node.default["nginx"]["passenger"]["root"] = "/usr/lib/ruby/gems/1.8/gems/passenger-3.0.12"
-node.default["nginx"]["passenger"]["ruby"] = ""
+node.default["nginx"]["passenger"]["ruby"] = %x{which ruby}.chomp
 node.default["nginx"]["passenger"]["max_pool_size"] = 10
