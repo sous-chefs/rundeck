eaba5bdd3ef30917d14ae5b3930a69bcab46978c
Tim Smith <tim.smith@webtrends.com>
2012-09-20 03:24:51 -0700

    Fix firefox download URL
    Former-commit-id: cbcb93bc2867cc20bebe1c9e0e2313b619f986b4 [formerly a6a973b222616bffa9ba0de96b1ac126ae71d7cb] [formerly ae1cdd61d05a88a326093ec9d37eee6acef4b598 [formerly 8ad3cb2236d37d758872628fcdf5af86700df761 [formerly ea2cd512156f3d841daa5f40b78a7ef4e3abe209]]]
    Former-commit-id: 72527e02815129a3d19a781956afb93651cbc447 [formerly b1550f5ec6bbe5cf2a9ece56145643f91b01c414]
    Former-commit-id: 0b9a9a390bf0c6e867b57015a0c857d184ce63a2
    Former-commit-id: d1462a61b036d736aeebf0a831a628a0529c35ac

--- cookbooks/firefox/metadata.rb
+++ cookbooks/firefox/metadata.rb
@@ -4,6 +4,6 @@ license           "Apache 2.0"
 license          "All rights reserved"
 description      "Installs/Configures Firefox for Windows"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.2"
+version          "1.0.3"
 supports         "windows"
 depends          "windows"
