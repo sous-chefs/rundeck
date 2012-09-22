a6b3e9cbffd9c8b229d8afed211107f140475ee8
Tim Smith <tim.smith@webtrends.com>
2012-09-21 19:31:24 -0700

    Initial commit of the wt_lfm_ftp cookbook
    Former-commit-id: b6cb6df5805d80e4d3962aede561bbbfa907c7d4 [formerly 1a4ae1e414a41bf48d766ec9a101a3af0bcf2d61] [formerly 579d9311ce12347d24512992b7efcf61c41f6411] [formerly 1d5fab2d4f2345982390973e41ddaef0fac5442a [formerly f714c2f6fc8f2678382195aae4ff5613bef92a88]] [formerly bb60c776b61e959b30d68be1d06be1f8cc305cfa [formerly c81a4b28e66420f37b6ddf98690a1cedbef43b09] [formerly 47584ae1db59d5552bb0ac1f54834c62c14dca66 [formerly 2a59b91da40c88d342cfebd8c21c505b13bb1008 [formerly 34d5b6990197b59aabc96ee95369425963da79d7]]]]
    Former-commit-id: bb02cf72284c52b65d017fed7a71b18636bce74a [formerly 3b9c7c9656a6cf19d7ef0e4e73a9269b1b3b55b3] [formerly 7b31a1cfcf61f8d782a0089558851b1a2be83e35 [formerly 2a59b91da40c88d342cfebd8c21c505b13bb1008]]
    Former-commit-id: dc03b53984ca4c86a603fdd09e6f76ea67c951c1 [formerly 050bd9d854aa84b4c774413fc03360a0bc071aba]
    Former-commit-id: 38e3c8faa683dced6a42d55ff700c5f2bfc136e7
    Former-commit-id: 64ed4a717ee59cc7d638b73b4ee0a215cf81165d

---
+++ cookbooks/wt_lfm_ftp/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Tim Smith"
+maintainer_email "tim.smith@webtrends.com"
+license          "All rights reserved"
+description      "Installs/Configures the FTP servers for LFM"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+depends          "vsftp"
