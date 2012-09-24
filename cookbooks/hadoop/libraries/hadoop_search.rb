15dacf61b193403e690cb2e81f9a4569c2cab107
David Dvorak <david.dvorak@webtrends.com>
2012-09-24 19:56:22 +0000

    treat default and no cluster_name as the same cluster when searching for roles.
    Former-commit-id: 6dd3647ca4b92b5ae10436748fbcfa60c68573a6 [formerly 6a3059dda8fb67863e59dc2a83803cd67f734ae0] [formerly 26e11a6ef497a1677639d7e4aba37266c1f9cfc1 [formerly f9b1f2187e0bb7b130398c48390822dfb00a3999 [formerly 5bdb4516424ee9058fc5d7214fc3ce1c7e5bf869]]]
    Former-commit-id: 4f55817a13fd7c3db1f96b1c2a06579bef0e4c04 [formerly 98adb68565e1d49b8140a458879bfc091174e5c7]
    Former-commit-id: 5c1db1f32a83b61dc6d12300b6184b7c6d6adc3b
    Former-commit-id: 2676721edf8fd2c3e129341e93f3c344df0db5b2

--- cookbooks/hadoop/libraries/hadoop_search.rb
+++ cookbooks/hadoop/libraries/hadoop_search.rb
@@ -12,20 +12,59 @@ module HadoopSearch
 
 	def hadoop_search(role, limit = 1000)
 
-		query =  "chef_environment:#{node.chef_environment}"
-		query << " AND roles:#{role}"
-		query << " AND hadoop_cluster_name:#{node[:hadoop][:cluster_name]}"
+		search_timeout = 120 # seconds
+
+		Chef::Log.info "hadoop_cluster_name: #{node[:hadoop][:cluster_name]}"
 
 		results = Array.new
-		search(:node, query).each do |n|
-			results << n[:fqdn]
+
+		if node[:hadoop][:cluster_name] == 'default'
+
+			# search for nodes with default cluster name
+			query =  "chef_environment:#{node.chef_environment}"
+			query << " AND role:#{role}"
+			query << " AND hadoop_cluster_name:#{node[:hadoop][:cluster_name]}"
+
+			i = 0
+			while results.count == 0 && i < search_timeout
+				search(:node, query).each {|n| results << n[:fqdn] }
+				if results.count == 0
+					Chef::Log.warn "hadoop_search: no results, sleeping..."
+					i += 5
+					sleep 5
+				end
+			end
+
+			# search for nodes that have no cluster name
+			query =  "chef_environment:#{node.chef_environment}"
+			query << " AND role:#{role}"
+			query << " AND NOT hadoop_cluster_name:*"
+			search(:node, query).each {|n| results << n[:fqdn] }
+
+		else
+
+			# search for nodes with a non-default cluster name
+			query =  "chef_environment:#{node.chef_environment}"
+			query << " AND role:#{role}"
+			query << " AND hadoop_cluster_name:#{node[:hadoop][:cluster_name]}"
+
+			i = 0
+			while results.count == 0 && i < search_timeout
+				search(:node, query).each {|n| results << n[:fqdn] }
+				if results.count == 0
+					Chef::Log.warn "hadoop_search: no results, sleeping..."
+					i += 5
+					sleep 5
+				end
+			end
+
 		end
 
-		if (results.length == 0 || results.length > limit)
-			Chef::Log.error "hadoop_search: #{role}: nodes found: #{results.length}"
+		if results.count == 0 || results.count > limit
+			Chef::Log.error "hadoop_search: #{role}: nodes found: #{results.count}"
 		end
-
-		Chef::Log.debug "hadoop_search: #{role}: nodes found: #{results.length}"
+		Chef::Log.debug "hadoop_search: #{role}: nodes found: #{results.count}"
+		Chef::Log.warn  "hadoop_search: slept for #{i} seconds." if i > 0
 
 		results
 
