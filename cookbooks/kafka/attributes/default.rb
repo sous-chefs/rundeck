555c8d15ce17662dc91208f366207bc097fc1d44
Marek Dolgos <marek.dolgos@webtrends.com>
2012-09-24 18:57:43 -0700

    Kafka update
    Former-commit-id: e8010fa810c1f5cae5736b1bf89588b8b09f4e9e [formerly 8b9302545e575e0e588a69fcdedd88dc948a72a2] [formerly eadf3862a7d39a889424982cbdeddcfda2493326 [formerly 72f4b919f7fc60235e19d123271d159a60823408]]
    Former-commit-id: fdc7a136c1997e2258bf32c79de2687d3420a0f1 [formerly 4f28d5f2e0debc9cefb65f1cae71e31f0c971325]
    Former-commit-id: a3808453f528f8c9e9d9334695d604f3d269c895

--- cookbooks/kafka/attributes/default.rb
+++ cookbooks/kafka/attributes/default.rb
@@ -34,6 +34,7 @@ default[:kafka][:log_flush_interval] = 10000
 default[:kafka][:log_flush_time_interval] = 1000
 default[:kafka][:log_flush_scheduler_time_interval] = 1000
 default[:kafka][:log_retention_hours] = 168
+default[:kafka][:zk_connectiontimeout] = 10000
 
 default[:kafka][:user] = "kafka"
 default[:kafka][:group] = "kafka"
