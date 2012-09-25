4c25780204303662debf5f977b28c69394584b7a
David Dvorak <david.dvorak@webtrends.com>
2012-09-25 23:19:42 +0000

    add unbound cookbook
    Former-commit-id: 5ee2ac0485fbd0b5f27446a22bbff4771e2aa366 [formerly 4895aba4fa0c40cce2ea4de15e8b814d69765983] [formerly c0bcec87b8448aa2d95e14b3e32d5b494e4fbafc] [formerly 0c4c32460f6b512fb2f38b191b87b92569653a15 [formerly 02815d5c79c563c03fbb2dc6e014d12885ba04cf]] [formerly 4cd618421a39578f81f4e80f179fdebc712a5817 [formerly 84766d86dec9edd1e860c200248fc7ff9d0ab030] [formerly 455bd5ae78cbedc47216594c917727e036254420 [formerly a40aabd7f83d189e552919e6fb7161ebc0849885 [formerly fb17ecedebd57dc4cb4b56884fab56c4c3b36307]]]]
    Former-commit-id: 16da6f1aaed07adf68d3c067c58d7f789bf65fb3 [formerly 36ee0613bd7ad539027b4531367245744e6011a4] [formerly 0ee6e477da640ffb42ceeb0e0a76cf6a67474e6a [formerly a40aabd7f83d189e552919e6fb7161ebc0849885]]
    Former-commit-id: 421a76893c2e075b12d1cc5c9a1c69252cd789bf [formerly 637a6d8829437c105ded95843a23d532e195e24d]
    Former-commit-id: 5195d14728231b13410946be551f3cc768c2680c
    Former-commit-id: f339d94da2244fb2a7f29caa59de1d8dcf636dd0

---
+++ cookbooks/unbound/recipes/default.rb
@@ -0,0 +1,77 @@
+#
+# Cookbook Name:: unbound
+# Recipe:: default
+#
+# Copyright 2011, Joshua Timberman
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+root_group = value_for_platform(
+  "freebsd" => { "default" => "wheel" },
+  "default" => "root"
+)
+
+package "unbound" do
+  action :upgrade
+end
+
+directory "#{node['unbound']['directory']}/conf.d" do
+  mode 0755
+  owner "root"
+  group root_group
+end
+
+template "#{node['unbound']['directory']}/unbound.conf" do
+  source "unbound.conf.erb"
+  mode 0644
+  owner "root"
+  group root_group
+  notifies :reload, "service[unbound]"
+end
+
+# setup logfile
+file "#{node['unbound']['logfile']}" do
+  owner 'unbound'
+  group 'unbound'
+  mode 00755
+  action :create_if_missing
+end 
+
+#begin
+#  local_zone = data_bag_item("dns", node['dns']['domain'].gsub(/\./, "_"))
+#rescue
+#  local_zone = node['dns']['domain']
+#end
+
+#%w{ local forward stub }.each do |type|
+%w{ forward stub }.each do |type|
+
+  template "#{node['unbound']['directory']}/conf.d/#{type}-zone.conf" do
+    source "#{type}-zone.conf.erb"
+    mode 0644
+    owner "root"
+    group root_group
+    # variables(:local_zone => local_zone)
+    notifies :reload, "service[unbound]"
+  end
+
+end
+
+# Not yet supported.
+# include_recipe "unbound::remote_control" if node['unbound']['remote_control']['enable']
+
+service "unbound" do
+  supports :restart => true, :reload => true
+  action [:enable, :start]
+end
