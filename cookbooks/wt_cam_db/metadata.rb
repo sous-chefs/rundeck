60f58d5704a5496bbd56f71d0a9bc9312ed65f11
martink <kendrick.martin@webtrends.com>
2012-09-26 12:24:38 -0700

    Updating cam_db cookbook to close connections when dropping
    Former-commit-id: 517636c180065ae5a9e828106da378689ff385d8 [formerly f113a6c7c2289cc115a9abf52d09d7c1b07ebf88] [formerly 4e185609c104c847f09f43bcbac32b444173aade [formerly 3d057343127d79a3c32610669bf37154ff67a02a]]
    Former-commit-id: 6aafb98bd44dd0fc31e2fc8670461874d13d1f7a [formerly c70ba20659fccc0ec209317f0870f5f0ceee1f7f]
    Former-commit-id: d20905f60aee45b4ac06fc43356401781fe0f266

--- cookbooks/wt_cam_db/metadata.rb
+++ cookbooks/wt_cam_db/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "adam.sinnet@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures wt_cam_db"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.1.2"
 depends          "database"
 depends          "windows"
