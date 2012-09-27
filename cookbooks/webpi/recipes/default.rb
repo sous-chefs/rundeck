c585f5254554a988467dc1bb1b6241d58cfed5e7
martink <kendrick.martin@webtrends.com>
2012-09-27 14:34:23 -0700

    Changing WebpiCmdLine to WebpiCmd
    Former-commit-id: 5e251554c712739c18b49d124c2f44d002d2a7cc [formerly 6cce9a73a7a30ac2ba8f0f390d4ff4518c3a8824] [formerly 8f760c88ea62d84a4b2bf03ed22588b4bc98a362 [formerly 6fcbf1f5ab00a387e76101d98768623a3682aee3]]
    Former-commit-id: f95bca01566c7dd7fa03ab57ad00ab736bf44927 [formerly 6cf7e39f2f82cbfa11bd782396b4ec985fe1a83f]
    Former-commit-id: 524c5331f65194392849f75191fd6ee68967b7a2

--- cookbooks/webpi/recipes/default.rb
+++ cookbooks/webpi/recipes/default.rb
@@ -30,5 +30,5 @@ windows_zipfile "webpicmdline" do
   path node['webpi']['home']
   source "#{Chef::Config[:file_cache_path]}/#{file_name}"
   action :nothing
-  not_if { ::File.exists?("#{node['webpi']['home']}/WebpiCmdLine.exe") }
+  not_if { ::File.exists?("#{node['webpi']['home']}/WebpiCmd.exe") }
 end
