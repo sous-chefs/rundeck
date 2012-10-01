78bb70ff727c30a811632b1fc6adef084e4545d8
vonnagy <ivan.vonnagy@webtrends.com>
2012-10-01 08:43:08 -0700

    Fixed snappy version
    Former-commit-id: 26b400d3a6d188b5f73257bb08ee7525c15b0e81 [formerly 65394fc05f0f4d676b39668abcf7579c753efa04] [formerly 3b50c2b421d3dbe9a0569e3e349bd704933e7c5b [formerly 0ee9363dbfade6933cca0ad13572d13d5bc01948 [formerly bd34188c4c024442968016ecfc7b75e9afccb201]]]
    Former-commit-id: 2463ecd1e6180e4a9063684d0d98c0fa5e5aa7a8 [formerly 15f85be5b6c349a811de282ba90cb7615310640d]
    Former-commit-id: d701c836aa103f0cecee7b985160209c852e37c4
    Former-commit-id: 92ca6ae08b51bcbbaf73f510f33acff7d7ef37ed

--- cookbooks/wt_storm_streaming/recipes/default.rb
+++ cookbooks/wt_storm_streaming/recipes/default.rb
@@ -145,7 +145,7 @@ plexus-utils-1.5.6.jar
 protobuf-java-2.4.0a.jar
 regexp-1.3.jar
 scala-library-2.8.0.jar
-snappy-java-1.0.3.2.jar
+snappy-java-1.0.4.1.jar
 stax-api-1.0.1.jar
 streaming-analysis.jar
 UserAgentUtils-1.2.4.jar
