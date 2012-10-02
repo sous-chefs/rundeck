42c21010ff8d01f68e59e551439405c6445ec4f5
martink <kendrick.martin@webtrends.com>
2012-10-02 09:37:38 -0700

    Revert "Removed the force-stop from undeploy.rb and bumped the cookbook version"
    This reverts commit 673c2eb820cdf540be251a01e0855b8b39b06451 [formerly b5874f4b9f0ec8f999cbe031223bb98c84731003] [formerly a78ecf52bb8b4c4feddca38300edf199533f7796] [formerly e888f0d579fe583962992ac2d9f55ec2ae42a69d [formerly cad8417230a9a2e31f98b19214fe18724f4f8bed]] [formerly fdfb5e659f7325037e851b7f97e999dae4661f55 [formerly e437bb4f543478c6e3e1c25e3eae9b563ca8ad0f] [formerly 5dfdd8d585257fcdce918d094a6140e05662f333 [formerly c4c5732e2ae49ee72febd99f3041696be6f2cc4a [formerly cc0ae787e199dfdaf5e40818806fda2c030eb97f]]]].
    
    
    Former-commit-id: dac280ce775945ee6ebd5a522a3333d659c037d1 [formerly c53e09e711e393c9de30e19bf2eafdfbfa140fe4] [formerly b1fe649d150a36afe78fdb967efbb3034c743998 [formerly 2d1bbf45cab3e0d6d88e424d586324e30393f04c [formerly 4c86ad91b6ebd4171d1de939804fd545fdf1eaf9]]]
    Former-commit-id: e398ad33921e91de4a85329dbd28735a4f00ae2c [formerly a0b5e483ef8aab861f3d0b8ba2e1928bc71d2f23]
    Former-commit-id: c90690f85d9c02c3af6be0277e1c771b7d3de3c7
    Former-commit-id: 5866dae04f3125c498dc9582427f1b6b82f51553

--- cookbooks/wt_streamingconfigservice/recipes/undeploy.rb
+++ cookbooks/wt_streamingconfigservice/recipes/undeploy.rb
@@ -21,6 +21,13 @@ service "streamingconfigservice" do
   ignore_failure true
 end
 
+# force stop the service in case the stop failed
+service "streamingconfigservice" do
+  action [:stop]
+  stop_command "force-stop"
+  ignore_failure true
+end
+
 directory log_dir do
   recursive true
   action :delete
