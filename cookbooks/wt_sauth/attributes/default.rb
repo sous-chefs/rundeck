f94580692e5c74e9b745c7ed766a6b30d6bc1ab1
martink <kendrick.martin@webtrends.com>
2012-10-03 12:10:28 -0700

    Adding sauth properties
    Former-commit-id: 8c4b108960e9850f9569ab5d718373c93bca0e5a [formerly e87f1daf1331812c7161a2fd2ae21b970e0ab5df] [formerly e281f7db65c67dccb5254b453230f9cb45ccd1b7] [formerly 096c4878e04b8763e407806455f9a28273896c5c [formerly 4090c59718f61b8e4477d0d7640399a63b8d6164]] [formerly 5840bbeceb08afb257fdcf7d882830e5def01d9d [formerly 7ec91de6dab56a4e04d12eafdb30cf2d596344b0] [formerly 114e3a6bacab0a9d5931cd4834d9135a38b80708 [formerly e95f50f3352d39549a16428ed50b3d09b89b658d [formerly e3d99b6166676513ccfb1f511f3aad8c69c4b63c]]]]
    Former-commit-id: 6f3a790c7b175e310b364c7806a8aa736e2e069d [formerly b97184cf1052ace661b1cfb27c0b93e08679a96f] [formerly 2079f0d1966623d58035057b7b2378fe2831e2ab [formerly e95f50f3352d39549a16428ed50b3d09b89b658d]]
    Former-commit-id: 698394b519821ddfc45cf11d1b2d77533253665d [formerly 18736057e1571a5b94d1329ae632c1a411d4cc2e]
    Former-commit-id: 853c6f1abe4406d4de8666748b31969575e798e5
    Former-commit-id: 920610506a0a6b4dad6d5844804f20919c207505

--- cookbooks/wt_sauth/attributes/default.rb
+++ cookbooks/wt_sauth/attributes/default.rb
@@ -7,9 +7,7 @@
 #
 
 default['wt_sauth']['app_pool'] = "SAUTH"
-default['wt_sauth']['auth']['download_url'] = ""
-default['wt_sauth']['db_server'] = "(local)"
-default['wt_sauth']['db_name'] = "Cam"
+default['wt_sauth']['download_url'] = ""
 default['wt_sauth']['tokenExpirationMinutes'] = 60
 default['wt_sauth']['port'] = 81
 default['wt_sauth']['log_level'] = "INFO"
