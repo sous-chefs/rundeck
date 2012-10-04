d2fb6dab989fcb7d93145595f132423d5ea4f4f9
Tim Smith <tim.smith@webtrends.com>
2012-10-03 17:48:45 -0700

    Fix typo in wt_realtime_hadoop
    Former-commit-id: ff21652d4e050344b59bde857fd48f169558dbe5 [formerly d1c7c4adbe106d6664e1f7211b9d14a24186a509] [formerly 9fe7915e8e32972e088903dc5810c3fff8455936 [formerly 5bd2ab1add98870ecdbfa8e9ff96ee840bd85fb9]]
    Former-commit-id: e4bd6cfdd5513ccd4de1cd632b7d195509b38981 [formerly 2cbb8c11c07e88cc43250d2d950e65c6014f5962]
    Former-commit-id: 47cfb7fa4514ce11d32ee41d838253af60fb2045

--- cookbooks/wt_realtime_hadoop/metadata.rb
+++ cookbooks/wt_realtime_hadoop/metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "sean.mcnamara@webtrends.com"
 license          "All rights reserved"
 description      "HBase Schema Script Installer for Realtime"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.3"
+version          "1.0.4"
 
