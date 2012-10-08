9d4dbf9581a32d825205ed1ea79bac0d8d70cfc3
Tim Smith <tim.smith@webtrends.com>
2012-10-08 13:22:01 -0700

    Remove the force yes when install debian packages for storm
    Former-commit-id: 7e2e58107110422ce662476fe48df046578fb69e [formerly e3d2000b8bb568fd514a24c8593c159ff313d60c] [formerly 555ac18b82b81f70ae5f0e0bc1fcf2505d869246] [formerly 619940d67683d1dddb0cc27581b7e97239078ae3 [formerly 18167b30470324ac1fac09b53d3cf4e089577961]] [formerly 8c760edd6a476f2f68671881891df948f7719cdc [formerly cb51eec97f2dff606b3b82c03bce692ef19ac415] [formerly 0ff28b2b8ad1d538182a704e3cbb6e1fd2fdcc28 [formerly 19fa82f1dac8b586f1a8fc831ee338f206eb93a8 [formerly a605f37814c3e468e112d1cb403e1de0105c5daa]]]]
    Former-commit-id: 2878a747448fb4faf672bde7c71f03ab58c38b74 [formerly 9c4c3ceed6288337a48e8cbfb03e19841bdbbdc1] [formerly 60b6793d6357080b2df49faa52558f29f55d4cc9 [formerly 19fa82f1dac8b586f1a8fc831ee338f206eb93a8]]
    Former-commit-id: 8f084668cf4a3996c114068cced11e0ae0d9eafb [formerly 4df1e5fca5478673b9b12a60256676c0fafae853]
    Former-commit-id: ff8094ce947d1eceaeee6aace0b43b3e8a645420
    Former-commit-id: 8e286ad172e4700b7c7b726ed530932a2249c068

--- cookbooks/storm/attributes/default.rb
+++ cookbooks/storm/attributes/default.rb
@@ -17,14 +17,12 @@ default['storm']['local_dir'] = "/mnt/storm"
 default['storm']['local_mode_zmq'] = "false"
 default['storm']['cluster_mode'] = "distributed"
 
-
 # zookeeper attributes
 default['storm']['zookeeper']['port'] = 2181
 default['storm']['zookeeper']['root'] = "/storm"
 default['storm']['zookeeper']['session_timeout'] = 20000
 default['storm']['zookeeper']['retry_times'] = 5
 default['storm']['zookeeper']['retry_interval'] = 1000
-
 
 # supervisor attributes
 default['storm']['supervisor']['workers'] = 4
@@ -35,7 +33,6 @@ default['storm']['supervisor']['monitor_frequecy_secs'] = 3
 default['storm']['supervisor']['heartbeat_frequency_secs'] = 5
 default['storm']['supervisor']['enable'] = true
 
-
 # worker attributes
 default['storm']['worker']['childopts'] = "-Xmx768m -XX:+UseConcMarkSweepGC -Dcom.sun.management.jmxremote"
 default['storm']['worker']['heartbeat_frequency_secs'] = 1
@@ -43,7 +40,6 @@ default['storm']['task']['heartbeat_frequency_secs'] = 3
 default['storm']['task']['refresh_poll_secs'] = 10
 default['storm']['zmq']['threads'] = 1
 default['storm']['zmq']['longer_millis'] = 5000
-
 
 # nimbus attributes
 default['storm']['nimbus']['host'] = ""
@@ -58,22 +54,18 @@ default['storm']['nimbus']['task_launch_secs'] = 120
 default['storm']['nimbus']['reassign'] = true
 default['storm']['nimbus']['file_copy_expiration_secs'] = 600
 
-
 # ui attributes
 default['storm']['ui']['port'] = 8080
 default['storm']['ui']['childopts'] = "-Xmx768m"
-
 
 # drpc attributes
 default['storm']['drpc']['port'] = 3772
 default['storm']['drpc']['invocations_port'] = 3773
 default['storm']['drpc']['request_timeout_secs'] = 600
 
-
 # transactional attributes
 default['storm']['transactional']['zookeeper']['root'] = "/storm-transactional"
 default['storm']['transactional']['zookeeper']['port'] = 2181
-
 
 # topology attributes
 default['storm']['topology']['debug'] = false
@@ -90,4 +82,3 @@ default['storm']['topology']['state_synchronization_timeout_secs'] = 60
 default['storm']['topology']['stats_sample_rate'] = 0.05
 default['storm']['topology']['fall_back_on_java_serialization'] = true
 default['storm']['topology']['worker_childopts'] = "null"
-
