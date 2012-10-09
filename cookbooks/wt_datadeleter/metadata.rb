99754f794a43e798041ed19278d9a1b020712413
Michael Parsons <parsonsm@sops02.staging.dmz>
2012-10-09 22:55:59 +0000

    Add datadeleter recipe
    Former-commit-id: 3198e25cf021aa829ed0f7ad65f1ff5ebda10cb0 [formerly 864bb0f35aeda2e9841be53d9ea7b751780ce311] [formerly 47ecce8ffcef0c85f1c7df7dc59e5b9861e697d7] [formerly 03a0bdbd202f229482475aaf1d50d3e004398663 [formerly 5459642fc13f68ab2c836d260777640783cd1e2d]] [formerly 62ccb279f5c963d1aa43182200a5f163f1c2e208 [formerly 019314e952e74954dc46453c845f21be60fdd7c6] [formerly 3b85002cb89aedb8befb0f64f3c1e487db3f4593 [formerly 9458f6edaf041e8e9dddc88509de3f4826945143 [formerly c21fcab683ec9d3d448e0dd5c554fb1828b31f72]]]]
    Former-commit-id: 433ced284e5c5e00fabcf14dd312d6fbb3a014c9 [formerly 4a5df6a3d6727f4ef301d4050681c50cc6e07045] [formerly a81603008b8bd30a9d1f42b101312b833e1b6ccd [formerly 9458f6edaf041e8e9dddc88509de3f4826945143]]
    Former-commit-id: 31e19a80e19acc1c16b25f1aebac0b592d0f0cd2 [formerly a005560b2d895a091f095de55570d10ecb8d4f3e]
    Former-commit-id: c351ad37a075a35aa5d29234ad4266b74639cf88
    Former-commit-id: c450b5787447df44a0d38607efb30827b047c41c

---
+++ cookbooks/wt_datadeleter/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Webtrends"
+maintainer_email "kendrick.martin@webtrends.com"
+license          "All rights reserved"
+description      "Installs/Configures Webtrends Data Deleter Service"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.0.0"
+depends          "wt_base"
+depends          "powershell"
