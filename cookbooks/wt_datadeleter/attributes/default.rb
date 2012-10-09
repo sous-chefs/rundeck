3198e25cf021aa829ed0f7ad65f1ff5ebda10cb0
Michael Parsons <parsonsm@sops02.staging.dmz>
2012-10-09 22:55:59 +0000

    Add datadeleter recipe
    Former-commit-id: 62ccb279f5c963d1aa43182200a5f163f1c2e208 [formerly 019314e952e74954dc46453c845f21be60fdd7c6] [formerly 3b85002cb89aedb8befb0f64f3c1e487db3f4593 [formerly 9458f6edaf041e8e9dddc88509de3f4826945143 [formerly c21fcab683ec9d3d448e0dd5c554fb1828b31f72]]]
    Former-commit-id: 03a0bdbd202f229482475aaf1d50d3e004398663 [formerly 5459642fc13f68ab2c836d260777640783cd1e2d]
    Former-commit-id: 47ecce8ffcef0c85f1c7df7dc59e5b9861e697d7
    Former-commit-id: 864bb0f35aeda2e9841be53d9ea7b751780ce311

---
+++ cookbooks/wt_datadeleter/attributes/default.rb
@@ -0,0 +1,15 @@
+#
+# Author:: Kendrick Martin(<kendrick.martin@webtrends.com>)
+# Cookbook Name:: wt_datadeleter
+# Attribute:: default
+#
+# Copyright 2012, Webtrends Inc.
+#
+
+default['wt_datadeleter']['artifact'] = "DataDeleter.zip"
+default['wt_datadeleter']['tc_proj'] = "Data Deleter"
+default['wt_datadeleter']['service_name'] = "Webtrends Data Deleter Service"
+default['wt_datadeleter']['service_binary'] = "DataDeleter.exe"
+default['wt_datadeleter']['install_dir'] = "\\DataDeleter\\bin"
+default['wt_datadeleter']['log_dir'] = "\\DataDeleter\\logs"
+default['wt_datadeleter']['download_url'] = ""
