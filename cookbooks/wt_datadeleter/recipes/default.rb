f4d5c0dd79940e457d7eeb3aaa811d86a9f2653a
Michael Parsons <parsonsm@sops02.staging.dmz>
2012-10-10 23:29:17 +0000

    changing from system.process.start to using the ampersand method

--- cookbooks/wt_datadeleter/recipes/default.rb
+++ cookbooks/wt_datadeleter/recipes/default.rb
@@ -92,14 +92,14 @@ if ENV["deploy_build"] == "true" then
 	powershell "install data deleter" do
 		environment({'install_dir' => install_dir, 'service_binary' => node['wt_datadeleter']['datadeleter_binary']})
 		code <<-EOH
-		[System.Diagnostics.Process]::Start($env.install_dir+"\\"+$env.service_binary, "--install")
+		& "$env.install_dir\$env.service_binary" --install
 		EOH
 	end
 
 	powershell "install deletion scheduler" do
 		environment({'install_dir' => install_dir, 'service_binary' => node['wt_datadeleter']['deletionscheduler_binary']})
 		code <<-EOH
-		[System.Diagnostics.Process]::Start($env.install_dir+"\\"+$env.service_binary, "--install")
+		& "$env.install_dir\$env.service_binary" --install
 		EOH
 	end
 	share_wrs 
