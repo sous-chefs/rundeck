1dd82d93d320266bbb0db1859d3e99343f560d0f
Adam Keene <adam.keene@webtrends.com>
2012-10-11 19:50:27 +0000

    wt_sauth: Fix a comma.
    Former-commit-id: 463ae11b48109f240ae65633d775a3a10526b020 [formerly 73424f5ffdf9e658f50bff432d91f2af7093f443] [formerly 7f508dc68fb1ba581e42ceab2122ac9301c94795 [formerly 55599f4c78ea80a7dd93b54da9dd42f9d7694665]]
    Former-commit-id: fbeb5b1185838fd1a426ec2c82939d97a9bbf1f1 [formerly 80eeac41a0d8e8ce5fdb4981f269b3ffffdcd6d4]
    Former-commit-id: 44a0df9e7cc0dab595a09bc73cef38fe50662e0c

--- cookbooks/wt_sauth/recipes/default.rb
+++ cookbooks/wt_sauth/recipes/default.rb
@@ -98,7 +98,7 @@ template "#{install_dir}\\web.config" do
 		:ldap_host => node['wt_common']['ldap_host'],
 		:ldap_port => node['wt_common']['ldap_port'],
 		:ldap_user => user_data['wt_common']['ldap_user'],
-		:ldap_password => user_data['wt_common']['ldap_password']
+		:ldap_password => user_data['wt_common']['ldap_password'],
 		:smtp_host => node['wt_common']['smtp_server'],
 		# Header bar UI locations
 		:auth_url => node['wt_sauth']['auth_service_url'],
