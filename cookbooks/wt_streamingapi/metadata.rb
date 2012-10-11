806c4e01d8ae03da09ce0f01ed8fcd6fd19cedc8
Mark Alexander <mark.alexander@webtrends.com>
2012-10-11 17:59:57 +0000

    fixed messed up version number
    Former-commit-id: 06c2aa7cf573c7f9460ff832908aa074bf53fc57 [formerly 24bbb2e704cb97eea798af4dd066cf118c64570f] [formerly 471d386803cd7d536442f3c10928f7bc7f861188] [formerly ff308a0717e98d1fa3f451652fc3c5d533331032 [formerly e6a4ca95966da23dffd18ad0c505fbeadf0f62f5]] [formerly e6e6b2412bcb9f14ba943015916ee98135ef6b99 [formerly d1de2ac4b3e8df10ac9cb0b61cf3344a243ac3c8] [formerly e2ecf053b9338135ea84c084dac3f507e34f5aaa [formerly 1a3f22d450179e96dbc9515d57b537c129dd6865 [formerly 7cf279d68d03946d615e407973c4838ccb360291]]]]
    Former-commit-id: a18841165b227e15c5a270b39eabfb49f33d7d88 [formerly 888b2238f54e53c4877e7b4b35ae554ad2fc9924] [formerly e22abef3b9ebd7ee6a1d7e3cb497253529310729 [formerly 1a3f22d450179e96dbc9515d57b537c129dd6865]]
    Former-commit-id: af3a0a6f559ed405ea6b7c877822cfb8aaa21fbc [formerly cacd6ce08ac604c742424210b4ab15538260d4ef]
    Former-commit-id: 72eddf894e5fbb13d5d0f8e55052bc5872e33e3b
    Former-commit-id: efa1dc195431d096b8d9f27923fe6b2438daaabe

--- cookbooks/wt_streamingapi/metadata.rb
+++ cookbooks/wt_streamingapi/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email    "ivan.vonnagy@webtrends.com"
 license             "All rights reserved"
 description         "Installs/Configures Webtrends Streaming API Service"
 long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version             "1.0.88888888"
+version             "1.0.8"
 depends             "java"
 depends             "runit"
