10b829af6fd010104f17617e1895099b96b577b9
Marek Dolgos <marek.dolgos@webtrends.com>
2012-10-12 17:23:11 -0700

    Fixed SAPI cookbook and its proxy config.
    Former-commit-id: e420a4e529c4085d5860949079c53c8fe1a880c4 [formerly feb9154926b540c9fcde4f4f5756d9e687a319c4] [formerly b7c0de5be4436ffe6926368c47462b4a0a81d824] [formerly 1538e791c5d78443b36ef447aa885797395beb8e [formerly cccc8dd514049ec51783b838142252fd2e8987cf]] [formerly f4104d64bfbd28e58afb40e6dcbea266cf1829bf [formerly 0a78e1723d38d9f429b9fe1e4cc8c0050ce9886a] [formerly a5c69da8ca4d4e4c9c6669b81705955494b0b480 [formerly e3855c18620d16c3feb272a05ec19162b1e764f7 [formerly c6cca6225e97b485e355f76bba274665930f37d2]]]]
    Former-commit-id: 21747112869479d07932053a6b1c7f6b972b0d40 [formerly b8aa585568ecd61f2d57f42c6ff738eb8b47d195] [formerly 4ef19765cfb65b511219209fa0ff6ddfc9731e71 [formerly e3855c18620d16c3feb272a05ec19162b1e764f7]]
    Former-commit-id: 885d9e5f87a8cfd3de0ae4b1831b011dd3d9dcd3 [formerly 03546fd2310caa11fdf94e1280554519c04e52d8]
    Former-commit-id: c1f0ac18f9e5f86203df253861577f7d4e6d9a1b
    Former-commit-id: b0b260a45c795b0eace12f1fcea11b121ec617ee

--- cookbooks/wt_streamingapi/recipes/default.rb
+++ cookbooks/wt_streamingapi/recipes/default.rb
@@ -91,7 +91,7 @@ def processTemplates (install_dir, node, zookeeper_quorum, datacenter, pod, kafk
     auth_host = auth_uri.host
 
     proxy_uri = URI(node['wt_common']['http_proxy_url'])
-    proxy_host = proxy_uri.host
+    proxy_host = "#{proxy_uri.host}:#{proxy_uri.port}"
     
     cam_url = node['wt_cam']['cam_service_url']
     port = node['wt_streamingapi']['port']
