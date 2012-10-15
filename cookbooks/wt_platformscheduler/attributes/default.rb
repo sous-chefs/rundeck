dc1543ca830c34b2a75632f1f9ff3956a76a0222
martink <kendrick.martin@webtrends.com>
2012-10-15 11:15:07 -0700

    Adding updated platformscheduler
    Former-commit-id: 5750870120be0e26121daee3f549d8c2732ab0d4 [formerly fc84c28262600d90aa564b909b2f508b89581643] [formerly 5d64ce1d27ab53361f4220f0ecab005f1b85ad42] [formerly ab89433b7c15b0076bdf8f771c192a3d6e281608 [formerly 46d960fe7244e042791b72ca81337269c57054d1]] [formerly adc61818cda79528dd84bc797e7713e9fe7602b6 [formerly 03ea667c7c4ee42a76e14272dbb0b489f99929a3] [formerly 73f70936aaf00a31fbb627e7ecafc1777360734e [formerly 85812c3bba2bb0450a5720cd727e6c08755e4543 [formerly 1c0034499267d947747db3c4903772c9321a6eb7]]]]
    Former-commit-id: 782f7a0b87730dea938e425e931c397487bb0bb2 [formerly bf32432a2ef3978ac9c7195c724fc8641ea77920] [formerly 1d234edca9f2190b5449311750d7914a91e9f00c [formerly 85812c3bba2bb0450a5720cd727e6c08755e4543]]
    Former-commit-id: c2ebdf5c4b02d8223b5d3f3e84eac05b5f2e4141 [formerly b4c125e2804812cba31d40226f4d66d4ca1fbb57]
    Former-commit-id: 86ee3a90377e0df0dcd43d2cda932e3858e7f539
    Former-commit-id: 54a081ffe2ca8fd02e468908c7d81a2255f440c5

---
+++ cookbooks/wt_platformscheduler/attributes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: wt_platformscheduler
+# Attribute:: default
+# Author:: Kendrick Martin
+#
+# Copyright 2012, Webtrends
+
+default['wt_platformscheduler']['agent'['msi'] = "WebTrends VDM Scheduler Agent.msi"
+default['wt_platformscheduler']['agent']['download_url'] = ""
