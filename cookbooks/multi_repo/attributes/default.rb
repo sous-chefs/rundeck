537f9d6404d70f98ceac4ec733666b0da24d5d21
Tim Smith <tim.smith@webtrends.com>
2012-10-15 21:01:57 -0700

    Take the production specific path out for multi_repo.  Time to open source this one
    Former-commit-id: 3be3085dd0d484cdf0321cdd858c42eac77da1e6 [formerly 267b902075e5267c3bb5f233dd3b693dfcbdabba] [formerly 4b5f2e25c17dbf58a9baf185d8aa5df736f8d2fd [formerly f0c688a2da454618be938b58f39a44535ecaa270]]
    Former-commit-id: 3314e4107c4a67a63e986205e45e745d5110dcb6 [formerly f5d4f066cd1f051982852856bb3da66da80e2c0c]
    Former-commit-id: 275101d3b010f7222859c16f11efa37a63e05bfc

--- cookbooks/multi_repo/attributes/default.rb
+++ cookbooks/multi_repo/attributes/default.rb
@@ -18,7 +18,7 @@
 #
 
 default['multi_repo']['repo_path'] = "/srv/repo"
-default['multi_repo']['repo_mount'] = "pdxstore10.netiq.dmz:/ifs/data/osdeploy/repo"
+default['multi_repo']['repo_mount'] = ""
 default['multi_repo']['sysadmin_email'] = ""
 default['multi_repo']['chef_repo_path'] = ""
 default['multi_repo']['repo_dropbox_path'] = "/root/repo_dropbox"
