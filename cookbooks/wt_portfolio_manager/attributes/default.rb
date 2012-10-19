44353a00a201a3ad9629c75113c65e9358171e84
Adam Keene <adam.keene@webtrends.com>
2012-10-19 21:21:00 +0000

    Add new cookbook wt_portfolio_manager with corresponding role.
    Former-commit-id: 3a2d8a9ac62b9c55b94fda8dde6717c20493c786 [formerly 8086ad293bd3cbdac16b30c469d7c7900bc2a68b] [formerly 356cd0b785a26c8153d51e0f0423afd0a71b703c [formerly 878cd45e3499d146741e2ee4256a1750cec289ee [formerly d469ef1b55bd42d4ca4a944d337d8961343b9b25]]]
    Former-commit-id: a48d0bc04dc479eb771bd4d9518e6ecf4cb0f36a [formerly 621dc59e29d133bf57eb36d7f20247310209b98c]
    Former-commit-id: 92e014f1076d76ea98e8f72a14acc7a439de93fd
    Former-commit-id: 26e49b860b152044ede04269e1f72a68f937dd62

---
+++ cookbooks/wt_portfolio_manager/attributes/default.rb
@@ -0,0 +1,17 @@
+#
+# Author:: Kendrick Martin(<kendrick.martin@webtrends.com>)
+# Cookbook Name:: wt_portfolio_admin
+# Attribute:: default
+#
+# Copyright 2012, Webtrends Inc.
+#
+
+default['wt_portfolio_manager']['app_pool'] = "PortfolioManager"
+default['wt_portfolio_manager']['download_url'] = ""
+default['wt_portfolio_manager']['port'] = 80
+default['wt_portfolio_manager']['log_level'] = "INFO"
+default['wt_portfolio_manager']['elmah_remote_access'] = "no"
+default['wt_portfolio_manager']['custom_errors'] = "On"
+default['wt_portfolio_manager']['proxy_enabled'] = "true"
+default['wt_portfolio_manager']['cam_service_url_base'] = ""
+default['wt_portfolio_manager']['manager_ui_url'] = ""
