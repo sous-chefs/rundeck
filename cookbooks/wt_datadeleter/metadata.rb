3505e6c3b4fdcdb70ca2993bbeabd43308341b55
Tim Smith <tim.smith@webtrends.com>
2012-10-19 16:05:50 +0000

    Fix the metadata for the data deleter
    Former-commit-id: f10ff00756ece8ca654799fb75577d9b680a1e77 [formerly aa758da1f819f2af8a8833b9f4958dac5cf9cf0d] [formerly b15a97becfe3d07130faea6ad109715ae429d0b4 [formerly af896f65e455ae9e64d9b606dff80a27623e6692]]
    Former-commit-id: 22f36baddef8fd369276cd9464566d65df6b9440 [formerly b6e48094bcb6d8b6633678d490e385d04a281c9a]
    Former-commit-id: b2b588f32afbe7f468e438b252ba82dc57d1ea19

--- cookbooks/wt_datadeleter/metadata.rb
+++ cookbooks/wt_datadeleter/metadata.rb
@@ -6,4 +6,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 depends          "wt_base"
 depends          "powershell"
-requires         "windows"
+supports         "windows"
