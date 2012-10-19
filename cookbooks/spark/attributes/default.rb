a41ddb332e94563d8fdb4167b3e92a74d7d81e78
seanm <sean.mcnamara@webtrends.com>
2012-10-19 12:58:00 -0600

    laying down initial meat and potatoes of the spark cookbook
    Former-commit-id: d34a911d4ead8832bf1787799193b0c9a91a8fd1 [formerly 9b83186a5119b04756e0f8ab3d56a13f6b7c146a] [formerly 07e072c9a7d99fca03dd371b8c567463e8a813f2] [formerly ad849a8b17778b2e83c4d47365ad0e3a5c4bb163 [formerly 39fc6cc9aa7f8952d3960e423321094b579636d8]] [formerly 1b70ad7ac3980f1d71c207c3eb91d5b7b37244e1 [formerly e872905b6ab39b89c8fe4fea26d3949ff5286d03] [formerly 46d4040bcfc51b40e0ccd338c7594775bdeeb1cb [formerly b9092b8ec069a1060d04fb0012f82833e3d82856 [formerly 4ea5b7b602fd2b28a39b99e43cfeb0462815db46]]]]
    Former-commit-id: 0a916f9fb22ec6d8008d3b18e2caa622e8c7cf5b [formerly 0675db87d5307e09ecce1bb7d005d9cfaf2b7433] [formerly 824dcf5199104772715b38daa32a12888d9a75aa [formerly b9092b8ec069a1060d04fb0012f82833e3d82856]]
    Former-commit-id: fac56a1ded60827440c43a6902cf121344edcce8 [formerly cbba7b688b120c326abc9f074eed67a64083b34a]
    Former-commit-id: fd8c79cec3f575b1f66c8f428193896dff6929cf
    Former-commit-id: d578063a14d96936c959a8a41d0745455e39531c

---
+++ cookbooks/spark/attributes/default.rb
@@ -0,0 +1,11 @@
+#
+# Author:: Sean McNamara (<sean.mcnamara@webtrends.com>)
+# Cookbook Name:: storm
+# Attribute:: default
+#
+# Copyright 2012, Webtrends Inc.
+#
+
+default['spark']['version'] = "0.6.0"
+default['spark']['download_url'] = "http://github.com/downloads/mesos/spark/"
+default['spark']['install_dir'] = "/opt/spark"
