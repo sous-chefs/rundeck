630b09e672566ac07bdd3570b89059a8ebd8b8e6
Tim Smith <tim.smith@webtrends.com>
2012-10-20 15:21:35 +0000

    Import cron version 1.2.0
    Former-commit-id: 0ebc6754cb21067d98257f8441ca4daaef9d5635 [formerly cc57c5f699868b4f419abc3273776f9e1f73da61] [formerly 25a8457f744f4f65401b637647a02e9511caa63f [formerly 7a173c46a02786f70797f2506199ad7e8ffb39e1]]
    Former-commit-id: 6156bbb5a18c56e2ac70317da15c3a4b6ce3c758 [formerly 05a3540fb6013fdae73228c7323ab1f99c9cc770]
    Former-commit-id: 580af79c5f1555865849a8c94d65885a4be12605

--- cookbooks/cron/metadata.rb
+++ cookbooks/cron/metadata.rb
@@ -3,9 +3,8 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs cron"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.4"
+version          "1.2.0"
 
 %w{redhat centos scientific fedora amazon debian ubuntu}.each do |os|
   supports os
 end
-
