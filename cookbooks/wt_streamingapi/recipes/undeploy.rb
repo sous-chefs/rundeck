012c3baa0f036b70dc8b896aacb3707fb183ba1d
Tim Smith <tim.smith@webtrends.com>
2012-10-20 21:05:58 -0700

    formatting
    Former-commit-id: 9fe88dd96ce6be0be172627e744bcb2b0aada105 [formerly f5ad0674c30b2c0cb36f82b277f7b942829b8a8f] [formerly a9f8b7e07c83cf0227a7cdd1ce1e669b0c4582dd] [formerly 536440b57c59255182156b3443eae3cc22dc1252 [formerly 3db72f1d901bb64f12a19dbda28bf139dfb73057]] [formerly 9205dcd160ed7a536c4bb3fbeb0dcf755c86f9fa [formerly aae441aa7e40b893af63f56c2a091737bf885cb0] [formerly c793380b1025a23360870e100b258ba054ef34bc [formerly 624b4ed3dcd26a7fec0b8c32cd7e43d52ce2da31 [formerly 97fc19c6b730f1870ac72ce3f2889ce2ea6c1361]]]]
    Former-commit-id: 980ec4ab7f5f1c6eb9b7dd6f61a890c7b466695b [formerly acb69c50d527ba938b021751ecd42dbe93746c1c] [formerly 4f4803ee156fe047789baab2a5dc369c69651988 [formerly 624b4ed3dcd26a7fec0b8c32cd7e43d52ce2da31]]
    Former-commit-id: 2104b19dac3c839c3ddaec41e20627d5df96bac0 [formerly c70e1f85882bdbcdcd6aa4225f1064044a29cc8b]
    Former-commit-id: 48363d8df329538f699d87d3fcc65ce143514d20
    Former-commit-id: 595b84e56412556b6b987d6e5bd56f3a746d8fc4

--- cookbooks/wt_streamingapi/recipes/undeploy.rb
+++ cookbooks/wt_streamingapi/recipes/undeploy.rb
@@ -10,11 +10,10 @@
 log_dir      = "#{node['wt_common']['log_dir_linux']}/streamingapi"
 install_dir  = "#{node['wt_common']['install_dir_linux']}/streamingapi"
 
-
 runit_service "streamingapi" do
-    action :disable
-    run_restart false
-end 
+  action :disable
+  run_restart false
+end
 
 # try to stop the service, but allow a failure without printing the error
 service "streamingapi" do
