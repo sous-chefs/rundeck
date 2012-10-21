7a10939370ef580feae43330a17e0c4782eee4e5
Tim Smith <tim.smith@webtrends.com>
2012-10-20 20:57:19 -0700

    First chunk of running code-cleaner to remove newlines, extra spaces, and convert tabs to double spaces
    Former-commit-id: 9d75628b52e20803fc435b908a4df408462a044b [formerly 92c79e497856b5c7fa452c116bb151e86ffb1178] [formerly 46a0b13ef409e54ac91f304c33c5c1029f671606] [formerly d55e2202eea296c7dadc2f06182b93bbdb293976 [formerly abe74e0d2b9f40393e035052d5be55c61054ed1c]] [formerly c285e31de78b478c864cf19d277b01fdf3fe2766 [formerly 986e8b3416549335b435efcc0baf6da2e06ad8d7] [formerly f2a4799ecbc39433fdba43cae4da899f4e23f3b5 [formerly d46560f3cfced8c43f5f5d1bf0e753c2c234fd58 [formerly a5ce9bb97d7ac7e17656b9275afd10ca3f61030f]]]]
    Former-commit-id: 9b9f84c1eb94c9214e57190487b6d24662cd8926 [formerly 6ab0d88438e6af6b1d6351a4d0cb3a54214db1fb] [formerly 39b2523237e066e109fe91df67dd3b8e11efdffd [formerly d46560f3cfced8c43f5f5d1bf0e753c2c234fd58]]
    Former-commit-id: 08a2f2f8b70f99d55adfe23a94a8fc586db4d518 [formerly 64fc5443b4e8770c9c0ebfdef2b6ba3836f156b3]
    Former-commit-id: 21664c43fdf49f71ff9a5234889eb92a091a4c3d
    Former-commit-id: a75736488e91c2b4283994af421055caa6cfce2b

--- cookbooks/beyond_compare/recipes/default.rb
+++ cookbooks/beyond_compare/recipes/default.rb
@@ -17,44 +17,44 @@ is_installed = in_reg_uninstall? node['beyond_compare']['display_name']
 
 # license key from authorization data bag
 unless is_installed
-	key_data = wt_auth['beyond_compare_v3']['license_key'].join("\n") rescue nil
-	raise Chef::Exceptions::AttributeNotFound, "No license key provided for beyond compare." if key_data.nil?
+  key_data = wt_auth['beyond_compare_v3']['license_key'].join("\n") rescue nil
+  raise Chef::Exceptions::AttributeNotFound, "No license key provided for beyond compare." if key_data.nil?
 end
 
 # setup inf file
 cookbook_file inf_file do
-	source "bc3inf.txt"
-	backup false
-	action :create
-	not_if { is_installed }
+  source "bc3inf.txt"
+  backup false
+  action :create
+  not_if { is_installed }
 end
 
 # license key file
 file key_file do
-	content key_data
-	backup false
-	action :create
-	not_if { is_installed }
+  content key_data
+  backup false
+  action :create
+  not_if { is_installed }
 end
 
 # install
 windows_package node['beyond_compare']['display_name'] do
-	source node['beyond_compare']['download_url']
-	options "/sp- /verysilent /loadinf=\"#{inf_file}\""
-	installer_type :custom
-	action :install
+  source node['beyond_compare']['download_url']
+  options "/sp- /verysilent /loadinf=\"#{inf_file}\""
+  installer_type :custom
+  action :install
 end
 
 # remove key file
 file "rm key_file" do
-	path key_file
-	backup false
-	action :delete
+  path key_file
+  backup false
+  action :delete
 end
 
 # remove inf file
 file "rm inf_file" do
-	path inf_file
-	backup false
-	action :delete
+  path inf_file
+  backup false
+  action :delete
 end
