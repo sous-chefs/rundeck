89053e0ccf140823ef2d3dfad535b2f49c44e166
Jeremy Chartrand <chartrandj@sops02.staging.dmz>
2012-10-22 17:21:34 +0000

    Initial checkin of log preprocessor cookbook
    Former-commit-id: 1b8c8acd17a168aceee83aa7550094606625ccd6 [formerly fc8a042b8c2471360d98dec99eb4ccfffe906d4e] [formerly 256e85e3710c55db5ca4230105a689593323345b [formerly 036470232848ae4430f4c6b154fb666b31a98ab3]]
    Former-commit-id: 46c91d54d5c4d2f4d76e78803b31fb595a7e4df0 [formerly ee928345d7925aca34ccf7666465fe76c74be4f5]
    Former-commit-id: 93a1386c012edfddf0f98f7a2754419214f1fff5

---
+++ cookbooks/wt_logpreproc/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Webtrends"
+maintainer_email "kendrick.martin@webtrends.com"
+license          "All rights reserved"
+description      "Installs/Configures Webtrends Lop Preprocessor"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.0.0"
+depends          "wt_base"
+depends          "powershell"
