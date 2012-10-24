c154cd2fd925056077f110a900d23e4241b16eb6
vonnagy <ivan.vonnagy@webtrends.com>
2012-10-24 11:30:20 -0700

    Initial checkin of wt_streaminganalysis_monitor cookbook

---
+++ cookbooks/wt_streaminganalysis_monitor/attributes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: wt_streaminganalysis_monitor
+# Attributes:: default
+#
+# Copyright 2012, Webtrends
+#
+# All rights reserved - Do Not Redistribute
+#
+
+default['wt_streaminganalysis_monitor']['user']         = "webtrends"
+default['wt_streaminganalysis_monitor']['group']        = "webtrends"
+default['wt_streaminganalysis_monitor']['download_url'] = ""
+default['wt_streaminganalysis_monitor']['java_opts']    = "-Djava.net.preferIPv4Stack=true"
+default['wt_streaminganalysis_monitor']['jmx_port']     = 9999
+default['wt_streaminganalysis_monitor']['graphite_enabled'] = "true"
+default['wt_streaminganalysis_monitor']['graphite_interval'] = 5
+default['wt_streaminganalysis_monitor']['statuslistener.enabled'] = "true"
+default['wt_streaminganalysis_monitor']['statuslistener.znode.root'] = "/StormStatus/Lab_H"
+default['wt_streaminganalysis_monitor']['metricslistener.enabled'] = "true"
+default['wt_streaminganalysis_monitor']['metricslistener.topic'] = "storm-metrics"
+default['wt_streaminganalysis_monitor']['stats_interval'] = 30
