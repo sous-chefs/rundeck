c7639fccdfb5e295df3cc250431c882228a0630c
Tim Smith <tim.smith@webtrends.com>
2012-10-27 13:33:17 -0700

    Add a read me to the VC2010 cookbook and add a changelog

--- cookbooks/vc2010/metadata.rb
+++ cookbooks/vc2010/metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Webtrends Inc."
 maintainer_email "david.dvorak@webtrends.com"
 license          "All rights reserved"
 description      "Installs Microsoft Visual C++ 2010 Redistributable Package"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "1.0.0"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.0.1"
 supports         "windows"
 depends          "windows", ">= 1.2.6"
