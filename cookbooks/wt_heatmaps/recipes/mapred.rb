e03aeb7adc1fce618f5972ecb06532b175245500
Tim Smith <tim.smith@webtrends.com>
2012-10-26 17:46:06 -0700

    heatmaps food critic fixes

--- cookbooks/wt_heatmaps/recipes/mapred.rb
+++ cookbooks/wt_heatmaps/recipes/mapred.rb
@@ -10,14 +10,14 @@
 package "php-cli"
 
 template "/etc/config_distrib" do
-  source "config_distrib"
+  source "config_distrib.erb"
   owner "hadoop"
   group "hadoop"
   mode 00755
 end
 
 template "/etc/heatmap_reducers" do
-  source "heatmap_reducers"
+  source "heatmap_reducers.erb"
   owner "hadoop"
   group "hadoop"
   mode 00755
