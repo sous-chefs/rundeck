cc5291eb8219463f97846d048a935803de491e82
John Easterday <easterdayj@sops02.staging.dmz>
2012-10-29 22:54:28 +0000

    getting latest
    Former-commit-id: 3ff3469ea1db37769a1a63db061cc754a630d78b [formerly 0ffc95ec849f0d4ed7b99449818925e1ed85d34d] [formerly 76e86d4ca13474b14c59049af7c707f8de049b50 [formerly 3f55b8b45e522aa5a05268fa567a7529ab9d77e2]]
    Former-commit-id: 676a191219ba071995db8e47596bf193b970269f [formerly e03524aa1a45e6311ea6bc41bd2d35ce5ac6b625]
    Former-commit-id: 001b4c571476ad5c4953390bed00b03feef5f263

--- cookbooks/wt_devicedataupdater/attributes/default.rb
+++ cookbooks/wt_devicedataupdater/attributes/default.rb
@@ -6,7 +6,13 @@
 # Copyright 2012, Webtrends Inc.
 #
 
-default['wt_devicedataupdater']['artifact'] = "DeviceDataUpdater.zip"
 default['wt_devicedataupdater']['download_url'] = ""
-default['wt_devicedataupdater']['install_dir'] = "\\DeviceDataUpdater"
-default['wt_devicedataupdater']['log_dir'] = "\\logs"
+default['wt_devicedataupdater']['artifact'] = "DeviceDataUpdater.zip"
+default['wt_devicedataupdater']['service_name'] = "Webtrends Device Data Updater Service"
+default['wt_devicedataupdater']['service_binary'] = "DDU.exe"
+default['wt_devicedataupdater']['devicedataupdater_binary'] = "DDU.exe"
+default['wt_devicedataupdater']['devicedatacheduler_binary'] = "DeviceDataUpdater.exe"
+default['wt_devicedataupdater']['install_dir'] = "\\DeviceDataUpdater\\bin"
+default['wt_devicedataupdater']['log_dir'] = "\\DeviceDataUpdater\\logs"
+default['wt_devicedataupdater']['download_url'] = ""
+
