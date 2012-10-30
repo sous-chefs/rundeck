7f581e4872107be44ec645476105b6e9cc5d8e96
David Dvorak <david.dvorak@webtrends.com>
2012-10-30 22:50:17 +0000

    removed templates, added check for running Agent service
    Former-commit-id: 528c7a454c5d622fcefd37951a9b9a64ef3d1948 [formerly 00d154b7dcb88fea40143dd7e0627723588f51f1] [formerly bba3d9ceed0c3571966bcaabf4bbde72da791b72 [formerly a15020c23632bf82887c467a22f30d48e632fbe6 [formerly 59f437c5d24fee51d6dbeef73bcf4e82e6a94f1a]]]
    Former-commit-id: 347918fd85b10be160b44c0eaba72231866d84ca [formerly 028922b953984979004f0fdc679a1c7ba10940ee]
    Former-commit-id: 9d12bbc775fdd334dd4fa495ab1a4cff2a14faf7
    Former-commit-id: 36f213acab09fb1aee9a3f3dd090091cce86d68e

--- cookbooks/wt_devicedataupdater/metadata.rb
+++ cookbooks/wt_devicedataupdater/metadata.rb
@@ -6,5 +6,4 @@ description      "Installs/Configures Webtrends Device Data Updater"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 supports         "windows"
-depends           "powershell"
 depends          "wt_base"
