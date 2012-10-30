36f213acab09fb1aee9a3f3dd090091cce86d68e
David Dvorak <david.dvorak@webtrends.com>
2012-10-30 22:50:17 +0000

    removed templates, added check for running Agent service
    Former-commit-id: 528c7a454c5d622fcefd37951a9b9a64ef3d1948 [formerly 00d154b7dcb88fea40143dd7e0627723588f51f1] [formerly bba3d9ceed0c3571966bcaabf4bbde72da791b72 [formerly 59f437c5d24fee51d6dbeef73bcf4e82e6a94f1a]]
    Former-commit-id: 347918fd85b10be160b44c0eaba72231866d84ca [formerly 028922b953984979004f0fdc679a1c7ba10940ee]
    Former-commit-id: 9d12bbc775fdd334dd4fa495ab1a4cff2a14faf7

--- cookbooks/wt_devicedataupdater/attributes/default.rb
+++ cookbooks/wt_devicedataupdater/attributes/default.rb
@@ -1,16 +1,12 @@
 #
-# Author:: Tim Smith(<tim.smith@webtrends.com>)
 # Cookbook Name:: wt_devicedataupdater
 # Attribute:: default
 #
 # Copyright 2012, Webtrends Inc.
 #
 
-default['wt_devicedataupdater']['download_url'] = ""
-default['wt_devicedataupdater']['artifact'] = "DeviceDataUpdater.zip"
-default['wt_devicedataupdater']['service_name'] = "Webtrends Device Data Updater Service"
-default['wt_devicedataupdater']['service_binary'] = "DDU.exe"
-default['wt_devicedataupdater']['devicedataupdater_binary'] = "DDU.exe"
-default['wt_devicedataupdater']['install_dir'] = "\\DeviceDataUpdater\\bin"
-default['wt_devicedataupdater']['log_dir'] = "\\DeviceDataUpdater\\logs"
-default['wt_devicedataupdater']['download_url'] = ""
+default['wt_devicedataupdater']['download_url'] = ''
+default['wt_devicedataupdater']['service_name'] = 'Webtrends Device Data Updater Service'
+default['wt_devicedataupdater']['service_binary'] = 'DDU.exe'
+default['wt_devicedataupdater']['install_dir'] = 'DeviceDataUpdater\\bin'
+default['wt_devicedataupdater']['log_dir'] = 'DeviceDataUpdater\\logs'
