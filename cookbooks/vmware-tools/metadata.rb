63245c7387286609733e2da2eac42c2cc0bd3149
Tim Smith <tim.smith@webtrends.com>
2012-10-31 15:49:44 -0700

    Bump version and remove the rdoc
    Former-commit-id: d6dfc337f4211d6a039f196a0223d883474cf60d [formerly a113e236ed5ca607313538f311b56c4161246a37] [formerly e83752f5e5760a741ff4bf27e78b1f4801e7c106 [formerly e5a52aa5d563e2e96c29cc526dc824b5d1b0b8c0]]
    Former-commit-id: 13a2b2d72cb3c7a96fb4558b12378068c3bc6d61 [formerly cc246f24b015bd84d459d9ead5c572810baf4850]
    Former-commit-id: 19a77b2f6944f136c76cebcb2a99e890452e65d1

--- cookbooks/vmware-tools/metadata.rb
+++ cookbooks/vmware-tools/metadata.rb
@@ -2,6 +2,5 @@ maintainer       "Bryan W. Berry"
 maintainer_email "bryan.berry@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures vmware-tools with a fugly bash script"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.2"
-
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.3"
