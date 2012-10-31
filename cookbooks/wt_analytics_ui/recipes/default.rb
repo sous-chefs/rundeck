26f19aa7debc7c1872d3353830f48bb2f78d33cf
martink <kendrick.martin@webtrends.com>
2012-10-31 14:35:13 -0700

    Removing the file delete
    Former-commit-id: 700bee55717ffd7b56c36e7537a0b6a52a318e7b [formerly b43b4a4f790ae23d8eda949fbfb67c691887f3f6] [formerly 10c74b9b55e539926bc081cc545c03161d151927 [formerly 30db8455ac4c2a4c77a4a2f485ee284d38731514]]
    Former-commit-id: e943d66de29d0ff2f152deb5f459f486b2663caa [formerly 485a3e7e37a381e3008bf2a4a84d8995b6a21181]
    Former-commit-id: 3ffc5d5b879941a217db499843462a46b06f1cac

--- cookbooks/wt_analytics_ui/recipes/default.rb
+++ cookbooks/wt_analytics_ui/recipes/default.rb
@@ -21,6 +21,7 @@ install_dir = File.join(node['wt_common']['install_dir_windows'], node['wt_analy
 install_url = node['wt_analytics_ui']['download_url']
 
 user_data = data_bag_item('authorization', node.chef_environment)
+googleplay_key = data_bag_item('rsa_keys', 'googleplay')
 ui_user   = user_data['wt_common']['ui_user']
 ui_pass   = user_data['wt_common']['ui_pass']
 rsa_user = user_data['wt_common']['system_acct']
@@ -219,14 +220,14 @@ if ENV["deploy_build"] == "true" then
   template "#{install_dir}\\bin\\PublicPrivateKeys.rsa" do
     source "PublicPrivateKeys.rsa.erb"
     variables(
-      :modulus => user_data['wt_analytics_ui']['rsa']['modulus'],
-      :exponent => user_data['wt_analytics_ui']['rsa']['exponent'],
-      :p => user_data['wt_analytics_ui']['rsa']['p'],
-      :q => user_data['wt_analytics_ui']['rsa']['q'],
-      :dp => user_data['wt_analytics_ui']['rsa']['dp'],
-      :dq => user_data['wt_analytics_ui']['rsa']['dq'],
-      :inverse_q => user_data['wt_analytics_ui']['rsa']['inverse_q'],
-      :d => user_data['wt_analytics_ui']['rsa']['d']
+      :modulus => googleplay_key['modulus'],
+      :exponent => googleplay_key['exponent'],
+      :p => googleplay_key['p'],
+      :q => googleplay_key['q'],
+      :dp => googleplay_key['dp'],
+      :dq => googleplay_key['dq'],
+      :inverse_q => googleplay_key['inverse_q'],
+      :d => googleplay_key['d']
     )
   end
 
@@ -255,17 +256,17 @@ if ENV["deploy_build"] == "true" then
   # run iss command on the .rsa file
 
   execute "asp_regiis_pi" do
-    command  "aspnet_regiis pi WebTrends.UI.Reporting #{install_dir}\\bin\\PublicPrivateKeys.rsa"
+    command  "C:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319\\aspnet_regiis pi WebTrends.UI.Reporting #{install_dir}\\bin\\PublicPrivateKeys.rsa"
   end
 
   execute "asp_regiis_pa" do
-    command  "aspnet_regiis pa WebTrends.UI.Reporting #{rsa_user}"
+    command  "C:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319\\aspnet_regiis pa WebTrends.UI.Reporting #{rsa_user}"
   end
 
   # delete the .rsa file
-  file "#{install_dir}\\bin\\PublicPrivateKeys.rsa" do
-    action :delete
-  end
+  # file "#{install_dir}\\bin\\PublicPrivateKeys.rsa" do
+    # action :delete
+  # end
 
 	share_wrs
 end
