66072fa911fde829f78fe546f5c4161ad9083272
Toby Mosby <toby.mosby@webtrends.com>
2012-11-01 20:01:39 +0000

    Merge conflict lines.
    Former-commit-id: 288dca62e5fa15d2bab80339f9ec637d7e94fcb1 [formerly 93312445706fd0a4c50d0dc732b2943d4d5dd4f0] [formerly cf59cbabe9f53a9a1cfe807caad50102761cae11] [formerly 99d82f5176dde0796cf5e19f7317aacfaf87c9be [formerly 1375ef9ca66b8b5043f9db908c122d256a3a4711]] [formerly f986e30d9197f2079e6c8cfa242b2820a812cabf [formerly 1e1409282ec0db3f859038ed2714df29e3756d2a] [formerly f7ef457466d2325bcdb9fecb180ab4bde4f38235 [formerly 2a5a50c7d8bd9d6544b27205a8b8d77c501e5f22 [formerly 226e9dcfe63e8a40cb2a680f5de5c9075eb7cbbb]]]]
    Former-commit-id: a22504c6f57ae718b0fae1cbacba2187ab3891fb [formerly aca5de640bc0c9b084085f4081741df0c73f6dee] [formerly 75fb69b5af28c45cb9d20089968d90dca52ee4b4 [formerly 2a5a50c7d8bd9d6544b27205a8b8d77c501e5f22]]
    Former-commit-id: 76a4441e98a7820d1c0d34b46ce22bec60fbfab4 [formerly 28f85b55ad44c41cf6b4a0c4e5443a073814d36d]
    Former-commit-id: c77f8ad1b66b642302a746b949e6adc92333a41e
    Former-commit-id: 0c837bc479d73ce8a8b638001cb94c2a4a6284c5

--- cookbooks/wt_portfolio_manager/metadata.rb
+++ cookbooks/wt_portfolio_manager/metadata.rb
@@ -3,11 +3,7 @@ maintainer_email "kendrick.martin@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures Portfolio Manager IIS site"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-<<<<<<< HEAD
 version          "1.1.10"
-=======
-version          "1.1.7"
->>>>>>> 1f5c1fb39a3990e1093d5935c929f06b4fbc80a4
 supports         "windows"
 depends          "iis"
 depends          "windows"
