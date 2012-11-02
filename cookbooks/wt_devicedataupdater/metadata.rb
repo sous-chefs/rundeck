4c3f223118fd6046365abc3cb517c0005a7f00b9
John Easterday <easterdayj@sops02.staging.dmz>
2012-11-02 22:45:16 +0000

    various config changes to wt_devicedataupdater
    Former-commit-id: 483a72030b626db1b7704543779a85cddfd2e3ac [formerly 0f222f40eab36b9012fc2f405708ab8f94e2415b] [formerly 4f303e152a18fc9f6f2b67bf05c10a9026236581 [formerly 945fd5416c8532324305d82804ccaf27caab58a6]]
    Former-commit-id: ec4a70cd621f13246e4825275345590b17c6136c [formerly e4f7ff9fdfff4ecae8f5011853c757efc7d8a57d]
    Former-commit-id: db6ba3a4c040c8d1518dd6c36ba2baf3d81d0282

--- cookbooks/wt_devicedataupdater/metadata.rb
+++ cookbooks/wt_devicedataupdater/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "John.Easterday@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures Webtrends Device Data Updater"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 supports         "windows"
 depends          "wt_base"
