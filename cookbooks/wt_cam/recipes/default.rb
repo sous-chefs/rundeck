1320b23bfbddd1a8f57295195e98e23e94478b0c
martink <kendrick.martin@webtrends.com>
2012-11-05 10:43:42 -0800

    Changes to sauth and cam to fix whitespace, typos, and food critic errors
    Former-commit-id: 4a73effbdc80413aff2b3bb642c8f7b5b65c632e [formerly 2a03126275d9415b0b271155efcf7b256aa3b61c] [formerly c49dd0c43f36a1b0ec6a86ab056269baf8d974c9] [formerly 72065229d952794c64e5c2e2d6777ea3800d45c2 [formerly b0863a1aa0c8fec94055abd65129be74a6d05528]] [formerly dce07d54ee37d714751e0103d0580d0133b33927 [formerly 44f4e22c035c0cde95b4bec8b85e2aa0e368f7a1] [formerly 988bd785381ee6d923752e6de456e5c59e67cb6b [formerly e2b6b6839bff64b349eff642aa1acf59e3e30d57 [formerly 4a89815dabbb30b04614e5f19c63874f7b29cd49]]]]
    Former-commit-id: a0b1fd348a45ea0365c4e6f90044818dd3745560 [formerly 3a06426e6a42b1b92742b53dae71b13b38e474b3] [formerly 2c644f5cd11f994627fed160e9092fd48605e096 [formerly e2b6b6839bff64b349eff642aa1acf59e3e30d57]]
    Former-commit-id: 21a3475029c5fb260c4bdac42738fd8f23a5197c [formerly 49aad4a4d1b1e0a533db7a793cb67fa502438eee]
    Former-commit-id: d29661868065f36effb3da54a4f643c1df27360b
    Former-commit-id: 72bb7593371e20b6ca3b888699ad82776edb16e0

--- cookbooks/wt_cam/recipes/default.rb
+++ cookbooks/wt_cam/recipes/default.rb
@@ -8,7 +8,7 @@
 # All rights reserved - Do Not Redistribute
 # This recipe installs the CAM IIS app
 
-if deploy_mode?
+if ENV["deploy_build"] == "true" then
   include_recipe "ms_dotnet4::regiis"
   include_recipe "wt_cam::uninstall"
 end
@@ -23,9 +23,9 @@ auth_cmd = "/section:applicationPools /[name='#{app_pool}'].processModel.identit
 http_port = node['wt_cam']['port']
 
 iis_pool app_pool do
-    pipeline_mode :Integrated
-    runtime_version "4.0"
-    action [:add, :config]
+  pipeline_mode :Integrated
+  runtime_version "4.0"
+  action [:add, :config]
 end
 
 iis_site 'Default Web Site' do
@@ -74,55 +74,57 @@ wt_base_icacls log_dir do
 end
 
 wt_base_netlocalgroup "Performance Monitor Users" do
-        user user_data['wt_common']['ui_user']
-        returns [0, 2]
-        action :add
+  user user_data['wt_common']['ui_user']
+  returns [0, 2]
+  action :add
 end
 
-if deploy_mode?
+if ENV["deploy_build"] == "true" then
   windows_zipfile install_dir do
     source node['wt_cam']['cam']['download_url']
     action :unzip
   end
+end
 
-  template "#{install_dir}\\web.config" do
-    source "web.config.erb"
-    variables(
-      :db_server => node['wt_cam']['db_server'],
-      :db_name   => node['wt_cam']['db_name'],
-      :ldap_host => node['wt_common']['ldap_host'],
-      :ldap_port => node['wt_common']['ldap_port'],
-      :ldap_user => user_data['wt_common']['ldap_user'],
-      :ldap_password => user_data['wt_common']['ldap_password'],
-      :smtp_host => node['wt_common']['smtp_server'],
-      :streams_ui_url => node['wt_streaming_viz']['streams_ui_url']
-  	)
-  end
+template "#{install_dir}\\web.config" do
+	source "web.config.erb"
+	variables(
+		:db_server => node['wt_cam']['db_server'],
+		:db_name   => node['wt_cam']['db_name'],
+		:ldap_host => node['wt_common']['ldap_host'],
+		:ldap_port => node['wt_common']['ldap_port'],
+		:ldap_user => user_data['wt_common']['ldap_user'],
+		:ldap_password => user_data['wt_common']['ldap_password'],
+		:smtp_host => node['wt_common']['smtp_server'],
+		:streams_ui_url => node['wt_streaming_viz']['streams_ui_url']
+	)
+end
 
-  template "#{install_dir}\\log4net.config" do
-    source "log4net.config.erb"
-    variables(
-      :log_level => node['wt_cam']['log_level']
-    )
-  end
+template "#{install_dir}\\log4net.config" do
+	source "log4net.config.erb"
+	variables(
+		:log_level => node['wt_cam']['log_level']
+	)
+end
 
-  # add the plugins here
-  include_recipe "wt_cam::cam_plugins"
+# add the plugins here
+include_recipe "wt_cam::cam_plugins"
 
-  iis_config auth_cmd do
-  	action :config
-  end
-	
+iis_config auth_cmd do
+	action :config
+end
+
+if ENV["deploy_build"] == "true" then	
   #add the user to the admin group to create perfmon counters
   wt_base_netlocalgroup "Administrators" do
-        user user_data['wt_common']['ui_user']
-        returns [0, 2]
-        action :add
+    user user_data['wt_common']['ui_user']
+    returns [0, 2]
+    action :add
   end  
 
-	ruby_block "prehead app pool" do
-	  block do
-		  require 'net/http'
+	ruby_block "preheat app pool" do
+    block do
+      require 'net/http'
       uri = URI("http://localhost/")
       puts Net::HTTP.get(uri)
 	  end
@@ -131,8 +133,8 @@ if deploy_mode?
 	
   #remove the user from the admin group
   wt_base_netlocalgroup "Administrators" do
-        user user_data['wt_common']['ui_user']
-        returns [0, 2]
-        action :remove
+    user user_data['wt_common']['ui_user']
+    returns [0, 2]
+    action :remove
   end
 end
