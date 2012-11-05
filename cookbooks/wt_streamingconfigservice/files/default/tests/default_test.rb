038ca5566ec3eda7b8a5f585864a3a7032004597
Adam Sinnett <sinneta@webtrends.com>
2012-11-05 14:06:57 -0800

    Fixed a bug in unit test.
    Former-commit-id: 81c9c67ce6813431a36ef85b80b1843969e8d289 [formerly a15bc3847ec498f4e90ebf1fa1b98b796743a13a] [formerly db4769862e9b35145beb2f97bae0789d38bcdd97] [formerly 5e4ea8f2d58144e60fb3fcc6ad5202ef6248706f [formerly 05d773cb2f20b6999c72b3475d57a670a4fe1d72]] [formerly 113c650551f153451bfd94140a413c5a50b7a40e [formerly 663671d97422d0edd6c03a714145f973d455a2d7] [formerly 26423f40cb47e4b3573ca959e285bf39d25e7fbe [formerly dfc107f1a4f9880f7e4bd14903f3defd515942cd [formerly da1661f8c3b1ec4734474546d32154d68c103eeb]]]]
    Former-commit-id: d22fd56db3f70b6718fb41785c4e212e02599829 [formerly 898206d42a0978de6b13f4da78e2a3e81ae925ac] [formerly 311c90b1c27c596b27852aa2acfa800c0092214e [formerly dfc107f1a4f9880f7e4bd14903f3defd515942cd]]
    Former-commit-id: eb4f30a2e50021aafcea672c7cc1569e118f1463 [formerly 9d5c24cbff3479617ce05cc3405fbf791f4f9ffe]
    Former-commit-id: f05b170ad3acf3ee3cb6a16a0e473f39e1222f4b
    Former-commit-id: 452cc82c76f482485911f520dd438655539876d8

--- cookbooks/wt_streamingconfigservice/files/default/tests/default_test.rb
+++ cookbooks/wt_streamingconfigservice/files/default/tests/default_test.rb
@@ -3,11 +3,10 @@ class Wt_streamingconfigserviceTest < MiniTest::Chef::TestCase
   require 'yajl'
 
   def test_healthcheck
-    sleep(40404040)
-    res = Net::HTTP.get("localhost","/healthcheck",9000)
-    status = JSON.parse(res)
-    status["checks"].each do |check|
-      assert_equal 'true', check["healthy"]
+    res = Net::HTTP.get(node['fqdn'],"/healthcheck",9000)
+    status = Yajl::Parser.parse(res)
+    status["checks"].each do |check, value|
+      assert_equal 'true', value["healthy"]
     end
   end
 end
