ffcb06d333c883728117bcbdb8a8b35dad1aeb0c
Adam Sinnett <sinneta@webtrends.com>
2012-11-05 13:47:21 -0800

    Added minitest_handler to depends

--- cookbooks/wt_streamingconfigservice/metadata.rb
+++ cookbooks/wt_streamingconfigservice/metadata.rb
@@ -6,3 +6,4 @@ long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version             "1.0.3"
 depends             "java"
 depends             "runit"
+depends             "minitest-handler"
