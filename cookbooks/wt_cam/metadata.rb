0c5ad358e7ee01373cb6b20f680c1a2ad390aa0f
martink <kendrick.martin@webtrends.com>
2012-11-06 08:28:57 -0800

    Changing wt_base icalcs calls to be chef directory rights calls
    Former-commit-id: e6536ced2df7080225e87ad51bf27711e658003d [formerly 3ba1225ed9b3b3eb3a52fecf50b2f5dc4b227959] [formerly 83f6ecee522b10af987d367341f4a8c43bc3e2d3] [formerly 08c8f16cdf4c7b18599d017ddad7fa57ec4a9ed4 [formerly 8261452a25074a9ea141170751985824a10820f3]] [formerly 0dcbeb53b420992169d0560bbe0baab052e9b2e1 [formerly d40f4af1a2c274db40dd8fa9b6ca4b877b0c9579] [formerly 38ad8f7655fceb54d6d3b47b2c302a8b936e9020 [formerly cc8d2324bb8ddeff6599753ef9fd459962b60970 [formerly da7bb5b1593e975b9d43770a47ba89a0f4626265]]]]
    Former-commit-id: 3aea978370625416455788767ddd8c5438136f2c [formerly a7462686684fdb1d01d7c002b9e8d34d0113bf87] [formerly 477f3d384c9914e6e6a5f71b384904dac0f13b26 [formerly cc8d2324bb8ddeff6599753ef9fd459962b60970]]
    Former-commit-id: 2b0cfa9b89040ee3eae43a12169fc06275bfaee5 [formerly ff36fd2436a8de81e68a5003a1651ecbc65f73ce]
    Former-commit-id: 3d45659a0ebab11b98fbf4dbb24a6adcc1e2af27
    Former-commit-id: c445874a39e873e0121dbd5686a1136cd59be241

--- cookbooks/wt_cam/metadata.rb
+++ cookbooks/wt_cam/metadata.rb
@@ -1,9 +1,9 @@
 maintainer       "Webtrends"
-maintainer_email "kendrick.martin@webtrends.com"
+maintainer_email "mark.alexander@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures CAM IIS site"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.0"
+version          "1.1.1"
 supports         "windows"
 depends          "iis"
 depends          "windows"
