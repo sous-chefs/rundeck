31315c2b6841295c5bca5584588c9a4cde8227a8
Adam Sinnett <sinneta@webtrends.com>
2012-11-06 12:34:24 -0800

    Updated streamingconfigservice minitests to use common unit-test healthcheck test.

--- cookbooks/wt_streamingconfigservice/recipes/default.rb
+++ cookbooks/wt_streamingconfigservice/recipes/default.rb
@@ -182,7 +182,6 @@ if node.attribute?("nagios")
 
 end
 
-minitest_handler cookbook_name do
-  recipe_name recipe_name
-  action :run
+minitest_handler "wt_unittests" do
+  test_name %w{healthcheck}
 end
