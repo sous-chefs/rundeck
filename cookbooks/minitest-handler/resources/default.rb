5bfb913a02dce74a4be28db77029bbe30c294819
Adam Sinnett <sinneta@webtrends.com>
2012-11-06 12:46:24 -0800

    Tweaking minitest to ensure pulling from the right cookbook.
    Former-commit-id: 603db8beb4200045938650d053be1a09808ab134 [formerly 83ef5cb4712d0447fe4a93c41e10c635626514fc] [formerly c6f14edcbeef18428e951df16e62974ef5333c3f [formerly 5e61880d0761dfa00293d3e3818731cd13fe876c [formerly 90e59faca1f61defb8f00c7fa725df62b18d379a]]]
    Former-commit-id: 6a2215a382be6a415e8f88b73a226d0d2805da3f [formerly a93eeef3b380f69dd64e8bbae1877b3293f40c8b]
    Former-commit-id: 0ac959a65b70905c7ad261b00b0e49934f35ad0d
    Former-commit-id: 9be6c1ba4333bd72c1f157682f8482a12dc33842

--- cookbooks/minitest-handler/resources/default.rb
+++ cookbooks/minitest-handler/resources/default.rb
@@ -6,8 +6,8 @@
 
 actions :run, :nothing
 
-attribute :cookbook_name, :kind_of => String,       :name_attribute => true
-attribute :test_name,     :kind_of => String,       :default => "default"
+attribute :cookbook,      :kind_of => String,       :name_attribute => true
+attribute :test_name,     :kind_of => Array,        :default => %w{default}
 attribute :test_type,     :kind_of => String,       :default => "test"
 attribute :mode,          :kind_of => /^0\d{3,4}$/, :default => 00755
 attribute :owner,         :kind_of => String,       :default => "root"
