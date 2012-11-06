7df6f5429dd507a479ccc8277b3ea0c0ca36b65e
Marek Dolgos <marek.dolgos@webtrends.com>
2012-11-06 14:41:36 -0800

    Updated kafka and wt_streamingcollection cookbooks.
    Former-commit-id: 3a16ac16f642137cf128d0fec014c6e292f6db6a [formerly 25c2d3a181d0e6fb904cd6bf11de78f2dca763cf] [formerly d6339e4689b530b439995ebb68c3f2c3259ff897 [formerly d538d27cd2ae05d0c6947014ff9bb5a1a575d3a1]]
    Former-commit-id: 6adaa7fe8120c10b1879cfc720806cac152b08eb [formerly fc45617e33168074178d1a7ea09f820304cc3482]
    Former-commit-id: 0e412abeffe72985d35cb52436bd52f64f919c56

--- cookbooks/kafka/metadata.rb
+++ cookbooks/kafka/metadata.rb
@@ -3,7 +3,8 @@ maintainer_email  "ivan.vonnagy@webtrends.com"
 license           "Apache 2.0"
 description       "Intalls and configures a Kafka broker"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.19"
+version           "1.0.20"
+
 depends           "java"
 depends           "runit"
 depends           "zookeeper"
