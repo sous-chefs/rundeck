b764312467989e48052c7e1a356186f6631d8e36
Adam Sinnett <sinneta@webtrends.com>
2012-11-06 12:34:45 -0800

    Created metacookbook to hold common test cases.
    Former-commit-id: 0c6f435623ff6f163cb79943ed46cb056e495080 [formerly 79de6dea9f59bcd22ab0d7c798e8bd327b78a9b4] [formerly d99c85c31841f489c5ddff06feda3c5f179cf0fe] [formerly 31a8ea658e3e773080c2ee89cb67a0acc8681cda [formerly 150502606e7bbbe7e25a43df80d183d7092f0b26]] [formerly b79c02bdf6209d7523d24e89658cb65af627f407 [formerly 94d89f7d6bda001c44dbef938b842f0e0eddf11c] [formerly 73a674c156190e3fc8b58865b861718e80c789a5 [formerly 414f14f1d8ee48b3a3f93dcf143e80a3b78fe794 [formerly 40b6254d6073243683197309a1e933b0142f299e]]]]
    Former-commit-id: ab9b6bc30183ab7ea7b9012f4145fe8d678b392d [formerly 1abfb3d27fd84f7a946fe152e06276d59a2d68d4] [formerly 28e53e35b7a63c5812ad817156d7f879051bed84 [formerly 414f14f1d8ee48b3a3f93dcf143e80a3b78fe794]]
    Former-commit-id: 8aced2e7b4ad6bc3a42575906ffca61c3d429122 [formerly 03b44f5d175310bd57d508747eeef5c7aa90501e]
    Former-commit-id: 591c2b0f64b3cd82a30225fb1322c4a292241b4d
    Former-commit-id: 006e96d067cd135d30f7c690d8a1412cd082f565

---
+++ cookbooks/unit-tests/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Webtrends, Inc."
+maintainer_email "adam.sinnet@webtrends.com"
+license          "All rights reserved"
+description      "Meta cookbook to contain common unit tests"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
