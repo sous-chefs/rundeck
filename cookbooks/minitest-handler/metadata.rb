a22058c09f79f248ac65298f554162218e069fbc
Adam Sinnett <sinneta@webtrends.com>
2012-11-06 11:36:21 -0800

    Removed overeager vim repeat on minitest handler version.
    Former-commit-id: 93762e6b8ef73e22046af1c0ba5faa7d5cae34f7 [formerly a5ea061ad06ccc9b7813c3f6ff99e3345e580a12] [formerly a3a2f1aa34db520128b767a0b15c78aea3240ace] [formerly 36894fa3f980f7b34027dcb6e6b0e8b299bbaed9 [formerly eb1934d87b13e2b4a0d549d72e5349065cd7d2a5]] [formerly d98740738213f06a9d93e2703a5484149bd6908e [formerly 9e9a7ede54c20ef4cc168b3356a05a5059880c15] [formerly 85f6c8c910aba8f480b882248e606e1ada5c7a8c [formerly b1e4250597251cbeb8bcf62eb8fa394301c507ac [formerly b35b42d2fc8240e53a1f1b525224b5a48c54588d]]]]
    Former-commit-id: 1035d50be01f6ce86c5c36ee506667800a4d920c [formerly b80c6c87c4b0db3fdf972cec5f24f3c57c02db34] [formerly 3bcfcd13bd34ba02aa6e66c22dc4a9ba0ddfa2fc [formerly b1e4250597251cbeb8bcf62eb8fa394301c507ac]]
    Former-commit-id: 6e27d7521ae0bf963d75361287286f4d77e5b540 [formerly a2357bd9b626e265dadcdfc6701ec53bf1761581]
    Former-commit-id: 0700ad5dacda105a49e9c89932e7162c9190e5d2
    Former-commit-id: e627d8b5fd64fecc9824d15cfd0308fbbeb935fc

--- cookbooks/minitest-handler/metadata.rb
+++ cookbooks/minitest-handler/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "adam.sinnet@webtrends.com"
 license          "All rights reserved"
 description      "Collection of meta-recipes to include testing in other cookbooks."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.333.0"
+version          "0.3.0"
 
 depends "chef_handler"
