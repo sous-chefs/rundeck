b35aeeb5c4a5ce32a4e52901fd22965c7816606c
Michael Parsons <parsonsm@sops02.staging.dmz>
2012-11-07 00:38:44 +0000

    bumping version for config changes
    Former-commit-id: d0d714ddd395a5afa9c2d356ebbb2fe393b67bed [formerly 1ec1fad97bf33a9c1a47521e3abd7b223523278a] [formerly 8ca4e10324fd496d8d403fe272fa3b4a23c4ca8d [formerly 672f21e8332bc8e47c5a1b481d7b0b9331953eb3 [formerly 0836b9a3e73c7d9a94f3dd556e7d50289677ef68]]]
    Former-commit-id: 0bdb81b8fd7e817f7b4323fa93470b4c31886d0d [formerly 8031dc46482720994e321e24cc21aabac24db2bf]
    Former-commit-id: f24e3770184be793c9c2b256ca71729713676709
    Former-commit-id: c8560785ef72140312cacfca9ca0068e4f8c98e8

--- cookbooks/wt_data_deleter/metadata.rb
+++ cookbooks/wt_data_deleter/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "Michael.Parsons@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures Webtrends Data Deleter Service"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.2"
+version          "1.0.3"
 depends          "wt_base"
 supports         "windows"
