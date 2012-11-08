27cd07bbf8945f4d9cde49b772f3d720318bf3f0
Jeff Berger <Jeff.Berger@webtrends.com>
2012-11-08 13:42:33 -0800

    Add wt_edge_server cookbook
    Former-commit-id: 2b4395ae0ce906f7ba7acc53033687745d432e91 [formerly 97e9f733512193f358772b240bdfeab2eee7af25] [formerly 6b420bd623ceb4d077028cee1756ec57180a2494 [formerly 64ca2e541c48fd00fc5fc93cdad91d2a10915421]]
    Former-commit-id: 8a3720ab960aa127b92053737cb6b04fae2c2a23 [formerly e8e9a5665c321e6a6ff498785e0a451a5662be59]
    Former-commit-id: 3b0037392c7565f58104470c56d17d5afa1b482d

---
+++ cookbooks/wt_edge_server/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Webtrends Inc."
+maintainer_email "Jeff.Berger@webtrends.com"
+license          "All rights reserved"
+description      "Installs/Configures wt_edge_server"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
