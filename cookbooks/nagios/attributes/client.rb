6ffa36971c150019f648e19160501b041b7ad633
Tim Smith <tim.smith@webtrends.com>
2012-11-11 19:23:41 -0800

    Reformat attributes to line up.  Also add fedora support to the client installs
    Former-commit-id: e21b4d862bf08a84abf2cbd4128a5058ba3c55d3 [formerly da3bc0ff9f5643f70bba1ae2faa7849af37f9c01] [formerly b475a2f4a9b853a75437ff65f6120412bfffce2b [formerly bcb46ac004ceba5c510a9631175c61191a4617a2]]
    Former-commit-id: 3fb068202e1c8ab5656431433fa09dc99eb16fb2 [formerly 9a88aac41b792b407cc4efcbf2002f2999fdefed]
    Former-commit-id: 6047a7b1b1441130db605e3b7bc84e0f62bb2731

--- cookbooks/nagios/attributes/client.rb
+++ cookbooks/nagios/attributes/client.rb
@@ -25,36 +25,36 @@
 case node['platform_family']
 when 'debian'
   default['nagios']['client']['install_method'] = 'package'
-  default['nagios']['nrpe']['pidfile'] = '/var/run/nagios/nrpe.pid'
-  default['nagios']['nrpe']['home'] = '/usr/lib/nagios'
+  default['nagios']['nrpe']['pidfile']          = '/var/run/nagios/nrpe.pid'
+  default['nagios']['nrpe']['home']             = '/usr/lib/nagios'
 when 'rhel','fedora'
   default['nagios']['client']['install_method'] = 'package'
-  default['nagios']['nrpe']['pidfile'] = '/var/run/nrpe.pid'
-  default['nagios']['nrpe']['home'] = '/usr/lib64/nagios'
+  default['nagios']['nrpe']['pidfile']          = '/var/run/nrpe.pid'
+  default['nagios']['nrpe']['home']             = '/usr/lib64/nagios'
 else
   default['nagios']['client']['install_method'] = 'source'
-  default['nagios']['nrpe']['pidfile'] = '/var/run/nrpe.pid'
-  default['nagios']['nrpe']['home'] = '/usr/lib/nagios'
+  default['nagios']['nrpe']['pidfile']          = '/var/run/nrpe.pid'
+  default['nagios']['nrpe']['home']             = '/usr/lib/nagios'
 end
 
-default['nagios']['nrpe']['conf_dir'] = '/etc/nagios'
-default['nagios']['nrpe']['dont_blame_nrpe'] = 0
-default['nagios']['nrpe']['command_timeout'] = 60
+default['nagios']['nrpe']['conf_dir']           = '/etc/nagios'
+default['nagios']['nrpe']['dont_blame_nrpe']    = 0
+default['nagios']['nrpe']['command_timeout']    = 60
 
 # for plugin from source installation
-default['nagios']['plugins']['url'] = 'http://prdownloads.sourceforge.net/sourceforge/nagiosplug'
-default['nagios']['plugins']['version'] = '1.4.16'
+default['nagios']['plugins']['url']      = 'http://prdownloads.sourceforge.net/sourceforge/nagiosplug'
+default['nagios']['plugins']['version']  = '1.4.16'
 default['nagios']['plugins']['checksum'] = 'b0caf07e0084e9b7f10fdd71cbd3ebabcd85ad78df64da360b51233b0e73b2bd'
 
 # for nrpe from source installation
-default['nagios']['nrpe']['url'] = 'http://prdownloads.sourceforge.net/sourceforge/nagios'
-default['nagios']['nrpe']['version'] = '2.13'
+default['nagios']['nrpe']['url']      = 'http://prdownloads.sourceforge.net/sourceforge/nagios'
+default['nagios']['nrpe']['version']  = '2.13'
 default['nagios']['nrpe']['checksum'] = 'bac8f7eb9daddf96b732a59ffc5762b1cf073fb70f6881d95216ebcd1254a254'
 
 default['nagios']['checks']['memory']['critical'] = 150
-default['nagios']['checks']['memory']['warning'] = 250
-default['nagios']['checks']['load']['critical'] = '30,20,10'
-default['nagios']['checks']['load']['warning'] = '15,10,5'
-default['nagios']['checks']['smtp_host'] = String.new
+default['nagios']['checks']['memory']['warning']  = 250
+default['nagios']['checks']['load']['critical']   = '30,20,10'
+default['nagios']['checks']['load']['warning']    = '15,10,5'
+default['nagios']['checks']['smtp_host']          = String.new
 
 default['nagios']['server_role'] = 'monitoring'
