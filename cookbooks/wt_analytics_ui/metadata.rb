b31471501e48cc8a7df7a7735207b56dd04d89ab
martink <kendrick.martin@webtrends.com>
2012-11-13 10:24:05 -0800

    Updating analytics recipe to allow for correct rsa key install

--- cookbooks/wt_analytics_ui/metadata.rb
+++ cookbooks/wt_analytics_ui/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "kendrick.martin@webtrends.com"
 license          "All rights reserved"
 description      "Installs and configures A10"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.16"
+version          "1.0.17"
 depends          "webpi"
 depends          "iis"
 depends          "windows"
