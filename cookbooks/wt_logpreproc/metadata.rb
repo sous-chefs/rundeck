037d030b48d5e0ea7431c9ea3ef706c7e8b3522b
David Dvorak <david.dvorak@webtrends.com>
2012-11-14 01:05:54 +0000

    lpp, moved templates, added service resource, etc
    Former-commit-id: bac50f8c25bba30cfc4bb8559a36528c9ca74283 [formerly f2b37337452af499aa7726190339ee1ee755195f] [formerly 7b9be59ed58290446f1a0a6e743837f5866a9e1d [formerly 4b3d9b195e718ad452b8b0e4362cb4ba1f60f019]]
    Former-commit-id: 503dbfc297134257c27af9125afa4329570dc482 [formerly 817fe00e840724bdad4236aaf3bd197dad3c07cf]
    Former-commit-id: a555bf8da148cfbc563ef54cde7a3d5e20b8a100

--- cookbooks/wt_logpreproc/metadata.rb
+++ cookbooks/wt_logpreproc/metadata.rb
@@ -1,8 +1,7 @@
 maintainer       "Webtrends"
-maintainer_email "kendrick.martin@webtrends.com"
+maintainer_email "Jeremy.Chartrand@WebTrends.com"
 license          "All rights reserved"
 description      "Installs/Configures Webtrends Lop Preprocessor"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.5"
+version          "1.0.6"
 depends          "wt_base"
-depends          "powershell"
--- cookbooks/wt_logpreproc/recipes/uninstall.rb
+++ cookbooks/wt_logpreproc/recipes/uninstall.rb
@@ -1,19 +1,18 @@
+#
 # Cookbook Name:: wt_logpreproc
 # Recipe:: uninstall
 # Author:: Jeremy Chartrand
 #
-# Copyright 2012, Webtrends, Inc
+# Copyright 2012, Webtrends Inc.
 #
 # All rights reserved - Do Not Redistribute
+#
 # This recipe uninstalls existing Log Preprocessor installs
+#
 
 # destinations
 install_dir = File.join(node['wt_common']['install_dir_windows'], node['wt_logpreproc']['install_dir'].gsub(/[\\\/]+/,"\\"))
-log_dir = File.join(node['wt_common']['install_dir_windows'], node['wt_logpreproc']['log_dir'].gsub(/[\\\/]+/,"\\"))
-
-# get data bag items
-auth_data = data_bag_item('authorization', node.chef_environment)
-svcuser = auth_data['wt_common']['system_user']
+log_dir     = File.join(node['wt_common']['install_dir_windows'], node['wt_logpreproc']['log_dir'].gsub(/[\\\/]+/,"\\"))
 
 logpreproc = File.join(install_dir, node['wt_logpreproc']['service_binary']).gsub(/[\\\/]+/,"\\")
 execute "#{node['wt_logpreproc']['service_binary']} uninstall" do
