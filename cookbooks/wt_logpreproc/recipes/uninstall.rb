a35a30ae6d4da7224fd71a02b1108df48e3187c7
David Dvorak <david.dvorak@webtrends.com>
2012-11-14 20:23:47 +0000

    fixed / to \\ substitution

--- cookbooks/wt_logpreproc/recipes/uninstall.rb
+++ cookbooks/wt_logpreproc/recipes/uninstall.rb
@@ -11,8 +11,8 @@
 #
 
 # destinations
-install_dir = File.join(node['wt_common']['install_dir_windows'], node['wt_logpreproc']['install_dir'].gsub(/[\\\/]+/,"\\"))
-log_dir     = File.join(node['wt_common']['install_dir_windows'], node['wt_logpreproc']['log_dir'].gsub(/[\\\/]+/,"\\"))
+install_dir = File.join(node['wt_common']['install_dir_windows'], node['wt_logpreproc']['install_dir']).gsub(/[\\\/]+/,"\\")
+log_dir     = File.join(node['wt_common']['install_dir_windows'], node['wt_logpreproc']['log_dir']).gsub(/[\\\/]+/,"\\")
 
 logpreproc = File.join(install_dir, node['wt_logpreproc']['service_binary']).gsub(/[\\\/]+/,"\\")
 execute "#{node['wt_logpreproc']['service_binary']} uninstall" do
