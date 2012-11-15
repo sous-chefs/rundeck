b62cb4c4a976d90c4b4caf20c22eedeeeed60d41
Tim Smith <tim.smith@webtrends.com>
2012-11-14 21:53:00 -0800

    Add RHEL 6 support
    Former-commit-id: 1fa2c5648bb49f5a894c4554c6b8111031d37c39 [formerly 74441487d22022fe0c1c58662b3ebce67e8e0532] [formerly 22769239d7de83b905480b3b04ccded0d62a101f [formerly 9567718ba7c0612844b71c87fde1183bd6fef485 [formerly 2b0f5dcf7303dc84bf8a04082de5fa5e9213f077]]]
    Former-commit-id: 336b56d53739f9f71bcdc4855d8c5917e8b924aa [formerly 64891989a9572b5b2b98d9b31908fe0672361d5c]
    Former-commit-id: 722473e97880a39763a434518df6012eed2f6e02
    Former-commit-id: 5cb94c80ada754d716ad95ef06366215155bb4da

--- cookbooks/squid/attributes/default.rb
+++ cookbooks/squid/attributes/default.rb
@@ -23,10 +23,11 @@ default['squid']['network'] = nil
 default['squid']['config_file'] = "/etc/squid/squid.conf"
 default['squid']['timeout'] = "10"
 default['squid']['opts'] = ""
-default['squid']['version'] = ""
 
 if node['platform_family'] = "debian" and node['platform_version'].to_f >= 11.10
     default['squid']['service_name'] = "squid3"
+elsif node['platform_family'] = "rhel" and node['platform_version'].to_f >= 6.0
+	default['squid']['service_name'] = "squid3"
 else
 	default['squid']['service_name'] = "squid"
 end
