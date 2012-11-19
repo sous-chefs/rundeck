0ccafe33fbd6625d308394d65f2a537eab0951c7
Adam Keene <adam.keene@webtrends.com>
2012-11-19 21:27:31 +0000

    wt_portfolio_manager: Add current node domain to proxy bypass list

--- cookbooks/wt_portfolio_manager/metadata.rb
+++ cookbooks/wt_portfolio_manager/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "kendrick.martin@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures Portfolio Manager IIS site"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.15"
+version          "1.1.16"
 supports         "windows"
 depends          "iis"
 depends          "windows"
