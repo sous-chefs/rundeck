54ff5eef10f97b186bb861c45ddb9cbd6c748917
Mark Alexander <mark.alexander@webtrends.com>
2012-11-27 00:12:31 +0000

    removed cam db specific references
    Former-commit-id: 2bedc7a7a40db1d270fbd62c97cb6e16ac264962 [formerly edee6cc8cc0181c80591715461109b6684cff5c2] [formerly ce7f6dbbfa03fc724ee4cdc59c0fd8d6814c0ebe] [formerly 883d4a31eaa44ecbf5462f5ae572a793206cf8b4 [formerly 1aa5906b4542e2be5e3a845d7319f9b831713282]] [formerly fe349d6d9f6d6dacd5de0cd778e357d4fdd2df4d [formerly 0f05ab036fc119bcea03d067581016fe8994db61] [formerly 90543cf9fe7bd9c34b61bdefa7a4faddc76066f8 [formerly 08bf2b01e8880103ac4178222823dcf73eb1f18c [formerly 85a80d1a07c0d59a645a2a5fc6c5b0ae064f1181]]]]
    Former-commit-id: c20956092b0031f8b42484b918218d9c8e562beb [formerly 5d44f826564e8b88d1a7eb18202c407463f56b34] [formerly 8c042c726169234b0fb85a0008e5f5a9d03bacbf [formerly 08bf2b01e8880103ac4178222823dcf73eb1f18c]]
    Former-commit-id: 6195eba1e6dd52cc000f60e7c394efa02bf6d5c7 [formerly 66d399619920e23c55b5d1c533b49ba304bbb323]
    Former-commit-id: 08402c5f504aad50413d8e74ef2e080f36a7ba06
    Former-commit-id: 81b70bd3657f57793beb5c2fb8eab0eb10055442

--- cookbooks/wt_streamingmanagementservice/metadata.rb
+++ cookbooks/wt_streamingmanagementservice/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email    "ivan.vonnagy@webtrends.com"
 license             "All rights reserved"
 description         "Installs/Configures the Webtrends Streaming Management Service"
 long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version             "1.0.0"
+version             "1.0.1"
 depends             "java"
 depends             "runit"
 depends             "minitest-handler"
