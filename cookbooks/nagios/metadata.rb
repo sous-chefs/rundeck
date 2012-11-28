8043a5744047d7449e6523301018495a8cc74ff2
Tim Smith <tim.smith@webtrends.com>
2012-11-28 11:48:04 -0800

    Sync from upstream
    Former-commit-id: ee9eba51780eec0439b9ac98a7e7329800b064ec [formerly d756e4a4fa506ee07a25d821d66ce99391b3c567] [formerly 30905130cba07d66d03da1787c8ecbabb560109a [formerly 11c8718dbeccd576a9aa3b936bd4694a04b96c4b]]
    Former-commit-id: 449b85399a18fa934dcdaa2fd582265d983ee689 [formerly 78fdbb836a5cd4fccad01ecd1c7181fcc9b2252c]
    Former-commit-id: 5ee94b58a79dfb8f6f0e0af3f8bf07b9e6a9f267

--- cookbooks/nagios/metadata.rb
+++ cookbooks/nagios/metadata.rb
@@ -1,7 +1,8 @@
+name              "nagios"
 maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
-description       "Installs and configures nagios"
+description       "Installs and configures Nagios3 server and the NRPE client"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.6.3"
 
