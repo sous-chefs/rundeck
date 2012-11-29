a6e6b01061a60857778c99e48574e8df4fb378ef
David Dvorak <david.dvorak@webtrends.com>
2012-11-29 02:26:35 +0000

    set JAVA_HOME in hadoop-env.sh based on platform

--- cookbooks/hadoop/recipes/default.rb
+++ cookbooks/hadoop/recipes/default.rb
@@ -23,9 +23,11 @@ case node['platform_family']
 when 'debian'
 	package_list   = %w[libsnappy1 python-simplejson python-cjson]
 	hadoop_version = node.hadoop_attrib(:version)[/(.*?)(-\d+)?$/, 1]  # strip out rpm revision
+	java_home      = '/usr/lib/jvm/default-java/jre'
 when 'rhel'
 	package_list   = %w[snappy-devel python-simplejson python-cjson]
 	hadoop_version = node.hadoop_attrib(:version)
+	java_home      = '/usr/lib/jvm/java/jre'
 end
 
 package_list.each do |pkg|
@@ -103,7 +105,8 @@ end
 			:jobtracker     => hadoop_jobtracker,
 			:backupnamenode => hadoop_backupnamenode,
 			:datanodes      => hadoop_datanodes,
-			:local_dir      => local_dir
+			:local_dir      => local_dir,
+			:java_home      => java_home
 		)
 	end
 end
