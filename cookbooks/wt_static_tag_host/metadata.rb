0591a26457782f395614fc3839b1f9e43bceba13
Tim Smith <tim.smith@webtrends.com>
2012-11-29 15:16:00 -0800

    Update metadata with additional supports and depends as well as the name attribute that Chef 11 is going to require

--- cookbooks/wt_static_tag_host/metadata.rb
+++ cookbooks/wt_static_tag_host/metadata.rb
@@ -1,6 +1,8 @@
+name             "wt_static_tag_host"
 maintainer       "Webtrends, Inc."
 maintainer_email "hostedops@webtrends.com"
 license          "Apache 2.0"
 description      "Configures a static tag host host"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.3"
+depends          "apache2"
--- cookbooks/wt_sync/metadata.rb
+++ cookbooks/wt_sync/metadata.rb
@@ -7,3 +7,5 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.5"
 depends          "wt_base"
 depends          "powershell"
+depends          "windows"
+supports         "windows"
