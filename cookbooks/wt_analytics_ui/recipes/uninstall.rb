47b300ed6f53501e9b0c6b816e5a112a41f72c18
Tim Smith <tim.smith@webtrends.com>
2012-11-29 16:27:54 -0800

    Reformat A10
    Former-commit-id: ccaeeea2bb2006b03a46731624bce1b9ba045f66 [formerly cfcd1e2f70579fd3ef9b8be2ab42ffd91f9262c4] [formerly 3548e5d8657b7a8b522c9f6334fb0c5bd45ea5e9 [formerly e0a1192bb13819f129a3534746ed873c8405ef55 [formerly df5a20fa42229ee5027f749001fe004f37e2ef53]]]
    Former-commit-id: 6e45392b81dff393a896faa50d557f562e84ecd8 [formerly bafd71a6bd9a4c1a1b20bfb0e773f3cf67468ad1]
    Former-commit-id: ae6f83aa867373c146939cf80bd1447c586c728f
    Former-commit-id: ae6af582d500867523efe34a9b21f01c78e93e91

--- cookbooks/wt_analytics_ui/recipes/uninstall.rb
+++ cookbooks/wt_analytics_ui/recipes/uninstall.rb
@@ -12,27 +12,27 @@ app_pool = node['wt_analytics_ui']['app_pool_name']
 install_dir = File.join(node['wt_common']['install_dir_windows'], node['wt_analytics_ui']['install_dir']).gsub(/[\\\/]+/,"\\")
 
 iis_pool app_pool do
-	action [:stop, :delete]
-	ignore_failure true
+  action [:stop, :delete]
+  ignore_failure true
 end
 
 iis_site 'Analytics' do
-	action [:stop, :delete]
-	ignore_failure true
+  action [:stop, :delete]
+  ignore_failure true
 end
 
 directory install_dir do
-	recursive true
-	action :delete
+  recursive true
+  action :delete
 end
 
 directory "d:\\wrs\\Insight" do
-	recursive true
-	action :delete
+  recursive true
+  action :delete
 end
 
 wt_base_netlocalgroup "Administrators" do
-	user wt_config['ui_user']
-	returns [0, 2]
-	action :remove
+  user wt_config['ui_user']
+  returns [0, 2]
+  action :remove
 end
