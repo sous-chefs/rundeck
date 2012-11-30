64cb263c91d562e2b6f8f8e9cec5fa4a1dee6f63
Tim Smith <tim.smith@webtrends.com>
2012-11-30 10:15:53 -0800

    wt_netacuity: Remove the webUI nagios check. No longer needed

--- cookbooks/wt_netacuity/metadata.rb
+++ cookbooks/wt_netacuity/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email  "tim.smith@webtrends.com"
 license           "All rights reserved"
 description       "Installs/Configures NetAcuity with a Webtrends license"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.11"
+version           "1.0.12"
