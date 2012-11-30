627cf9e9a41c8f5c6263c39250e96820ce7a951e
Tim Smith <tim.smith@webtrends.com>
2012-11-29 16:32:33 -0800

    Reformat
    Former-commit-id: fee19815b57ce0e0286dda8b3cc842339fb10610 [formerly a83bb1a44d072ca929388f55e83d900ad8a1ca3b] [formerly 466c616972ea7c370fe864fa78c808f2db5b2baa [formerly b7e3d18e1efddd98aababb6943a7bf389ded0232]]
    Former-commit-id: 67c821228f7219b27449f3e2759da5b9a219e586 [formerly 6d49f9ae03d1919e6595e19723be041c3a84ab9c]
    Former-commit-id: a99b556f59741e05a592e051dde4fe3ead5a5edd

--- cookbooks/wt_base/recipes/msdtc.rb
+++ cookbooks/wt_base/recipes/msdtc.rb
@@ -8,31 +8,31 @@
 #
 
 windows_registry 'HKLM\SOFTWARE\Microsoft\MSDTC' do
-	values 'TurnOffRpcSecurity' => 1, 'FallbackToUnsecureRPCIfNecessary' => 0, 'AllowOnlySecureRpcCalls' => 0
-	notifies :restart, "service[MSDTC]"
+  values 'TurnOffRpcSecurity' => 1, 'FallbackToUnsecureRPCIfNecessary' => 0, 'AllowOnlySecureRpcCalls' => 0
+  notifies :restart, "service[MSDTC]"
 end
 
 windows_registry 'HKLM\SOFTWARE\Microsoft\MSDTC\Security' do
-	values 'NetworkDtcAccessInbound' => 1, 'NetworkDtcAccess' => 1, 'NetworkDtcAccessOutbound' => 1, 'NetworkDtcAccessClients' => 1, 'NetworkDtcAccessTransactions' => 1
-	notifies :restart, "service[MSDTC]", :immediately
+  values 'NetworkDtcAccessInbound' => 1, 'NetworkDtcAccess' => 1, 'NetworkDtcAccessOutbound' => 1, 'NetworkDtcAccessClients' => 1, 'NetworkDtcAccessTransactions' => 1
+  notifies :restart, "service[MSDTC]", :immediately
 end
 
 wt_base_firewall "Distributed Transaction Coordinator (RPC)" do
-	action :enable
+  action :enable
 end
 
 wt_base_firewall "Distributed Transaction Coordinator (RPC-EPMAP)" do
-	action :enable
+  action :enable
 end
 
 wt_base_firewall "Distributed Transaction Coordinator (TCP-In)" do
-	action :enable
+  action :enable
 end
 
 wt_base_firewall "Distributed Transaction Coordinator (TCP-In)" do
-	action :enable
+  action :enable
 end
 
 service "MSDTC" do
-	action :nothing
+  action :nothing
 end
