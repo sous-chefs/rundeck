29d622d287ae82bd3df7dda94f89ecc028c55eaa
Tim Smith <tim.smith@webtrends.com>
2012-11-29 16:44:18 -0800

    spaces!
    Former-commit-id: f87fbf53349ba261990e942edc7b8f7c2cae87b7 [formerly 245ebe8657431e48e8d39615661b20d9f812b3bb] [formerly 168400b796e334b1f2389861edf08075e0dc4473 [formerly 7c90d4866c5aec2e581a7722092638e017a817a0 [formerly b1ddb85426bf8e330883011df4453f45d677e23f]]]
    Former-commit-id: 7a4f0164d5aa1249f49c9a80a606fe2af533ba21 [formerly a0989e036f3ea1c2a9a58237518c074dcfd32a42]
    Former-commit-id: c214a69f970e03de7b5fbf078224a38c965e2c40
    Former-commit-id: 6fb610d20f09885bf6f18875a34ff9c208bc81ae

--- cookbooks/wt_logpreproc/recipes/default.rb
+++ cookbooks/wt_logpreproc/recipes/default.rb
@@ -11,10 +11,10 @@
 #
 
 if ENV["deploy_build"] == "true" then
-	log "The deploy_build value is true so un-deploying first"
-	include_recipe "wt_logpreproc::uninstall"
+  log "The deploy_build value is true so un-deploying first"
+  include_recipe "wt_logpreproc::uninstall"
 else
-	log "The deploy_build value is not set or is false so we will only update the configuration"
+  log "The deploy_build value is not set or is false so we will only update the configuration"
 end
 
 # get parameters
@@ -30,91 +30,91 @@ svcpass = auth_data['wt_common']['system_pass']
 
 # create the install directory
 directory install_dir do
-	recursive true
-	action :create
+  recursive true
+  action :create
 end
 
 # grant service account access
 wt_base_icacls node['wt_common']['install_dir_windows'] do
-	user svcuser
-	perm :modify
-	action :grant
+  user svcuser
+  perm :modify
+  action :grant
 end
 
 if ENV["deploy_build"] == "true" then
 
-	log "Source URL: #{download_url}"
+  log "Source URL: #{download_url}"
 
-	# unzip the install package
-	windows_zipfile install_dir do
-		source download_url
-		action :unzip
-	end
+  # unzip the install package
+  windows_zipfile install_dir do
+    source download_url
+    action :unzip
+  end
 
-	svcbin = "#{install_dir}\\wtlogpreproc.exe"
-	execute 'wtlogpreproc.exe' do
-		command "#{svcbin} --install startup=auto username=#{svcuser} password=\"#{svcpass}\""
-		notifies :start, 'service[wtlogpreproc]'
-	end
+  svcbin = "#{install_dir}\\wtlogpreproc.exe"
+  execute 'wtlogpreproc.exe' do
+    command "#{svcbin} --install startup=auto username=#{svcuser} password=\"#{svcpass}\""
+    notifies :start, 'service[wtlogpreproc]'
+  end
 
-	share_wrs
+  share_wrs
 end
 
 template "#{install_dir}\\geoclient.ini" do
-	source "geoclient.ini.erb"
-	variables(
-		:netacuity_host => node['wt_netacuity']['geo_url']
-	)
+  source "geoclient.ini.erb"
+  variables(
+    :netacuity_host => node['wt_netacuity']['geo_url']
+  )
 end
 
 template "#{install_dir}\\wtlogpreproc.ini" do
-	source "wtlogpreproc.ini.erb"
-	variables(
-		:debuglevel            => node['wt_logpreproc']['debuglevel'],
-		:sleepinterval         => node['wt_logpreproc']['sleepinterval'],
-		:dnsenabled            => node['wt_logpreproc']['dnsenabled'],
-		:geotrendsenabled      => node['wt_logpreproc']['geotrendsenabled'],
-		:addgeofield           => node['wt_logpreproc']['addgeofield'],
-		:addgeoqueryparams     => node['wt_logpreproc']['addgeoqueryparams'],
-		:geotrendsretrytimeout => node['wt_logpreproc']['geotrendsretrytimeout'],
-		:logfilebatchsize      => node['wt_logpreproc']['logfilebatchsize'],
-		:debugmsgsbatchcount   => node['wt_logpreproc']['debugmsgsbatchcount'],
+  source "wtlogpreproc.ini.erb"
+  variables(
+    :debuglevel            => node['wt_logpreproc']['debuglevel'],
+    :sleepinterval         => node['wt_logpreproc']['sleepinterval'],
+    :dnsenabled            => node['wt_logpreproc']['dnsenabled'],
+    :geotrendsenabled      => node['wt_logpreproc']['geotrendsenabled'],
+    :addgeofield           => node['wt_logpreproc']['addgeofield'],
+    :addgeoqueryparams     => node['wt_logpreproc']['addgeoqueryparams'],
+    :geotrendsretrytimeout => node['wt_logpreproc']['geotrendsretrytimeout'],
+    :logfilebatchsize      => node['wt_logpreproc']['logfilebatchsize'],
+    :debugmsgsbatchcount   => node['wt_logpreproc']['debugmsgsbatchcount'],
 
-		:source_paths                        => node['wt_common']['ifr_locations'],
-		:wtlogpreproc1_label                 => node['wt_logpreproc']['wtlogpreproc1_label'],
-		:wtlogpreproc1_fileextension         => node['wt_logpreproc']['wtlogpreproc1_fileextension'],
-		:wtlogpreproc1_doneextension         => node['wt_logpreproc']['wtlogpreproc1_doneextension'],
-		:wtlogpreproc1_compresslogfile       => node['wt_logpreproc']['wtlogpreproc1_compresslogfile'],
-		:wtlogpreproc1_compresslogfile_level => node['wt_logpreproc']['wtlogpreproc1_compresslogfile_level'],
-		:wtlogpreproc1_deleteoriginallogs    => node['wt_logpreproc']['wtlogpreproc1_deleteoriginallogs'],
+    :source_paths                        => node['wt_common']['ifr_locations'],
+    :wtlogpreproc1_label                 => node['wt_logpreproc']['wtlogpreproc1_label'],
+    :wtlogpreproc1_fileextension         => node['wt_logpreproc']['wtlogpreproc1_fileextension'],
+    :wtlogpreproc1_doneextension         => node['wt_logpreproc']['wtlogpreproc1_doneextension'],
+    :wtlogpreproc1_compresslogfile       => node['wt_logpreproc']['wtlogpreproc1_compresslogfile'],
+    :wtlogpreproc1_compresslogfile_level => node['wt_logpreproc']['wtlogpreproc1_compresslogfile_level'],
+    :wtlogpreproc1_deleteoriginallogs    => node['wt_logpreproc']['wtlogpreproc1_deleteoriginallogs'],
 
-		:dns_serverlist   => node['wt_logpreproc']['dns_serverlist'],
-		:dns_retrycount   => node['wt_logpreproc']['dns_retrycount'],
-		:dns_retrytimeout => node['wt_logpreproc']['dns_retrytimeout'],
-		:dns_servermethod => node['wt_logpreproc']['dns_servermethod'],
-		:dns_logfile      => node['wt_logpreproc']['dns_logfile'],
+    :dns_serverlist   => node['wt_logpreproc']['dns_serverlist'],
+    :dns_retrycount   => node['wt_logpreproc']['dns_retrycount'],
+    :dns_retrytimeout => node['wt_logpreproc']['dns_retrytimeout'],
+    :dns_servermethod => node['wt_logpreproc']['dns_servermethod'],
+    :dns_logfile      => node['wt_logpreproc']['dns_logfile'],
 
-		:wtda_numthreads                   => node['wt_logpreproc']['wtda_numthreads'],
-		:wtda_compressedext                => node['wt_logpreproc']['wtda_compressedext'],
-		:wtda_encryptedext                 => node['wt_logpreproc']['wtda_encryptedext'],
-		:wtda_maxconsecutiveinvalidentries => node['wt_logpreproc']['wtda_maxconsecutiveinvalidentries'],
+    :wtda_numthreads                   => node['wt_logpreproc']['wtda_numthreads'],
+    :wtda_compressedext                => node['wt_logpreproc']['wtda_compressedext'],
+    :wtda_encryptedext                 => node['wt_logpreproc']['wtda_encryptedext'],
+    :wtda_maxconsecutiveinvalidentries => node['wt_logpreproc']['wtda_maxconsecutiveinvalidentries'],
 
-		:auditlog_limitbysize       => node['wt_logpreproc']['auditlog_limitbysize'],
-		:auditlog_limitbysizemethod => node['wt_logpreproc']['auditlog_limitbysizemethod'],
-		:auditlog_maxsize           => node['wt_logpreproc']['auditlog_maxsize'],
-		:auditlog_trimsize          => node['wt_logpreproc']['auditlog_trimsize'],
-		:auditlog_filenameprefix    => node['wt_logpreproc']['auditlog_filenameprefix'],
-		:auditlog_filenameext       => node['wt_logpreproc']['auditlog_filenameext'],
+    :auditlog_limitbysize       => node['wt_logpreproc']['auditlog_limitbysize'],
+    :auditlog_limitbysizemethod => node['wt_logpreproc']['auditlog_limitbysizemethod'],
+    :auditlog_maxsize           => node['wt_logpreproc']['auditlog_maxsize'],
+    :auditlog_trimsize          => node['wt_logpreproc']['auditlog_trimsize'],
+    :auditlog_filenameprefix    => node['wt_logpreproc']['auditlog_filenameprefix'],
+    :auditlog_filenameext       => node['wt_logpreproc']['auditlog_filenameext'],
 
-		:install_dir  => install_dir
-	)
+    :install_dir  => install_dir
+  )
 end
 
 service 'wtlogpreproc' do
-	supports :start => true, :restart => true
-	subscribes :restart, resources(
-		:template => "#{install_dir}\\geoclient.ini",
-		:template => "#{install_dir}\\wtlogpreproc.ini"
-	)
-	action :nothing
+  supports :start => true, :restart => true
+  subscribes :restart, resources(
+    :template => "#{install_dir}\\geoclient.ini",
+    :template => "#{install_dir}\\wtlogpreproc.ini"
+  )
+  action :nothing
 end
