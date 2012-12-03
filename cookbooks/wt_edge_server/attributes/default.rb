7b91e8aec30a60cac0385e616f5c9100909b7180
Jeff Berger <Jeff.Berger@webtrends.com>
2012-12-03 11:26:16 -0800

    Moving edge server attributes from environment to cookbook
    Former-commit-id: 834248d75930381a10dad9bfd2a0927879bf2e63 [formerly bd76d7eaeed5c49ca3e3f9f255434bab22575ad7] [formerly 192e58e5ae994f285e5ae3f590a843e909763c6b] [formerly 128538d09165abe2e3de1d3a5ae70540f5db776b [formerly aa4c1c2f71eec2a8b621542d492217ef564a8374]] [formerly 462d859ffbf3a3ead77f14aa2af9157b037a96a3 [formerly e711bf5a39cd95f667762154a60f3fb42f99bced] [formerly cd0bdd4c291f8b4036e8b35d7caf8b9b6a7d29e8 [formerly e11474ab1f23958bc9ebcc05b197676e47d01a59 [formerly aa9b7290e8043266777b4270ee043682374f6a71]]]]
    Former-commit-id: e55aea2b64fcad5255c6efce8b041eaf9265bb85 [formerly 474c761133d3753cdf9aca7a13dea13e187376f3] [formerly 339a5899d62c1ec8e58c9b1e61493d670614c7b9 [formerly e11474ab1f23958bc9ebcc05b197676e47d01a59]]
    Former-commit-id: 172604fefdd609c26ca8e96993427cf8899eff8a [formerly 2643207c60dfcbc826490d10d243e733f2424500]
    Former-commit-id: b25b6fbecc508a61b3f4051fe095714b37fc06ca
    Former-commit-id: 88fb0aba0802964467542aff92e768b2c5fd2da8

--- cookbooks/wt_edge_server/attributes/default.rb
+++ cookbooks/wt_edge_server/attributes/default.rb
@@ -14,3 +14,8 @@ default['wt_edge_server']['port']         = 8081
 default['wt_edge_server']['java_opts']    = "-Xms1024m -Djava.net.preferIPv4Stack=true"
 default['wt_edge_server']['jmx_port']     = 10001
 default['wt_edge_server']['log_level']     = "INFO"
+
+default['wt_edge_server']['active_configs_endpoint'] = "/v1/rulesdata/"
+default['wt_edge_server']['config_service_rule_endpoint'] = "/v1/rulesdata/{configid}"
+default['wt_edge_server']['graphite_enabled'] = "true"
+default['wt_edge_server']['graphite_interval'] = "5"
