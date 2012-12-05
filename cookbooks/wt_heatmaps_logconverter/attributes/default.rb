2b58a2d0218d169a58df42bd180c85cc5a3e2168
Tim Smith <tim.smith@webtrends.com>
2012-12-04 16:08:00 -0800

    Add dcswhitelist attribute
    Former-commit-id: 79e4fd00d0f24f825429a3273093a99a59ec7f16 [formerly c2046276e6c9d62af721dd0096889fb8fe216eff] [formerly 499b51696b6be05b451090b44d9cf35fd2e74f47] [formerly f03207440949c962037a32a3085d4c7d10d70908 [formerly 3dbb256f4c7c1c2c1d11bcd86792e0842b930751]] [formerly df4f529fbe2bcc448e22b5e44e912c996ca2a046 [formerly 25b109ff5ac81cb2c6dd203afdb8a24d010b60d8] [formerly bb75ed3169aa92985c8817cdfbba4a207f112583 [formerly a981e67d9e016711ec31025e9c5dd32d7baf0881 [formerly 9dc6f5cced36da484bcf7cd9f64f904f0b1b7ccc]]]]
    Former-commit-id: 94913045ed1112e3643cb72a9a6d5b52f330b9ba [formerly e99a7151da8b392d07a04121aaee6f7b7c348686] [formerly cee78b24b2b0805147a8fb9c2fa27f4565bbb24c [formerly a981e67d9e016711ec31025e9c5dd32d7baf0881]]
    Former-commit-id: 895c9efe6d5d2fae76d5507191ba298c99155a1b [formerly 6cd47f405fdb3fa232e93b62aeb06f532db7eb08]
    Former-commit-id: c2dfaa8bd10e0993472731c8a900996894498e4b
    Former-commit-id: 6c267d3ee532a4c10a157994dc33927c1d0d61a6

--- cookbooks/wt_heatmaps_logconverter/attributes/default.rb
+++ cookbooks/wt_heatmaps_logconverter/attributes/default.rb
@@ -13,8 +13,10 @@ default['wt_heatmaps_logconverter']['download_url'] = ""
 default['wt_heatmaps_logconverter']['java_opts']    = "-Xms1G -Xmx4G"
 default['wt_heatmaps_logconverter']['jmx_port']     = 10000
 
+
 default['wt_heatmaps_logconverter']['log_level'] = "WARN"
 default['wt_heatmaps_logconverter']['nfs_export'] = ""
+default['wt_heatmaps_logconverter']['dcsidWhiteList'] = ""
 default['wt_heatmaps_logconverter']['nfs_mount_dir'] = "/srv/heatmaps_logconverter/"
 default['wt_heatmaps_logconverter']['deleteLogs'] = "true"
 default['wt_heatmaps_logconverter']['aggregateSmallFileRollSize'] = 64
