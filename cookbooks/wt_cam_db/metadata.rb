b5d28b9210fb2daba20aff55dbef18fa3058d90f
Adam Sinnett <sinneta@webtrends.com>
2012-12-05 16:51:13 -0800

    Removed outdated cam_db variables from cookbook
    Former-commit-id: 7ec3c7f75d288088d0084bcf732744e667ffd1ca [formerly 9bed5fdffd8661f1a72261e2bb129c507f116b90] [formerly efa566a88188b1e2b534d7e869a398e824abb64b [formerly 36d65802c137fa942793c7b28fe640648c6b079c [formerly 78744b3cfe9b42b3d97ec428d4a187f823d7f551]]]
    Former-commit-id: 2f75002900dd1becbb235b93fd8a5034d9f08444 [formerly 1cab4d8ed8b70787f8e7329dbc630705b19b0720]
    Former-commit-id: ad4e59c666aeb295d64297b3c68dfe06bc352f05
    Former-commit-id: a6a8a0bc3684f33733d3670739292f19f98bec65

--- cookbooks/wt_cam_db/metadata.rb
+++ cookbooks/wt_cam_db/metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "adam.sinnet@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures the Webtrends CAM database"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.4"
+version          "0.1.5"
 depends          "windows"
