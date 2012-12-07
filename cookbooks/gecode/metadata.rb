acb51f88c9ba7285492563f59249f2f544649d61
Tim Smith <tim.smith@webtrends.com>
2012-12-07 21:40:19 +0000

    Import gecode version 2.0.0

--- cookbooks/gecode/metadata.rb
+++ cookbooks/gecode/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs gecode"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.3.0"
+version          "2.0.0"
 
 %w{ debian ubuntu redhat centos scientific fedora mac_os_x }.each do |os|
   supports os
