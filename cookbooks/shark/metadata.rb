f10e3321442e472b172b9d5cf00667f870bca038
seanm <sean.mcnamara@webtrends.com>
2012-12-07 16:33:19 -0700

    not using shark now.  may revisit later
    Former-commit-id: 27801cfd67fda9ce70c21b2f6b7a4347fc01107b [formerly 59f3fbc13986a437ff90a849c2acd7f6cc853d73] [formerly 695830834924cb7f0bc0f07e3a236be58fff0b53 [formerly d8680d0064299a98354ebb6136a3343c9bc7418e [formerly 8cfe35166429688237732b2b74fb62783742b219]]]
    Former-commit-id: 4f4198782229ee9fbac5d5e9464bef28879dced4 [formerly 097fdd9ba211419ab439346d53ce48e90a79a053]
    Former-commit-id: fa04ff71b2234ce87e7bbc1638fa1ac9235df782
    Former-commit-id: b076860b57ec4762f3920ea02e430c9912fcfa25

--- cookbooks/shark/metadata.rb
+++
@@ -1,9 +0,0 @@
-maintainer        "Webtrends Inc."
-maintainer_email  "sean.mcnamara@webtrends.com"
-license           "Apache 2.0"
-description       "Installs Shark"
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.0"
-depends           "java"
-depends           "spark"
-
