302f7c2f50249bd2a4e32089c655ae19cdf27c4f
Tim Smith <tim.smith@webtrends.com>
2012-12-11 18:44:00 -0800

    Symbol to string conversion in webtrends cookbooks
    Former-commit-id: 35604d93061a633458c84b95801f56d28f1bb2d3 [formerly 38594178ab2a6e834d940dcf9594262356d6a663] [formerly 2210a16847326e33a15ebc862d0cd34e108277eb [formerly 117a1864899ef24f3bdcafb0488b9d72223e50a6]]
    Former-commit-id: a38fd36d8652b6a556da8919eb5557f98dab74b2 [formerly f3d8ffded08bc8f31b8e4e36fa572f510fe465ed]
    Former-commit-id: 2b7fbdaec3597e49bec8189088dacd2b424c715b

--- cookbooks/wt_monitoring/recipes/nagios_metrics.rb
+++ cookbooks/wt_monitoring/recipes/nagios_metrics.rb
@@ -11,10 +11,10 @@
 
 cron "email_metrics" do
   minute "*/30"
-  command "cat /var/log/nagios3/nagios.log | grep \"SERVICE NOTIFICATION: shaggy;\" | grep \-P \-o '(\?<=\\[)[0-9]*(\?=\\])' | uniq -c |sed -e 's/^[ \\t]*//' -e 's/^/echo \"#{node[:wt_monitoring][:metric_prefix]}.#{node[:hostname]}.nagios.alerts.emails /' -e 's/$/\" | nc #{node[:wt_monitoring][:graphite_server]} #{node[:wt_monitoring][:graphite_port]}/' | sh"
+  command "cat /var/log/nagios3/nagios.log | grep \"SERVICE NOTIFICATION: shaggy;\" | grep \-P \-o '(\?<=\\[)[0-9]*(\?=\\])' | uniq -c |sed -e 's/^[ \\t]*//' -e 's/^/echo \"#{node['wt_monitoring']['metric_prefix']}.#{node['hostname']}.nagios.alerts.emails /' -e 's/$/\" | nc #{node['wt_monitoring']['graphite_server']} #{node['wt_monitoring']['graphite_port']}/' | sh"
 end
 
 cron "pager_metrics" do
   minute "*/30"
-  command "cat /var/log/nagios3/nagios.log | grep \"SERVICE NOTIFICATION: noc-pager;\" | grep \-P \-o '(\?<=\\[)[0-9]*(\?=\\])' | uniq -c |sed -e 's/^[ \\t]*//' -e 's/^/echo \"#{node[:wt_monitoring][:metric_prefix]}.#{node[:hostname]}.nagios.alerts.pages /' -e 's/$/\" | nc #{node[:wt_monitoring][:graphite_server]} #{node[:wt_monitoring][:graphite_port]}/' | sh"
+  command "cat /var/log/nagios3/nagios.log | grep \"SERVICE NOTIFICATION: noc-pager;\" | grep \-P \-o '(\?<=\\[)[0-9]*(\?=\\])' | uniq -c |sed -e 's/^[ \\t]*//' -e 's/^/echo \"#{node['wt_monitoring']['metric_prefix']}.#{node['hostname']}.nagios.alerts.pages /' -e 's/$/\" | nc #{node['wt_monitoring']['graphite_server']} #{node['wt_monitoring']['graphite_port']}/' | sh"
 end
