715553c40b10fa14c6dee364fba84c2c37e422d3
David Dvorak <david.dvorak@webtrends.com>
2012-12-13 21:26:58 +0000

    test lib
    Former-commit-id: 089ba7cf36b5ac9159fe2495878c5c933f4422a7 [formerly f253713aa4849fcf4cf37c37edeffec84fe5ac7f] [formerly 5e65357adce7d93d993bd7cc1d3c4c45d598826c] [formerly 740d6248f9658e2b138e7b86605ebbb9d24c316c [formerly 440c433001b45c14aff500815de445ab64f3f14b]] [formerly 7d2a50c5836470a272e4b811e481f3d270e2fc3a [formerly f727f93590438ab50f21a8f5069de01058e98de6] [formerly 3f2e55e387673c49064185b8203e2cc18ad7e2fa [formerly 64d493d157024be68e174e175cba57611c488f12 [formerly defae9a375d231f0d548baa2631db349491a4c7c]]]]
    Former-commit-id: 863c67d088957745a7115d6545bf0ab658f5b95d [formerly d9822cda768bd5a9de055b6215120d23393929a1] [formerly 2b2d6bf039aa04ff95e1e6343914a0becf6bde0c [formerly 64d493d157024be68e174e175cba57611c488f12]]
    Former-commit-id: b5566c2253977f96f27b8b680c4702c3fee4b509 [formerly 865ec9e878ae3d8e88de5f80abca81befb43f9a4]
    Former-commit-id: 750dd369df478c7b7e6ccf1e4ae748a759f952a2
    Former-commit-id: 2d4b04dbc03600365ee1d3a44f8568ff6309eafa

--- cookbooks/wt_publishver/recipes/windows.rb
+++ cookbooks/wt_publishver/recipes/windows.rb
@@ -7,13 +7,34 @@
 # All rights reserved - Do Not Redistribute
 #
 
-def publish_version (role, build_dir, key_file, status)
-	pd_cmd = 'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe -NonInteractive -ExecutionPolicy RemoteSigned -Command \\\\sstore02.staging.dmz\\install\\adm\\PodDetails.ps1 -Credential WTENGLAB\\wtInstaller -Password Bijoux1'
-	cmd = "#{pd_cmd} -Hostname '#{node['hostname']}' -Pod '#{node.chef_environment}' -Role '#{role}' -BuildDir '#{build_dir}' -KeyFile '#{key_file}' -Status '#{status}'"
-	log cmd
-	execute 'publish_version_ps' do
-		command cmd
+require 'rest_client'
+require 'rexml/document'
+require 'teamcity-rest-client'
+
+def publish_version (role, branch, key_file, status)
+
+	pod_details_cmd = "#{Chef::Config[:file_cache_path]}\\PodDetails.ps1 -Credential WTENGLAB\\wtInstaller -Password Bijoux1"
+	pod_details_cmd << " -Hostname '#{node['hostname']}'"
+	pod_details_cmd << " -Pod '#{node.chef_environment}'"
+	pod_details_cmd << " -Role '#{role}'"
+	pod_details_cmd << " -KeyFile '#{key_file}'"
+	pod_details_cmd << " -Status '#{status}'"
+	pod_details_cmd << " -Branch '#{branch}'"
+
+	log pod_details_cmd
+	
+	powershell 'publish version' do
+		command pod_details_cmd
+		flags '-NonInteractive'
 		ignore_failure true
+	end
+	
+end
+
+%w{ PodDetails.ps1 Functions.ps1 }.each do |ps|
+	cookbook_file "#{Chef::Config[:file_cache_path]}\\#{ps}" do
+		source ps
+		action :create_if_missing
 	end
 end
 
@@ -22,13 +43,13 @@ end
 	case r
   		when 'wt_analytics_ui'
 			log "publishing #{r}"
-			publish_version(
-				'A10 UI',
-				node['wt_analytics_ui']['download_url'],
-				# File.join(node['wt_common']['install_dir_windows'], node['wt_analytics_ui']['install_dir'], 'bin/WebTrends.UI.Reporting.dll').gsub(/[\\\/]+/,"\\"),
-				File.join(node['wt_common']['install_dir_windows'], 'Analytics/bin/WebTrends.UI.Reporting.dll').gsub(/[\\\/]+/,"\\"),
-				'Up'
-			)
+#			publish_version(
+#				'A10 UI',
+#				node['wt_analytics_ui']['download_url'],
+#				# File.join(node['wt_common']['install_dir_windows'], node['wt_analytics_ui']['install_dir'], 'bin/WebTrends.UI.Reporting.dll').gsub(/[\\\/]+/,"\\"),
+#				File.join(node['wt_common']['install_dir_windows'], 'Analytics/bin/WebTrends.UI.Reporting.dll').gsub(/[\\\/]+/,"\\"),
+#				'Up'
+#			)
   		when 'wt_search'
   		when 'wt_sync'
   		else
