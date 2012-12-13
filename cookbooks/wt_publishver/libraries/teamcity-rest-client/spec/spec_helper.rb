defae9a375d231f0d548baa2631db349491a4c7c
David Dvorak <david.dvorak@webtrends.com>
2012-12-13 21:26:58 +0000

    test lib

---
+++ cookbooks/wt_publishver/libraries/teamcity-rest-client/spec/spec_helper.rb
@@ -0,0 +1,16 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'rspec'
+require 'teamcity-rest-client'
+
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+RSpec.configure do |config|
+  
+end
+
+def sample_xml name
+  File.open("#{File.dirname(__FILE__)}/samples/#{name}.xml").read
+end
