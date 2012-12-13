747f09fcd15a0b798e33fb5514c8d466625d605b
David Dvorak <david.dvorak@webtrends.com>
2012-12-13 18:23:50 +0000

    adding cookbook wt_publishver (not usable yet)
    Former-commit-id: 2018bc70335a846eae0282fc59bf082480a0dc81 [formerly ed9aefb78bd7985f9cfef8adf7dc9f01e60602ca] [formerly 20d4435c8a60f294b79828455d05d14d748d7782 [formerly 65fd6eed415853ca510b88a04fed908440b3ce17 [formerly 5138a8a6206c367fa01f3ec9ce9794b321dee885]]]
    Former-commit-id: 233ae446ee2005603608cacb4679fa0d414890cf [formerly e96d26c1752d3cccd4eca819f583a9fb7fad63c8]
    Former-commit-id: 41237916c929f2142c4c96ad1d88419a45f379cf
    Former-commit-id: 6245112f5154ae74bfacaaa99506898ffbf2dfe2

---
+++ cookbooks/wt_publishver/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Webtrends Inc."
+maintainer_email "david.dvorak@webtrends.com"
+license          "All rights reserved"
+description      "Installs/Configures wt_publishver"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
