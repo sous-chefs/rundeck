08e864c4ed55793b8de935530186df5ba0a524cb
David Dvorak <david.dvorak@webtrends.com>
2012-12-14 21:36:46 +0000

    add wt_publishver to role wt_labserver
    Former-commit-id: d1cb654d2822177a58b4b5361df7c3ef68088431 [formerly 710082ff775b79b5b4e9e5d69b3f0c98723df427] [formerly 7cceab4d96d153e75eeafff5125541ad5d31abc8 [formerly c29e2ac73e0acfd1ff8e4f1796712022b35bea2c [formerly 73d4b4c8148db3587f8e36c6e9e267d0b6702d7f]]]
    Former-commit-id: 14f1de77c3e05e7f128229be59c983b5ee25382e [formerly c0b1b011db36ca61e682be6598afbd81b205717a]
    Former-commit-id: 73fa12ec4dc8a00c178d1d0d3bfb20bae219f7fe
    Former-commit-id: dacb6308f050957bfc6a6214ba1910840703cb83

--- cookbooks/wt_publishver/metadata.rb
+++ cookbooks/wt_publishver/metadata.rb
@@ -1,7 +1,7 @@
 maintainer       "Webtrends Inc."
 maintainer_email "david.dvorak@webtrends.com"
 license          "All rights reserved"
-description      "Installs/Configures wt_publishver"
+description      "Publishes build version to Sharepoint list."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 depends          "powershell"
