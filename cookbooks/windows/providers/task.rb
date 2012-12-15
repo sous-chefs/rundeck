56e6f3c12d07169e3bce71caa6ef233f2cd61ce0
Tim Smith <tim.smith@webtrends.com>
2012-12-15 02:27:50 +0000

    Import windows version 1.5.0
    Former-commit-id: 34605f8d60b03de1e64703000a04ab22c232d804 [formerly 64a76b68c50a86b94ee4469e831107b6747dd575] [formerly c63534daef8f367c2a3e7b3230f30596147b5527 [formerly 0ddaebd82d0c5708850d865c50063cc78514e4b4 [formerly dea0e4d1fcee2d4f868358b66baa4fe6a621e9ca]]]
    Former-commit-id: bef8ee7056414363930aac212edcdc10580d53e1 [formerly 46e7428b11ccf1c3821d02b5a35ed3c0608085ba]
    Former-commit-id: 35c94dd9d197d1122500729ccc5ab469cb3786e4
    Former-commit-id: 52bb0875be88ad1bbb899b47306c68216591d5dd

--- cookbooks/windows/providers/task.rb
+++ cookbooks/windows/providers/task.rb
@@ -31,7 +31,7 @@ action :create do
     if @new_resource.user && @new_resource.password
       cmd += "/RU \"#{@new_resource.user}\" /RP \"#{@new_resource.password}\" "
     elsif (@new_resource.user and !@new_resource.password) || (@new_resource.password and !@new_resource.user)
-      Chef::Log.warn "#{@new_resource.name}: Can't specify user or password without both!"
+      Chef::Log.fatal "#{@new_resource.name}: Can't specify user or password without both!"
     end
     cmd += "/RL HIGHEST " if @new_resource.run_level == :highest
     shell_out!(cmd, {:returns => [0]})
@@ -55,6 +55,23 @@ action :run do
   end
 end
 
+action :change do
+  if @current_resource.exists
+    cmd =  "schtasks /Change /TN \"#{@current_resource.name}\" "
+    cmd += "/TR \"#{@new_resource.command}\" " if @new_resource.command
+    if @new_resource.user && @new_resource.password
+      cmd += "/RU \"#{@new_resource.user}\" /RP \"#{@new_resource.password}\" "
+    elsif (@new_resource.user and !@new_resource.password) || (@new_resource.password and !@new_resource.user)
+      Chef::Log.fatal "#{@new_resource.name}: Can't specify user or password without both!"
+    end
+    shell_out!(cmd, {:returns => [0]})
+    @new_resource.updated_by_last_action true
+    Chef::Log.info "Change #{@new_resource} task ran"
+  else
+    Chef::Log.debug "#{@new_resource} task doesn't exists - nothing to do"
+  end
+end
+
 action :delete do
   if @current_resource.exists
     cmd = "schtasks /Delete /TN \"#{@current_resource.name}\""
@@ -71,7 +88,7 @@ def load_current_resource
   @current_resource.name(@new_resource.name)
 
   task_hash = load_task_hash(@current_resource.name)
-  if task_hash
+  if task_hash[:TaskName] == '\\' + @new_resource.name
     @current_resource.exists = true
     if task_hash[:Status] == "Running"
       @current_resource.status = :running
