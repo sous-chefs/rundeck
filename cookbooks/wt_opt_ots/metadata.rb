aa55be249884e4d009f86a788a0557f7d0ce00af
Tim Smith <tim.smith@webtrends.com>
2012-12-17 17:57:39 -0800

    Delete these optimize test recipes
    Former-commit-id: 0877d7c3b379b2d3415d9faca79b7cccb4d2c041 [formerly b7e91544de1ef55c2160d0f6be4d0f6d53948610] [formerly 900b465adca054dff27b7e0dcfe73e9e4bab468c [formerly 7d8888cd7bba2f23c6a483ec5b953c65d62b941f [formerly 7280ceee43ceb3036364f0b93d593f1e643e4c72]]]
    Former-commit-id: 55dd9a42cff8e44c07c8b5188b683d33a1dfafd3 [formerly c70651490b81607684764f9658ae9fd36a1eb21e]
    Former-commit-id: e7ef6463aa3b3ccc9a1f9d1a85f58369d2ea86bf
    Former-commit-id: 504b19b26e88ca628bc8623d1c1743be753f51d7

--- cookbooks/wt_opt_ots/metadata.rb
+++
@@ -1,8 +0,0 @@
-maintainer       "Tim Smith - Webtrends, Inc."
-maintainer_email "hostedops@webtrends.com"
-license          "All rights reserved"
-description      "Installs/Configures Optimize OTS Data Collection"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
-depends          "apache2"
-depends          "java"
