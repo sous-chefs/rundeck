7e1140be9325311b6391063821273a5fa703b18e
seanm <sean.mcnamara@webtrends.com>
2012-12-19 18:12:22 -0700

    updating zookeeper to latest stable version
    Former-commit-id: 629e53d2ec51b33ab87943100f9ca9d163aaa369 [formerly 9eb5403299a03681e8efed6dcdeca38c43b61139] [formerly e2736b48ce358f2339541106debc7fe65d20a467] [formerly 46d5f24e3ef265e65355524a4fe0cfc35fb0de9d [formerly b279e67b73a96cbc354fbe4e770252c3b76e7a8c]] [formerly e531dd1fe9eed4b2a43f497c21ec8cd7e3c1b438 [formerly fdd57410ab5fed5e6c1e0a897978fea049bf3c58] [formerly 229841c50a2f3bdeece69c26ec077112eaa3cdda [formerly 912500f2d25ecaf168eac5876383bcaae2a73ef3 [formerly b77d0f347bd2b2b480db47199b2a1b7f9ace4aee]]]]
    Former-commit-id: 071950b38889d920d2c4271eb706b671d30747b8 [formerly 3c416157140d37151e2bce66395e6477166c9533] [formerly ce75f6face7821309020bfcda0eb03a6596eb56c [formerly 912500f2d25ecaf168eac5876383bcaae2a73ef3]]
    Former-commit-id: 83c44a9f63d3b20beb54f1a796d81591afd81cb5 [formerly 64a2f4b3204ea88bd37ee89d01f37286e3c931ea]
    Former-commit-id: d40e265ff2e3f37929f3c76afaaa0c810d0b6159
    Former-commit-id: 9529c10cc90f6703ddaac79ade542f1ed5359e37

--- cookbooks/zookeeper/attributes/default.rb
+++ cookbooks/zookeeper/attributes/default.rb
@@ -8,20 +8,21 @@
 # cluster name
 default[:zookeeper][:cluster_name] = 'default'
 
-default[:zookeeper][:default][:version] = '3.3.6'
-default[:zookeeper][:default][:download_url] = 'http://mirror.uoregon.edu/apache/zookeeper/zookeeper-3.3.6/zookeeper-3.3.6.tar.gz'
+default[:zookeeper][:default][:version] = '3.4.5'
+default[:zookeeper][:default][:download_url] = 'http://mirror.uoregon.edu/apache/zookeeper/zookeeper-3.4.5/zookeeper-3.4.5.tar.gz'
 
 default[:zookeeper][:default][:install_dir] = '/opt/zookeeper'
-default[:zookeeper][:default][:config_dir] = '/etc/zookeeper'
+default[:zookeeper][:default][:config_dir] = '/opt/zookeeper/current/conf'
 default[:zookeeper][:default][:log_dir] = '/var/log/zookeeper'
 default[:zookeeper][:default][:data_dir] = '/var/zookeeper'
-default[:zookeeper][:default][:snapshot_dir] = '/var/zookeeper/snapshots'
+default[:zookeeper][:default][:data_log_dir] = '/var/zookeeper/snapshots'
 
 default[:zookeeper][:default][:tick_time] = 2000
 default[:zookeeper][:default][:init_limit] = 10
 default[:zookeeper][:default][:sync_limit] = 5
 default[:zookeeper][:default][:client_port] = 2181
 default[:zookeeper][:default][:snapshot_num] = 3
+default[:zookeeper][:default][:purge_interval] = 1 # in hours
 default[:zookeeper][:default][:max_client_cnxns] = 60
 
 default[:zookeeper][:default][:jmx_port] = 10201
