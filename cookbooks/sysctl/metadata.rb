bc7c6235c35c57d2c4a7160f58fb3d34a7d67cf2
Tim Smith <tim.smith@webtrends.com>
2012-12-20 16:22:32 +0000

    Revert "Import sysctl version 0.1.0"
    This reverts commit b4c76e45c7c7e505cca7c1802087db7d29592b40 [formerly 82ae571613d63efc337b3b280116620738986b9e] [formerly 254fd87e8dca20852525a75f2d0dc343f0179fcb [formerly c29d180287eafe6722540351a37565c29f237f1f]] [formerly 1564767d4b70a551c4c79e5a05572529da37d7f6 [formerly 45b9272862635aa4a8acd2bde9bb6b0d13b10254] [formerly bac678bd68b09832aa9975fa4adb3cdcfac4dfd5 [formerly d0892fe1590178496e1c71b897a801f2515a2523]]].
    
    
    Former-commit-id: 8624a0019831c2a30337ee89550998add4d55efb [formerly 4f24f8e72d430c02d8aeedd96b1f9ddbe47540d0] [formerly 02fece0eb7599ee8cdf74cd6d46c0cb0332db803 [formerly d9e5e8c01e38fc740de24ae6a12f86ba991d5bc4]]
    Former-commit-id: 8b1660822f3482d879bddb78c96117e63bbe6bf7 [formerly 5d0fcc7ee1a2548147c0c37709f50d7da801cb88]
    Former-commit-id: 8045dd0eeb19f584120488561cbd9e817f5f8e31

--- cookbooks/sysctl/metadata.rb
+++ cookbooks/sysctl/metadata.rb
@@ -1,8 +1,9 @@
-maintainer       "Fewbytes Technologies"
-maintainer_email "avishai@fewbytes.com"
+maintainer       "Guilhem Lettron"
+maintainer_email "guilhem.lettron@youscribe.com"
 license          "Apache v2.0"
 description      "Configures sysctl parameters"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.0"
-supports         "ubuntu"
-supports         "debian"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.8"
+%w{ debian ubuntu centos redhat }.each do |os|
+      supports os
+end
