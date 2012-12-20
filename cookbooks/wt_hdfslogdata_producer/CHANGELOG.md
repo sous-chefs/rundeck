65bb6ca1ba9578ada01f35a8c82d86b2200c9cee
John Easterday <john.easterday@webtrends.corp>
2012-12-20 23:18:29 +0000

    Added cookbook for HDFS Log Data Producer

---
+++ cookbooks/wt_hdfslogdata_producer/CHANGELOG.md
@@ -0,0 +1,3 @@
+
+## 0.0.5:
+* Initial release with a changelog
---
+++ cookbooks/wt_hdfslogdata_producer/metadata.rb
@@ -0,0 +1,10 @@
+name                "wt_hdfslogdata_producer"
+maintainer          "Webtrends, Inc"
+maintainer_email    "marek.dolgos@webtrends.com"
+license             "All rights reserved"
+description         "Installs/Configures the Webtrends HDFS Log Data Producer"
+long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version             "1.1.1"
+depends             "java"
+depends             "runit"
+depends             "minitest-handler"
