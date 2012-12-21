555ca8636b95dcc23bb48c497e22240aad0f9cc6
David Dvorak <david.dvorak@webtrends.com>
2012-12-21 01:09:16 +0000

    adding some prereqs
    Former-commit-id: 67feb60124e5c9bc6aa4fc785527f31733c73c3b [formerly 30d054529889c3d60d299f9b9c64fe3733d107e7] [formerly 811b43da37304aaf76872202b42e473464649418 [formerly 06979290edc72e343556603ac0261b1c5ecff54a [formerly 8b2779a963d1f720d4642fa272f3d6d4db81a61c]]]
    Former-commit-id: bbb72006e9fd7d58106ac9e3624cd7ed0c40e5c2 [formerly 5a987920e6b3e94c73a07466a147dbc0ae289184]
    Former-commit-id: 448b8037045bd10a65f7c49a52dcc7acf8fcbd56
    Former-commit-id: de8234ce921f8cdfe6192cd0dba5102fdd24de74

---
+++ cookbooks/wt_publishver/files/ubuntu/prereqs/lib/xsltConf.sh
@@ -0,0 +1,7 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/home/dvorakd/dev/local/lib"
+XSLT_LIBS="-lxslt  -lxml2 -lm -lrt"
+XSLT_INCLUDEDIR="-I/home/dvorakd/dev/local/include"
+MODULE_VERSION="xslt-1.1.28"
