c4b9ef22b36cb08b535f2d9e29a34a51ed0ede35
Tim Smith <tim.smith@webtrends.com>
2012-12-26 20:22:40 -0800

    Switch to strings, remove long description, add name metadata, update read me, add oracle support
    Former-commit-id: b7cc46f158cff38c53814737998e1d8fb21e0010 [formerly 45acff219154ad28d9ba642946338a4c696e1a84] [formerly 5c12865ffde432f7cbfadb9772b767597319b8d8 [formerly 47212b9030695b3651a4a179931f0f3c91e66a93 [formerly cabda3b969fab2579feaaf3c15ae3e9ec9b3f912]]]
    Former-commit-id: bc59ea5037886a2d230b793ff397a8c760e10c80 [formerly d21378c7c3377c6c34ba2b326e7c32b2a6254f36]
    Former-commit-id: e41b0be10fe2328afb137087d7d45aac6d61aaac
    Former-commit-id: 85febe095cc4f4871c021f3d82d6628204a8ffb5

--- cookbooks/hosts/metadata.rb
+++ cookbooks/hosts/metadata.rb
@@ -1,10 +1,10 @@
-maintainer       "Tim Smith - Webtrends Inc"
+name             "hosts"
+maintainer       "Tim Smith - Webtrends Inc."
 maintainer_email "tim.smith@webtrends.com"
 license          "All rights reserved"
 description      "Configures the /etc/hosts file"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.1"
 
-%w{ centos redhat amazon scientific }.each do |os|
+%w{ centos redhat amazon scientific oracle }.each do |os|
   supports os
 end
