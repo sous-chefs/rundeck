3162856ce1e128aef553f3dcdb9b617105891033
Tim Smith <tim.smith@webtrends.com>
2012-12-27 23:58:27 -0800

    Set appropriate nrpe home directory on 32bit RHEL based systems
    Former-commit-id: b246bd3ec4afff7b32c19bd6817e9e5ee0981cfb [formerly 894c6a0e03a52418631e0868281c9e53cf62a474] [formerly 983be5d7dc8bfc1d4f0cb8275471b26df26c7eee [formerly 97258ebd263cf153b97fb91c83a2f7a373e434a4]]
    Former-commit-id: 484d4b2f33db96aaf8334ac034f064ae8e0b7e56 [formerly a18ad590d82388934a79d78759b08332b95ef5c0]
    Former-commit-id: 229fd9d2feb1debc83f89688cd632730d5ecb517

--- cookbooks/nagios/attributes/client.rb
+++ cookbooks/nagios/attributes/client.rb
@@ -24,22 +24,26 @@
 
 case node['platform_family']
 when 'debian'
-  default['nagios']['client']['install_method'] = 'package'
-  default['nagios']['nrpe']['pidfile']          = '/var/run/nagios/nrpe.pid'
-  default['nagios']['nrpe']['home']             = '/usr/lib/nagios'
+  default['nagios']['client']['install_method']  = 'package'
+  default['nagios']['nrpe']['pidfile']           = '/var/run/nagios/nrpe.pid'
+  default['nagios']['nrpe']['home']              = '/usr/lib/nagios'
 when 'rhel','fedora'
-  default['nagios']['client']['install_method'] = 'package'
-  default['nagios']['nrpe']['pidfile']          = '/var/run/nrpe.pid'
-  default['nagios']['nrpe']['home']             = '/usr/lib64/nagios'
+  default['nagios']['client']['install_method']  = 'source'
+  default['nagios']['nrpe']['pidfile']           = '/var/run/nrpe.pid'
+  if node['kernel']['machine'] == "i686"
+    default['nagios']['nrpe']['home']            = '/usr/lib/nagios'  
+  else
+    default['nagios']['nrpe']['home']            = '/usr/lib64/nagios'
+  end  
 else
-  default['nagios']['client']['install_method'] = 'source'
-  default['nagios']['nrpe']['pidfile']          = '/var/run/nrpe.pid'
-  default['nagios']['nrpe']['home']             = '/usr/lib/nagios'
+  default['nagios']['client']['install_method']  = 'source'
+  default['nagios']['nrpe']['pidfile']           = '/var/run/nrpe.pid'
+  default['nagios']['nrpe']['home']              = '/usr/lib/nagios'
 end
 
-default['nagios']['nrpe']['conf_dir']           = '/etc/nagios'
-default['nagios']['nrpe']['dont_blame_nrpe']    = 0
-default['nagios']['nrpe']['command_timeout']    = 60
+default['nagios']['nrpe']['conf_dir']          = '/etc/nagios'
+default['nagios']['nrpe']['dont_blame_nrpe']   = 0
+default['nagios']['nrpe']['command_timeout']   = 60
 
 # for plugin from source installation
 default['nagios']['plugins']['url']      = 'http://prdownloads.sourceforge.net/sourceforge/nagiosplug'
