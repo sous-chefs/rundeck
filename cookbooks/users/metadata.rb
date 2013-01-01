df2f9427db708854d90bfd02dbe7b6ccfb535bba
Tim Smith <tim.smith@webtrends.com>
2013-01-01 23:34:25 +0000

    Import users version 1.3.0
    Former-commit-id: 8eff58cc54c3a75bb5b41de14ce720d5c65bb8f6 [formerly bef3c58d845be44f8ebaa325a59db95cdf4e8fe4] [formerly fda3b5f453a5c66a2c5e6223bddd621217a1dc90 [formerly 0a4763ab65bc52d68eeb88108c5da5898d567eaf]]
    Former-commit-id: 126a4b1b2c4e338941d16f097774b762e65ce3d6 [formerly a153e85188bb73b4a121819244b258121b8e4b9e]
    Former-commit-id: 801ac7c5002749bd2acc33f219c395bbbb4f1f96

--- cookbooks/users/metadata.rb
+++ cookbooks/users/metadata.rb
@@ -1,9 +1,12 @@
+name             "users"
 maintainer       "Opscode, Inc."
 maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Creates users from a databag search"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.4"
+version          "1.3.0"
+recipe           "users", "Empty recipe for including LWRPs"
+recipe           "users::sysadmins", "Create and manage sysadmin group"
 
 %w{ ubuntu debian redhat centos fedora freebsd}.each do |os|
   supports os
--- cookbooks/users/providers/manage.rb
+++ cookbooks/users/providers/manage.rb
@@ -18,6 +18,10 @@
 # limitations under the License.
 #
 
+def whyrun_supported?
+  true
+end
+
 def initialize(*args)
   super
   @action = :create
@@ -28,7 +32,7 @@ action :remove do
     Chef::Log.warn("This recipe uses search. Chef Solo does not support search.")
   else
     search(new_resource.data_bag, "groups:#{new_resource.search_group} AND action:remove") do |rm_user|
-      user rm_user['id'] do
+      user rm_user['username'] ||= rm_user['id'] do
         action :remove
       end
     end
@@ -43,7 +47,8 @@ action :create do
     Chef::Log.warn("This recipe uses search. Chef Solo does not support search.")
   else
     search(new_resource.data_bag, "groups:#{new_resource.search_group} AND NOT action:remove") do |u|
-      security_group << u['id']
+      u['username'] ||= u['id']
+      security_group << u['username']
 
       if node['apache'] and node['apache']['allowed_openids']
         Array(u['openid']).compact.each do |oid|
@@ -56,21 +61,21 @@ action :create do
       if u['home']
         home_dir = u['home']
       else
-        home_dir = "/home/#{u['id']}"
+        home_dir = "/home/#{u['username']}"
       end
 
       # The user block will fail if the group does not yet exist.
       # See the -g option limitations in man 8 useradd for an explanation.
       # This should correct that without breaking functionality.
       if u['gid'] and u['gid'].kind_of?(Numeric)
-        group u['id'] do
+        group u['username'] do
           gid u['gid']
         end
       end
 
       # Create user object.
       # Do NOT try to manage null home directories.
-      user u['id'] do
+      user u['username'] do
         uid u['uid']
         if u['gid']
           gid u['gid']
@@ -88,8 +93,8 @@ action :create do
 
       if home_dir != "/dev/null"
         directory "#{home_dir}/.ssh" do
-          owner u['id']
-          group u['gid'] || u['id']
+          owner u['username']
+          group u['gid'] || u['username']
           mode "0700"
         end
 
@@ -97,10 +102,34 @@ action :create do
           template "#{home_dir}/.ssh/authorized_keys" do
             source "authorized_keys.erb"
             cookbook new_resource.cookbook
-            owner u['id']
-            group u['gid'] || u['id']
+            owner u['username']
+            group u['gid'] || u['username']
             mode "0600"
             variables :ssh_keys => u['ssh_keys']
+          end
+        end
+
+        if u['ssh_private_key']
+          key_type = u['ssh_private_key'].include?("BEGIN RSA PRIVATE KEY") ? "rsa" : "dsa"
+          template "#{home_dir}/.ssh/id_#{key_type}" do
+            source "private_key.erb"
+            cookbook new_resource.cookbook
+            owner u['id']
+            group u['gid'] || u['id']
+            mode "0400"
+            variables :private_key => u['ssh_private_key']
+          end
+        end
+
+        if u['ssh_public_key']
+          key_type = u['ssh_public_key'].include?("ssh-rsa") ? "rsa" : "dsa"
+          template "#{home_dir}/.ssh/id_#{key_type}.pub" do
+            source "public_key.pub.erb"
+            cookbook new_resource.cookbook
+            owner u['id']
+            group u['gid'] || u['id']
+            mode "0400"
+            variables :public_key => u['ssh_public_key']
           end
         end
       end
