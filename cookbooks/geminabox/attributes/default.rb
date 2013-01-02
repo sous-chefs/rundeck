0ec6b021808247a0d6dd6a680527ccf270d5635e
martink <kendrick.martin@webtrends.com>
2013-01-02 15:05:19 -0800

    Initial creation of geminabox
    Former-commit-id: 8ac671ef121fdc7a47bdc12f97e017dd1b19ab9a [formerly e065215458189c0ca99a4547191ebb73dd370a79] [formerly 2b301ab1b366cda8809cf8f2e49cdadced432fd8 [formerly 06a34e2ac8b7efe3b10aa7f293d260b6567d92a4]]
    Former-commit-id: 1e2dac9fc177b59b8fdb8d1269bb2e883031bb5c [formerly c9fd5cb616bb30f4c1300e114fca390c80a807a1]
    Former-commit-id: aa36dad00ea48d6ad01ac85ecf0eea627daa1f7e

---
+++ cookbooks/geminabox/attributes/default.rb
@@ -0,0 +1,9 @@
+#
+# Author:: Kendrick Martin(<kendrick.martin@webtrends.com>)
+# Cookbook Name:: geminabox
+# Attribute:: default
+#
+# Copyright 2012, Webtrends Inc.
+
+
+default['geminabox']['installdir'] = "/var/geminabox"
