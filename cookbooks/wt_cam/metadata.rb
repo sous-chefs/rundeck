a82b550e0f840b5af6c2e5997911ce541edbfb4f
Toby Mosby <toby.mosby@webtrends.com>
2013-01-02 20:10:17 +0000

    Publish sms_service_url, and consume it in wt_cam.
    Former-commit-id: 08d8ebf99c7b5c904a6591b11c6ddc19c3c300e5 [formerly 3541ceb13edddef7521b3a38611e3ce67a35a410] [formerly 88d9d9d6550fa3da6e690fa8c3afccfb248eb32c [formerly 0efdba79d63347427609fa71ea83e63f9578c8ee [formerly e2b9f5821c371acc432a3cd476e02119fa279f35]]]
    Former-commit-id: 8eaa0e701d51ab6a78b6a0f5158b5b09d25162ca [formerly 576d3e3204242332e5f97482f2e05d1a11e14a6f]
    Former-commit-id: f635124dabd8d537b12475a68a3f90492abb15a7
    Former-commit-id: bdaa26e5cfafafd6a5442e45ac5c2ba2e4d820b5

--- cookbooks/wt_cam/metadata.rb
+++ cookbooks/wt_cam/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "mark.alexander@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures CAM IIS site"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.4"
+version          "1.1.5"
 supports         "windows"
 depends          "iis"
 depends          "windows"
