9824ef36103ca2666bd22162d7f175b6c0a37952
martink <kendrick.martin@webtrends.com>
2013-01-03 10:19:14 -0800

    Changing owner from root to zookeeper
    Former-commit-id: 89c30140821f7d45200a818ad661846ce46d43a3 [formerly 87ee22494d895c86a4964acd7bd9ef5c34df026a] [formerly ead42ea87d30a24b48f9bf16cedd22e053def9d6] [formerly bb8c81a20a61b54d1e881a3d18db7ab02b26ef3b [formerly 77bc1d4fcbd0d4b61c4432f6e84b103f825ae9c1]] [formerly 91559810bad8c8084d885d41457655ac4eebbf8d [formerly 58c1fff5064946fe6942f45b19008d58bd1ee608] [formerly bebf49a0354a360bf1e8f2a4458d9736eb6d0c27 [formerly 93bf2a460b4c617d429df62e8ec669a0d6ed076a [formerly 6faf31e0f19a0b9f56db89c9102dae904c8a2d48]]]]
    Former-commit-id: 9500bfcf5eae6a8ae6c7aaa7c98ccd37dac943ff [formerly ed509e093f9224ae1ac4fde3042720453446dc60] [formerly 5f9d684377214ac5e045794d86096f00664cc72f [formerly 93bf2a460b4c617d429df62e8ec669a0d6ed076a]]
    Former-commit-id: aa0fa6e3b76520ea81c7d5f6ad9c66c348342e3f [formerly 701a9754774d13598a2066ad4937274bd7aaf836]
    Former-commit-id: c752569c232b449a194b6c49f2acb51b2f4f64aa
    Former-commit-id: 8f5fd0063ba2d161b025ce9e1ac8881bb7cdb0c2

--- cookbooks/zookeeper/metadata.rb
+++ cookbooks/zookeeper/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "sean.mcnamara@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures ZooKeeper"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.2.2"
+version          "1.2.3"
 depends          "java", ">= 1.5"
 depends          "runit"
 
