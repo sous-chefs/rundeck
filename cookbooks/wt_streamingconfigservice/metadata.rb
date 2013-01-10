68fc249b387bdc52f561a964cf278b2e0c38952b
vonnagy <ivan.vonnagy@webtrends.com>
2013-01-10 09:26:03 -0800

    STR-876 Fixed Nagios regular expression to be ignore whitespace
    Former-commit-id: 4eb64a5771cc27d770b14604a63a91a3e094a48b [formerly c754ecb300f60e8f4946258f1698fdbe9058dba3] [formerly 0d8377380134a737d0d1fcdc931e8d7e5768af85] [formerly 74ad2d20417566f218c3aa2b190b64de858e8c87 [formerly 75a9a4a5a269f6069049633cc6a9ef2a08a66788]] [formerly a4a9c1d6b43b60536867a1c4d1c30dccfd078ad8 [formerly a15263c4073a7a555cc9fe34c587282f833d564c] [formerly 2f2195ab61c387bca335d83909ee92f137ea7c36 [formerly a1c22a482409387be472c3eb88127f5dfe62d93f [formerly 12da7964c29dd482a65f6bd3de2d1a7271874cbd]]]]
    Former-commit-id: 695e88935cfc002774f9c5b879c368a3a39d4fc0 [formerly f3ef69d2a528da02005096f38d74f3327dca47c8] [formerly efd601b3446745c99cd666cfc1608173ea09ce4f [formerly a1c22a482409387be472c3eb88127f5dfe62d93f]]
    Former-commit-id: 6b930ecd62c2a72e6be443323226494e6e9161f6 [formerly 5ba44f8f2ebaf63a9b4d807fa3d15b2f0e9900ae]
    Former-commit-id: 575520aadbace705534736fc5d4439582070b34d
    Former-commit-id: eb4117b3783b1431fc4fee11d3f3a17734f61590

--- cookbooks/wt_streamingconfigservice/metadata.rb
+++ cookbooks/wt_streamingconfigservice/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email    "mark.alexander@webtrends.com"
 license             "All rights reserved"
 description         "Installs/Configures the Webtrends Streaming Config Service"
 long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version             "1.0.8"
+version             "1.0.9"
 depends             "java"
 depends             "runit"
 depends             "minitest-handler"
