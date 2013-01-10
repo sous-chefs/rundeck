bc05f0ebc9eec7aaa37cdc1c661fae605d16502a
martink <kendrick.martin@webtrends.com>
2013-01-10 21:15:34 +0000

    Import java version 1.8.0
    Former-commit-id: 08001096ab007aa1e8831aa8ebbc090b2354172e [formerly b39f211fe110718bb997faf992c3b5803ef78aed] [formerly 9ed7727781bf52db5a15a1ef5d2954391c44d485 [formerly ff47121ac31a44c929260bdb8549ec815d4719f2]]
    Former-commit-id: 6e04314e590f1de936e5dd0f8f4eba3c369e0bde [formerly af19e41b44cd0a9c0a1f80fceec7d72143ca0363]
    Former-commit-id: e47589106196d83d2268792ad7538fe544ce4c25

--- cookbooks/java/metadata.rb
+++ cookbooks/java/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs Java runtime."
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "2.0.1"
+version           "1.8.0"
 
 recipe "java", "Installs Java runtime"
 recipe "java::openjdk", "Installs the OpenJDK flavor of Java"
@@ -12,7 +12,7 @@ recipe "java::oracle", "Installs the Oracle flavor of Java"
 recipe "java::oracle_i386", "Installs the 32-bit jvm without setting it as the default"
 
 
-%w{ debian ubuntu centos redhat scientific fedora amazon arch freebsd windowsd }.each do |os|
+%w{ debian ubuntu centos redhat scientific fedora amazon arch freebsd windows }.each do |os|
   supports os
 end
 
