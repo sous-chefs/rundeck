cef66088969e9bca1151b0f754f9b53b49eaee10
vonnagy <ivan.vonnagy@webtrends.com>
2013-01-10 09:28:54 -0800

    Fixed Nagios regular expression
    Former-commit-id: e867c92330521a78b51c5af788864d28342ef036 [formerly 0c90e92e1fdb7c18e2c4ca701544cd201c2a67cc] [formerly dd2430e306152eb55a9ae1a2a3019fe07f3aed84 [formerly 5b7b8c5ce04fcf4b752a17a1d1b26647b9e46296]]
    Former-commit-id: a04644b1f17966d2e89943dd8c799f8532752e64 [formerly 5f8ad732014f52b6a9eb4e8c6f217cbb30c15ace]
    Former-commit-id: d26ab484e6d0252f2438a3d1e78342da435a2212

--- cookbooks/wt_streamingauditor/recipes/default.rb
+++ cookbooks/wt_streamingauditor/recipes/default.rb
@@ -185,7 +185,7 @@ if node.attribute?("nagios")
   # Create a nagios nrpe check for the overall streaming health
   nagios_nrpecheck "wt_streaming_healthcheck" do
     command "#{node['nagios']['plugin_dir']}/check_http"
-    parameters "-H #{node['fqdn']} -u /healthcheck -p 9000 -r \"\\\"streaming_healthcheck\\\":\\s*\\{\\\"healthy\\\":\\s*\\\"true\\\"\""
+    parameters "-H #{node['fqdn']} -u /healthcheck -p 9000 -r \"\\\"streaming_healthcheck\\\":\\s*\\{\\\"healthy\\\":\\s*true\""
     action :add
   end
 end
