625bf413766984a17ca38e4f11747e645cfebe22
vonnagy <ivan.vonnagy@webtrends.com>
2013-01-15 23:18:06 -0800

    Bumped the stream api version
    Former-commit-id: e49d692d88809786552c83ccbce68aaaceddbd78 [formerly f65f53047a304f27776e9e357d3595a462409a62] [formerly 701e09b1738a53f44ce04d3628247368cb4ad823] [formerly b5fc3650e2a5737400778a3e7011cf74ea0b1144 [formerly 695bac165d9844bf4ed6ecdbb09de633f57fc438]] [formerly f946f294a1d06e6da2af8b952f06f3b2161d3e52 [formerly bbb8fccb547b4149404c293de74488d04fb6335b] [formerly f513ad40ed7478bf35d201b000ac5dccaea351c6 [formerly 22c4c78603baf51f2de4b8891bb17d6f0e17f170 [formerly c32efaa7e499ec490ab551cf3e12c26563b65bc9]]]]
    Former-commit-id: 5a74862da3b2c1547bc13e3ced735df66898dac6 [formerly 07f8dac1db4a81b84486777cc5df232938189f22] [formerly 50b014b4496bc980e51fc914d8b235d7ef8756cb [formerly 22c4c78603baf51f2de4b8891bb17d6f0e17f170]]
    Former-commit-id: c99e05404c2f8b05bc451cab806197a4cabdaf80 [formerly 6b198345723730354d2eb037b789800686e21cf0]
    Former-commit-id: f7c8837fabd3fe77c42a6c75d8f1cca4ed588077
    Former-commit-id: 788a3833e4af214a73be2ecaea1061572caca553

--- cookbooks/wt_portfolio_edge_stream_api/attributes/default.rb
+++ cookbooks/wt_portfolio_edge_stream_api/attributes/default.rb
@@ -9,4 +9,4 @@
 
 default['wt_portfolio_edge_stream_api']['user']           = "webtrends"
 default['wt_portfolio_edge_stream_api']['group']          = "webtrends"
-default['wt_portfolio_edge_stream_api']['download_url']   = ""
+default['wt_portfolio_edge_stream_api']['download_url']   = "http://teamcity.webtrends.corp/guestAuth/repository/download/bt343/.lastSuccessful/webtrends-stream-edge-api-develop-SNAPSHOT-bin.tar.gz"
