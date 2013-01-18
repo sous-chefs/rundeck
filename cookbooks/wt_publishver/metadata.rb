a950020cea228377b034f637bbe25167ee9eee64
David Dvorak <david.dvorak@webtrends.com>
2013-01-18 23:43:33 +0000

    added lpp; made changes to prereq gems

--- cookbooks/wt_publishver/metadata.rb
+++ cookbooks/wt_publishver/metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "david.dvorak@webtrends.com"
 license          "All rights reserved"
 description      "Publishes build version to Sharepoint list."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 depends          "powershell"
