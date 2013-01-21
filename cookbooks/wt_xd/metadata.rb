8ea4fd8c911916c7841fbf8afeb1c286d50bda78
Kendrick Martin <kendrick.martin@webtrends.com>
2013-01-21 13:33:52 -0800

    Adding new stuff for xd services
    Former-commit-id: 10d17a58ed51213bfa8f2279c5ca5569ae627970 [formerly a60b172d49559b187a2ede58b5fb2f1ca5bd924b] [formerly 77f3fc9ef2f01f2190bea8a4a11c16910bfec73b] [formerly 7fb9ba9c03bfaf71fc05878d7734faa428b274cf [formerly f75b2d12722017c5ed627f15795b1317916dd18a]] [formerly f82dbbb482e07246a281180ea6efbb74f2dd5aa0 [formerly 4524e54082f6a846e96db9bad900203de08a483d] [formerly 492f2a9a4074e4946faeb1dddf5e569a805be823 [formerly 451dc262837993606e6d155d8e5464d47eac7bbb [formerly f1d482353555acda3fd1879aff83734d7d391191]]]]
    Former-commit-id: b1c76c5a175cddc625f231afdf1fd410ecb1a788 [formerly 532cadefd1757c78f40b925b14dc65cf7c2c1b7b] [formerly bfad3ea8181195807023a2db1fd7190a91c6c2de [formerly 451dc262837993606e6d155d8e5464d47eac7bbb]]
    Former-commit-id: ab527f35a75907850547ec95519981c6522f2ee3 [formerly d1fb18e220cb572cac1a51e55fc4a6ea0d846de2]
    Former-commit-id: 96c44bbdde6b25021a28b5ee39678f6554586ac5
    Former-commit-id: 7d5801cda0452770d79bc8128f9eb45af9372118

--- cookbooks/wt_xd/metadata.rb
+++ cookbooks/wt_xd/metadata.rb
@@ -4,5 +4,6 @@ maintainer_email "david.dvorak@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures wt_xd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.3"
+version          "2.0.0"
 depends          "wt_base"
+depends			 "wt_actioncenter"
