37f9cd8c91a690c7c592f45cc77f422938953f88
seanm <sean.mcnamara@webtrends.com>
2013-01-21 11:33:18 -0700

    updating sparking cookbook

--- cookbooks/spark/attributes/default.rb
+++ cookbooks/spark/attributes/default.rb
@@ -6,8 +6,10 @@
 # Copyright 2012, Webtrends Inc.
 #
 
-default['spark']['version'] = "0.7.0-alpha20130108"
-default['spark']['download_url'] = "http://repo.staging.dmz/repo/linux/spark"
+default['spark']['version'] = "0.7.0-SNAPSHOT"
+
+#http://repo.staging.dmz/repo/linux/spark
+default['spark']['download_url'] = "http://streaming-sparkmaster01.os.staging.dmz"
 default['spark']['install_dir'] = "/opt/spark"
 
 default['spark']['mem']['master'] = "6g"
