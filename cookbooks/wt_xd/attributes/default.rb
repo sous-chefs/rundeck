f1d482353555acda3fd1879aff83734d7d391191
Kendrick Martin <kendrick.martin@webtrends.com>
2013-01-21 13:33:52 -0800

    Adding new stuff for xd services

--- cookbooks/wt_xd/attributes/default.rb
+++ cookbooks/wt_xd/attributes/default.rb
@@ -10,3 +10,11 @@
 default['wt_xd']['download_url'] = ''
 default['wt_xd']['log_level']    = 'INFO'
 default['wt_xd']['mapred_child_java_opts'] = '-server -Xmx2048m -Djava.net.preferIPv4Stack=true'
+
+##These attributes are for the windows services
+default['wt_xd']['importer']['download_url'] = ''
+default['wt_xd']['importer']['install_dir'] = "xd\\bin"
+default['wt_xd']['retrieval']['service_name'] = "Webtrends.ExternalData.RetrievalService.exe"
+default['wt_xd']['storage']['service_name'] = "Webtrends.ExternalData.StorageService.exe"
+default['wt_xd']['retrieval']['service_binary'] = "Webtrends.ExternalData.RetrievalService.exe"
+default['wt_xd']['storage']['service_binary'] = "Webtrends.ExternalData.StorageService.exe"
