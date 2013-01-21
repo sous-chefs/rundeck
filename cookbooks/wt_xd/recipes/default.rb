10d17a58ed51213bfa8f2279c5ca5569ae627970
Kendrick Martin <kendrick.martin@webtrends.com>
2013-01-21 13:33:52 -0800

    Adding new stuff for xd services
    Former-commit-id: f82dbbb482e07246a281180ea6efbb74f2dd5aa0 [formerly 4524e54082f6a846e96db9bad900203de08a483d] [formerly 492f2a9a4074e4946faeb1dddf5e569a805be823 [formerly 451dc262837993606e6d155d8e5464d47eac7bbb [formerly f1d482353555acda3fd1879aff83734d7d391191]]]
    Former-commit-id: 7fb9ba9c03bfaf71fc05878d7734faa428b274cf [formerly f75b2d12722017c5ed627f15795b1317916dd18a]
    Former-commit-id: 77f3fc9ef2f01f2190bea8a4a11c16910bfec73b
    Former-commit-id: a60b172d49559b187a2ede58b5fb2f1ca5bd924b

--- cookbooks/wt_xd/recipes/default.rb
+++ cookbooks/wt_xd/recipes/default.rb
@@ -6,3 +6,86 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+if ENV["deploy_build"] == "true" then
+  log "The deploy_build value is true so un-deploy first"  
+  #include_recipe "wt_xd::uninstall"
+else
+  log "The deploy_build value is not set or is false so we will only update the configuration"
+end
+
+download_url = node['wt_xd']['importer']['download_url']
+
+# destinations
+install_dir = File.join(node['wt_common']['install_dir_windows'], node['wt_xd']['importer']['install_dir']).gsub(/[\\\/]+/,"\\")
+log_dir = File.join(node['wt_common']['install_dir_windows'], "logs")
+
+# get data bag items
+auth_data = data_bag_item('authorization', node.chef_environment)
+svcuser = auth_data['wt_common']['system_user']
+svcpass = auth_data['wt_common']['system_pass']
+
+# create the install directory
+directory install_dir do
+  recursive true
+  action :create
+  rights :read, user_data['wt_common']['system_user']
+  rights :write, user_data['wt_common']['system_user']
+end
+
+directory log_dir do
+  recursive true
+  action :create
+  rights :read, user_data['wt_common'][['system_user']
+  rights :write, user_data['wt_common']['system_user']
+end
+
+if ENV["deploy_build"] == "true" then
+
+	# unzip the install package
+	windows_zipfile install_dir do
+		source download_url
+		action :unzip
+	end
+	
+    %[Webtrends.ActionCenterControl.exe.config, Webtrends.ExternalData.Refresh.exe.config, Webtrends.ExternalData.RetrievalService.exe.config, 
+    	Webtrends.ExternalData.StorageService.exe.config, Webtrends.ExternalData.Common.dll.config, Webtrends.ExternalData.Plugins.ExactTargetConnector.dll.config.erb,
+    	Webtrends.ExternalData.StorageService.log4net.config.erb].each do |template_file|
+	  template "#{install_dir}\\#{template_file}"
+	  source "#{template_file}.erb"
+	  variables(
+	  	  :log_dir => log_dir,
+		  :master_host => node['wt_masterdb']['host'],
+		  :cass_host => node['cassandra']['cassandra_host'],
+		  :report_column => node['cassandra']['cassandra_report_column'],
+		  :thrift_port => node['cassandra']['cassandra_thrift_port'],
+		  :metadata_column => node['cassandra']['cassandra_meta_column'],
+		  :admin_email => node['wt_common']['admin_email'],
+		  :support_email => node['wt_common']['tech_support_email'],
+		  :target_email => node['wt_actioncenter']['exact_target_email'],
+		  :bcc_email => node['wt_actioncenter']['bcc_email']
+	  )
+	end
+
+  # Creates the services and issues initial startup commands
+  xd_rs = File.join(install_dir, node['wt_xd']['retrieval']['service_binary']).gsub(/[\\\/]+/,"\\")
+  execute xd_rs do
+    command "#{xd_rs} -install"
+  end
+
+  xd_ss = File.join(install_dir, node['wt_xd']['storage']['service_binary']).gsub(/[\\\/]+/,"\\")
+  execute xd_ss do
+    command "#{xd_ss} -install"
+  end
+	
+	share_wrs
+end
+
+service node['wt_xd']['retrieval']['service_name'] do
+	action :start
+end
+
+service node['wt_xd']['storage']['service_name'] do
+	action :start
+end
+
