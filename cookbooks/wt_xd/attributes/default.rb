5c0c7580ac17ecab211ac60d380f034f32aa45b4
Kendrick Martin <kendrick.martin@webtrends.com>
2013-01-21 17:05:46 -0800

    Fixing service name
    Former-commit-id: 13ea078fa40c52c991cf16f63313b9a7e79dd84a [formerly 81e439af0a5c71e7acbd0bae254b1cf43651f8a9] [formerly 41646d1658b9d5bcd322da915abd7481e9fb5cc1] [formerly df22542f087e2d1d758771e4a32c2e241aa562c6 [formerly beb8b360ee92e089569ae77f377cc96aa241f0ef]] [formerly 1a28f57f88c2ae6e5dbfd1371f85f6208c28043c [formerly bd991022349f63a8183e54a4f87713086c536e01] [formerly 2523948c24b238ef7de86f5dfbfd28bc822a72f6 [formerly 4d632fa1b3352f221d17cfa8227c9d162abd919d [formerly 8bcd66d36a9743f5119f5617481f2fdf2c144e3b]]]]
    Former-commit-id: f2c68ce23e4d3ddd1f84e1fda2fbe6d38017af35 [formerly 7db3611f07a2ce6281511c93c2fc7ddfa5e9c069] [formerly 95f183fd4cf858c24f101bdd95abf2bc88317b57 [formerly 4d632fa1b3352f221d17cfa8227c9d162abd919d]]
    Former-commit-id: ce7095b1af5559d5701d443df57d71770738f821 [formerly 934d71355b0bd5457a554e49aa5b6ec56e7a1090]
    Former-commit-id: c622ebe239ff604bb5092694b53be061be493630
    Former-commit-id: fcaeb510e718da1a02f11644c3a9fd9d98af8476

--- cookbooks/wt_xd/attributes/default.rb
+++ cookbooks/wt_xd/attributes/default.rb
@@ -14,7 +14,7 @@ default['wt_xd']['mapred_child_java_opts'] = '-server -Xmx2048m -Djava.net.prefe
 ##These attributes are for the windows services
 default['wt_xd']['importer']['download_url'] = ''
 default['wt_xd']['importer']['install_dir'] = "xd\\bin"
-default['wt_xd']['retrieval']['service_name'] = "Webtrends.ExternalData.RetrievalService.exe"
-default['wt_xd']['storage']['service_name'] = "Webtrends.ExternalData.StorageService.exe"
+default['wt_xd']['retrieval']['service_name'] = "Webtrends External Data Retrieval Service"
+default['wt_xd']['storage']['service_name'] = "Webtrends External Data Storage Service"
 default['wt_xd']['retrieval']['service_binary'] = "Webtrends.ExternalData.RetrievalService.exe"
 default['wt_xd']['storage']['service_binary'] = "Webtrends.ExternalData.StorageService.exe"
