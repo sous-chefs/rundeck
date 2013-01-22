c05757aa5528fb2f0b1f582c1d6396671ea9c7bc
Kendrick Martin <kendrick.martin@webtrends.com>
2013-01-22 15:30:38 -0800

    Pulling out hard coded jar check and using recipe from wt_base
    Former-commit-id: d3966f19cf954d2c9354c116901449930a5d3c12 [formerly c2e3f037e98540dc07c34a965f6defbf426f52dc] [formerly 92d8c414283f8d4f617ee630b226a7206b9ca856 [formerly e655c6437887c8043a5f6d48dddddebb76724c22]]
    Former-commit-id: 1b78cb512f617e0b66d3edffa3c03f9da89b4e3e [formerly c172b3902e7bfb83d13d24837ae7cdeb3f8530bf]
    Former-commit-id: 1718a0cadf35dfc76c694dc3b8ec21999eb6aeca

--- cookbooks/wt_streamingapi/recipes/default.rb
+++ cookbooks/wt_streamingapi/recipes/default.rb
@@ -220,23 +220,8 @@ if node.attribute?("nagios")
     end
 end
 
-#Pull jar version out
-ruby_block "Grep jar version" do
-  block do
-    wt_version = `jar xvf #{install_dir}/lib/#{node['wt_streamingapi']['jar']} META-INF/MANIFEST.MF > /dev/null  && grep Implementation-Version: META-INF/MANIFEST.MF| sed s/Implementation-Version://g | tr -d '\r\n'`
-    wt_build = `grep Implementation-Build: META-INF/MANIFEST.MF | sed s/Implementation-Build://g | tr -d '\r\n'`
-    version_block = node['webtrends_server']['product_versions']
-    version = "#{wt_version}-#{wt_build}".gsub(/\s/,'')
-    version_block.each do |v|
-      if v['name'] == "wt_streamingapi"
-        v['version'] = version
-      end
-    end
-    node.set['webtrends_server']['product_versions'] = version_block 
-  end
-end
+@install_dir = install_dir
+@jar_name = node['wt_streamingapi']['jar']
+@role_name = "wt_streamingapi"
 
-directory "#{install_dir}/lib/META-INF" do
-  recursive true
-  action :delete
-end
+include_recipe "wt_base::jar_version"
