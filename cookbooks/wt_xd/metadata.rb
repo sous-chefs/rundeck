01e2e7da798a4dccc24055753f414057861f345b
Denis Makarenko <denis.makarenko@webtrends.com>
2013-01-28 14:20:24 -0800

    Updated XD services config files to properly configure service bus endpoints

--- cookbooks/wt_xd/metadata.rb
+++ cookbooks/wt_xd/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "david.dvorak@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures wt_xd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.0.14"
+version          "2.0.15"
 depends          "wt_base"
-depends		 "wt_actioncenter"
+depends		 	     "wt_actioncenter"
