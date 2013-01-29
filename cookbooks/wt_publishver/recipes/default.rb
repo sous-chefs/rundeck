dec10f18b1967d444d683563f8ee848aa2313b46
David Dvorak <david.dvorak@webtrends.com>
2013-01-29 20:33:07 +0000

    add more roles
    Former-commit-id: fe0bae0f744a4285642149b944663ebc553b28ff [formerly d0569b5a70e9335ad254acd4561c2c0e6e5254ec] [formerly a2ca3caa854df549954bf68a38b51110c50578a3 [formerly 10d08ad6021ede849696913e7695aa6f8da6be84 [formerly bffa4aefdb5db389d5b0b9d263be65b6bcba918b]]]
    Former-commit-id: 21fb32dbe42dd47671280673dca030a43f1e70c4 [formerly 1eb54788362caa6b3f97445d8c3797ff67934f04]
    Former-commit-id: 09822b2af30548d40a0cbff86328b9b4a0826dcd
    Former-commit-id: d37d5ac37da2b01b683e37645215da21636fc90a

--- cookbooks/wt_publishver/recipes/default.rb
+++ cookbooks/wt_publishver/recipes/default.rb
@@ -10,6 +10,13 @@
 # old ubuntu platforms are not supported
 return if node['platform_family'] == 'debian' && node['platform_version'].to_i < 12
 
+class Chef::Resource
+	# returns a path with backslashes (windows friendly)
+	def win_path(*args)
+		::File.join(args).gsub(/[\\\/]/, '\\')
+	end
+end
+
 #
 # install prerequisites
 #
@@ -30,49 +37,79 @@ node['roles'].each do |r|
 		log "publishing #{r}"
 		wt_publishver 'A10 UI' do
 			download_url node['wt_analytics_ui']['download_url']
-			key_file     File.join(wdir, node['wt_analytics_ui']['install_dir'], 'bin/WebTrends.UI.Reporting.dll').gsub(/[\\\/]+/,'\\')
+			key_file     win_path(wdir, node['wt_analytics_ui']['install_dir'], 'bin/WebTrends.UI.Reporting.dll')
+		end
+	when 'wt_cam'
+		log "publishing #{r}"
+		wt_publishver 'CAM' do
+			download_url node['wt_cam']['download_url']
+			key_file     win_path(wdir, 'Webtrends.Cam/bin/Webtrends.Cam.dll')
 		end
 	when 'wt_data_deleter'
 		log "publishing #{r}"
 		wt_publishver 'Data Deleter' do
 			download_url node['wt_data_deleter']['download_url']
-			key_file     File.join(wdir, node['wt_data_deleter']['install_dir'], 'DataDeleter.exe').gsub(/[\\\/]+/,'\\')
+			key_file     win_path(wdir, node['wt_data_deleter']['install_dir'], 'DataDeleter.exe')
 		end
 	when 'wt_devicedataupdater'
 		log "publishing #{r}"
 		wt_publishver 'Device Data Updater' do
 			download_url node['wt_devicedataupdater']['download_url']
-			key_file     File.join(wdir, node['wt_devicedataupdater']['install_dir'], 'DDU.exe').gsub(/[\\\/]+/,'\\')
+			key_file     win_path(wdir, node['wt_devicedataupdater']['install_dir'], 'DDU.exe')
 		end
 	when 'wt_logpreproc'
 		log "publishing #{r}"
 		wt_publishver 'LPP' do
 			download_url node['wt_logpreproc']['download_url']
-			key_file     File.join(wdir, node['wt_logpreproc']['install_dir'], 'wtlogpreproc.exe').gsub(/[\\\/]+/,'\\')
+			key_file     win_path(wdir, node['wt_logpreproc']['install_dir'], 'wtlogpreproc.exe')
 		end
 	when 'wt_platformscheduler_agent'
 		log "publishing #{r}"
 		wt_publishver 'Platform Scheduler Agent' do
 			download_url node['wt_platformscheduler']['agent']['download_url']
-			key_file     File.join(wdir, 'common/agent/Webtrends.Agent.exe').gsub(/[\\\/]+/,'\\')
+			key_file     win_path(wdir, 'common/agent/Webtrends.Agent.exe')
+		end
+	when 'wt_portfolio_admin'
+		log "publishing #{r}"
+		wt_publishver 'Portfolio Admin' do
+			download_url node['wt_portfolio_admin']['download_url']
+			key_file     win_path(wdir, 'Webtrends.Portfolio.Admin/bin/Webtrends.Portfolio.Admin.dll')
+		end
+	when 'wt_portfolio_manager'
+		log "publishing #{r}"
+		wt_publishver 'Portfolio Manager' do
+			download_url node['wt_portfolio_manager']['download_url']
+			key_file     win_path(wdir, 'Webtrends.Portfolio.Manager/bin/Webtrends.PortfolioManager.dll')
 		end
 	when 'wt_roadrunner'
 		log "publishing #{r}"
 		wt_publishver 'RoadRunner Service' do
 			download_url node['wt_roadrunner']['download_url']
-			key_file     File.join(wdir, node['wt_roadrunner']['install_dir'], 'Webtrends.RoadRunner.Service.exe').gsub(/[\\\/]+/,'\\')
+			key_file     win_path(wdir, node['wt_roadrunner']['install_dir'], 'Webtrends.RoadRunner.Service.exe')
 		end
 	when 'wt_sauth'
 		log "publishing #{r}"
 		wt_publishver 'Streaming Auth' do
 			download_url node['wt_sauth']['download_url']
-			key_file     File.join(wdir, 'Webtrends.Auth/bin/Webtrends.Auth.dll').gsub(/[\\\/]+/,'\\')
+			key_file     win_path(wdir, 'Webtrends.Auth/bin/Webtrends.Auth.dll')
 		end
 	when 'wt_search'
 		log "publishing #{r}"
 		wt_publishver 'Search Service' do
 			download_url node['wt_search']['download_url']
-			key_file     File.join(wdir, node['wt_search']['install_dir'], 'Webtrends.Search.Service.exe').gsub(/[\\\/]+/,'\\')
+			key_file     win_path(wdir, node['wt_search']['install_dir'], 'Webtrends.Search.Service.exe')
+		end
+	when 'wt_storm_streaming_topo'
+		log "publishing #{r}"
+		wt_publishver 'Streaming Storm Topology' do
+			download_url node['wt_storm_streaming']['download_url']
+			key_file     File.join(node['storm']['install_dir'], "storm-#{node['storm']['version']}", 'lib/webtrends-core-*')
+		end
+	when 'wt_streaminganalysis_monitor'
+		log "publishing #{r}"
+		wt_publishver 'Streaming Analysis Monitor' do
+			download_url node['wt_streaminganalysis_monitor']['download_url']
+			key_file     File.join(ldir, 'streaminganalysis_monitor/lib/webtrends-streaming-analysis-monitor.jar')
 		end
 	when 'wt_streaming_api_server'
 		log "publishing #{r}"
@@ -80,17 +117,41 @@ node['roles'].each do |r|
 			download_url node['wt_streamingapi']['download_url']
 			key_file     File.join(ldir, 'streamingapi/lib/webtrends-streamingapi.jar')
 		end
+	when 'wt_streaming_auditor'
+		log "publishing #{r}"
+		wt_publishver 'Streaming Auditor' do
+			download_url node['wt_streamingauditor']['download_url']
+			key_file     File.join(ldir, 'streamingauditor/lib/webtrends-auditing-master-*')  
+		end
+	when 'wt_streaming_configservice'
+		log "publishing #{r}"
+		wt_publishver 'Streaming Config Service' do
+			download_url node['wt_streamingconfigservice']['download_url']
+			key_file     File.join(ldir, 'streamingconfigservice/lib/webtrends-streaming-configservice.jar')
+		end
+	when 'wt_streaming_logreplayer'
+		log "publishing #{r}"
+		wt_publishver 'Log Replayer' do
+			download_url node['wt_streaminglogreplayer']['download_url']
+			key_file     File.join(ldir, 'streaminglogreplayer/lib/webtrends-streaminglogreplayer.jar')
+		end
+	when 'wt_streaming_managementservice'
+		log "publishing #{r}"
+		wt_publishver 'Streaming Management Service' do
+			download_url node['wt_streamingmanagementservice']['download_url']
+			key_file     File.join(ldir, 'streamingmanagementservice/lib/webtrends-streaming-managementservice.jar')
+		end
 	when 'wt_streaming_viz'
 		log "publishing #{r}"
 		wt_publishver 'Streaming Viz' do
 			download_url node['wt_streaming_viz']['download_url']
-			key_file     File.join(wdir, 'Webtrends.Streaming.Viz/bin/Webtrends.Streaming.Viz.dll').gsub(/[\\\/]+/,'\\')
+			key_file     win_path(wdir, 'Webtrends.Streaming.Viz/bin/Webtrends.Streaming.Viz.dll')
 		end
 	when 'wt_sync'
 		log "publishing #{r}"
 		wt_publishver 'Sync Service' do
 			download_url node['wt_sync']['download_url']
-			key_file     File.join(wdir, node['wt_sync']['install_dir'], 'Webtrends.SyncService.exe').gsub(/[\\\/]+/,'\\')
+			key_file     win_path(wdir, node['wt_sync']['install_dir'], 'Webtrends.SyncService.exe')
 		end
 	else
 		log "no publishing data for role => #{r}"
