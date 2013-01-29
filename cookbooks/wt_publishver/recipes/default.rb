d24cdb49ba6bf0ca4478354907057732ed2fbb9f
David Dvorak <david.dvorak@webtrends.com>
2013-01-29 01:22:16 +0000

    support ubuntu; use custom LWRP
    Former-commit-id: 31fa7abcd29722d4255dbe4e63575f7c58091432 [formerly 58a91c1093745cdf374d5a7ef824d9bef28b3e59] [formerly 51988e38e00dbcbdac0162058295984c3abf74ef [formerly 7bb1142e348518710e3d7c42c268986f19af946a [formerly dcc819739fbb1a0a00e381ec49a3f6544c05d86d]]]
    Former-commit-id: f712f6b9dce9b2e9a86de37a81a8696fb8b6add3 [formerly 5f16ec00f6f36a63ca9931eaca0ec3badc008839]
    Former-commit-id: 842f473d014d1fccb43e8a863d8e789fb3aae8f4
    Former-commit-id: 48baaf0ada49e704dc9f468e91a8a840bc08f949

--- cookbooks/wt_publishver/recipes/default.rb
+++ cookbooks/wt_publishver/recipes/default.rb
@@ -7,11 +7,92 @@
 # All rights reserved - Do Not Redistribute
 #
 
-case node['platform_family']
-  when 'debian', 'rhel'
-    include_recipe 'wt_publishver::linux'
-  when 'windows'
-    include_recipe 'wt_publishver::windows'
-  else
-    log "unknown platform family => #{node['platform_family']}"
+# old ubuntu platforms are not supported
+return if node['platform_family'] == 'debian' && node['platform_version'].to_i < 12
+
+#
+# install prerequisites
+#
+wt_publishver 'Install Prerequisites' do
+	action :deploy_prereqs
+end
+
+# linux install directory
+ldir = node['wt_common']['install_dir_linux']
+
+
+# windows install directory
+wdir = node['wt_common']['install_dir_windows']
+
+node['roles'].each do |r|
+	case r
+	when 'wt_analytics_ui'
+		log "publishing #{r}"
+		wt_publishver 'A10 UI' do
+			download_url node['wt_analytics_ui']['download_url']
+			key_file     File.join(wdir, node['wt_analytics_ui']['install_dir'], 'bin/WebTrends.UI.Reporting.dll').gsub(/[\\\/]+/,'\\')
+		end
+	when 'wt_data_deleter'
+		log "publishing #{r}"
+		wt_publishver 'Data Deleter' do
+			download_url node['wt_data_deleter']['download_url']
+			key_file     File.join(wdir, node['wt_data_deleter']['install_dir'], 'DataDeleter.exe').gsub(/[\\\/]+/,'\\')
+		end
+	when 'wt_devicedataupdater'
+		log "publishing #{r}"
+		wt_publishver 'Device Data Updater' do
+			download_url node['wt_devicedataupdater']['download_url']
+			key_file     File.join(wdir, node['wt_devicedataupdater']['install_dir'], 'DDU.exe').gsub(/[\\\/]+/,'\\')
+		end
+	when 'wt_logpreproc'
+		log "publishing #{r}"
+		wt_publishver 'LPP' do
+			download_url node['wt_logpreproc']['download_url']
+			key_file     File.join(wdir, node['wt_logpreproc']['install_dir'], 'wtlogpreproc.exe').gsub(/[\\\/]+/,'\\')
+		end
+	when 'wt_platformscheduler_agent'
+		log "publishing #{r}"
+		wt_publishver 'Platform Scheduler Agent' do
+			download_url node['wt_platformscheduler']['agent']['download_url']
+			key_file     File.join(wdir, 'common/agent/Webtrends.Agent.exe').gsub(/[\\\/]+/,'\\')
+		end
+	when 'wt_roadrunner'
+		log "publishing #{r}"
+		wt_publishver 'RoadRunner Service' do
+			download_url node['wt_roadrunner']['download_url']
+			key_file     File.join(wdir, node['wt_roadrunner']['install_dir'], 'Webtrends.RoadRunner.Service.exe').gsub(/[\\\/]+/,'\\')
+		end
+	when 'wt_sauth'
+		log "publishing #{r}"
+		wt_publishver 'Streaming Auth' do
+			download_url node['wt_sauth']['download_url']
+			key_file     File.join(wdir, 'Webtrends.Auth/bin/Webtrends.Auth.dll').gsub(/[\\\/]+/,'\\')
+		end
+	when 'wt_search'
+		log "publishing #{r}"
+		wt_publishver 'Search Service' do
+			download_url node['wt_search']['download_url']
+			key_file     File.join(wdir, node['wt_search']['install_dir'], 'Webtrends.Search.Service.exe').gsub(/[\\\/]+/,'\\')
+		end
+	when 'wt_streaming_api_server'
+		log "publishing #{r}"
+		wt_publishver 'Streaming API' do
+			download_url node['wt_streamingapi']['download_url']
+			key_file     File.join(ldir, 'streamingapi/lib/webtrends-streamingapi.jar')
+		end
+	when 'wt_streaming_viz'
+		log "publishing #{r}"
+		wt_publishver 'Streaming Viz' do
+			download_url node['wt_streaming_viz']['download_url']
+			key_file     File.join(wdir, 'Webtrends.Streaming.Viz/bin/Webtrends.Streaming.Viz.dll').gsub(/[\\\/]+/,'\\')
+		end
+	when 'wt_sync'
+		log "publishing #{r}"
+		wt_publishver 'Sync Service' do
+			download_url node['wt_sync']['download_url']
+			key_file     File.join(wdir, node['wt_sync']['install_dir'], 'Webtrends.SyncService.exe').gsub(/[\\\/]+/,'\\')
+		end
+	else
+		log "no publishing data for role => #{r}"
+	end
 end
