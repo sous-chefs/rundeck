1d7d7e1e10ad66862a914b94e54a91537c0c48c2
Kendrick Martin <kendrick.martin@webtrends.com>
2013-01-31 14:07:04 -0800

    Pulled xd importer code into it's own cookbook to avoid clashing with mapreduce job work

---
+++ cookbooks/wt_xd_importer/recipes/default.rb
@@ -0,0 +1,148 @@
+#
+# Cookbook Name:: wt_xd
+# Recipe:: default
+#
+# Copyright 2012, Webtrends Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+if ENV["deploy_build"] == "true" then
+  log "The deploy_build value is true so un-deploy first"  
+  include_recipe "wt_xd::uninstall"
+else
+  log "The deploy_build value is not set or is false so we will only update the configuration"
+end
+
+download_url = node['wt_xd_importer']['download_url']
+
+# destinations
+install_dir = File.join(node['wt_common']['install_dir_windows'], node['wt_xd_importer']['install_dir']).gsub(/[\\\/]+/,"\\")
+log_dir = File.join(node['wt_common']['install_dir_windows'], "logs")
+
+# get data bag items
+auth_data = data_bag_item('authorization', node.chef_environment)
+svcuser = auth_data['wt_common']['system_user']
+svcpass = auth_data['wt_common']['system_pass']
+
+googleplay_key = data_bag_item('rsa_keys', 'googleplay')
+
+# create the install directory
+directory install_dir do
+  recursive true
+  action :create
+end
+
+directory log_dir do
+  recursive true
+  action :create
+end
+
+wt_base_icacls install_dir do
+    user svcuser
+    perm :modify
+    action :grant
+end
+
+wt_base_icacls log_dir do
+    user svcuser
+    perm :modify
+    action :grant
+end
+
+if ENV["deploy_build"] == "true"
+
+	# unzip the install package
+	windows_zipfile install_dir do
+		source download_url
+		action :unzip
+	end
+
+  template "#{install_dir}\\PublicPrivateKeys.rsa" do
+    source "PublicPrivateKeys.rsa.erb"
+    variables(
+      :modulus => googleplay_key['modulus'],
+      :exponent => googleplay_key['exponent'],
+      :p => googleplay_key['p'],
+      :q => googleplay_key['q'],
+      :dp => googleplay_key['dp'],
+      :dq => googleplay_key['dq'],
+      :inverse_q => googleplay_key['inverse_q'],
+      :d => googleplay_key['d']
+    )
+  end
+	
+    %w[Webtrends.ActionCenterControl.exe.config Webtrends.ExternalData.Refresh.exe.config Webtrends.ExternalData.RetrievalService.exe.config
+    	Webtrends.ExternalData.StorageService.exe.config].each do |template_file|
+	  template "#{install_dir}\\#{template_file}" do
+	  source "#{template_file}.erb"
+	  variables({
+	  	  :log_dir => log_dir,
+		  :master_host => node['wt_masterdb']['host'],
+		  :cass_host => node['cassandra']['cassandra_host'],
+		  :report_column => node['cassandra']['cassandra_report_column'],
+		  :thrift_port => node['cassandra']['cassandra_thrift_port'],
+		  :metadata_column => node['cassandra']['cassandra_meta_column'],
+		  :admin_email => node['wt_common']['admin_email'],
+		  :support_email => node['wt_common']['tech_support_email'],
+		  :target_email => node['wt_actioncenter']['exact_target_email'],
+		  :bcc_email => node['wt_actioncenter']['bcc_email'],
+		  :hbase_location => node['hbase']['location'],
+		  :hbase_data_center_id => node['hbase']['data_center_id'],
+		  :hbase_pod_id => node['hbase']['pod_id'],
+		  :hbase_thrift_port => node['hbase']['thrift_port']
+	  })
+	  end
+    end
+
+# run iss command on the .rsa file  
+
+  wt_base_icacls "C:\\ProgramData\\Microsoft\\Crypto\\RSA\\MachineKeys" do
+    user node['current_user']
+    perm :modify
+    action :grant
+  end
+
+  node['wt_xd_importer']['plugins'].each do |plugin|
+
+    execute "asp_regiis_pi" do   
+      command  "C:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319\\aspnet_regiis -pi #{plugin} #{install_dir}\\PublicPrivateKeys.rsa"
+    end
+
+    execute "asp_regiis_pa" do
+      command  "C:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319\\aspnet_regiis -pa #{plugin} #{svcuser}"
+    end
+  end
+
+  # delete the .rsa file
+  file "#{install_dir}\\bin\\PublicPrivateKeys.rsa" do
+    action :delete
+  end
+
+  wt_base_icacls "C:\\ProgramData\\Microsoft\\Crypto\\RSA\\MachineKeys" do
+    user node['current_user']
+    perm :modify
+    action :remove
+  end
+
+  # Creates the services and issues initial startup commands
+  xd_rs = File.join(install_dir, node['wt_xd_importer']['retrieval']['service_binary']).gsub(/[\\\/]+/,"\\")
+  execute xd_rs do
+    command "#{xd_rs} --install --SERVICEACCT=#{svcuser} --SERVICEPASS=#{svcpass}"
+  end
+
+  xd_ss = File.join(install_dir, node['wt_xd_importer']['storage']['service_binary']).gsub(/[\\\/]+/,"\\")
+  execute xd_ss do
+    command "#{xd_ss} --install --SERVICEACCT=#{svcuser} --SERVICEPASS=#{svcpass}"
+  end  
+
+  xd_refresh = File.join(install_dir, node['wt_xd_importer']['refresh']['binary']).gsub(/[\\\/]+/,"\\")
+  execute xd_refresh do
+    command "#{xd_refresh} --install"
+    returns 21	
+  end  
+
+  share_wrs
+
+end
+
---
+++ cookbooks/wt_xd_importer/recipes/uninstall.rb
@@ -0,0 +1,49 @@
+# Cookbook Name:: wt_xd_importer
+# Recipe:: uninstall
+# Author:: Kendrick Martin
+#
+# Copyright 2012, Webtrends, Inc
+#
+# All rights reserved - Do Not Redistribute
+# This recipe uninstalls existing Search Service installs
+
+# destinations
+install_dir = File.join(node['wt_common']['install_dir_windows'], node['wt_xd_importer']['install_dir']).gsub(/[\\\/]+/,"\\")
+log_dir = node['wt_common']['install_dir_windows']
+
+
+# determine root drive of install_dir - ENG390500
+if (install_dir =~ /^(\w:)\\.*$/)
+	install_dir_drive = $1
+end
+
+execute "Uninstall retrieval service" do
+	command "#{install_dir}\\#{node['wt_xd_importer']['retrieval']['service_binary']} --uninstall"
+	ignore_failure true
+end
+
+execute "Uninstall storage service" do
+	command "#{install_dir}\\#{node['wt_xd_importer']['storage']['service_binary']} --uninstall"
+	ignore_failure true
+end
+
+# DM: doesn't make sense to do this as agent is already removed along with configuration. It will just cause refresh to wait till timeout
+#execute "Unregister from scheduler agent" do
+#	command "#{install_dir}\\#{node['wt_xd_importer']['refresh']['binary']} --uninstall"
+#	ignore_failure true
+#	returns 21
+#end
+
+# delays to give the service plenty of time to actually stop
+ruby_block "wait" do
+	block do
+		sleep(120)		
+	end
+	action :create
+end
+
+# delete install folder
+directory install_dir do
+	recursive true
+	action :delete
+end
