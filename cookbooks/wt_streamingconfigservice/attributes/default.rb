5fe5ffa938c7b2030e567c964df1a83623a40fee
Mark Alexander <mark.alexander@webtrends.com>
2013-02-05 23:31:32 +0000

    STR-906
    Former-commit-id: 252cae6c93e6a23762bab62a3b59591fb1438443 [formerly 40dbec458595c43243e6fa6961c4808c48df1fdc] [formerly e4577e63a94bac596341b858eabff29696c66614 [formerly db34b7b91bcf5d81f14efcd84320f07f8d33bf87]]
    Former-commit-id: f6015dacfd0f864e4378fcb95ab34f12cff0cb48 [formerly 5e8fef298d1d2461d6189d0093952d30fb064f43]
    Former-commit-id: c6bf270181728746b70310af8aa2e2c098974a68

--- cookbooks/wt_streamingconfigservice/attributes/default.rb
+++ cookbooks/wt_streamingconfigservice/attributes/default.rb
@@ -12,8 +12,6 @@ default['wt_streamingconfigservice']['group'] = "webtrends"
 default['wt_streamingconfigservice']['java_opts'] = "-Xms1024m -Djava.net.preferIPv4Stack=true"
 default['wt_streamingconfigservice']['download_url'] = ""
 default['wt_streamingconfigservice']['port'] = 8080
-default['wt_streamingconfigservice']['camdbname'] = "cam"
-default['wt_streamingconfigservice']['camdbserver'] = ""
 default['wt_streamingconfigservice']['includeUnmappedAnalyticsIds'] = "true"
 default['wt_streamingconfigservice']['jmx_port'] = 9999
 default['wt_streamingconfigservice']['healthcheck_port'] = 9000
