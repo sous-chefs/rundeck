ea89d942a9295264111632ab6730f5199efae789
Marek Dolgos <marek.dolgos@webtrends.com>
2013-02-08 15:17:26 -0800

    Adding new version of config service.

--- cookbooks/wt_streamingconfigservice/recipes/undeploy.rb
+++ cookbooks/wt_streamingconfigservice/recipes/undeploy.rb
@@ -12,7 +12,6 @@ install_dir  = "#{node['wt_common']['install_dir_linux']}/streamingconfigservice
 
 runit_service "streamingconfigservice" do
   action :disable
-  run_restart false
 end
 
 # try to stop the service, but allow a failure without printing the error
