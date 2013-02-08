a6f270726bc4757c30625d0c4fbba51eb00f8496
Marek Dolgos <marek.dolgos@webtrends.com>
2013-02-08 15:13:41 -0800

    New cookbook version of SMS

--- cookbooks/wt_streamingmanagementservice/recipes/undeploy.rb
+++ cookbooks/wt_streamingmanagementservice/recipes/undeploy.rb
@@ -12,20 +12,12 @@ install_dir  = "#{node['wt_common']['install_dir_linux']}/streamingmanagementser
 
 runit_service "streamingmanagementservice" do
   action :disable
-  run_restart false
 end
 
 # try to stop the service, but allow a failure without printing the error
 service "streamingmanagementservice" do
   action [:stop, :disable]
   ignore_failure true
-end
-
-# shell out to force-stop the service if it hasn't stopped cleanly
-execute "force-stop" do
-  command "sv force-stop streamingmanagementservice"
-  action :run
-  returns [0,1]  
 end
 
 directory log_dir do
