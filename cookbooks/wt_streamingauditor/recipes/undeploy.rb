471f8c939dd21ad1ad356be2955ae7a8952bae78
vonnagy <ivan.vonnagy@webtrends.com>
2013-02-08 10:45:29 -0800

    Resolved runit issues

--- cookbooks/wt_streamingauditor/recipes/undeploy.rb
+++ cookbooks/wt_streamingauditor/recipes/undeploy.rb
@@ -10,6 +10,10 @@
 log_dir      = "#{node['wt_common']['log_dir_linux']}/streamingauditor"
 install_dir  = "#{node['wt_common']['install_dir_linux']}/streamingauditor"
 
+runit_service "streamingauditor" do
+    action :disable
+end
+
 # try to stop the service, but allow a failure without printing the error
 service "streamingauditor" do
   action [:stop, :disable]
