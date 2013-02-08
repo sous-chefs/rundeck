3e0330d13f8ab03c9527a833adbb20c8fb3603dc
David Dvorak <david.dvorak@webtrends.com>
2013-02-08 02:09:20 +0000

    config iis to standard setup, other minor fixes
    Former-commit-id: 88a0b442eff49aaf398cc0991005131ea7ec481a [formerly 1a19536b69ab15a46a0acabca989fe580f51a5d6] [formerly 92f33ab4252d4e849c8cc2a40422ab8269de23b9] [formerly f725fac72e8b723f6e639380cf86eed0a01988d6 [formerly 41f9d165ed86fbea5190105acdc15cba72eb0812]] [formerly 5d4906c7a59b61ff754e85b3e52ecdb2f8e56b84 [formerly 61bd98bee33c16767600f8ee3017994386a3d57b] [formerly b83f83d35ad11b5c126d1a1b7154ba2fa24a9b13 [formerly d48de82697ade85f6f22b17219d2a71755239706 [formerly 373592fd80854d2f1a79dcf4e1dc7f3af6c0047a]]]]
    Former-commit-id: b45052b6941b907d996c19bb52f31fa75f9c580d [formerly 8859891d8e43fd0f521f89dc29672f0c82f045bf] [formerly a0a8ffa45b89243c6767699f900dfe2227143604 [formerly d48de82697ade85f6f22b17219d2a71755239706]]
    Former-commit-id: f2e24e4ef06cbd0b2095a897c292956a2dd3a619 [formerly 0046f803837c38655568c31bd94cd1c2df7f24eb]
    Former-commit-id: 7afcb5336c816b802db57df743cb33dc82a2d0c3
    Former-commit-id: 88d7990c89a637c7e349dfc480b21d4f138f7584

--- cookbooks/wt_actioncenter/attributes/default.rb
+++ cookbooks/wt_actioncenter/attributes/default.rb
@@ -1,17 +1,16 @@
-# Author:: Marcus Vincent(marcus.vincent@webtrends.com>)
+#
 # Cookbook Name:: wt_actioncenter
-# Attribute:: default
+# Attributes:: default
+# Author:: Marcus Vincent (<marcus.vincent@webtrends.com>)
 #
 # Copyright 2012, Webtrends Inc.
 #
-# build info
-default['wt_actioncenter']['artifact'] = 'ActionCenterRestService.zip'
-default['wt_actioncenter']['tc_proj'] = 'Action Center'
-default['wt_actioncenter']['app_pool'] = "ActionCenter"
-default['wt_actioncenter']['download_url'] = ""
-default['wt_actioncenter']['static_download_url'] = ""
+
+default['wt_actioncenter']['app_pool'] = 'ActionCenter'
+default['wt_actioncenter']['download_url'] = ''
+default['wt_actioncenter']['static_download_url'] = ''
 default['wt_actioncenter']['port'] = 80
-default['wt_actioncenter']['log_level'] = "INFO"
+default['wt_actioncenter']['log_level'] = 'INFO'
 default['wt_actioncenter']['cache_enabled'] = 'false'
 default['wt_actioncenter']['cache_region'] = 'ActionCenter'
-default['wt_actioncenter']['static_content_appender'] = "/actioncenter/v1"
+default['wt_actioncenter']['static_content_appender'] = '/actioncenter/v1'
