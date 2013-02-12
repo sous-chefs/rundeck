fdb993f69f037e237c2b02b28639118cef1836bf
Fabrice Tatieze <tatiezef@sops02.staging.dmz>
2013-02-12 17:12:29 +0000

    Adding the appKey setting to the appSettings.config.tmpl
    Former-commit-id: 974f1668bb020600f536e1709b95a05d80f91201 [formerly 1b312a70852397082cc345f30ac5deabfac07996] [formerly 1a0853e159ad6d0a34ffa217834531f06f1d394e [formerly bb894ff26cf20270aaa4b484ff803f28c344c0f5]]
    Former-commit-id: 92757061c31ef8947d86e1bf0eb678676af10775 [formerly 05550380ff2d01ae048d05f089033dc59d687094]
    Former-commit-id: 7417176ee9eaaa987e6c56ac981f9e141f7884eb

--- cookbooks/wt_streaming_viz/metadata.rb
+++ cookbooks/wt_streaming_viz/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "kendrick.martin@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures Streams UI IIS site"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.19"
+version          "1.1.20"
 supports         "windows"
 depends          "iis"
 depends          "windows"
