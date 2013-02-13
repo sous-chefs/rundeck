6afe238e06ad88a49bc9e69a764ea69fd62b6f6c
martink <kendrick.martin@webtrends.com>
2013-02-13 20:43:46 +0000

    Import apache2 version 1.5.0
    Former-commit-id: f0b8d94332347fddacde75f2d224469111d92395 [formerly b90adcf29521f60553dca07535d01c1c67ac5b4b] [formerly ba523294ff061c4b6aebd77a18eead0cab1dc70d [formerly 909e1956ea8439856e7c17ba39f0aacfd77667f8 [formerly efb895ef45763801d3ca79d3aa215a152dd3be6e]]]
    Former-commit-id: 79ebef09d68000f01e74c676d3c6464cdc864df6 [formerly e4360810d4198f7e11631590116cf8e3c19eafee]
    Former-commit-id: efea46018f7a19f7d7bb5ea9ba645b7038a243d4
    Former-commit-id: 709e4a9fc072ec9b7a219cc673cc628f8045c991

--- cookbooks/apache2/attributes/default.rb
+++ cookbooks/apache2/attributes/default.rb
@@ -21,7 +21,7 @@ default['apache']['root_group']  = "root"
 
 # Where the various parts of apache are
 case platform
-when "redhat", "centos", "scientific", "fedora", "suse", "amazon"
+when "redhat", "centos", "scientific", "fedora", "suse", "amazon", "oracle"
   default['apache']['package'] = "httpd"
   default['apache']['dir']     = "/etc/httpd"
   default['apache']['log_dir'] = "/var/log/httpd"
@@ -117,6 +117,9 @@ default['apache']['traceenable'] = "On"
 
 # mod_auth_openids
 default['apache']['allowed_openids'] = Array.new
+
+# mod_status Allow list, space seprated list of allowed entries.  
+default['apache']['status_allow_list'] = "localhost ip6-localhost"
 
 # mod_status ExtendedStatus, set to 'true' to enable
 default['apache']['ext_status'] = false
