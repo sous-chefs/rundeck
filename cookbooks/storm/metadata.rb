4bf568b96e5ba5b8f80ad67ff575a6ac2c971aec
Kendrick Martin <kendrick.martin@webtrends.com>
2013-02-14 14:17:42 -0800

    Fixes for storm cookbook to correctly template new instance of supervisor

--- cookbooks/storm/metadata.rb
+++ cookbooks/storm/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "sean.mcnamara@webtrends.com"
 license          "All rights reserved"
 description      "Installs Twitter's Storm distributed computation system"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.32"
+version          "1.0.33"
 depends          "java"
 depends          "runit"
 supports         "ubuntu"
