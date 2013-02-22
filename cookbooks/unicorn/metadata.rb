ae84ec73bdc5e039d41ef2f9a0c9b325e745f6ce
martink <kendrick.martin@webtrends.com>
2013-02-22 17:11:03 +0000

    Import unicorn version 1.2.0
    Former-commit-id: 25ea474b1090ab88538819469dde423ab092dc47 [formerly 41fd92bc1cfebff05ae52903b015aee6aaaa59a6] [formerly 88d1b9bbd53e0bb091d6f49f57488c037e9dd80a [formerly b2304b03758e0ef5a0c51ebdad7207c9379faede [formerly 7f46e0caf29621c536ff943c9aff09c5e6de6946]]]
    Former-commit-id: f3f2a149a0579d7ea3eb2183717ca1af81d4ce38 [formerly 86a2ee8b1276f1c678ab421a9be60237179b1981]
    Former-commit-id: fecd03ef6289b0f49e37dd6a57e9e3aaf9eeef97
    Former-commit-id: f530a52a192e6f38ee3417bd2e79adc35b176097

--- cookbooks/unicorn/metadata.rb
+++ cookbooks/unicorn/metadata.rb
@@ -1,7 +1,8 @@
+name             "unicorn"
 maintainer       "Opscode, Inc"
 maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures unicorn"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.0"
+version          "1.2.0"
 recipe "unicorn", "Installs unicorn rubygem"
