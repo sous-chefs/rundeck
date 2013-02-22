c67852360df49f21e6ccd41806cec601612f5db9
martink <kendrick.martin@webtrends.com>
2013-02-22 17:18:06 +0000

    Import maven version 0.14.0

--- cookbooks/maven/metadata.rb
+++ cookbooks/maven/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "bryan.berry@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures maven"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.13.0"
+version          "0.14.0"
 
 depends "java"
 depends "ark"
