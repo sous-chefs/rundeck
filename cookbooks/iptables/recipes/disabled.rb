cc3cb566696d60fde5ab7678fe3cfc68d87760b3
martink <kendrick.martin@webtrends.com>
2013-02-22 17:13:16 +0000

    Import iptables version 0.12.0
    Former-commit-id: 88d6d696804c4dcbb84cf316fbadc5687575d946 [formerly c048615a5d095dcdb35fddb5ecdbce7f19a38cea] [formerly 875a0c2e4cc7059cf4d4f41594f2d66c06405f01 [formerly adbe16df435b6985d6111201df71d0965145b5c8 [formerly eafd4c77d613013ce773fa96e7bbb6ae119ffb8e]]]
    Former-commit-id: 42cfc54f90e8541bcb91499a3796bb4046579026 [formerly 0d3b227a74fb34516d51ad18a71d4775b3ebf0df]
    Former-commit-id: 95d40ff09dcfb9b20c77e52c85f3449084e6b303
    Former-commit-id: 5b8b5f8c237fb5314ae7181fa6dcc966cfafd60e

--- cookbooks/iptables/recipes/disabled.rb
+++ cookbooks/iptables/recipes/disabled.rb
@@ -18,33 +18,7 @@
 #
 
 package "iptables" 
-package "perl"
-
-execute "rebuild-iptables" do
-  command "/usr/sbin/rebuild-iptables"
-  action :nothing
+service "iptables" do
+      action [ :disable, :stop ]
+      supports :status => true, :start => true, :stop => true, :restart => true
 end
-
-directory "/etc/iptables.d" do
-  action :create
-end
-
-cookbook_file "/usr/sbin/rebuild-iptables" do
-  source "rebuild-iptables"
-  mode 0755
-end
-
-case node[:platform]
-when "ubuntu", "debian"
-  iptables_save_file = "/etc/iptables/general"
-
-  template "/etc/network/if-pre-up.d/iptables_load" do
-    source "iptables_load.erb"
-    mode 0755
-    variables :iptables_save_file => iptables_save_file
-  end
-end
-
-
-iptables_rule "all_established"
-iptables_rule "all_icmp"
