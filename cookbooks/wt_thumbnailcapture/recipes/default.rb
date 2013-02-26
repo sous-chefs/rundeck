466067122e1986022e3158f47c9f37c6d417e059
Toby Mosby <toby.mosby@webtrends.com>
2013-02-26 20:38:09 +0000

    partial cleanup
    Former-commit-id: 044594093c3e924fec0d48703b29b4843a57d22a [formerly 352138b686776c4448955294a96fd43cf82b3c71] [formerly 0a01b9fe922d300445094ff286987aaef320ab9b] [formerly 43d4ee7d46bd76ad5d019f0a2f27b7020786746b [formerly 0a7630ba2d4b37131c77371c3cb8eddfc404adae]] [formerly 1d2f3bcea63d4c6c730f00b7cba6d33b8611a836 [formerly 5189e4de6f107470ee483b687d0178f4ca6b87bc] [formerly 4057de158a73bd2561cb3c2a0ff2ab5094e64154 [formerly d73565c752edf15c684fad348273bef9fbb64499 [formerly 946c20fd4e02b20904097c6b371c1c3f5f7e8fd9]]]]
    Former-commit-id: 24988359652892ea4d0923b2559a27aea22295c2 [formerly 3e67d59baebfac7786655b526d0f414883f8eb92] [formerly cf64f7493c3b24d779d19fca02751cfdaeedf61c [formerly d73565c752edf15c684fad348273bef9fbb64499]]
    Former-commit-id: 68dcf4ca3771c2d5774ff82f46bc27d17bb920bf [formerly 234f5f8b1acdb8a76644fdf89dd0c9c32d887bd7]
    Former-commit-id: 10afbac14496c7c621c3864a90eb6e8231f58ed2
    Former-commit-id: 4571cfa9f73861b6b352715bc6d959c08306f03f

--- cookbooks/wt_thumbnailcapture/recipes/default.rb
+++ cookbooks/wt_thumbnailcapture/recipes/default.rb
@@ -6,13 +6,8 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-# TODO: clean out unused entries!
-
 # include runit so we can create a runit service
 include_recipe "runit"
-
-# install package for mounting nfs volumes
-package "nfs-common"
 
 # if in deploy mode then uninstall the product first
 if ENV["deploy_build"] == "true" then
@@ -63,67 +58,8 @@ directory "#{install_dir}/conf" do
   action :create
 end
 
-# create the conf directory
-directory "#{install_dir}/conf/cache" do
-  owner "webtrends"
-  group "webtrends"
-  mode 00755
-  recursive true
-  action :create
-end
-
-# create the share mount dir
-directory node['wt_thumbnailcapture']['share_mount_dir'] do
-  action :create
-end
-
-# mount the NFS mount and add to /etc/fstab
-mount node['wt_thumbnailcapture']['share_mount_dir'] do
-  device node['wt_thumbnailcapture']['log_share_mount']
-  fstype "nfs"
-  options "rw"
-  action [:mount, :enable]
-end
-
-def getZookeeperPairs(node)
-
-  # get the correct environment for the zookeeper nodes
-  zookeeper_port = node['zookeeper']['client_port']
-  zookeeper_env = node.chef_environment
-  unless node['wt_thumbnailcapture']['zookeeper_env'].nil? || node['wt_thumbnailcapture']['zookeeper_env'].empty?
-    zookeeper_env = node['wt_thumbnailcapture']['zookeeper_env']
-  end
-
-  # grab the zookeeper nodes that are currently available
-  zookeeper_pairs = Array.new
-  if not Chef::Config.solo
-    search(:node, "role:zookeeper AND chef_environment:#{zookeeper_env}").each do |n|
-      zookeeper_pairs << n['fqdn']
-    end
-  end
-
-  # fall back to attribs if search doesn't come up with any zookeeper roles
-  if zookeeper_pairs.count == 0
-    node['zookeeper']['quorum'].each do |i|
-      zookeeper_pairs << i
-    end
-  end
-
-  # append the zookeeper client port (defaults to 2181)
-  i = 0
-  while i < zookeeper_pairs.size do
-      zookeeper_pairs[i] = zookeeper_pairs[i].concat(":#{zookeeper_port}")
-      i += 1
-  end
-
-  return zookeeper_pairs
-end
-
 def processTemplates (install_dir, node, user, group, log_dir)
   log "Updating the templated config files"
-
-  # grab the zookeeper nodes that are currently available
-  zookeeper_pairs = getZookeeperPairs(node)
 
   %w[monitoring.properties producer.properties config.properties log4j.xml].each do |template_file|
     template "#{install_dir}/conf/#{template_file}" do
@@ -133,7 +69,6 @@ def processTemplates (install_dir, node, user, group, log_dir)
       mode  00755
       variables({
         :wt_thumbnailcapture => node['wt_thumbnailcapture'],
-        :zookeeper_pairs => zookeeper_pairs,
         :wt_monitoring => node['wt_monitoring'],
         :log_dir => log_dir
       })
@@ -151,36 +86,23 @@ if ENV["deploy_build"] == "true" then
     mode 00644
     end
 
-    # uncompress the application tarball into the install dir
-    execute "tar" do
-    user  "root"
-    group "root"
-    cwd install_dir
-    command "tar zxf #{Chef::Config['file_cache_path']}/#{tarball}"
-    end
-
-    template "#{install_dir}/bin/service-control" do
-        source  "service-control.erb"
-        owner "root"
-        group "root"
-        mode  00755
-        variables({
-            :log_dir => log_dir,
-            :install_dir => install_dir,
-            :java_home => java_home,
-            :java_opts => java_opts
-        })
-    end
+    # invoke the service using java command and java_opts
+###    execute "java" do
+###    user  "root"
+###    group "root"
+###    cwd install_dir
+###    command "java -jar #{Chef::Config['file_cache_path']}/#{tarball}"
+###    end
 
     processTemplates(install_dir, node, user, group, log_dir)
 
     # delete the application tarball
-    execute "delete_install_source" do
-        user "root"
-        group "root"
-        command "rm -f #{Chef::Config['file_cache_path']}/#{tarball}"
-        action :run
-    end
+###    execute "delete_install_source" do
+###        user "root"
+###        group "root"
+###        command "rm -f #{Chef::Config['file_cache_path']}/#{tarball}"
+###        action :run
+###    end
 
     # create a runit service
     runit_service "thumbnailcapture" do
@@ -195,27 +117,3 @@ else
     processTemplates(install_dir, node, user, group, log_dir)
 end
 
-#Create collectd plugin for thumbnailcapture JMX objects if collectd has been applied.
-if node.attribute?("collectd")
-  template "#{node['collectd']['plugin_conf_dir']}/collectd_thumbnailcapture.conf" do
-    source "collectd_thumbnailcapture.conf.erb"
-    owner "root"
-    group "root"
-    mode 00644
-    notifies :restart, resources(:service => "collectd")
-  end
-end
-
-  #Create a nagios nrpe check for the healthcheck page
-###  nagios_nrpecheck "wt_healthcheck_page" do
-###    command "#{node['nagios']['plugin_dir']}/check_http"
-###    parameters "-H #{node['fqdn']} -u /healthcheck -p 9000 -r \"\\\"all_services\\\":\\s*\\\"ok\\\"\""
-###    action :add
-###  end
-  #Create a nagios nrpe check for the log file
-###  nagios_nrpecheck "wt_garbage_collection_limit_reached" do
-###    command "#{node['nagios']['plugin_dir']}/check_log"
-###    parameters "-F /var/log/webtrends/thumbnailcapture/logreplayer.log -O /tmp/thumbnailcapture_old.log -q 'GC overhead limit exceeded'"
-###    action :add
-###  end
-end
