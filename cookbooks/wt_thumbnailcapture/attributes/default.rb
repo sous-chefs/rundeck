6450b730d931bf5593661b90b98cf900393a0f92
Toby Mosby <toby.mosby@webtrends.com>
2013-02-27 00:16:50 +0000

    thumbnail recipe
    Former-commit-id: f064f77c922515cdb1b6d7151531948f444051f5 [formerly 5c442d3353539b9c6d9ce04e85a228aeed26bdf3] [formerly 86a06613a7d9a17b85dd71647a86f534e79e0173 [formerly 78dd33f955ab39763e057da4a49c3b0705c6b95b [formerly 11c66859b17771f9ef59d570f589eeb964cc887c]]]
    Former-commit-id: 9d5eeb7c0b95b485dafb03a7d1bdafcb912d7823 [formerly 5eb5c4c7d04aa8dd68f9787a1b47493864d05abb]
    Former-commit-id: d40754bf6ce78889e69f475b6068c15fb33d4235
    Former-commit-id: 33b9b874e8f7e30b88d6c4a78860bbd0d74e4b49

--- cookbooks/wt_thumbnailcapture/attributes/default.rb
+++ cookbooks/wt_thumbnailcapture/attributes/default.rb
@@ -8,7 +8,8 @@
 default['wt_thumbnailcapture']['user'] = "webtrends"
 default['wt_thumbnailcapture']['group'] = "webtrends"
 default['wt_thumbnailcapture']['download_url'] = ""
-
+default['wt_thumbnailcapture']['port'] = 8085
+default['wt_thumbnailcapture']['thread_pool_size'] = 500
 default['wt_thumbnailcapture']['java_opts'] = "-Xms1024m -Djava.net.preferIPv4Stack=true"
 
 # optionally specify the environment where ZooKeeper nodes are located
