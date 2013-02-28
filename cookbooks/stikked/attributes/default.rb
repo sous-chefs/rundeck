443f2920027d6855a5cfa269c08d75cc72c47288
Kendrick Martin <kendrick.martin@webtrends.com>
2013-02-28 13:42:10 -0800

    Updates for stikked

---
+++ cookbooks/stikked/attributes/default.rb
@@ -0,0 +1,4 @@
+default['stikked']['db_name'] = "stikked"
+default['stikked']['download_url'] = "https://github.com/claudehohl/Stikked/archive/0.8.4.tar.gz"
+default['stikked']['version'] = "Stikked-0.8.4"
+default['stikked']['docroot'] = "/opt/Stikked-0.8.4/htdocs"
