3ed4e6966efb266fc01ce9f48c78935de857c26b
Kendrick Martin <kendrick.martin@webtrends.com>
2013-03-06 11:10:48 -0800

    Changed service name

--- cookbooks/squid/metadata.rb
+++ cookbooks/squid/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "matt@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures squid as a simple caching proxy"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.2"
+version          "0.2.3"
 
 %w{ debian ubuntu centos fedora redhat scientific suse amazon}.each do |os|
   supports os
