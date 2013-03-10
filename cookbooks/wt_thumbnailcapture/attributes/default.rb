13ad8672d9b6daf4b5c56d4245da2697ce67d886
Toby Mosby <toby.mosby@webtrends.com>
2013-03-10 02:50:22 +0000

    Add healthcheck port
    Former-commit-id: 9494213c5f4c2adb5284639ba46c9e58fbe1ee6c [formerly fbfe2c7da7663c06be779208cdcdcb9f62f9d293] [formerly 95e6f79043e50b07bb487e5e2be2c0be19a84362 [formerly 454250f7b33daf70b1abb001895a0945cade1bdf [formerly bbf5668f7ac01189e7c61496b50b6dacf15ef12f]]]
    Former-commit-id: 48a94610dc68b41a3e76b3e971d10f9e9a4ee60b [formerly 92515b0e92aae603864ab3d583f2513b77c34647]
    Former-commit-id: 3763d3e0d6b238b0021241fe73852f707616736a
    Former-commit-id: 59bb8ea977bed02a8d86e91a8f63a5df4213e6ab

--- cookbooks/wt_thumbnailcapture/attributes/default.rb
+++ cookbooks/wt_thumbnailcapture/attributes/default.rb
@@ -12,5 +12,9 @@ default['wt_thumbnailcapture']['port'] = 8085
 default['wt_thumbnailcapture']['thread_pool_size'] = 500
 default['wt_thumbnailcapture']['java_opts'] = "-Xms1024m -Djava.net.preferIPv4Stack=true"
 
+default['wt_thumbnailcapture']['jmx_port'] = 9999
+default['wt_thumbnailcapture']['healthcheck_port'] = 9000
+default['wt_thumbnailcapture']['thumbnailcapture_service_url'] = ""
+
 # optionally specify the environment where ZooKeeper nodes are located
 default['wt_thumbnailcapture']['zookeeper_env'] = nil
