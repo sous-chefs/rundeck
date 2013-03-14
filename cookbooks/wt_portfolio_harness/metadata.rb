ac78f948321ba843b951f6046ffda4798aab9846
Michael Parsons <parsonsm@sops02.staging.dmz>
2013-03-14 20:53:12 +0000

    changing version

--- cookbooks/wt_portfolio_harness/metadata.rb
+++ cookbooks/wt_portfolio_harness/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email    "michael.parsons@webtrends.com"
 license             "All rights reserved"
 description         "Installs/Configures Webtrends Portfolio Edge Service"
 long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version             "1.0.2"
+version             "1.0.0"
 depends             "java"
 depends             "runit"
