a912b638a19979223cb008bda804d79d5e4e7054
Michael Parsons <parsonsm@sops02.staging.dmz>
2013-03-14 19:35:41 +0000

    changing log directory location
    Former-commit-id: 53b50155d7ca32645aa3d4f555ca0ff117a52944 [formerly 59ffbd87035eca4ca78fa5ab97e27d3a725e5c38] [formerly 84aea18a2f97b85f7c6b2ed65d1ff530d9f0303b [formerly af8411e9256e24270b0d11d6910086bc17bc18e8 [formerly 075f68bab55ce04e4cac5b555f7dc73d28c6cee3]]]
    Former-commit-id: 69bc303c5e1ccad1c26f10e32ec56a8b04adda7c [formerly a459677542e91942197b32bd6baaa46530ed2a17]
    Former-commit-id: 1a442f4bdad74885602d2758324c6e6fd53295a3
    Former-commit-id: 84a8cfac50d7e7b44877682918e5356cc0e377dc

--- cookbooks/wt_portfolio_harness/attributes/default.rb
+++ cookbooks/wt_portfolio_harness/attributes/default.rb
@@ -13,7 +13,7 @@ default['wt_portfolio_harness']['download_url']   = ""
 default['wt_portfolio_harness']['port']           = 8080
 default['wt_portfolio_harness']['java_opts']      = "-Xms1024m -XX:+UseG1GC -Djava.net.preferIPv4Stack=true"
 default['wt_portfolio_harness']['jmx_port']       = 9999
-default['wt_portfolio_harness']['log_dir']        = "/var/log/webtrends/edgeservice"
+default['wt_portfolio_harness']['log_dir']        = "/var/log/webtrends/harness"
 default['wt_portfolio_harness']['router_uri']     = "tcp://localhost:5761"
 default['wt_portfolio_harness']['sauth_version']  = "v1"
 default['wt_portfolio_harness']['conf_url']       = "conf/application.conf"
