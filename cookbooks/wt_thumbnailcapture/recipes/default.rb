c3445e742d3409348d832eca1ea75b440f5b6501
Kendrick Martin <Kendrick.Martin@Webtrends.com>
2013-03-14 10:19:24 -0700

    Fixing default attribute name

--- cookbooks/wt_thumbnailcapture/recipes/default.rb
+++ cookbooks/wt_thumbnailcapture/recipes/default.rb
@@ -105,7 +105,7 @@ def processTemplates (install_dir, node, user, group, log_dir, java_home, mBoxes
     variables({
       :port => node['wt_thumbnailcapture']['port'],
       :memcache_boxes => mBoxes,
-      :memcache_expireseconds => node['wt_thumbnailcapture']['memcache_expireseconds'],
+      :memcache_expireseconds => node['wt_thumbnailcapture']['memcache_cacheexpireseconds'],
       :proxy_address => node['wt_common']['http_proxy_url'],
       :healthcheck_port => node['wt_thumbnailcapture']['healthcheck_port'],
       :wt_monitoring => node[:wt_monitoring]
