dbe68e5454a5ca4f365bfac9d7e8bb1b862bd7f6
vonnagy <ivan.vonnagy@webtrends.com>
2013-03-15 16:41:35 -0700

    Added logging fix
    Former-commit-id: 77daa3acbeeed7c4feed04debe52a9f82942e84e [formerly 436ef62c5ba16a8430d5eef3f6edc45c215dbcdc] [formerly df3197088f72c0f1806c903da0e660ec398c0a2c [formerly 326661f43300e1fcb551c0b5ac88a4866706bc87]]
    Former-commit-id: c48edc2d5daff9636bf4bb4f838925d407e10ef1 [formerly c57646687ad8723147aae6a4efb2bbe393d9ddee]
    Former-commit-id: f1f847bcc3a5603bfb0c35742816401b37c7c7ad

--- cookbooks/wt_portfolio_harness/attributes/default.rb
+++ cookbooks/wt_portfolio_harness/attributes/default.rb
@@ -13,6 +13,10 @@ default['wt_portfolio_harness']['download_url']   = ""
 default['wt_portfolio_harness']['port']           = 8080
 default['wt_portfolio_harness']['java_opts']      = "-Xms1024m -XX:+UseG1GC -Djava.net.preferIPv4Stack=true"
 default['wt_portfolio_harness']['jmx_port']       = 9999
+default['wt_portfolio_harness']['graphite_enabled'] = true
+default['wt_portfolio_harness']['graphite_interval'] = 5
+default['wt_portfolio_harness']['graphite_vmmetrics'] = "true"
+default['wt_portfolio_harness']['graphite_regex'] = ""
 default['wt_portfolio_harness']['log_dir']        = "/var/log/webtrends/harness"
 default['wt_portfolio_harness']['router_uri']     = "tcp://localhost:5761"
 default['wt_portfolio_harness']['sauth_version']  = "v1"
