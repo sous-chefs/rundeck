e5a6332f20ce8f581e85a627505bd2adb2b63f09
Michael Parsons <parsonsm@sops02.staging.dmz>
2013-03-15 17:50:45 +0000

    adding responsys chef recipe
    Former-commit-id: a9416933eec4e1f693bc46e3663d52d1e45ed634 [formerly e8890eef1b4ae00367a7fbd5c17798cf80cd6c5a] [formerly 51e5758923c988ed6842969d10cb05ffb4941988 [formerly 7c7a943ec2306ecbfc5380ab1a74463b9a5566ff [formerly fcf5a005f7cfa0263b09df3472b7fa90a468593b]]]
    Former-commit-id: 2c0b704223778c9bbc66f26117347aeaafba1a4c [formerly ac1ab34da9a9881f42ec746a95306766569fac55]
    Former-commit-id: fc906c330160dc343e848321e8459d48e5ad29bc
    Former-commit-id: ad70367102e0c691fa1844636544a5df5a80b6e4

---
+++ cookbooks/wt_actioncenter_dd_responsys/recipes/default.rb
@@ -0,0 +1,112 @@
+#
+# Cookbook Name:: wt_actioncenter_dd_responsys
+# Recipe:: default
+#
+# Copyright 2012, Webtrends
+#
+# All rights reserved - Do Not Redistribute
+#
+
+if ENV["deploy_build"] == "true" then
+  log "The deploy_build value is true so un-deploying first"
+  include_recipe "wt_actioncenter_dd_responsys::undeploy"
+else
+  log "The deploy_build value is not set or is false so we will only update the configuration"
+end
+
+install_dir = File.join(node['wt_common']['install_dir_linux'],
+"harness/plugins/actioncenter_dd_responsys")
+conf_dir = File.join(node['wt_common']['install_dir_linux'], "harness/conf")
+tarball      = node['wt_actioncenter_dd_responsys']['download_url'].split("/")[-1]
+download_url = node['wt_actioncenter_dd_responsys']['download_url']
+user = node['wt_actioncenter_dd_responsys']['user']
+group = node['wt_actioncenter_dd_responsys']['group']
+
+log "Install dir: #{install_dir}"
+
+# create the install directory
+directory "#{install_dir}" do
+  owner "root"
+  group "root"
+  mode 00755
+  recursive true
+  action :create
+end
+
+def processTemplates(conf_dir)
+	log "Updating template files"
+
+	%w[key.pem ResponsysServerCertificate.cer].each do | template_file|
+		template "#{conf_dir}/#{template_file}" do
+			source "#{template_file}.erb"
+			owner "root"
+			group "root"
+			mode 00644
+			variable({})
+		end
+	end
+end
+
+if ENV["deploy_build"] == "true" then
+  log "The deploy_build value is true so we will grab the tar ball and install"
+
+  # download the application tarball
+  remote_file "#{Chef::Config[:file_cache_path]}/#{tarball}" do
+    source download_url
+    mode 00644
+  end
+
+    # uncompress the application tarball into the install dir
+    execute "tar" do
+        user  "root"
+        group "root"
+        cwd install_dir
+        command "tar zxf #{Chef::Config[:file_cache_path]}/#{tarball}"
+    end
+
+	execute "copy_lib_contents" do
+		user "root"
+		group "root"
+		cwd install_dir
+		command "cp lib/* ."
+		action :run
+	end
+
+	execute "delete_lib_directory" do
+		user "root"
+		group "root"
+		cwd install_dir
+		command "rm -rf lib"
+		action :run
+	end
+
+	processTemplates(conf_dir)	
+
+    #processors = Array.new
+    #if not Chef::Config.solo
+    #  search(:node, "role:wt_stream_processor AND chef_environment:#{node.chef_environment}").each do |n|
+    #    processors << "\"akka://StreamProcessor@#{n[:fqdn]}:2552/user/processor\"" 
+    #  end
+    #end
+
+    #template "#{conf_dir}/application.conf" do
+    #    source  "application.conf.erb"
+    #    owner   "root"
+    #    group   "root"
+    #    mode    00644
+    #    variables({
+    #      :processors => processors
+    #    })
+    #end
+
+  # delete the install tar ball
+  execute "delete_install_source" do
+    user "root"
+    group "root"
+    command "rm -f #{Chef::Config[:file_cache_path]}/#{tarball}"
+    action :run
+  end
+
+else
+end
+
