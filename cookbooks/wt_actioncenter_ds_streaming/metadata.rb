531b910a70a48f348751a1840a04ea34cc86abfe
Michael Parsons <parsonsm@sops02.staging.dmz>
2013-03-15 17:24:13 +0000

    Adding streaming data source chef
    Former-commit-id: 768c195c08eba0f913ecbc2d32ea25e5029c6d90 [formerly e93c74ec7f33f74384b63addc2178a9397e25786] [formerly 1f5517e7a346adc478886714a19d8233d7aafef5 [formerly 99864d9c6bf92e7703960df32bf8e147238804d6]]
    Former-commit-id: 33cc1ebdd56c41a98379114ede81fcd65afcd363 [formerly c6f3d33304a49ec5cb83e9c346898f6817f7a76b]
    Former-commit-id: 3485e2c5c77255b457e5c9dbaede808a16c0c5da

---
+++ cookbooks/wt_actioncenter_ds_streaming/metadata.rb
@@ -0,0 +1,10 @@
+name                "wt_actioncenter_ds_streaming"
+maintainer          "Webtrends, Inc."
+maintainer_email    "michael.parsons@webtrends.com"
+license             "All rights reserved"
+description         "Installs/Configures Webtrends Action Center Mangaement
+Service"
+long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version             "1.0.0"
+depends             "java"
+depends             "runit"
