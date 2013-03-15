fcf5a005f7cfa0263b09df3472b7fa90a468593b
Michael Parsons <parsonsm@sops02.staging.dmz>
2013-03-15 17:50:45 +0000

    adding responsys chef recipe

---
+++ cookbooks/wt_actioncenter_dd_responsys/attributes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: wt_actioncenter_dd_responsys
+# Attributes:: default
+#
+# Copyright 2012, Webtrends
+#
+# All rights reserved - Do Not Redistribute
+#
+
+default['wt_actioncenter_dd_responsys']['user']           = "webtrends"
+default['wt_actioncenter_dd_responsys']['group']          = "webtrends"
+default['wt_actioncenter_dd_responsys']['download_url']   =
+"http://teamcity.webtrends.corp/guestAuth/repository/download/bt372/.lastSuccessful/Webtrends.ActionCenter.Management-bin.tar.gz"
+default['wt_actioncenter_dd_responsys']['message_port']   = 2552
