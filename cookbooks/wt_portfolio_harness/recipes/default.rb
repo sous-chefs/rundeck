30e4e49b1066ced84a81182de9ccb5e46bfb326b
Michael Parsons <parsonsm@sops02.staging.dmz>
2013-03-15 00:10:52 +0000

    cleaning up config files
    Former-commit-id: c9dd359aabd628028e3f6e64093b801c1cbe47c3 [formerly 02fb2d87e657daaa101def14f9f002589266b8c7] [formerly e7cdb655fad0e6a366abf7baa3221d0e348fac38] [formerly be89d90a5857a1a34a6102472ebfbaeaabc48d16 [formerly 7905aba8c8c4837f3e44ab86dd4f10be523006b0]] [formerly 6001472d637283564718377a1f12dd6ade3315be [formerly 551ba81e1229fa48d05d219e841b5111374828e9] [formerly f82b5af4aa456622c2f27993bf178a799d8f7b55 [formerly d983861261484912884a9367a39fe1a20321c50c [formerly 9f5768943d8e904dd9ac07ca829e1184cdf3e4ff]]]]
    Former-commit-id: 75434f3217a470e776c9725a99b323d23184ca71 [formerly 3a463d7cfd157e0c8c9eba39b5c48fa161618b27] [formerly 228008b179131041f9b89eea29e31a286e2a0a1a [formerly d983861261484912884a9367a39fe1a20321c50c]]
    Former-commit-id: 468cf313006fa94354192049f1a26e4879db9776 [formerly 37869915fdb921cefafe080e1fe6930fd57f7f2b]
    Former-commit-id: 58e4198873954e3a01587cb5ec28568517a66bab
    Former-commit-id: bef038d760ee0e8dd2d32361c8ea7c330a0d6c09

--- cookbooks/wt_portfolio_harness/recipes/default.rb
+++ cookbooks/wt_portfolio_harness/recipes/default.rb
@@ -104,7 +104,7 @@ def processTemplates (install_dir, conf_url, node, zookeeper_quorum, datacenter,
   cam_url = node['wt_cam']['cam_service_url']
   port = node['wt_portfolio_harness']['port']
 
-  %w[log4j.xml config.properties netty.properties application.conf logback.xml].each do | template_file|
+  %w[config.properties application.conf logback.xml].each do | template_file|
     template "#{install_dir}/conf/#{template_file}" do
       source    "#{template_file}.erb"
       owner "root"
@@ -185,7 +185,7 @@ if ENV["deploy_build"] == "true" then
   end
 
 else
-  processTemplates(install_dir, conf_url, node, zookeeper_quorum, datacenter, pod, usagedbuser, usagedbpwd)
+  processTemplates(install_dir, conf_url, node, zookeeper_quorum, datacenter, pod)
 end
 
 if node.attribute?("nagios")
