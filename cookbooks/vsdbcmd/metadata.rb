0acbfd8bbefb3ca5dac71c2b6e5cfeaf41b5c1c5
Aaron Stanley <stanleya@sops02.staging.dmz>
2013-03-21 20:34:56 +0000

    renamed cookbook to vsdbcmd
    Former-commit-id: 410fe86c561b12493427fd083cb061f92d300ffe [formerly b5e72b3347a9e05ed7dfa05bf121227ba6a897ac] [formerly 69989296ddb5e6c179f2ed37d554cd5998acf738 [formerly 603e1dbc73ed677946769e2dd6e51cdec3da4e66]]
    Former-commit-id: 545e7503c0ec308fe396f0afd172d53166d8a4c2 [formerly 5027d890ca08cccc37d1073848fe342fc55c4c02]
    Former-commit-id: 713bdc7d87eb80b29641f0e4c63e6471eba03220

---
+++ cookbooks/vsdbcmd/metadata.rb
@@ -0,0 +1,10 @@
+maintainer	"Webtrends Inc."
+maintainer_email "aaron.stanley@webtrends.com"
+license		"All rights reserved"
+description	"Installs pre-requisites for database project deploys to Windows"
+version		"1.0"
+supports	"windows"
+depends		"windows"
+depends		"ms_dotnet4"
+
+
