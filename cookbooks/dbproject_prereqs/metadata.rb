f51c96499a1866b9fdffd8068b5a4ef18c5e892b
Aaron Stanley <ajs@ubuntu.(none)>
2013-03-21 10:41:53 -0700

    added initial stab at dbpro prereqs

---
+++ cookbooks/dbproject_prereqs/metadata.rb
@@ -0,0 +1,11 @@
+maintainer	"Webtrends Inc."
+maintainer_email "aaron.stanley@webtrends.com"
+license		"All rights reserved"
+description	"Installs pre-requisites for database project deploys to Windows"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md')
+version		"1.0"
+supports	"windows"
+depends		"windows"
+depends		"ms_dotnet4"
+
+
