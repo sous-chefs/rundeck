a94047745b1e79d93418dc9600b99f4122c69ad2
Aaron Stanley <ajs@ubuntu.(none)>
2013-03-21 11:37:11 -0700

    fixed type in metadata.rb
    Former-commit-id: b35f9408078edd4d0455bbe9167862c2330ecacb [formerly 808f20d0be337cb29eaa8ac96b6150ed919f6b83] [formerly 177ee7db7b284b372d56004643080316ecab17d0] [formerly 08bf37afb43554fe8c605f3fcf657fbd25fdc788 [formerly 6bedff405fc9c6e58d14d1168d7aa184b4245ff1]] [formerly cbe119108fbfb049ec4e79b37eaf66a39022bec8 [formerly ef058d0fed50b74fb40a57e462d3606cee4c2457] [formerly d57091d0ab0c32e233d71108802c0ba5b352a867 [formerly c7bf6adbec6e85d309ea32360994773c5e7dc151 [formerly 76d0cca9ad482a2a7566b50c4b4b5a80c82b0ca4]]]]
    Former-commit-id: 7c10919462817128e6f0fa90cc30f67b3e6afb10 [formerly 4a086de0d6c623921a334b662dfca36036014e54] [formerly 282451de8931a5d96c1651554b12e371f3e0edf1 [formerly c7bf6adbec6e85d309ea32360994773c5e7dc151]]
    Former-commit-id: f77debeb9106938a8d76894da532284fb3a2cb73 [formerly 57b441a1e2a7cb621091964bb49d16df179a10d4]
    Former-commit-id: 3640c2921551a420956be8f3f3ef73d1b9ae156d
    Former-commit-id: 8ef032f0e87fec62cc388ae174f21e7b68818e3c

--- cookbooks/dbproject_prereqs/metadata.rb
+++ cookbooks/dbproject_prereqs/metadata.rb
@@ -2,7 +2,6 @@ maintainer	"Webtrends Inc."
 maintainer_email "aaron.stanley@webtrends.com"
 license		"All rights reserved"
 description	"Installs pre-requisites for database project deploys to Windows"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md')
 version		"1.0"
 supports	"windows"
 depends		"windows"
