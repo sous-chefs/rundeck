28ce5910788b17141cf5a48179092b98dd0038b8
Michael Parsons <parsonsm@sops02.staging.dmz>
2013-03-22 13:27:33 +0000

    changing artifact for datasource

--- cookbooks/wt_actioncenter_ds_streaming/attributes/default.rb
+++ cookbooks/wt_actioncenter_ds_streaming/attributes/default.rb
@@ -10,5 +10,5 @@
 default['wt_actioncenter_ds_streaming']['user']           = "webtrends"
 default['wt_actioncenter_ds_streaming']['group']          = "webtrends"
 default['wt_actioncenter_ds_streaming']['download_url']   =
-"http://teamcity.webtrends.corp/guestAuth/repository/download/bt370/.lastSuccessful/action-center-datasources-streaming-example-develop-SNAPSHOT-bin.tar.gz"
+"http://teamcity.webtrends.corp/guestAuth/repository/download/bt370/.lastSuccessful/action-center-datasource-processor-develop-SNAPSHOT-bin.tar.gz"
 default['wt_actioncenter_ds_streaming']['message_port']   = 2552
