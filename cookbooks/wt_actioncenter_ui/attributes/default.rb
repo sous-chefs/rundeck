6a4f90d71bec858f914ede2ac3ef52af753cfb06
Adam Keene <adam.keene@webtrends.com>
2013-03-26 21:58:50 +0000

    Add apache2 configuration to wt_actioncenter_ui recipe
    Former-commit-id: 720f55115339ffa2707cd1d02a24f0373ef3314d [formerly aaa785e220131f7370f0ebdadebe47c76a1ef283] [formerly fabd08f300fbe9d3d6320ac0ae195b3345b317fd [formerly 8b0944637f97cff386cecc7772a8ee4988c98045]]
    Former-commit-id: 73f9689af87dc092bad9b8efdc844b7c25e45512 [formerly 96483940a8ae050b81fe8fceffd424a551a30312]
    Former-commit-id: 8eee142008f0d204d9efcb30edeac89d24c21fb7

--- cookbooks/wt_actioncenter_ui/attributes/default.rb
+++ cookbooks/wt_actioncenter_ui/attributes/default.rb
@@ -1,10 +1,11 @@
 default[:wt_actioncenter_ui][:auth_service_version] = "v1"
+default[:wt_actioncenter_ui][:allow_http] = false
 default[:wt_actioncenter_ui][:user] = "webtrends"
 default[:wt_actioncenter_ui][:group] = "webtrends"
 default[:wt_actioncenter_ui][:download_url] = "http://teamcity.webtrends.corp/guestAuth/repository/download/bt375/.lastSuccessful/action-center.tar.gz"
 default[:wt_actioncenter_ui][:help_url] = "http://help.webtrends.com"
-default[:wt_actioncenter_ui][:port] = 8080
 default[:wt_actioncenter_ui][:bundle_without] = %w(test development assets)
+default[:wt_actioncenter_ui][:unicorn][:port] = 5000
 default[:wt_actioncenter_ui][:unicorn][:options] = { :tcp_nodelay => true, :backlog => 100 }
 default[:wt_actioncenter_ui][:unicorn][:worker_timeout] = 60
 default[:wt_actioncenter_ui][:unicorn][:preload_app] = false
