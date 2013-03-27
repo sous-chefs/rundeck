2cc208bae38a46b63f4abcf1afd2c1a174f6b22c
Kendrick Martin <Kendrick.Martin@Webtrends.com>
2013-03-27 15:17:34 -0700

    Updating support for icacls on links. Removing unneeded error logging
    Former-commit-id: 60865348bf01f093ef2a5c2db127dffcee8bd07e [formerly fab9720e697d3def3abb11a445cff611a5786f19] [formerly df74dbd00a68ebbd5d79d49daf2485ecc084770c] [formerly 6fd3f6bd284339949a425647dd037c040306dd85 [formerly c0a57e2eb738fbdff3c6e61c06cde91148f75c78]] [formerly faab147a00a7988dc60b8e1c0d03c81a297d099d [formerly c6e7175b2730f07b2ba4e8ac9ff05c6d1833fdb5] [formerly 4a90ce4838049608b142c98473ff5b73e3d3f532 [formerly c92ee2b113a072e145b113fe82dc6b1a30857e2b [formerly af519d9d63249dc46e01fff5529642dfc56a93ad]]]]
    Former-commit-id: 69b249a17437bb177a2116113ca01cce7967afa7 [formerly bdad6fa1e10664013ea35b225e18668bd85fb278] [formerly 0519c3538c1937ff39ba119fb644db0563592101 [formerly c92ee2b113a072e145b113fe82dc6b1a30857e2b]]
    Former-commit-id: 3aa7caa3a191ece88beb384b045c4ca3d4a21954 [formerly f48a811462ba5b320859315b956015dac1f09814]
    Former-commit-id: 66579dc6b66edc7a622d5fb8eeca7fcbdf4b47b5
    Former-commit-id: 83bfc4fd374ae6ff21ef315ef61f194f48e352cf

--- cookbooks/wt_base/providers/icacls.rb
+++ cookbooks/wt_base/providers/icacls.rb
@@ -11,10 +11,7 @@
 include Chef::Mixin::ShellOut
 
 action :grant do
-  Chef::Log.debug("icacls.exe grant #{@new_resource.path} => #{@new_resource.user} => #{@new_resource.perm}")
-  if @new_resource.user == nil
-    raise Chef::Exceptions::AttributeNotFound, "user or group could not be determined"
-  end
+  Chef::Log.debug("icacls grant #{@new_resource.path} => #{@new_resource.user} => #{@new_resource.perm}")
   case @new_resource.perm
     when :full
       perm = "(oi)(ci)(f)"
@@ -25,21 +22,21 @@ action :grant do
     else
       raise Chef::Exceptions::AttributeNotFound, "perm could not be determined, please use :full, :modify or :read"
   end
-  shell_out!("icacls \"#{@new_resource.path}\" /grant:r \"#{@new_resource.user}\":#{perm}")
+  cmd = "#{icacls} /grant:r \"#{@new_resource.user}\":#{perm}"
+  cmd << " /l" if @new_resource.link
+  Chef::Log.debug(cmd)
+  shell_out!(cmd)
 end
 
 action :remove do
   Chef::Log.debug("icacls remove #{@new_resource.path} => #{@new_resource.user}")
-  if @new_resource.user == nil
-    raise Chef::Exceptions::AttributeNotFound, "user or group could not be determined"
-  end
-  shell_out!("icacls.exe \"#{@new_resource.path}\" /remove \"#{@new_resource.user}\" /t /c /l /q")
+  cmd = "#{icacls} /remove \"#{@new_resource.user}\" /t /c /l/ q"
+  shell_out!(cmd)
 end
 
-action :run do
-  Chef::Log.debug("icacls run #{@new_resource.options}")
-  if @new_resource.options == nil
-    raise Chef::Exceptions::AttributeNotFound, "options need to be provided"
+private
+def icacls
+  @icacls ||= begin
+    "icacls \"#{@new_resource.path}\""
   end
-  shell_out!("icacls.exe #{@new_resource.options}")
 end
