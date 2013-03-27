f38cd591ffba4379a7707ccd69342ea94bc9c231
Max Eng <Max.Eng@webtrends.com>
2013-03-27 16:02:51 -0700

    Adding message timeout configuration setting for WAPI

--- cookbooks/wt_portfolio_edge_stream_api/attributes/default.rb
+++ cookbooks/wt_portfolio_edge_stream_api/attributes/default.rb
@@ -7,7 +7,8 @@
 # All rights reserved - Do Not Redistribute
 #
 
-default['wt_portfolio_edge_stream_api']['user']           = "webtrends"
-default['wt_portfolio_edge_stream_api']['group']          = "webtrends"
-default['wt_portfolio_edge_stream_api']['download_url']   = "http://teamcity.webtrends.corp/guestAuth/repository/download/bt343/55227:id/webtrends-stream-edge-api-streaming-1.2.2-SNAPSHOT-bin.tar.gz"
-default['wt_portfolio_edge_stream_api']['message_port']   = 2552
+default['wt_portfolio_edge_stream_api']['user']            = "webtrends"
+default['wt_portfolio_edge_stream_api']['group']           = "webtrends"
+default['wt_portfolio_edge_stream_api']['download_url']    = "http://teamcity.webtrends.corp/guestAuth/repository/download/bt343/55227:id/webtrends-stream-edge-api-streaming-1.2.2-SNAPSHOT-bin.tar.gz"
+default['wt_portfolio_edge_stream_api']['message_port']    = 2552
+default['wt_portfolio_edge_stream_api']['message_timeout'] = 500 
