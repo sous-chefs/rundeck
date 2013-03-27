b1b45b1f3abcc27f42b5bf7a0f0aa19beb5c6428
Kendrick Martin <Kendrick.Martin@Webtrends.com>
2013-03-27 09:45:04 -0700

    Added platform independent cert import recipe and added steps to add cert to the system keystore

--- cookbooks/wt_labserver/metadata.rb
+++ cookbooks/wt_labserver/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "david.dvorak@webtrends.com"
 license          "All rights reserved"
 description      "Installs/Configures wt_labserver"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.2.0"
 
 %w{ ubuntu centos windows }.each do |os|
   supports os
