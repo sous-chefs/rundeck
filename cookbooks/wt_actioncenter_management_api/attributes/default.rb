db2b6533f5c4e8278efa98db13a74621d64fccdc
Marcus Vincent <marcus.vincent@webtrends.com>
2013-03-28 20:52:33 +0000

    Use configuration files

--- cookbooks/wt_actioncenter_management_api/attributes/default.rb
+++ cookbooks/wt_actioncenter_management_api/attributes/default.rb
@@ -12,3 +12,6 @@ default['wt_actioncenter_management_api']['group']          = "webtrends"
 default['wt_actioncenter_management_api']['download_url']   =
 "http://teamcity.webtrends.corp/guestAuth/repository/download/bt372/.lastSuccessful/action-center-management-service-bin.tar.gz"
 default['wt_actioncenter_management_api']['message_port']   = 2552
+default['wt_actioncenter_management_api']['ads_port'] = 8080
+default['wt_actioncenter_management_api']['ads_host'] = "hutil01.staging.dmz"
+
