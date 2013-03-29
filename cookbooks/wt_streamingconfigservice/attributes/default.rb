dcf53e83fd073bc0becea355a1e6dce7790f8aeb
vonnagy <ivan.vonnagy@webtrends.com>
2013-03-29 10:50:01 -0700

    STR-1151 Changed the cookbook recipe to utilize the environment attribute of [wt_monitoring][healthcheck_port]. The version was bumped to 1.0.18.
    STR-1153 Changed the cookbook recipe to utilize the environment attribute of [wt_monitoring][healthcheck_port]. The version was bumped to 1.1.3
    
    
    Former-commit-id: e3f03313a4723732e1ffcd9ea71dd782fd06c712 [formerly 2fc299f165d39fdcc1c5bdccc36d944ca35b45d6] [formerly 1e49df50d0863a2d76f6659c86cec5e0d6e45cb2 [formerly 3225571627b0073d9362158f41df125e0eb41a27]]
    Former-commit-id: 6532355d1fbdf373a363c2c3b2330484495f53f8 [formerly d2e91a2ed497981e149ac44b52105fb6f5ee1360]
    Former-commit-id: 4a45e85c7fddf437826bd9b3fdd2a07fa951ff90

--- cookbooks/wt_streamingconfigservice/attributes/default.rb
+++ cookbooks/wt_streamingconfigservice/attributes/default.rb
@@ -14,4 +14,3 @@ default['wt_streamingconfigservice']['download_url'] = ""
 default['wt_streamingconfigservice']['port'] = 8080
 default['wt_streamingconfigservice']['includeUnmappedAnalyticsIds'] = "true"
 default['wt_streamingconfigservice']['jmx_port'] = 9999
-default['wt_streamingconfigservice']['healthcheck_port'] = 9000
