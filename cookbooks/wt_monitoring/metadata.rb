a32ad43b9144210beaaa2b91540cf30b210f97ec
David Dvorak <david.dvorak@webtrends.com>
2013-04-04 21:22:25 +0000

    collectd python plugin for unbound
    Former-commit-id: f5928d637dfd701d24fa946f370c26a9fc392a34 [formerly 33961c7404d8bad47793199c015ed42a2c1c82ee] [formerly 1652bd8eea232a216546fc384ffa147dbe5baf3b [formerly 6512cc0b305c2047d6b6c7be89057fa3a46acb79]]
    Former-commit-id: 678604947fea885b06401752ac197a7a97e6ed35 [formerly 6c4da74af45ef0117d6182d01d24843f4c3e4f69]
    Former-commit-id: ce161e6206650a43b88168cca58359072013ba85

--- cookbooks/wt_monitoring/metadata.rb
+++ cookbooks/wt_monitoring/metadata.rb
@@ -1,7 +1,8 @@
 name             "wt_monitoring"
-maintainer       "Webtrends, Inc."
+maintainer       "Webtrends Inc."
 maintainer_email "tim.smith@webtrends.com"
 license          "All rights reserved"
 description      "Contains multiple recipes for use in monitoring particular components of the service"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.1"
+depends          "collectd"
