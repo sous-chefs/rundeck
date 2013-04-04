9fd03cb83c13105178e556b9c754e569f5ab2179
David Dvorak <david.dvorak@webtrends.com>
2013-04-04 21:22:25 +0000

    collectd python plugin for unbound
    Former-commit-id: 1983e0bf1c9f5ca9bc8291ed01ddab53a8e7894b [formerly a32ad43b9144210beaaa2b91540cf30b210f97ec] [formerly ce161e6206650a43b88168cca58359072013ba85] [formerly 678604947fea885b06401752ac197a7a97e6ed35 [formerly 6c4da74af45ef0117d6182d01d24843f4c3e4f69]] [formerly f5928d637dfd701d24fa946f370c26a9fc392a34 [formerly 33961c7404d8bad47793199c015ed42a2c1c82ee] [formerly 1652bd8eea232a216546fc384ffa147dbe5baf3b [formerly 1b9db65c92a1b5eb3bc63ad714a5de8944a798c6 [formerly 6512cc0b305c2047d6b6c7be89057fa3a46acb79]]]]
    Former-commit-id: fa92224534a7c2f27b608dfeacd1e063a47292ad [formerly 3de9f4cf552a027c70e53dbeb87956941128b0fe] [formerly 7051e2e1a7e6f9f92f496856a0ce02e7e6bf7de8 [formerly 1b9db65c92a1b5eb3bc63ad714a5de8944a798c6]]
    Former-commit-id: ae4f6dc14f69d93056e965dbe1345e37ad1fa873 [formerly 66695b74b7f59a0a5dbd97e0bf004ab08993aecc]
    Former-commit-id: 7621731e42503d542a811ed5b27fe944c27b41d7
    Former-commit-id: 13c3967cff6060106d64620bda2b7b194d0177cc

---
+++ cookbooks/wt_monitoring/files/default/unbound/unbound-stats.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python
+
+""" unbound-stats.py:  collectd plugin for Unbound
+
+    DESCRIPTION
+      
+    EXAMPLE
+
+    Copyright 2013, Webtrends Inc.
+    David Dvorak <david.dvorak@webtrends.com>
+"""
+
+import argparse
+import subprocess
+
+# verbose logging
+VERBOSE = False
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--display',  action="store_true", default=False, help='display stats (no collectd)')
+args = parser.parse_args()
+
+if not args.display:
+	import collectd
+
+def config_callback(conf):
+	global VERBOSE
+	for node in conf.children:
+		if node.key == 'Interval':
+			interval = int(node.values[0])
+			collectd.register_read(read_callback, interval)
+		elif node.key == 'Verbose':
+			VERBOSE = bool(node.values[0])
+		else:
+			collectd.warning('unbound-stats: unknown config key: %s.' % node.key)
+	log('configured interval: %s' % interval)
+
+def get_stats():
+
+	# get unbound stats
+	stats_lines = (subprocess.check_output(['/usr/sbin/unbound-control', 'stats_noreset'])).splitlines()
+
+	# parse the stats
+	stats = {}
+	for line in stats_lines:
+
+		# we are not intested in individual threads
+		if line.startswith('thread'):
+			continue
+
+		key, val = line.split('=')
+		stats[key] = val
+
+	return stats
+
+def dispatch_value(stats, key, epoch, type):
+	if args.display:
+		print "%s %s %s" % (key, stats[key], epoch)
+	else:
+		log('%s %s %s' % (key, stats[key], epoch))
+		value = collectd.Values(plugin='unbound', time = epoch, type = type, type_instance = key, values = [stats[key]])
+		value.dispatch()
+	return
+
+def read_callback():
+
+	log('read_callback')
+	stats = get_stats()
+
+	if args.display:
+		print stats
+
+	# use time reported by unbound
+	epoch = float(stats['time.now'])
+
+	dispatch_value(stats, 'total.num.queries', epoch, 'dns_query')
+	dispatch_value(stats, 'total.num.cachehits', epoch, 'dns_query')
+	dispatch_value(stats, 'total.num.cachemiss', epoch, 'dns_query')
+	dispatch_value(stats, 'total.num.prefetch', epoch, 'dns_query')
+	dispatch_value(stats, 'total.num.recursivereplies', epoch, 'dns_query')
+	dispatch_value(stats, 'total.requestlist.avg', epoch, 'gauge')
+	dispatch_value(stats, 'total.requestlist.max', epoch, 'gauge')
+	dispatch_value(stats, 'total.requestlist.overwritten', epoch, 'dns_query')
+	dispatch_value(stats, 'total.requestlist.exceeded', epoch, 'dns_query')
+	dispatch_value(stats, 'total.requestlist.current.all', epoch, 'gauge')
+	dispatch_value(stats, 'total.requestlist.current.user', epoch, 'gauge')
+	dispatch_value(stats, 'total.recursion.time.avg', epoch, 'gauge')
+	dispatch_value(stats, 'total.recursion.time.median', epoch, 'gauge')
+
+def log(msg):
+	if not VERBOSE:
+		return
+	collectd.info('unbound-stats: %s' % msg)
+
+if args.display:
+	read_callback()
+else:
+	collectd.register_config(config_callback)
