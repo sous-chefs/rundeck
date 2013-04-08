6aff61c00c7dca67950904b5b867cb0416b5927c
Jeff Berger <Jeff.Berger@webtrends.com>
2013-04-08 14:56:56 -0700

    Reverting previous check in
    Former-commit-id: 7808c1a367ca4f33a6aa9c3dee9606d3dec0aac8 [formerly a0fcf7c8f6c24d019de34a9866b38884a9512e7f] [formerly c6ca7766bd583c9f2e1a3819d730e5a32bcb65bc [formerly 5eca1b87f42ea092967bf6e60856a76bfa81fa58]]
    Former-commit-id: 67cac908d735ebc9786d90a107d70e181d44753d [formerly fce176b2f4a2e41ef15e4aab8aa253591b4302a8]
    Former-commit-id: 2cedda685d9c125400bbdb132a85bfb3dba65236

--- cookbooks/wt_streamingcollection/metadata.rb
+++ cookbooks/wt_streamingcollection/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email    "marek.dolgos@webtrends.com"
 license             "All rights reserved"
 description         "Installs/Configures the Webtrends Streaming Collection"
 long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version             "1.1.7"
+version             "1.1.6"
 depends             "java"
 depends             "runit"
 depends             "minitest-handler"
