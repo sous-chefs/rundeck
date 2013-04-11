642e2298afc5bfd192d991cef1606b101fc81ee1
Kendrick Martin <Kendrick.Martin@Webtrends.com>
2013-04-11 16:19:25 -0700

    Rewriting responsys strucuture to match updated harness

--- cookbooks/wt_actioncenter_dd_responsys/metadata.rb
+++ cookbooks/wt_actioncenter_dd_responsys/metadata.rb
@@ -5,6 +5,6 @@ license             "All rights reserved"
 description         "Installs/Configures Webtrends Action Center Mangaement
 Service"
 long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version             "1.0.0"
+version             "1.1.0"
 depends             "java"
 depends             "runit"
--- cookbooks/wt_actioncenter_dd_responsys/recipes/default.rb
+++ cookbooks/wt_actioncenter_dd_responsys/recipes/default.rb
@@ -14,45 +14,25 @@ else
   log "The deploy_build value is not set or is false so we will only update the configuration"
 end
 
-install_dir = File.join(node['wt_common']['install_dir_linux'],
-"harness/plugins/actioncenter_dd_responsys")
-conf_dir = File.join(install_dir, "conf")
+install_dir  = File.join(node['wt_portfolio_harness']['plugin_dir'], "responsys")
+conf_dir     = File.join(install_dir, "conf")
 tarball      = node['wt_actioncenter_dd_responsys']['download_url'].split("/")[-1]
 download_url = node['wt_actioncenter_dd_responsys']['download_url']
-user = node['wt_actioncenter_dd_responsys']['user']
-group = node['wt_actioncenter_dd_responsys']['group']
-config_host = URI(node['wt_streamingconfigservice']['config_service_url']).host
+user         = node['wt_actioncenter_dd_responsys']['user']
+group        = node['wt_actioncenter_dd_responsys']['group']
+config_host  = URI(node['wt_streamingconfigservice']['config_service_url']).host
+
 log "Install dir: #{install_dir}"
 
-# create the install directory
-directory "#{install_dir}" do
-  owner "root"
-  group "root"
-  mode 00755
-  recursive true
-  action :create
-end
-
-directory "#{conf_dir}" do
-  owner "root"
-  group "root"
-  action :create
-end
-
-def processTemplates(conf_dir, config_host)
-	log "Updating template files"
-
-	%w[key.pem ResponsysServerCertificate.cer config.properties].each do | template_file|
-		template "#{conf_dir}/#{template_file}" do
-			source "#{template_file}.erb"
-			owner "root"
-			group "root"
-			mode 00644
-			variables({
-				:config_host => config_host,
-			})
-		end
-	end
+# create the directories
+[install_dir, conf_dir].each do |dir|
+  directory dir do
+    owner "root"
+    group "root"
+    mode 00755
+    recursive true
+    action :create
+  end
 end
 
 if ENV["deploy_build"] == "true" then
@@ -64,17 +44,14 @@ if ENV["deploy_build"] == "true" then
     mode 00644
   end
 
-    # uncompress the application tarball into the install dir
-    execute "tar" do
-        user  "root"
-        group "root"
-        cwd install_dir
-        command "tar zxf #{Chef::Config[:file_cache_path]}/#{tarball}"
-    end
-
-
-	processTemplates(conf_dir, config_host)	
-
+  # uncompress the application tarball into the install dir
+  execute "tar" do
+    user  "root"
+    group "root"
+    cwd install_dir
+    command "tar zxf #{Chef::Config[:file_cache_path]}/#{tarball}"
+    notifies :restart, "runit_service[harness]", :delayed
+  end
 
   # delete the install tar ball
   execute "delete_install_source" do
@@ -83,7 +60,17 @@ if ENV["deploy_build"] == "true" then
     command "rm -f #{Chef::Config[:file_cache_path]}/#{tarball}"
     action :run
   end
-
-else
 end
 
+%w[key.pem ResponsysServerCertificate.cer config.properties].each do | template_file|
+  template "#{conf_dir}/#{template_file}" do
+    source "#{template_file}.erb"
+    owner "root"
+    group "root"
+    mode 00644
+    variables({
+      :config_host => config_host,
+    })
+    notifies :restart, "runit_service[harness]", :delayed
+  end
+end
