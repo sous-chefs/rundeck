fb0989b8987ca0ef46ee845473b69800eb234be7
Kendrick Martin <Kendrick.Martin@Webtrends.com>
2013-04-12 11:00:10 -0700

    Restructuring cookbook to match other plugin cookbooks. Changing templates to dynamically find zookeeper
    Former-commit-id: 560c6da86610866da7bd87524181c35a30650af9 [formerly a263d6e43a8a6c83683666bd47ed90dc7ed7e67d] [formerly 7fc80c1de5cebc211b29bec9f0015a387794b942 [formerly dd0885d8e7aab3e68ed1b76aa8a21ee814e6ac28]]
    Former-commit-id: 4f179dccf57dcb7d665533e23e86b1f0551a76aa [formerly 579616fd58b85d9ab419d48f79ba7126ad23687c]
    Former-commit-id: cb3a27b128b747ed1f952ac5a2a4713a96fad8d4

--- cookbooks/wt_actioncenter_ds_streaming/recipes/default.rb
+++ cookbooks/wt_actioncenter_ds_streaming/recipes/default.rb
@@ -14,74 +14,43 @@ else
   log "The deploy_build value is not set or is false so we will only update the configuration"
 end
 
-install_dir = File.join(node['wt_common']['install_dir_linux'],
-"harness/plugins/actioncenter_ds_streaming")
-conf_dir = File.join(install_dir, "conf")
+install_dir  = File.join(node['wt_portfolio_harness']['plugin_dir'], "actioncenter_ds_streaming")
+conf_dir     = File.join(install_dir, "conf")
 tarball      = node['wt_actioncenter_ds_streaming']['download_url'].split("/")[-1]
 download_url = node['wt_actioncenter_ds_streaming']['download_url']
-user = node['wt_actioncenter_ds_streaming']['user']
-group = node['wt_actioncenter_ds_streaming']['group']
+user         = node['wt_actioncenter_ds_streaming']['user']
+group        = node['wt_actioncenter_ds_streaming']['group']
 
-sapi_port = node['wt_actioncenter_ds_streaming']['sapi_port']
-client_id = node['wt_actioncenter_ds_streaming']['client_id']
+client_id     = node['wt_actioncenter_ds_streaming']['client_id']
 client_secret = node['wt_actioncenter_ds_streaming']['client_secret']
-auth_url = node['wt_actioncenter_ds_streaming']['auth_url']
-auth_user_id =  node['wt_actioncenter_ds_streaming']['auth_user_id']
-kafka_topic = node['wt_actioncenter_ds_streaming']['kafka_topic']
-zk_connect = node['wt_actioncenter_ds_streaming']['zk_connect']
-# grab the zookeeper nodes that are currently available
-zookeeper_quorum = Array.new
-if not Chef::Config.solo
-  search(:node, "role:zookeeper AND
-  chef_environment:#{node.chef_environment}").each do |n|
-      zookeeper_quorum << n[:fqdn]
-  end
+auth_url      = "#{node['wt_sauth']['auth_url']}/#{node['wt_portfolio_harness']['sauth_version']}/token"
+auth_user_id  = node['wt_actioncenter_ds_streaming']['auth_user_id']
+config_host   = URI(node['wt_streamingconfigservice']['config_service_url']).host
+sapi_port     = search(:node, "role:wt_streaming_api_server AND chef_environment:#{node.chef_environment}").first['wt_streamingapi']['port']
+
+#Dynamically builds kafka topic unless overridden
+unless node['wt_actioncenter_ds_streaming']['kafka_topic']
+  kafka_topic = "#{node['wt_realtime_hadoop']['datacenter']}_#{node['wt_realtime_hadoop']['pod']}_ActionRoutes"
+else
+  kafka_topic = node['wt_actioncenter_ds_streaming']['kafka_topic']
 end
 
+# grab the zookeeper nodes that are currently available
+zookeeper_search = Array.new
+if not Chef::Config.solo
+  zookeeper_search = search(:node, "role:zookeeper AND chef_environment:#{node.chef_environment}")
+end
 
 log "Install dir: #{install_dir}"
-
-# create the install directory
-directory "#{install_dir}" do
-  owner "root"
-  group "root"
-  mode 00755
-  recursive true
-  action :create
-end
-
-directory "#{conf_dir}" do
-  owner "root"
-  group "root"
-  mode 00755
-  action :create
-end
-
-def processTemplates(conf_dir, sapi_port, client_id, client_secret,
-auth_url,auth_user_id, zookeeper_quorum, kafka_topic, zk_connect)
-	config_host =
-	URI(node['wt_streamingconfigservice']['config_service_url']).host
-	%w[producer.properties config.properties].each do | template_file|
-		template "#{conf_dir}/#{template_file}" do
-			source "#{template_file}.erb"
-			owner "root"
-			group "root"
-			mode 00644
-			variables({
-				:sapi_port => sapi_port,
-				:client_id => client_id,
-				:client_secret => client_secret,
-				:auth_url => auth_url,
-				:auth_user_id => auth_user_id,
-				:config_host => config_host,
-				:zookeeper_quorum => zookeeper_quorum * ",",
-				:pod => node['wt_realtime_hadoop']['pod'],
-				:datacenter => node['wt_realtime_hadoop']['datacenter'],
-				:kafka_topic => kafka_topic,
-				:zk_connect => zk_connect,
-			})
-		end
-	end
+# create the directories
+[install_dir, conf_dir].each do |dir|
+  directory dir do
+    owner "root"
+    group "root"
+    mode 00755
+    recursive true
+    action :create
+  end
 end
 
 
@@ -94,15 +63,13 @@ if ENV["deploy_build"] == "true" then
     mode 00644
   end
 
-  processTemplates(conf_dir,sapi_port,client_id,client_secret,auth_url,auth_user_id,zookeeper_quorum,kafka_topic,
-  zk_connect)
-
     # uncompress the application tarball into the install dir
     execute "tar" do
         user  "root"
         group "root"
         cwd install_dir
         command "tar zxf #{Chef::Config[:file_cache_path]}/#{tarball}"
+        notifies :restart, "runit_service[harness]", :delayed
     end
 
   # delete the install tar ball
@@ -112,7 +79,25 @@ if ENV["deploy_build"] == "true" then
     command "rm -f #{Chef::Config[:file_cache_path]}/#{tarball}"
     action :run
   end
-
-else
 end
 
+%w[producer.properties config.properties].each do | template_file|
+    template "#{conf_dir}/#{template_file}" do
+      source "#{template_file}.erb"
+      owner "root"
+      group "root"
+      mode 00644
+      variables({
+        :client_id => client_id,
+        :client_secret => client_secret,
+        :auth_url => auth_url,
+        :auth_user_id => auth_user_id,
+        :config_host => config_host,
+        :pod => node['wt_realtime_hadoop']['pod'],
+        :datacenter => node['wt_realtime_hadoop']['datacenter'],
+        :kafka_topic => kafka_topic,
+        :zk_search => zk_search,
+      })
+      notifies :restart, "runit_service[harness]", :delayed
+    end
+  end
