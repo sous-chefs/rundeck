f949904f1dbaef06cf54e0600f2caff5d439dd75
Jeff Berger <Jeff.Berger@webtrends.com>
2013-04-17 11:44:21 -0700

    Adding streaming collection plugin role and cookbook
    Former-commit-id: ff7ee8cddc7bab840a25dbb78a87cbff992479b8 [formerly bfb2e11cc9f0bf583ab3b32cb08410f183ee42d3] [formerly bd1a14b5c070ede884c4ddfd22e93b14d0bd8b31] [formerly d8b827d57e31bec462de7866c39812eb51835544 [formerly eb4dc06c6dd484e9b9f3d6371fad66ad8be4348d]] [formerly 49829c08d483aedf73aff8273c8d106ccc844b4f [formerly 7829a638868e286db88172315c1e0b5e278cb817] [formerly e545ea16d68f7d629a52a995a4672cefaada9f7b [formerly 09951cc9137c3f5935b2d48800f0eb5056aac1f8 [formerly 04d497f3c68276469517fd14c6ee2377f6105041]]]]
    Former-commit-id: c6160ef308edfb37d8c5820ff0b79bfc8f8abbd1 [formerly 8ff6b5ea22db197e0cfb59fc3bb38cec0cbf902e] [formerly dd655f37ed8fec1dfa625c1306749fed585f7937 [formerly 09951cc9137c3f5935b2d48800f0eb5056aac1f8]]
    Former-commit-id: 52d3c111ed08cf580871ea84893e4df543f864aa [formerly a7eb7e069ca67ef1405ca9ceb29aa8c1910d3d1c]
    Former-commit-id: c74d3fd718cb188fdc1540b7cebefdbc36e73c28
    Former-commit-id: 660d5b193ce219318725e93666c6938049b6a7b4

---
+++ cookbooks/wt_streaming_collection/attributes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: wt_streaming_collection
+# Attributes:: default
+#
+# Copyright 2013, Webtrends
+#
+# All rights reserved - Do Not Redistribute
+#
+
+default['wt_streaming_collection']['user']           = "webtrends"
+default['wt_streaming_collection']['group']          = "webtrends"
+default['wt_streamingcollection']['kafka_connect_timeout_ms'] = 100
---
+++ cookbooks/wt_streaming_collection/recipes/default.rb
@@ -0,0 +1,89 @@
+#
+# Cookbook Name:: wt_streaming_collection
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+if ENV["deploy_build"] == "true" then
+  log "The deploy_build value is true so un-deploying first"
+  include_recipe "wt_streaming_collection::undeploy"
+else
+  log "The deploy_build value is not set or is false so we will only update the configuration"
+end
+
+#install_dir  = File.join(node['wt_portfolio_harness']['plugin_dir'], "streamingcollection")
+install_dir  = File.join("/opt/webtrends/harness/plugins", "streaming_collection")
+conf_dir     = File.join(install_dir, "conf")
+tarball      = node['wt_streaming_collection']['download_url'].split("/")[-1]
+download_url = node['wt_streaming_collection']['download_url']
+user         = node['wt_streaming_collection']['user']
+group        = node['wt_streaming_collection']['group']
+config_url   = node['wt_streamingconfigservice']['config_service_url']
+pod = node['wt_realtime_hadoop']['pod']
+datacenter = node['wt_realtime_hadoop']['datacenter']
+
+log "Install dir: #{install_dir}"
+
+# create the directories
+[install_dir, conf_dir].each do |dir|
+  directory dir do
+    owner "root"
+    group "root"
+    mode 00755
+    recursive true
+    action :create
+  end
+end
+
+if ENV["deploy_build"] == "true" then
+  log "The deploy_build value is true so we will grab the tar ball and install"
+
+  # download the application tarball
+  remote_file "#{Chef::Config[:file_cache_path]}/#{tarball}" do
+    source download_url
+    mode 00644
+  end
+
+  # uncompress the application tarball into the install dir
+  execute "tar" do
+    user  "root"
+    group "root"
+    cwd install_dir
+    command "tar zxf #{Chef::Config[:file_cache_path]}/#{tarball}"
+    notifies :restart, "service[harness]", :delayed
+  end
+
+  # delete the install tar ball
+  execute "delete_install_source" do
+    user "root"
+    group "root"
+    command "rm -f #{Chef::Config[:file_cache_path]}/#{tarball}"
+    action :run
+  end
+end
+
+#Template kafka.properties
+template "#{conf_dir}/kafka.properties" do
+    source  "kafka.properties.erb"
+    owner   "root"
+    group   "root"
+    mode    00644
+    variables({
+      :kafka_topic_scs => "#{datacenter}_#{pod}_scsRawHits",
+      :kafka_topic_dc => "#{datacenter}_#{pod}_dcRawHits"
+    })
+end
+
+#Template config.properties
+template "#{conf_dir}/config.properties" do
+    source  "config.properties.erb"
+    owner   "root"
+    group   "root"
+    mode    00644
+    variables({
+      :configservice => "#{config_url}"
+    })
+end
