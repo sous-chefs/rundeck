35180785745b876109296aac00a03b6292dc9d36
Jeff Berger <Jeff.Berger@webtrends.com>
2013-04-23 17:15:29 -0700

    Update wt_streaming_collection for new config options
    Former-commit-id: c082480aeaca10907c18f52f9f475d1bb9931bbc [formerly a20d7a3296977fc87c3cc07ed55226b60c8fe257] [formerly 2c739b90026fe7caba1524eb0d98437f2c8b186c [formerly 792db926b3331efd81563b26dddcacd47837721c [formerly 5958311be36c82279654a13e6c21aece4e1fa0b9]]]
    Former-commit-id: abd33c166da2d08a0bbd5e194867460e82e3a17f [formerly 46b2af3023523d0345f7de694157d34a2ae74ddf]
    Former-commit-id: a308ed9c83ca3200b5295e32a4f4a28893aa31a1
    Former-commit-id: f5b610a1d65fed74f7b139a1c360573f70ad2cc7

--- cookbooks/wt_streaming_collection/attributes/default.rb
+++ cookbooks/wt_streaming_collection/attributes/default.rb
@@ -9,4 +9,9 @@
 
 default['wt_streaming_collection']['user']           = "webtrends"
 default['wt_streaming_collection']['group']          = "webtrends"
-default['wt_streamingcollection']['kafka_connect_timeout_ms'] = 100
+default['wt_streaming_collection']['kafka_connect_timeout_ms'] = 100
+
+default["wt_streaming_collection"]["scs_worker_count"] = 5
+default["wt_streaming_collection"]["dc_worker_count"] = 5
+default["wt_streaming_collection"]["id_worker_count"] = 2
+
