6fefea6cae59b78c1182e6eac19dfed7d17b3d7b
Marcus Vincent <marcus.vincent@webtrends.com>
2013-04-30 18:55:10 +0000

    Add haproxy to cookbooks
    Former-commit-id: af02174ef45ff79cfa6c2fcc3be7a6aa7b18a8db [formerly be8c1b0b80716cab5172c939d79efe534a4b4755] [formerly e452d9d39eb9e92642d85ce2955ac19ef014b6d5 [formerly 53c26927e19834679a671336bd504fabed804ec2]]
    Former-commit-id: 7d53910ecf0475e17206c7533bc0d46a651e24ce [formerly d80e13dd7e7387a021544bb3f9bdcb218681243b]
    Former-commit-id: 3f7665500512453941fe8476360bbde56d67edbb

---
+++ cookbooks/wt_haproxy/recipes/default.rb
@@ -0,0 +1,41 @@
+#
+# Cookbook Name:: haproxy
+# Recipe:: default
+#
+# Copyright 2009, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "haproxy::install_#{node['haproxy']['install_method']}"
+
+cookbook_file "/etc/default/haproxy" do
+  source "haproxy-default"
+  owner "root"
+  group "root"
+  mode 00644
+  notifies :restart, "service[haproxy]"
+end
+
+template "#{node['haproxy']['conf_dir']}/haproxy.cfg" do
+  source "haproxy.cfg.erb"
+  owner "root"
+  group "root"
+  mode 00644
+  notifies :reload, "service[haproxy]"
+end
+
+service "haproxy" do
+  supports :restart => true, :status => true, :reload => true
+  action [:enable, :start]
+end
