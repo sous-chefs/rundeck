941cefc107859d8639eee843ec8baf63cf9b5a12
Marcus Vincent <marcus.vincent@webtrends.com>
2013-05-01 00:20:50 +0000

    Fix typo
    Former-commit-id: 9686685337579fbfc47003b4af1d97594957da8a [formerly e509a37e2e199a8b0500c0e51b2e3d3a9855bae2] [formerly cbc6b761cc1550f80f9b0e9d0bb2afb7dd25cddc] [formerly 9824247eace1a6abe152dc1ee05438c4e7cb98b2 [formerly bcc41e5adf10c643caef8b6582519a63d35e9e89]] [formerly 824aa00c7655e5d484e5f6fe0439bd782c7b0a4b [formerly 51ef134e6c4ebe6bfc9fe12855f8c742f06a41e0] [formerly 7f3ce6dc7803e12628550e31d2d02f52456e5226 [formerly 4dd7b5c3ada079e1deea1ae901f3067439e090f7 [formerly 6f4e5f7c685f03001b5b943ae7c4d4fecfea765e]]]]
    Former-commit-id: 3edd2f7f5452ecfb7bea11e2855cdf9692ea8e58 [formerly 92fb380c04c3c2f543104e75ef3e52a3250acf75] [formerly 4e38f8114f349750b05f34a55a077f6f841a941d [formerly 4dd7b5c3ada079e1deea1ae901f3067439e090f7]]
    Former-commit-id: a04ca4a0ee97b5fa7efb903fec37082130fe54b0 [formerly 2720852ec0c5355ce66431fb60567946cebe0659]
    Former-commit-id: e335c7231971b7dd973a20d3c92eca53b39ed2be
    Former-commit-id: 47e19c8b8d801552deeab4bff60943ae898ae9b0

--- cookbooks/wt_haproxy/recipes/default.rb
+++ cookbooks/wt_haproxy/recipes/default.rb
@@ -20,7 +20,7 @@
 include_recipe "wt_haproxy::install_#{node['haproxy']['install_method']}"
 
 defaults_timeouts={node['haproxy']['defaults_timeouts']}
-defaults_options={node['haproxy']'[defaults_options']}
+defaults_options={node['haproxy']['defaults_options']}
 
 cookbook_file "/etc/default/haproxy" do
   source "haproxy-default"
