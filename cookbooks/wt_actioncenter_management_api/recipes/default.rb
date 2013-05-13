d710aa390fda4f61a6031bcc9746563401d0825f
Kendrick Martin <Kendrick.Martin@Webtrends.com>
2013-05-13 11:47:56 -0700

    Updated version of cookbook

--- cookbooks/wt_actioncenter_management_api/recipes/default.rb
+++ cookbooks/wt_actioncenter_management_api/recipes/default.rb
@@ -8,80 +8,41 @@
 #
 
 
-auth_data = data_bag_item('authorization',node.chef_environment)
-install_dir  = File.join(node['wt_portfolio_harness']['plugin_dir'], "actioncenter_management_api")
-conf_dir     = File.join(install_dir,"conf")
-tarball      = node['wt_actioncenter_management_api']['download_url'].split("/")[-1]
-download_url = node['wt_actioncenter_management_api']['download_url']
-user         = node['wt_actioncenter_management_api']['user']
-group        = node['wt_actioncenter_management_api']['group']
+auth_data    = data_bag_item('authorization',node.chef_environment)
 ads_host     = URI(node['wt_streamingconfigservice']['config_service_url']).host
 ads_ssl_port = node['wt_streamingconfigservice']['config_service_ssl_port']
 authToken    = auth_data['wt_streamingconfigservice']['authToken']
-kafka_topic ="#{node['wt_realtime_hadoop']['datacenter']}_#{node['wt_realtime_hadoop']['pod']}_ActionRoutes"
-log "Install dir: #{install_dir}"
+kafka_topic  = "#{node['wt_common']['datacenter']}_#{node['wt_common']['pod']}_ActionRoutes"
 
-if ENV["deploy_build"] == "true" then
-   # delete the install tar ball
-  execute "delete_mgmtapi_source" do
-    user "root"
-    group "root"
-    command "rm -f #{Chef::Config[:file_cache_path]}/#{tarball}"
-    action :run
-  end
+wt_portfolio_harness_plugin "actioncenter_management_api" do
+  download_url node['wt_actioncenter_management_api']['download_url']
+  force_deploy true if ENV["deploy_build"] == "true"
+  after_deploy Proc.new {    
+    #copy messages jar to harness
+    #until we solve the class loader issues.
+    execute "copy messages" do
+      command "cp #{new_resource.install_dir}/lib/action-center-messages*.jar #{node['wt_portfolio_harness']['lib_dir']}/."
+      action :run
+    end
+  }
+  
+  configure Proc.new {
+    template "#{new_resource.conf_dir}/config.properties" do 
+      source "config.properties.erb" 
+      owner "root" 
+      group "root" 
+      mode 00644 
+      variables({ 
+        :ads_host => ads_host,
+        :secure_config_host => ads_host,
+        :authToken => authToken,
+        :secure_config_port => ads_ssl_port,
+        :cam_host => node['wt_cam']['cam_service_url'],
+        :cam_port => "80",
+        :kafka_topic => kafka_topic
+      })
+    end 
+  }
 end
 
 
-# download the application tarball
-remote_file "#{Chef::Config[:file_cache_path]}/#{tarball}" do
-  source download_url
-  mode 00644
-  action :create_if_missing
-  notifies :delete, "directory[#{install_dir}]", :immediately
-end
-
-# create the directories
-[install_dir, conf_dir].each do |dir|
-  directory dir do
-    owner "root"
-    group "root"
-    mode 00755
-    recursive true
-    action :create
-  end
-end
-
-# uncompress the application tarball into the install dir
-execute "Untar_mgmt_api" do
-  user  "root"
-  group "root"
-  cwd install_dir
-  command "tar zxf #{Chef::Config[:file_cache_path]}/#{tarball}"
-  creates "#{install_dir}/lib"
-  notifies :restart, "service[harness]", :delayed
-end 
-
-	#copy messages jar to harness
-	#until we solve the class loader issues.
-execute "copy messages" do
-  command "cp #{install_dir}/lib/action-center-messages*.jar #{node['wt_portfolio_harness']['lib_dir']}/."
-  action :nothing
-  subscribes :run, resources(:execute => "Untar_mgmt_api"), :immediately
-end
-
-template "#{conf_dir}/config.properties" do 
-  source "config.properties.erb" 
-  owner "root" 
-  group "root" 
-  mode 00644 
-  variables({ 
-    :ads_host => ads_host,
-    :secure_config_host => ads_host,
-    :authToken => authToken,
-    :secure_config_port => ads_ssl_port,
-    :cam_host => node['wt_cam']['cam_service_url'],
-    :cam_port => "80",
-	:kafka_topic => kafka_topic
-  })
-  notifies :restart, "service[harness]", :delayed
-end 
