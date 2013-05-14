855869e59938b0f0cdcb790879f55f6039b00bea
Kendrick Martin <Kendrick.Martin@Webtrends.com>
2013-05-14 08:59:36 -0700

    Fixing syntax
    Former-commit-id: ed281a717d4e34cab52116ee025221f719f7459e [formerly ff6075368801851d3de7d4a9e8a3b13a1ecc6f1c] [formerly 4e92fabf49a68b6011b488e9c61b33d6c53d5a14 [formerly 3506a28015cba13a47b6a56a4dbd50ca432126ef [formerly aae47a7ab9a92e72d31bba1a3c668e1f816f90ad]]]
    Former-commit-id: 6d345dbf058d25959b982b6ccf5c7c67fc4f7842 [formerly eb9ddc5c20fd08ec5ecff7b32424ed4ab3592e7d]
    Former-commit-id: 38bb284fa5314749ab052ff02671d935d7ce0188
    Former-commit-id: 177f9923d43471cad97c1fd0e0c31e4ca0776c05

--- cookbooks/webtrends_server/recipes/java_cert.rb
+++ cookbooks/webtrends_server/recipes/java_cert.rb
@@ -1,8 +1,7 @@
 # import trusted ca certificates
 java_home = node['java']['java_home']
 repo = node['wt_common']['ca_repo']
-<<<<<<< HEAD
-if ['wt_common']['ca_files']
+unless node['wt_common']['ca_files'].nil?
 	node['wt_common']['ca_files'].each do |ca|
 
 		remote_file "#{Chef::Config[:file_cache_path]}/#{ca}.cer" do
@@ -17,19 +16,4 @@ if ['wt_common']['ca_files']
 		end	
 		
 	end
-=======
-node['wt_common']['ca_files'].each do |ca|
-
-	remote_file "#{Chef::Config[:file_cache_path]}/#{ca}.cer" do
-	   source "#{repo}/#{ca}.cer"
-	 end
-	
-	#Imports into java
-	execute "keytool import #{ca}" do
-		command "#{java_home}/bin/keytool -import -trustcacerts -alias #{ca} -file #{Chef::Config[:file_cache_path]}/#{ca}.cer -keystore #{java_home}/jre/lib/security/cacerts -storepass changeit -noprompt"
-		only_if { File.exists?("#{java_home}/bin/keytool") }
-		not_if  "#{java_home}/bin/keytool -list -alias #{ca} -keystore #{java_home}/jre/lib/security/cacerts -storepass changeit"
-	end	
-	
->>>>>>> a5e07b219b403008727b5e03a7b9f6b9e91e9d68
 end
