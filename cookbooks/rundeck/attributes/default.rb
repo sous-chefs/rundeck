0fef9233e623c06519e1000016c611107aa04618
David Andrew <David.Andrew@Webtrends.com>
2013-05-15 14:26:22 -0700

    update for rdbms use
    Former-commit-id: e0aef2084c2ccecc24119156756b4a2763a8764b [formerly d5c8a0e4f346bedb8f5ddad73344f830640a7d59] [formerly 29c401f9a905f08cc57c816008a00a538d85cf75 [formerly a3299e2fdbd6d380a1f2f20732e34899c59feddb]]
    Former-commit-id: bb43f7dee9aed1fad0f45d09944a10f9700bc60b [formerly c132fa63d193ae34c3f0ed0c15de2861708c59cb]
    Former-commit-id: 991d7ec125510d2e96f6194160bd10ed59befe3a

--- cookbooks/rundeck/attributes/default.rb
+++ cookbooks/rundeck/attributes/default.rb
@@ -11,6 +11,16 @@ default['rundeck']['project_config'] = "/etc/chef/chef-rundeck.json"
 
 default['rundeck']['jaas'] = "internal"
 
+# External Database properties
+default['rundeck']['rdbms']['enable'] = "false"
+default['rundeck']['rdbms']['type'] = "mysql"
+default['rundeck']['rdbms']['location'] = "someIPorFQDN"
+default['rundeck']['rdbms']['dbname'] = "rundeckdb"
+default['rundeck']['rdbms']['dbuser'] = "rundeckdb"
+default['rundeck']['rdbms']['dbpassword'] = "Chang3ME"
+default['rundeck']['rdbms']['dialect'] = "Oracle10gDialect"
+default['rundeck']['rdbms']['port'] = "3306"
+
 #LDAP Properties
 default['rundeck']['ldap']['provider'] = "ldap://seadc01.webtrends.corp:389 ldap://seadc02.webtrends.corp:389"
 default['rundeck']['ldap']['binddn'] = "CN=winbind,ou=serviceaccountsOLD,ou=WebTrendsUsers,dc=webtrends,dc=corp"
