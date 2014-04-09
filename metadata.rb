2c3f1c7325a943d70384a934222899774f2abc19
Peter Crossley <peter.crossley@webtrends.com>
2014-04-08 19:49:05 -0700

    making version 2.0.3 release

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,16 @@ maintainer_email "Peter Crossley <peter.crossley@webtrends.com>"
 license          "All rights reserved"
 description      "Installs and configures Rundeck 2.0"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.0.4"
+version          "2.0.3"
 depends          "runit"
 depends          "sudo"
 depends          "java"
 depends          "apache2"
 depends          "rundeck"
+
+%w{ debian ubuntu centos suse fedora redhat freebsd windows }.each do |os|
+  supports os
+end
 
 recipe "rundeck::server", "Use this recipe to install the rundeck server on a node"
 recipe "rundeck::chef-rundeck", "Use this recipe to install the chef rundeck integration component, by default it is recommened to install on the chef server."
