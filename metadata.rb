a619322aa2b34794c6c4820a4366fe6ab84e31a8
Gavin Reynolds <g.reynolds@src.gla.ac.uk>
2014-06-04 09:21:52 +0100

    Revert "remove the runit dependency"
    This reverts commit b91a2eb9154397438b7e72b03d76d777207468c6 [formerly 4916eef5d50edaf84ec39f2e87026e080e3d1315] [formerly d6a5d91d367b9aa6bac0144542660d5a3d94d7ae] [formerly 4b48d64dcf39560f4696fe824cf335c79701a25f [formerly 586cd3dd624dfcb538acd30b7c2e19f46670025f]] [formerly ee9b210ca3b650b96206da89acb500f290462487 [formerly af115ebbd470e41bafe71b11b2d0bb3027de5a2d] [formerly fd15bb55ad044adac5059c75e15a16264ba1e562 [formerly c970574c6323d61ba71ea27e770b5b5188217344]]] [formerly 7a5952920fce533423e1f24ee82a40494f93e193 [formerly 59202dcd3eab007885bba0dea6e19918da82ccd0] [formerly f8d471bf4501a66f8d8364014e77e497c2ab6597 [formerly 469683351893a6b105540c1027c2a2cc3727ab89]] [formerly 4e765732d3537356e066967e5285d01a893df14b [formerly 807498777539e8ccb0c08b4c5dbe377e387ab421] [formerly ea25cec66fc36b028fe88d6defd4a60413072a4a [formerly c970574c6323d61ba71ea27e770b5b5188217344 [formerly 22f0fef602c93cdf7224d09a1d3ffbccd53ae458]]]]].
    
    
    Former-commit-id: 6213ab53718192e50121eff248f7cb2ad2c59444
    Former-commit-id: 6c99cbf3bf7eba825deafe6b7ae3ada234e22c3c [formerly 2716fecbf984da67dc186ff3f41132f5c935000a] [formerly 84a7dfdc6e511e892349b4d0b7966ea67d5d4daf [formerly 254a96939ef22e51ff1cfb477e98616b39dc1375 [formerly 6faeca459057ea4261a03bab82d1e16da0d80bfd]]]
    Former-commit-id: ec9342c619f28ddbc083e2aec9da92869acbfea2 [formerly 4f0044d33cd2fa88dc8f307b6403949ad8531257]
    Former-commit-id: e7748e5422024008416689a682aec908a7ece44a
    Former-commit-id: 779e0ed0a9c9839da849b6724d5adddda963a7d7

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,7 @@ license          "All rights reserved"
 description      "Installs and configures Rundeck 2.0"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "2.0.4"
+depends          "runit"
 depends          "sudo"
 depends          "java"
 depends          "apache2"
