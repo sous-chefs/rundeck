dd5f6ecef1f64a48956ea74ebc691fa76d5191ff
Peter Crossley <Peter.Crossley@Webtrends.com>
2014-09-02 15:01:38 -0700

    adding inital LWRP
    Former-commit-id: cb22d4dec5c3a4d74dec3a0c56992baf074ed73e
    Former-commit-id: 3437606aa9cdb2f748ff50f535a747975db939cb
    Former-commit-id: c3e3364a5b6021503b008ad96d128de245786dd9 [formerly 8893f6113b33fb58d37c95d082b44528a5f08865] [formerly 1d6c9ba8d7fccfb209ed10ae06214349bb43fe77 [formerly babf0f833b4d6ebecaae6c54e9628e9a97c99976]]
    Former-commit-id: b39e3c076fb60519e88ef8ce01bbbfb5abccee7b [formerly 73907742252988fe3c4e7237fdd0928e1562878e]
    Former-commit-id: c16ca78795bebdb46b0c28d203005aa20030a191
    Former-commit-id: 47625d7bd5a2cc88a1dfcd15cc01a9958ce00df3

---
+++ providers/plugin.rb
@@ -0,0 +1,40 @@
+#
+# Cookbook Name:: rundeck
+# Provider:: plugin
+#
+# Copyright 2012, Peter Crossley
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+action :create do
+  plugin_filename = ::File.basename(new_resource.url)
+  Chef::Application.fatal!("Invalid filename extension for Rundeck plugin #{new_resource.name}") \
+    if ! (::File.extname(plugin_filename).eql?('.zip') || ::File.extname(plugin_filename).eql?('.jar'))
+  a = remote_file "#{node['rundeck']['basedir']}/libext/#{new_resource.name}" do
+    owner node['rundeck']['user']
+    group node['rundeck']['group']
+    mode 00644
+    action :create
+    source new_resource.url
+    checksum(new_resource.checksum) if new_resource.checksum
+  end
+  new_resource.updated_by_last_action(a.updated_by_last_action?)
+end
+
+action :remove do
+  a = file "#{node['rundeck']['basedir']}/libext/#{new_resource.name}" do
+    action :delete
+  end
+  new_resource.updated_by_last_action(a.updated_by_last_action?)
+end
