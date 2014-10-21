20110465c355d840a777778f92a66e05d4760fbc
Scott Marshall <scott.marshall@johnmuirhealth.com>
2014-10-21 13:44:56 -0700

    Removed the secret file stuff because it breaks chef_solo
    Former-commit-id: 77a078cb95cad21c3d89b88e39955c47fc2dd740 [formerly aa0e115ef13581e536b39cba1332cb005351ec9e] [formerly 2217cd0eec88d2cd9dcc36587ffd327c1d087e6b [formerly c983eee99f76295df3a32863974d79bcaac6f9c0 [formerly 078bbc9147cc7a619e304d7f43df4908f6126e5b]]]
    Former-commit-id: cd4e506dd7eb6c58cd3a35168ad8fac5c48813a9 [formerly 01971dc38581dd82359e216ef529c4ca7b3131ce]
    Former-commit-id: f83f0c3c7b9f98e8bb21bdf253cfe202c04381c9
    Former-commit-id: 46689d3486a03efb6ed0063dd7c63a4db70f7855

--- recipes/node_unix.rb
+++ recipes/node_unix.rb
@@ -21,8 +21,7 @@
 rundeck_secure = data_bag_item('rundeck', 'secure')
 
 if !node['rundeck']['secret_file'].nil? then
-  rundeck_secret = Chef::EncryptedDataBagItem.load_secret(node['rundeck']['secret_file'])
-  rundeck_secure = Chef::EncryptedDataBagItem.load('rundeck', 'secure', rundeck_secret)
+  rundeck_secure = Chef::EncryptedDataBagItem.load('rundeck', 'secure')
 end  
 
 user node['rundeck']['user'] do
