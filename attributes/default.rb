8a07e4f66caf5a67385429d1cb1557a5da54d098
Jonathan Mickle <jmickle@Jonathans-MacBook-Pro.local>
2014-12-03 21:22:44 -0800

    rubocop fixes
    Former-commit-id: 96670ea233dfcefe97546d866679f8292f94bfae
    Former-commit-id: 755a45be7d24b91ad830289330e4b19cb5ac99b8

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,27 +1,27 @@
 default['rundeck'] = {}
-default['rundeck']['configdir'] = "/etc/rundeck"
-default['rundeck']['basedir'] = "/var/lib/rundeck"
-default['rundeck']['datadir'] = "/var/rundeck"
-default['rundeck']['deb']['package'] = "rundeck-2.3.2-1-GA.deb"
+default['rundeck']['configdir'] = '/etc/rundeck'
+default['rundeck']['basedir'] = '/var/lib/rundeck'
+default['rundeck']['datadir'] = '/var/rundeck'
+default['rundeck']['deb']['package'] = 'rundeck-2.3.2-1-GA.deb'
 default['rundeck']['deb']['options'] = false #--force-confdef --force-confold
 default['rundeck']['url'] = "http://download.rundeck.org/deb/#{node['rundeck']['deb']['package']}"
-default['rundeck']['checksum'] = "812ee51372659659715767ac923bd2a664d84fc2"
+default['rundeck']['checksum'] = '812ee51372659659715767ac923bd2a664d84fc2'
 default['rundeck']['port'] = 4440
-default['rundeck']['jaas'] = "internal"
-default['rundeck']['default_role'] = "user"
+default['rundeck']['jaas'] = 'internal'
+default['rundeck']['default_role'] = 'user'
 default['rundeck']['hostname'] = "rundeck.#{node['domain']}"
 default['rundeck']['email'] = "rundeck@#{node['domain']}"
 default['rundeck']['restart_on_config_change'] = false
-default['rundeck']['apache-template']['cookbook'] = "rundeck"
-default['rundeck']['log_dir'] = "/var/log/chef-rundeck"
+default['rundeck']['apache-template']['cookbook'] = 'rundeck'
+default['rundeck']['log_dir'] = '/var/log/chef-rundeck'
 default['rundeck']['server_url'] = node['rundeck']['hostname']
-default['rundeck']['log_level'] = "DEBUG" #ERR,WARN,INFO,VERBOSE,DEBUG
+default['rundeck']['log_level'] = 'DEBUG' # ERR,WARN,INFO,VERBOSE,DEBUG
 default['rundeck']['rss_enabled'] = true
 
-#databag name configuration
-default['rundeck']['rundeck_databag_secure'] = "secure"
-default['rundeck']['rundeck_databag'] = "rundeck"
-default['rundeck']['rundeck_projects_databag'] = "rundeck_projects"
+# databag name configuration
+default['rundeck']['rundeck_databag_secure'] = 'secure'
+default['rundeck']['rundeck_databag'] = 'rundeck'
+default['rundeck']['rundeck_projects_databag'] = 'rundeck_projects'
 
 # chef-rundeck
 if node['platform'] == 'ubuntu' || node['platform_family'] == 'rhel'
@@ -32,24 +32,24 @@ end
 
 default['rundeck']['chef_rundeck_gem'] = nil
 default['rundeck']['chef_rundeck_port'] = 9980
-default['rundeck']['chef_rundeck_host'] = "0.0.0.0"
+default['rundeck']['chef_rundeck_host'] = '0.0.0.0'
 default['rundeck']['chef_rundeck_partial_search'] = false
-default['rundeck']['user'] = "rundeck"
-default['rundeck']['group'] = "rundeck"
-default['rundeck']['user_home'] = "/home/rundeck"
-default['rundeck']['chef_config'] = "/etc/chef/rundeck.rb"
-default['rundeck']['chef_client_name'] = "chef-rundeck"
+default['rundeck']['user'] = 'rundeck'
+default['rundeck']['group'] = 'rundeck'
+default['rundeck']['user_home'] = '/home/rundeck'
+default['rundeck']['chef_config'] = '/etc/chef/rundeck.rb'
+default['rundeck']['chef_client_name'] = 'chef-rundeck'
 default['rundeck']['chef_rundeck_url'] = "http://chef.#{node['domain']}:#{node['rundeck']['chef_rundeck_port']}"
 default['rundeck']['chef_webui_url'] = "https://chef.#{node['domain']}"
 default['rundeck']['chef_url'] = "https://chef.#{node['domain']}"
-default['rundeck']['project_config'] = "/etc/chef/chef-rundeck.json"
+default['rundeck']['project_config'] = '/etc/chef/chef-rundeck.json'
 
 # SMTP settings for rundeck notification emails
 default['rundeck']['mail']['enable'] = false
-default['rundeck']['mail']['host'] = "localhost"
-default['rundeck']['mail']['port'] = "25"
-default['rundeck']['mail']['username'] = ""
-default['rundeck']['mail']['password'] = ""
+default['rundeck']['mail']['host'] = 'localhost'
+default['rundeck']['mail']['port'] = '25'
+default['rundeck']['mail']['username'] = ''
+default['rundeck']['mail']['password'] = ''
 
 #   If you want to use encrypted databags for your windows password and/or public/private key pairs generate a secret using:
 #     'openssl rand -base64 512 | tr -d '\r\n' > rundeck_secret'
@@ -58,14 +58,14 @@ default['rundeck']['mail']['password'] = ""
 default['rundeck']['secret_file'] = nil
 
 # External Database properties
-default['rundeck']['rdbms']['enable'] = "false"
-default['rundeck']['rdbms']['type'] = "mysql"
-default['rundeck']['rdbms']['location'] = "someIPorFQDN"
-default['rundeck']['rdbms']['dbname'] = "rundeckdb"
-default['rundeck']['rdbms']['dbuser'] = "rundeckdb"
-default['rundeck']['rdbms']['dbpassword'] = "Chang3ME"
-default['rundeck']['rdbms']['dialect'] = "Oracle10gDialect"
-default['rundeck']['rdbms']['port'] = "3306"
+default['rundeck']['rdbms']['enable'] = 'false'
+default['rundeck']['rdbms']['type'] = 'mysql'
+default['rundeck']['rdbms']['location'] = 'someIPorFQDN'
+default['rundeck']['rdbms']['dbname'] = 'rundeckdb'
+default['rundeck']['rdbms']['dbuser'] = 'rundeckdb'
+default['rundeck']['rdbms']['dbpassword'] = 'Chang3ME'
+default['rundeck']['rdbms']['dialect'] = 'Oracle10gDialect'
+default['rundeck']['rdbms']['port'] = '3306'
 
 # Windows Properties
 default['rundeck']['windows']['group'] = 'Administrators'
@@ -78,22 +78,22 @@ default['rundeck']['windows']['winrm_protocol'] = 'https'
 default['rundeck']['windows']['winrm_timeout'] = 'PT60.000S'
 
 # LDAP Properties
-default['rundeck']['ldap']['provider'] = "ldap://servername:389"
-default['rundeck']['ldap']['binddn'] = "CN=binddn,dc=domain,dc=com"
-default['rundeck']['ldap']['bindpwd'] = "BINDPWD"
-default['rundeck']['ldap']['authenticationmethod'] = "simple"
-default['rundeck']['ldap']['forcebindinglogin'] = "true"
-default['rundeck']['ldap']['userbasedn'] = "ou=Users,dc=domain,dc=com"
-default['rundeck']['ldap']['userrdnattribute'] = "cn"
-default['rundeck']['ldap']['useridattribute'] = "uid"
-default['rundeck']['ldap']['userpasswordattribute'] = "userPassword"
-default['rundeck']['ldap']['userobjectclass'] = "inetOrgPerson"
-default['rundeck']['ldap']['rolebasedn'] = "ou=Groups,dc=domain,dc=com"
-default['rundeck']['ldap']['rolenameattribute'] = "cn"
-default['rundeck']['ldap']['rolememberattribute'] = "uniqueMember"
+default['rundeck']['ldap']['provider'] = 'ldap://servername:389'
+default['rundeck']['ldap']['binddn'] = 'CN=binddn,dc=domain,dc=com'
+default['rundeck']['ldap']['bindpwd'] = 'BINDPWD'
+default['rundeck']['ldap']['authenticationmethod'] = 'simple'
+default['rundeck']['ldap']['forcebindinglogin'] = 'true'
+default['rundeck']['ldap']['userbasedn'] = 'ou=Users,dc=domain,dc=com'
+default['rundeck']['ldap']['userrdnattribute'] = 'cn'
+default['rundeck']['ldap']['useridattribute'] = 'uid'
+default['rundeck']['ldap']['userpasswordattribute'] = 'userPassword'
+default['rundeck']['ldap']['userobjectclass'] = 'inetOrgPerson'
+default['rundeck']['ldap']['rolebasedn'] = 'ou=Groups,dc=domain,dc=com'
+default['rundeck']['ldap']['rolenameattribute'] = 'cn'
+default['rundeck']['ldap']['rolememberattribute'] = 'uniqueMember'
 default['rundeck']['ldap']['roleusernamememberattribute'] = nil # "memberUid"
-default['rundeck']['ldap']['roleobjectclass'] = "groupOfUniqueNames"
-default['rundeck']['ldap']['roleprefix'] = "rundeck-"
-default['rundeck']['ldap']['cachedurationmillis'] = "300000"
-default['rundeck']['ldap']['reportstatistics'] = "true"
+default['rundeck']['ldap']['roleobjectclass'] = 'groupOfUniqueNames'
+default['rundeck']['ldap']['roleprefix'] = 'rundeck-'
+default['rundeck']['ldap']['cachedurationmillis'] = '300000'
+default['rundeck']['ldap']['reportstatistics'] = 'true'
 default['rundeck']['ldap']['supplementalroles'] = node['rundeck']['default_role']
