a236c3731e681de625b4a2c33559f625aa9ed9ba
Tim Smith <tsmith84@gmail.com>
2015-09-05 19:07:51 -0700

    Fix the license in the metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'rundeck'
 maintainer       'Webtrends, Inc.'
 maintainer_email 'Peter Crossley <peter.crossley@webtrends.com>'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs and configures Rundeck 2.0'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.0.7'
