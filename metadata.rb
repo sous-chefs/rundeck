78dfd229d814b3cb4c750ce5e8dcf10a91e90988
Ronald Gundlach-Chmara <chmarar@webtrends.com>
2015-12-18 21:53:48 +0000

    update recordkeeping to reflect minor edits

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'Peter Crossley <peter.crossley@webtrends.com>'
 license          'All rights reserved'
 description      'Installs and configures Rundeck 2.x'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.12'
+version          '2.0.13'
 depends          'runit'
 depends          'sudo'
 depends          'java'
