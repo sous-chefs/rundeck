e8e41f76cfc8026c06cc35d2a2e517468907fe36
Brian Cook <Brian.Cook@Cerner.com>
2016-08-26 14:29:17 -0500

    Fix SSL so that CLI works and correctly configure SSL offloading to proxy #93

--- attributes/default.rb
+++ attributes/default.rb
@@ -29,7 +29,7 @@ default['rundeck']['cert']['name'] = node['rundeck']['hostname']
 default['rundeck']['cert']['cookbook'] = 'rundeck'
 default['rundeck']['webcontext'] = '/'
 
-default['rundeck']['grails_server_url'] = "#{node['rundeck']['use_ssl'] ? 'https' : 'http'}://#{node['rundeck']['hostname']}"
+default['rundeck']['grails_server_url'] = "http://#{node['rundeck']['hostname']}"
 default['rundeck']['grails_port'] = node['rundeck']['use_ssl'] ? 443 : 80
 
 default['rundeck']['log_level'] = 'INFO' # ERR,WARN,INFO,VERBOSE,DEBUG
