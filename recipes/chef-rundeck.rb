ed533fb880461b205b2b61845b839e7e60415a09
Snehit Gajjar <snehit.gajjar@cerner.com>
2016-12-07 15:58:41 -0600

    Moved out chef environment setup logic from chef_rundeck to chef_server_config

--- recipes/chef-rundeck.rb
+++ recipes/chef-rundeck.rb
@@ -20,13 +20,7 @@
 require 'json'
 
 include_recipe 'rundeck::default'
-
-if node['rundeck']['secret_file'].nil?
-  rundeck_secure = data_bag_item(node['rundeck']['rundeck_databag'], node['rundeck']['rundeck_databag_secure'])
-else
-  rundeck_secret = Chef::EncryptedDataBagItem.load_secret(node['rundeck']['secret_file'])
-  rundeck_secure = Chef::EncryptedDataBagItem.load(node['rundeck']['rundeck_databag'], node['rundeck']['rundeck_databag_secure'], rundeck_secret)
-end
+include_recipe 'rundeck::chef_server_config'
 
 bags = data_bag(node['rundeck']['rundeck_projects_databag'])
 projects = {}
@@ -64,22 +58,6 @@ chef_gem 'chef-rundeck' do
 end
 
 chef_gem 'sinatra'
-
-template '/etc/chef/rundeck.rb' do
-  owner node['rundeck']['user']
-  group node['rundeck']['group']
-  source 'rundeck.rb.erb'
-  variables(
-    rundeck: node['rundeck']
-  )
-end
-
-file '/etc/chef/rundeck.pem' do
-  content rundeck_secure['chef_rundeck_pem']
-  owner node['rundeck']['user']
-  group node['rundeck']['group']
-  mode '0400'
-end
 
 directory node['rundeck']['log_dir'] do
   owner node['rundeck']['user']
