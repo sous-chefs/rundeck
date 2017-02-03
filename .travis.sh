1db13fb508232fa72c10a3fc4007c52f7ba36f3b
Austin Heiman <atheimanksu@gmail.com>
2017-02-03 11:54:36 -0600

    kitchen debug logs to debug problem in travis

--- .travis.sh
+++ .travis.sh
@@ -9,6 +9,6 @@ lint)
   rubocop --lint --display-style-guide --extra-details --display-cop-names
   ;;
 *)
-  KITCHEN_LOCAL_YAML='.kitchen.docker.yml' kitchen test "$SUITE" --concurrency=2
+  KITCHEN_LOCAL_YAML='.kitchen.docker.yml' kitchen test "$SUITE" --concurrency=2 --log-level=debug
   ;;
 esac
