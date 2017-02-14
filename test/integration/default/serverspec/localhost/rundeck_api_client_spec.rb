599735c695fb3ba4905005e85c210a2ea8c6e0b6
Austin Heiman <atheimanksu@gmail.com>
2017-02-14 12:18:12 -0600

    api client using rest-client gem (#138)
    * api client using rest-client gem
    
    * kitchen test with chef latest
    
    * @cookie_jar rather than @cookies
    
    * kitchen debug logs to debug problem in travis
    
    * bundle update in travis script
    
    
    Former-commit-id: 3865dc95cc3da033a9346680991a5cc29376c2be

--- test/integration/default/serverspec/localhost/rundeck_api_client_spec.rb
+++ test/integration/default/serverspec/localhost/rundeck_api_client_spec.rb
@@ -5,47 +5,29 @@ describe RundeckApiClient do
     described_class.connect('http://localhost', 'admin', 'adminpassword')
   end
 
-  # There's a bug with the current project implementation. Projects are not
-  # created on the first chef-client run, there are 0 projects until the second
-  # chef-client converges. This should be incremented to 1 once this bug is
-  # resolved and projects are being created by the first chef-client.
-  #
-  # https://github.com/Webtrends/rundeck/issues/136
-  let(:initial_project_count) { 0 }
-  let(:project_create_hash) do
-    { name: 'test-proj', config: { a: 'A', b: 'B' } }
-  end
-  let(:project) do
+  let(:project_def) do
     {
-      'url' => 'http://localhost/api/18/project/test-proj',
-      'name' => 'test-proj',
-      'description' => ''
+      name: 'kitchen-api-proj',
+      description: 'Created via api by test-kitchen',
+      config: { a: 'A', b: 'B' }
     }
   end
 
-  it 'correctly reads the rundeck server api version' do
+  it 'connects to the api' do
+    expect(client).to be_an_instance_of(described_class)
+  end
+
+  it 'retrieves the api version' do
     expect(client.version).to eql(18)
   end
 
   it 'manages projects' do
-    expect(client.get('projects').length).to eql(initial_project_count)
+    client.post('projects', project_def)
+    expect { client.post('projects', project_def) }.to raise_error(RestClient::Conflict)
 
-    client.post('projects', project_create_hash)
-    expect(client.get('projects').length).to eql(initial_project_count + 1)
-    expect(client.get('projects')).to include(project)
-
-    client.delete(project['url'])
-    expect(client.get('projects').length).to eql(initial_project_count)
-    expect(client.get('projects')).to_not include(project)
-
-    3.times do
-      client.post_or_put('projects', project_create_hash)
-      expect(client.get('projects').length).to eql(initial_project_count + 1)
-      expect(client.get('projects')).to include(project)
-    end
+    project = client.get('projects').select { |p| p['name'] == project_def[:name] }.first
+    expect(client.get(project['url'])['config']).to include({ 'a' => 'A', 'b' => 'B' })
 
     client.delete(project['url'])
-    expect(client.get('projects').length).to eql(initial_project_count)
-    expect(client.get('projects')).to_not include(project)
   end
 end
