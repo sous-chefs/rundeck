0fd36cd6825a459e5d74112c69fba9d0c39913c4
Austin Heiman <atheimanksu@gmail.com>
2017-02-22 20:50:30 -0600

    project resource changes

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,5 +1,5 @@
 if defined?(ChefSpec)
-  ChefSpec.define_matcher(:simple_passenger_app)
+  ChefSpec.define_matcher(:rundeck_project)
   def create_rundeck_project(name)
     ChefSpec::Matchers::ResourceMatcher.new(:rundeck_project, :create, name)
   end
--- recipes/_projects.rb
+++ recipes/_projects.rb
@@ -3,6 +3,7 @@ include_recipe 'rundeck::_connect_rundeck_api_client'
 
 node.run_state['rundeck']['projects'].each do |project_name, data_bag_item_contents|
   rundeck_project project_name do
+    api_client lazy { node.run_state['rundeck']['api_client'] }
     config RundeckHelper.build_project_config(data_bag_item_contents, project_name, node)
   end
 end
