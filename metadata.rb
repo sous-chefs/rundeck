7dd73f608e59cc16a713b1a1a971aedf7edc1dba
David Andrew <david.andrew@oracle.com>
2017-09-07 09:01:51 -0700

    Release 4.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'help@sous-chefs.org'
 license          'Apache-2.0'
 description      'Installs and configures Rundeck 2.x'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '4.1.1'
+version          '4.2.0'
 depends          'runit'
 depends          'sudo'
 depends          'java'
