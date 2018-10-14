8bb6301f5c519cfcb2fc216eb58d1274a72726b5
Ali Yahya <amyahya@gmail.com>
2018-10-13 20:21:47 -0400

    Use build-essential recipe, add foodcritic exemptions.
    Signed-off-by: Ali Yahya <amyahya@gmail.com>

--- recipes/_connect_rundeck_api_client.rb
+++ recipes/_connect_rundeck_api_client.rb
@@ -1,7 +1,6 @@
 # rest-client gem is needed for api client library
 # install at compile time so library can be used as soon as needed
-build_essential
-
+include_recipe 'build-essential'
 chef_gem 'rest-client' do
   compile_time true
   version '~> 2.0'
