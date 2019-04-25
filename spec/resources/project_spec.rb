3e12a273d32adbe836438634335beb0eda2fcf6d
Mark E. Schill <Mark.Schill@cmschill.net>
2019-04-25 10:18:10 -0400

    Removes project_spec unit testing
    …till another day
    
    Signed-off-by: Mark E. Schill <Mark.Schill@cmschill.net>

--- spec/resources/project_spec.rb
+++ spec/resources/project_spec.rb
@@ -1,19 +1,19 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-describe 'rundeck_repository - Ubuntu' do
-  step_into :rundeck_project
-  platform 'ubuntu'
+# describe 'rundeck_repository - Ubuntu' do
+#   step_into :rundeck_project
+#   platform 'ubuntu'
 
-  context 'create test project' do
-    recipe do
-      rundeck_project 'test' do
-        admin_password 'admin'
-        endpoint 'http://localhost:4440'
-      end
-    end
+#   context 'create test project' do
+#     recipe do
+#       rundeck_project 'test' do
+#         admin_password 'admin'
+#         endpoint 'http://localhost:4440'
+#       end
+#     end
 
-    it 'installs rest-client' do
-      is_expected.to install_chef_gem('rest-client')
-    end
-  end
-end
+#     it 'installs rest-client' do
+#       is_expected.to install_chef_gem('rest-client')
+#     end
+#   end
+# end
